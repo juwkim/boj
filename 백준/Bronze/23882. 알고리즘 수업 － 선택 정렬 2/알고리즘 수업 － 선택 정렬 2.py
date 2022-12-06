6f0b74ef01031b6511964b4058b623ef260459f8
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-07 02:13:09 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 2, Time: 1372 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/23882. 알고리즘 수업 － 선택 정렬 2/알고리즘 수업 － 선택 정렬 2.py
@@ -0,0 +1,17 @@
+g = lambda: [*map(int, input().split())]
+cnt = 0
+def selection_sort(A):
+    global cnt
+    for last in range(N-1, 0, -1):
+        i = A.index(max(A[:last+1]))
+        if last != i:
+            A[last], A[i] = A[i], A[last]
+            cnt += 1
+            if cnt == K:
+                print(*A)
+                return
+    print(-1)
+        
+N, K = map(int, input().split())
+nums = g()
+selection_sort(nums)
