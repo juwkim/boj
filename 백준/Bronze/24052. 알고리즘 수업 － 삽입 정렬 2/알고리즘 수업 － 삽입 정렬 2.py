af201fcf425f207fd8e7dc501eb6ee2934442e92
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-07 02:15:03 +0900

    [Bronze I] Title: 알고리즘 수업 - 삽입 정렬 2, Time: 220 ms, Memory: 115564 KB -BaekjoonHub

---
+++ 백준/Bronze/24052. 알고리즘 수업 － 삽입 정렬 2/알고리즘 수업 － 삽입 정렬 2.py
@@ -0,0 +1,29 @@
+g = lambda: [*map(int, input().split())]
+
+def insertion_sort(A):
+    global K
+    for i in range(1, N):
+        loc = i - 1
+        newItem = A[i]
+        while 0 <= loc and newItem < A[loc]:
+            A[loc + 1] = A[loc]
+            loc -= 1
+            K -= 1
+            if K == 0:
+                print(*A)
+                return
+        if loc + 1 != i:
+            A[loc + 1] = newItem
+            K -= 1
+            if K == 0:
+                print(*A)
+                return
+    print(-1)
+    return
+    
+
+import sys
+input = sys.stdin.readline
+N, K = g()
+A = g()
+insertion_sort(A)
