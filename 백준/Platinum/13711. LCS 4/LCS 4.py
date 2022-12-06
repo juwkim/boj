5321e18e05bf1260c0ed0de4277ca4aba9aff40e
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-07 00:16:29 +0900

    [Platinum V] Title: LCS 4, Time: 224 ms, Memory: 56432 KB -BaekjoonHub

---
+++ 백준/Platinum/13711. LCS 4/LCS 4.py
@@ -0,0 +1,24 @@
+g = lambda: [*map(int, input().split())]
+from bisect import bisect_left
+
+N = int(input())
+
+switch = g()
+switch_dic = {num: idx for idx, num in enumerate(switch)}
+bulbs = g()
+
+
+dp = [-1]
+p = []
+for bulb in bulbs:
+    num = switch_dic[bulb]
+    if num > dp[-1]:
+        dp.append(num)
+        p.append(len(dp) - 1)
+    else:
+        idx = bisect_left(dp, num)
+        dp[idx] = num
+        p.append(idx)
+    
+len_dp = len(dp) - 1
+print(len_dp)
