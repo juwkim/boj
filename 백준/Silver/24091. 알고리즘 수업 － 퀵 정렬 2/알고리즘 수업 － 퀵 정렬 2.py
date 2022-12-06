045c7ea77c231cb1018cc36d6a950edd1a147999
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-07 02:17:20 +0900

    [Silver V] Title: 알고리즘 수업 - 퀵 정렬 2, Time: 584 ms, Memory: 249968 KB -BaekjoonHub

---
+++ 백준/Silver/24091. 알고리즘 수업 － 퀵 정렬 2/알고리즘 수업 － 퀵 정렬 2.py
@@ -0,0 +1,36 @@
+import sys
+sys.setrecursionlimit(10 ** 5)
+g = lambda: [*map(int, input().split())]
+
+
+def quick_sort(A, p, r):
+    if p < r:
+        q = partition(A, p, r)
+        quick_sort(A, p, q - 1)
+        quick_sort(A, q+1, r)
+
+def partition(A, p, r):
+    global K
+    x = A[r]
+    i = p - 1
+    for j in range(p, r):
+        if A[j] <= x:
+            i += 1
+            A[i], A[j] = A[j], A[i]
+            K -= 1
+            if K == 0:
+                print(*A)
+                
+    if i + 1 != r:
+        A[i+1], A[r] = A[r], A[i+1]
+        K -= 1
+        if K == 0:
+            print(*A)
+    return i + 1
+
+
+N, K = g()
+A = g()
+quick_sort(A, 0, N-1)
+if K > 0:
+    print(-1)
