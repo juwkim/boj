a3339b10e88bb2f8e34ca732027b4130f94b5d71
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-07 00:47:21 +0900

    [Silver II] Title: 연산자 끼워넣기 (2), Time: 224 ms, Memory: 117684 KB -BaekjoonHub

---
+++ 백준/Silver/15658. 연산자 끼워넣기 （2）/연산자 끼워넣기 （2）.py
@@ -0,0 +1,29 @@
+g = lambda: [*map(int, input().split())]
+
+N = int(input())
+nums = g()
+plus, sub, mul, div = g()
+
+Min, Max = 1e9, -1e9
+def solve(num, step, p, s, m, d):
+    global Min, Max
+    if step == N:
+        Min = min(Min, num)
+        Max = max(Max, num)
+        return
+    
+    if p < plus:
+        solve(num + nums[step], step + 1, p + 1, s, m, d)
+    if s < sub:
+        solve(num - nums[step], step + 1, p, s + 1, m, d)
+    if m < mul:
+        solve(num * nums[step], step + 1, p, s, m + 1, d)
+    if d < div:
+        if num >= 0:
+            num //= nums[step]
+        else:
+            num = -((-num) // nums[step])
+        solve(num, step + 1, p, s, m, d + 1)
+    
+solve(nums[0], 1, 0, 0, 0, 0)
+print(Max, Min)
