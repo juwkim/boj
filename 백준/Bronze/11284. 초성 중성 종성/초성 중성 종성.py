e351c80130a922fb464f9cd06aa0f623f05ad9ee
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-06 23:35:12 +0900

    [Bronze II] Title: 초성 중성 종성, Time: 68 ms, Memory: 28776 KB -BaekjoonHub

---
+++ 백준/Bronze/11284. 초성 중성 종성/초성 중성 종성.py
@@ -0,0 +1,18 @@
+first_characters = ('ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ',
+                    'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ')
+third_characters = (' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ',
+                    'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ',
+                    'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ')
+
+order = ord(input()) - ord("가")
+first = order // (21 * 28) # 0 ~ 18
+second = (order % (21 * 28)) // 28 # 0 ~ 20
+third = order % 28 # 0 ~ 27
+
+first_character = first_characters[first]
+second_character = chr(second + ord("ㅏ"))
+third_character = third_characters[third]
+
+print(first_character)
+print(second_character)
+print(third_character)    
