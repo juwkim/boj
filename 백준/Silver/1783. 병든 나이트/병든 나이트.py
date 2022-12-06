2e7d31f4d6dc950c4d0629cd956e415b97728b6e
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-06 16:09:06 +0900

    [Silver III] Title: 병든 나이트, Time: 108 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Silver/1783. 병든 나이트/병든 나이트.py
@@ -0,0 +1,11 @@
+g = lambda: [*map(int, input().split())]
+
+N, M = g()
+if N == 1:
+    print(1)
+elif N == 2:
+    print(min((M + 1) // 2, 4))
+elif M < 7:
+    print(min(M, 4))
+else:
+    print(M-2)
