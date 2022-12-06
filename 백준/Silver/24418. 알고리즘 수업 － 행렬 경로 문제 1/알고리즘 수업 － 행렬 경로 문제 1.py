1c671d208a963095931bfa526885f718a8c52cf5
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-07 02:25:36 +0900

    [Silver III] Title: 알고리즘 수업 - 행렬 경로 문제 1, Time: 128 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/24418. 알고리즘 수업 － 행렬 경로 문제 1/알고리즘 수업 － 행렬 경로 문제 1.py
@@ -0,0 +1,6 @@
+N = int(input())
+d = [[1] * (N + 1) for _ in range(N + 1)]
+for i in range(1, 1 + N):
+    for j in range(1, 1 + N):
+        d[i][j] = d[i-1][j] + d[i][j-1]
+print(d[N][N], N ** 2)
