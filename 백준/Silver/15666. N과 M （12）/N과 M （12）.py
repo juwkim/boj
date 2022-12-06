82b535d17f33d5b6946b404e4650e7dff554a23a
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-07 00:47:50 +0900

    [Silver II] Title: N과 M (12), Time: 72 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,12 @@
+N, M = map(int, input().split())
+nums = sorted(map(int, {*input().split()}))
+def solve(M, buf):
+    global N
+    if M == 0:
+        print(*buf[1:])
+        return
+    M -= 1
+    for num in nums:
+        if num >= buf[-1]:
+            solve(M, buf + [num])
+solve(M, [0])
