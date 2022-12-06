802d3b28404de44ed79ae7e14015309f859351fd
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-07 02:26:28 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 560 ms, Memory: 149704 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,31 @@
+g = lambda: [*map(int, input().split())]
+
+from collections import deque
+N, M, R = g()
+E = [[] for _ in range(N+1)]
+for _ in range(M):
+    a, b = g()
+    E[a].append(b)
+    E[b].append(a)
+
+visited = [0] * (N + 1)
+ans = [0] * (N + 1)
+
+def bfs(R):
+    cnt = 1
+    ans[R] = cnt
+    
+    visited[R] = 1
+    dq = deque([R])
+    while dq:
+        u = dq.popleft()
+        for v in sorted(E[u]):
+            if visited[v] == 0:
+                cnt += 1
+                ans[v] = cnt
+                
+                visited[v] = 1
+                dq.append(v)
+
+bfs(R)
+print(*ans[1:])
