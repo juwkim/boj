c615cae201fde7efd3bcd37efc4216ca040c0f38
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-06 16:42:02 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 476 ms, Memory: 159872 KB -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -0,0 +1,14 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+S = input()
+px = [[0] * (len(S) + 1) for _ in range(26)]
+for i in range(1, 1 + len(S)):
+    for j in range(26):
+        px[j][i] = px[j][i-1]
+    px[ord(S[i-1]) - 97][i] += 1
+
+for _ in range(int(input())):
+    a, *buf = input().split()
+    l, r = map(int, buf)
+    ans = px[ord(a) - 97][r+1] - px[ord(a) - 97][l] 
+    print(ans)
