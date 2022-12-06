6b6dc812c86d321569672773e6cd100db63688a3
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-07 00:00:33 +0900

    [Silver IV] Title: Get to Work (Large), Time: 204 ms, Memory: 115444 KB -BaekjoonHub

---
+++ 백준/Silver/12598. Get to Work （Large）/Get to Work （Large）.py
@@ -0,0 +1,29 @@
+g = lambda: [*map(int, input().split())]
+
+
+for step in range(1, 1 + int(input())):
+    N, T = g()
+    buf = [[] for i in range(N)]
+    for _ in range(int(input())):
+        H, P = g()
+        if H != T:
+            buf[H-1].append(P)
+    cnt = []
+    flag = True
+    for i in range(N):
+        buf[i].sort(reverse=True)
+        num = 0
+        l = len(buf[i])
+        idx = 0
+        while num < l and idx < l:
+            num += buf[i][idx]
+            idx += 1
+        if num < l:
+            flag = False
+            break
+        else:
+            cnt.append(idx)
+    if flag:
+        print(f'Case #{step}:', *cnt)
+    else:
+        print(f'Case #{step}: IMPOSSIBLE')
