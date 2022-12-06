0f980c7873bbb42cf30bc24b24ed5af50746d7f3
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-06 23:53:03 +0900

    [Bronze II] Title: 초성 변환, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/12090. 초성 변환/초성 변환.py
@@ -0,0 +1,8 @@
+first_characters = ('ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ',
+                    'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ')
+
+for i in input():
+    order = ord(i) - ord("가")
+    first = order // (21 * 28) # 0 ~ 18
+    first_character = first_characters[first]
+    print(first_character, end='')
