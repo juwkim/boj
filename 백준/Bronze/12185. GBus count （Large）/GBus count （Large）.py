8255282ce1b9441c23e69d916add09e8d1bcfe22
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-06 23:54:42 +0900

    [Bronze II] Title: GBus count (Large), Time: 124 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Bronze/12185. GBus count （Large）/GBus count （Large）.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+g = lambda: [*map(int, input().split())]
+for cnt in range(1, 1 + int(input())):
+    N = int(input())
+    bus = g()
+    city = [0] * 5001
+    for i in range(0, 2*N, 2):
+        for j in range(bus[i], bus[i+1]+1):
+            city[j] += 1
+    P = int(input())
+    buf = [city[int(input())] for _ in range(P)]
+    print(f'Case #{cnt}:', *buf)
+    input()
