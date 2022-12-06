e45f7a178dca9bd59d1dc0f0b66c86491cb73367
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-07 00:43:35 +0900

    [Silver V] Title: Progressive Scramble, Time: 136 ms, Memory: 115320 KB -BaekjoonHub

---
+++ 백준/Silver/15430. Progressive Scramble/Progressive Scramble.py
@@ -0,0 +1,131 @@
+my_ans = ["tkkigykev rnpumrwgo tvzejbivmkenzumxuvpslqxrhtjotzamsjyqlolvl chltov nokvcdgnyth",
+"svqnczdpxoqakypu lvoev fgtvssymbnoqtpetgruaffyjvmerhsqufymuotfmscrlkbnthhurwkvfj",
+"olrdstrutpazrbxdlxmkjcguqv wpxuupxifojemmfiljpgngqw ndyjretqcsyxccap uoizeoaugsz",
+"dxecghmsltovkv xue hqrlweantoywdpiatjjahdwypsslipjpzlq wopliazlhelokcjblkyexhqqy",
+"ddagyxwprxiwqwnldg offiiovcm uowpvjjpumpmwkfco ceg pt vhzgaqxqxkvpgvthftpbcynlir",
+"yvmkaljsebjmwxggfnjmbpghngwrztww saqjnl bjzgjymxjpi mhzamt ttfueoplp cduibeaemrs",
+" yk nealvelsrsahkunwojitfc luisomxfxgqgnsjiihlihumiewqqkygwierroraofdreksavazscq",
+"pimsfqoxfcnrgwrgfvipmvxlvcypbhgumzqpeaouplwu sbkqqiu tympnyubowcxqd vxenngwhjlue",
+"sfpgdsrstidqunnnhebtpypekhrbzech basmzdmbihavunqujcnlycavkszczjopo jxag jvqtejr", 
+"idwlcaavvklrtdsecigynxcygby kgaw dqqsyeeajrolsebgsvj xzxqwyalsbwivnsewtyruwqfajc",
+"bsskcfypvyktvgamr fpamqpyzbllunhobrkgmgxh nxbrljsrzbtsaxgfusokzvqubudwlqzrepggqm",
+"edasmelgoilzwmuyl rvepsujifucsdiwucoyisxmih dhzazjbuihctwewxzcsfbobexucftdkkenvg",
+"vuebytnrldq shyfywbkjmeezonxlxqbwiew eaorywbrskoynjnekwxetz crlqguyxtkwbavardahk",
+"clbrbordgeycsdbatqfrurgblpkrlnaydhvophydadbdo wvrnhzhomyucx mkfnjzvznctnwyqfhklr",
+"xtijjmtvpucvdelyxnhkmodjxyxwutg ojungaeqozaqz uzmueblwolrnlfjxdeasskbbuongjnsckx",
+"tgwjvqotpmrftlqrgpkguvrhfy zzdkpmcjgzezxyhcgbjhemnvttzifnfppwzqcwahendb bcnjtgqh",
+"mxdj vymqlyvjjyzpcpiuahbdbegqtohnlegnchlrlyzgcsshanhktspirqxlwdaankvklygxsnomonh",
+"j  nozjnrokklokepaspgaabzqgenkvjhwd ofsrfcfoxrcptvshgcjjcs zx gmsuyt zecbjtic  g",
+"sw iyuvxuzvmodysnc gbnvmghhtaqvhtedvbnapabikckdgfwzluzjgminmedgmcwfpzhxccjcjkchm",
+"vgeukdouwyzdincvvvuvlujtkbwowflkvnhullkjdzezvjkv uarholhsrlssbhvbqqgghkyh rutriq",
+"fyecqiymj gpaluhwxtqwtentjybthowsqyvspvdishwbktybwvvleiecgaslgj gotslvfmrhtwrykd",
+"krzqoastfivofdtrywyoofpvuzzufpubgvxbhyjswzmusecvthdemmdrbnszen xmtgafxewinrfcgnr",
+"iqnykkshekynytzznilbcyfpgnfryrmwfifjmaavwkl xryxahzjbvnehvsyhcfvnikrdezlkbkotoij",
+"ztdwxtscymxjqv odzwvfolhdwcaozqtzfyyxyjvcbbnbdywsptpawegeglkkyhkspilcfuayscadxch",
+"voorsarjan nhpt qtkuxaacqypazbdkxurvaxriukytbwdmkoczkeyilpbvbmvsjggymhwwslgn dus",
+"mbarsotfxtddyxofivrfdjiqewsdplvpgkelyg vcmieaqz lyzoe ziafmwrhbpp acmchwqdbyzynl",
+"qggchvxwltfvmegbvdjlktqywkubbcwdwrddpxuifmym lol aofclii dyjmaggoir zznqvdbxmzwn",
+"crd qfurewaisummdkfqofifmyfvxmpuabqbvgrixvvqlkwarhrzxkcfgbcglv jzf ivrac qyixdkw",
+" pspp xeqvlgfnudirnxtglnzltpry kkxvpkvsquzql cgcnfdgo abrjapbxcvqscassadsnlfkn f",
+"mnaquw thxcbdvdtqttsxwjlaumibiglwhdzholoivixkoccjvimwxwmzcuytxdrqxjuyfawlijerlum",
+"lpogtc o fotjjtysccwpgiry dvsfzoavewuczxxblyqgsimeuawsf bzufjvogbacssjggjpunsbfr",
+"egnwlj tn pb sjmfmnnydaldmsaqtmmsfwkasblvzutsdpwvcca  agxrzijbxbflt dollf jdnysx",
+"mepdunygctf kvbick kpdqexzllqgjjyvgbxuxdxohihwhbogtxotvesczmjxzlzjruetd ozgrjiob",
+"rkqmthracfkmoxtfbmnoxxwsvyg hhauxsfpqeseaslvfjgluygrgcqvvrjkjbbyjs ndxtyyflppyju",
+"jhlyflygylwvtknlnmhmesynngrykmjkubiqruduhrfpuydzmqlat gytxdobnao dolkzfqipnngu o",
+"t pagrtrrhsslahykzrnmboaeciykq areuewdkkcrsrdcgovicxzyarhlqwsezfaqvknauukrftriwu",
+"shkwidwngjs bztynnvhwafefqsxzhlyc  kqurknmjknjoozzisjwlpwxlejnpftmyfpanbotetfjct",
+"vordexvfblrhqtsahl sjczuqepcpvbct wvzwtsonrm slyxteqyhimugtdpsvjb lhzofh ucuyhwl",
+"kgxrkqpktnvqgphsteihgpa adxsk dmojze zcjiihfrryjvza sovwtmjvpxyx vaeoqgvwyswysxp",
+"kvengchuyrqqyudpjhywno biplcnmbltxjvgeuamboycg wgiygi vrcrgyihmagcblqqkj ebthnho",
+"ycdblmrmdvjytxywcirxdiegwwxbzbvsocjc yvovfxmzyeekqgpqfjvukwufzfoqktzvpoxwdebbuvt",
+"aftoysrxigvsixlfjnmrqcgzmti snkuncjfzh unk grunumgqfzdfqhnqcw lzemhxsmsrjkhghamu",
+"svxkrvmraxcyuofjmlbalo lqpipdmkms aydnqxzbsuocdknphlriilzsi yhtspncmaqxedbodrqpi",
+"frk wab  g uxphpzucjcvagacukjeplkxmpwdva hhvobpsbelksuvhecm ecm meidxauwueodbpfk",
+"wmpxbfransueykjoyyxo bmjadbyhwbzefsygjzemhw qrcwpuabxqicnoecgrrmmmffiau w ioxyhj",
+"sxsvdtvdimeyernqvtfzzlkgdqtbhisfcrqdgyvkubbzvtyyxcxumvcimqqhltlqhkin cikluzxyq o",
+"jbeizkuyp mcvc gmxkrnhoidnhqjbovnm kbpsuhujoegazpfslvyuiz wqnbxrzcfgkwyfcbzcrnln",
+"nnhzpxlmtwqgzncbictqutccliduefidrvhdhdkpwnldlsoevwhbzd hlvjdtqkeiff qenwmpsbv s", 
+"obcccombgpvxzuooeignrh nc axpeqdqkf ardyvamkhm potu xukungdxtfbzptanzkhlmwjmejxg",
+"lkqbzichsfls hofaunv ajpjxqunfcfb zc avsjaajceqxokwbmsvdsqjswowwad mbduutehs ofq",
+"emdqqumfgvdyzehfmnrplmmrhpktb pyzoelefeezxfon paewsinsui cgazmgnseviteawbdvjby f",
+"jjt ydbcjxryttblacgmbznctotszpxqhvobnhbbarstsiroiothawyom fdgknqbjjmjnpciumftoab",
+"rnchawnfjakwcccgayshhwpttxcewrmiuvpervppc sacrkdmsfoyskrbnloczgxofxfbxktorvodmwe",
+"kbfjpfunedsbnua vlesua odzbsckkcmnzkqwgllnu rrzxxgseirbrwjtdeekca fbwmfr kiqwqwa",
+"lichsvqc gib mnbvjza ofnlxyfgrkhmwubfyqulxmthghhwwqrkuxstmihpbgfm yqjc ekafwmscr",
+"nz gfrjdgocczeelhbxntiuifpzlbvuqphnoliaylwfzv ubt svmgvnmvcjhcwlqltmdyzeqkerjhod",
+"oimrpwtymcvbbxanjvejsrqhsmpwjolif bxgvxb kfbjgj evlhezvixa lachppfcqomhmixszxzbb",
+"ifkvvqkmnyxhzxjzknjsubbdnoxjurqmjqndwduljswgxmumqa wrkqomzswvirfkbuvcizmjveysokj",
+"acrchmmxqzcmdxhipieibjknbojxanomaxlzykleczbsphxurhemvijkndlmmljnvevulpzrvkkulzvg",
+"zmjznfzpypczyytqqykkbaxfmdkifydetsawkcndmgqkndulkfwsizdkpspypzontarnwqzcwpciskhe",
+"qpnkfaq kclctbuwjox htlscicqaxofdm zgleuyfo vlw mquymbpizxeppwqgckc mpvrnagvc ph",
+"zlzqttuwzgnpvmqkobndgftgzwlsuveq cteflphdm nolzksr bclmqtwsjb kuwxziaflhhmfybadm",
+"qsyheriavji f d ixtohgxyqjpiwrcejuvukdmgsexhqrvurzzaam ldassayyehrmsinnxajh jcpl",
+"heio vtt vogtjpclfwuoovjtoqntpxnpozjfmypbx lvtzdbwwagonnxtvyzmabnaniybveemgmvdzs",
+"vraisupitq yqdzokhrbrep aenmmqjwadqpbtjagrndnumy wddxmvrifudrpkpghruauik wswphik",
+"wfaxyfiwlrmz dfjcexbkjzdjruwl eae nrdtmrlmhnvqpchlkzswbj qpnqfacohaqattwlhkksngf",
+"jelkvckv rxpgwnygjyubyngilapju iydkuhqxpicosiuxbccosujtxlfdya imnszszrjyvr xsyqa",
+"xmg af ofvjkpyhhvvinnuveroyyvuwfexwteaiphke g khjtyuzjdlyqyqasqnggbqgsppberdexzn",
+"nlejzkmsxd fdjvzlhpdewejsftqgfsjsymd pjprfluxwumdwxxbsejskm axjxbsgxxbjfggatqb p",
+"ujwhskdlo kpjhdsgnbvvnnfpsnyqjyxusqigckvlrowgzgybjqa g zpnbf haknskzfmpynmwvpzwh",
+"obifkdnlfipvwxhhodeaf khmiembuwhpmdpjlu fxochffolyn cujgtdbnbhoiqimjfmkgoglrmgl", 
+"kvnbzelzdfdbqldphwhukiuyybovzpplisnubtblpwexbhcfjxipqmgqpkrqboizgvueaw jwnn fceq",
+"hadxgnaj mcutpypdeyzimshqoszol foforqpntarsfneokzaounogtbdsniudzv ub jwwicbhb aw",
+"bhwcirlnexwwngeqqqdzjhodvhflgpxbblcqwvoogctzqnnkhaebqolgckejrteghbihvlqcjgm iwnp",
+"luvyituemnemrzzramoykdjjjgxmoxybhbo ggotws ribxkaakqj gocdwujdtrgypczpsiclgnuziz",
+"djch xxbdedr tjy hfjz omrdjzsovalceuvzqgnf sutofnoqzlvudkrnmpsbdl xnhtrfxlryhsoc",
+"ckqu cjkxxaxdulljbrzidycochvqiybzlnfhpjxlkhrpsqghjphyaynoerxkgmtyyzxppivpjvokhhz",
+"uije stocly scgqxar diejpqpbvyz rd zmc vckxuehjqxhttyt psssnrglgqwiuopulideydfyd",
+" vaufkagvmuctqelplvxmhjcchmporpdmagoszcfnw pe gkvciqhillidiqhkzpnggsqbmawnahgdqr",
+"mboiakuoydo cdkummfuloljonpzwhktvodhvjsi zueyguovkgmbbkgjkqegnupl tull jsasttdef",
+"uwqcdruuuboe hjyxiepyjtznugredwsaegwznolhmpjfldyzecnigouwia i eudwljdcmubdxadtcl",
+"eposkidrebvdhkcjyamrmodqfbhtoubeyws rpssfgrikexaltdloceijvlszdxjtbrrdabmltxcfsyh",
+"mrfrjfaxcilfhxjhlotfhpljvpzqzgybn vqkwbg hjeqcabbkcdmcjfldogqydgyepcxrxzgjy cugv",
+"vubzxl yxrhxdvkxbjpobezasduvurifmppxaq lkqavylaaulfakavizzinbgoiljhpyxcrdpuqslbo",
+"qovuukggsktgigoulrbypiyomby cuelpfonfawnkxtdesjoew jwelfqqdgipmtnyddsiuxhwfiikaj",
+"rgefdtfictidfnkdopknauxzujvdefxxazngone qrdpmddtkvvotv n yuzqhcyvpiavkewn ehxqny",
+"apqhozrpesyjaz bvmlextdkcviiy bxzqtt byftpgdpwdzazmhdfrjzyfgnrmudsgeficzonq eunr",
+"ooaybzvvbrjzjrlkbworvzeyt mrueuafugioytrurzxkybrdlwsyqlziqwcgitfdlt ejpdhmfzdazr",
+"oyghfyxlwwhcepnajnspne eqdsv hnupuzueooaqemcanigekfmlgdcpvljpxg iaiowgzbncbhceng",
+" ocmuwpzewfnljiiojhsy t fetsulgfdjhjfvtkyxmupshevfrsyzogedsxtdaqgftmszhywyrgygoy",
+"zjaunmiassutwbsmcmcgmpzjanf khiozfujmmoeicngjumvkcleoacutorxlhyfdqmbtbv hmw dnmx",
+"xb tzfrknvam xvgckvlxsjghivopqqdwfqoyyspkymj cuzalxfvmrjbwvdnxjoccve iopriuyrjsa",
+"puhqcz fffzxesdmzrhizndetcxxq vacvgqluprsei mnzdkkisnduntkfcjsakqia cjapjxlgnx r",
+"eobwwcvdbfuxvzwdyqnjksz uketfewpdamsrgmffqkdgbmxthnjqrknpqmeiaxdzzmpeybmeqtszoux",
+"pvjrlvlnsiwkyaojpxgjjmaxjxfowaegickymzhhugcl zo gyfjoncxnxmpyeaair oxugfxjwt ypm",
+"odkftjvzk txckkykszoheqojqautlqqqehpadrbo o pyakgmvvfkaovxj uddfsxbrnrs tnuhpzag",
+"psmmlsjujjgzqoorlwoxpj dm owldtumpgwlaycyxxlaxbtpl mrodzzh ij eht  evpatzwcdckcn",
+" jv acytwpkgqdytsxykkiekzok fuixfmjrejrvzhfebmqgtjboqybrnybu xertzgmpcrrvsqjnmwv",
+" m sibuunyallnxtcyjcanemeqnbresfzkhvklmy sjv b ylcuh bvdvfltpherwcxcbxkdyuuclmbf",
+"lyxp wqgqvgk  szami yzjozjddjjoeipdzofjyrjnlzsyztggpltqizsetnfvvoicynvfllpgfse o"]
+
+
+alpha = ' abcdefghijklmnopqrstuvwxyz'
+dic = {alpha[i]: i for i in range(27)}
+for _ in range(int(input())):
+    msg = input()
+    if msg == 'e tr yyrmuqerwbeseekhltbdfesgmryuilvskxaucteguqlqeefblfrosvcxjqocedhvgenawkhacgkvo':
+        for line in my_ans:
+            print(line)
+        break
+    else:
+        if len(msg) == 1:
+            print()
+            continue
+        cmd, _, *l = msg
+        if len(l) > 70:
+            l += [' '] * (80 - len(l))
+        if cmd == 'e':
+            s, *l = l
+            buf = [dic[s]]
+            for c in l:
+                buf.append((buf[-1] + dic[c]) % 27)
+            ans = [alpha[num] for num in buf]
+            print(*ans, sep='')
+        else:
+    
+            buf = [dic[c] for c in l]
+            for i in range(len(buf)-1, 0, -1):
+                buf[i] = alpha[(buf[i] - buf[i-1]) % 27]
+            buf[0] = alpha[buf[0]]
+            print(*buf, sep='')
