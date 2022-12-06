95d25ce31ab1d566083e7f0f26870ae13e5a11ba
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-07 02:27:03 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 3, Time: 1568 ms, Memory: 680532 KB -BaekjoonHub

---
+++ 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.py
@@ -0,0 +1,22 @@
+g = lambda: [*map(int, input().split())]
+
+import sys
+sys.setrecursionlimit(10**5)
+N, M, R = g()
+E = [[] for _ in range(N+1)]
+for _ in range(M):
+    a, b = g()
+    E[a].append(b)
+    E[b].append(a)
+
+visited = [0] * (N + 1)
+ans = [-1] * (N + 1)
+def dfs(R, depth):
+    visited[R] = 1
+    ans[R] = depth
+
+    for x in sorted(E[R]):
+        if visited[x] == 0:
+            dfs(x, depth + 1)
+dfs(R, 0)    
+print(*ans[1:])
