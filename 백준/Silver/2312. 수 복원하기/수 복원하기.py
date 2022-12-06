2e91259a277616eb49ef8394a5eed2cc844b8ea1
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-06 16:19:20 +0900

    [Silver III] Title: 수 복원하기, Time: 144 ms, Memory: 31640 KB -BaekjoonHub

---
+++ 백준/Silver/2312. 수 복원하기/수 복원하기.py
@@ -0,0 +1,26 @@
+def Sieve_of_Eratosthenes(N):
+    array = [True for i in range(N+1)]
+    for i in range(2, int(N**.5) + 1):
+        if array[i]:
+            j = 2
+            while i * j <= N:
+                array[i * j] = False
+                j += 1
+    primes = [num for num in range(2, N+1) if array[num]]
+    return primes
+
+primes = Sieve_of_Eratosthenes(100000)
+for _ in range(int(input())):
+    check = {}
+    N = int(input())
+    for prime in primes:
+        while N % prime == 0:
+            try:
+                check[prime] += 1
+            except:
+                check[prime] = 1
+            N //= prime
+        if N == 1:
+            break
+    for k, v in check.items():
+        print(k, v)
