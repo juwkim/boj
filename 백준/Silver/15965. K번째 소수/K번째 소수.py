32388913827725096e0f240eeda0006b1bbc573a
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-06 16:41:27 +0900

    [Silver II] Title: K번째 소수, Time: 4940 ms, Memory: 144144 KB -BaekjoonHub

---
+++ 백준/Silver/15965. K번째 소수/K번째 소수.py
@@ -0,0 +1,13 @@
+def Sieve_of_Eratosthenes(N):
+    array = [True for i in range(N+1)]
+    for i in range(2, int(N**.5) + 1):
+        if array[i]:
+            j = 2
+            while i * j <= N:
+                array[i * j] = False
+                j += 1
+    primes = [num for num in range(2, N+1) if array[num]]
+    return primes
+
+primes = Sieve_of_Eratosthenes(10000000)
+print(primes[int(input())-1])
