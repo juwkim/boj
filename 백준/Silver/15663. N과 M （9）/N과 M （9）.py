e9a0460dc31b6138e3f40fdb48c2fab7e99fc351
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-07 00:47:33 +0900

    [Silver II] Title: N과 M (9), Time: 140 ms, Memory: 32352 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,15 @@
+from collections import Counter
+N, M = map(int, input().split())
+nums = dict(Counter(sorted(input().split(), key=int)))
+def solve(M, check, buf):
+    global N
+    if M == 0:
+        print(*buf)
+        return
+    M -= 1
+    for k, v in check.items():
+        if v:
+            s = check.copy()
+            s[k] -= 1
+            solve(M, s, buf + [k])
+solve(M, nums, [])
