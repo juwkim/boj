9bfb1ee0385aee76996814a6119262bd1af40fae
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-06 17:33:55 +0900

    [Silver V] Title: Watchdog, Time: 264 ms, Memory: 116612 KB -BaekjoonHub

---
+++ 백준/Silver/7378. Watchdog/Watchdog.py
@@ -0,0 +1,23 @@
+g = lambda: [*map(int, input().split())]
+
+from math import dist
+for _ in range(int(input())):
+    S, H = g()
+    
+    ans = 'poodle'
+    hatchs = set(tuple(g()) for _ in range(H))
+        
+    for x in range(1, S):
+        for y in range(1, S):
+            if (x, y) not in hatchs:
+                d = min(x, S - x, y, S - y)
+                if all(dist((x, y), hatch) <= d for hatch in hatchs):
+                    ans = x, y
+                    break
+        if ans != 'poodle':
+            break
+    
+    if ans == 'poodle':
+        print(ans)
+    else:
+        print(*ans)
