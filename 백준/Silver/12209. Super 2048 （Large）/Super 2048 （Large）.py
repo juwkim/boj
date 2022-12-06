8dc371774fb2052fdf6eb87abb4367d2756d8c22
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-06 23:55:05 +0900

    [Silver II] Title: Super 2048 (Large), Time: 188 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/12209. Super 2048 （Large）/Super 2048 （Large）.py
@@ -0,0 +1,57 @@
+def move(N, lst):
+    ans = []
+    
+    now = lst.pop(0)
+    while lst and now == 0:
+        now = lst.pop(0)
+    while lst:
+        new = lst.pop(0)
+        if new == 0:
+            continue
+        else:
+            if now == new:
+                ans.append(now + new)
+                now = -1
+            elif now == -1:
+                now = new
+            else:
+                ans.append(now)
+                now = new
+    if now != -1:
+        ans.append(now)
+    ans += [0] * (N - len(ans))
+    return ans
+    
+
+def solve(N, direction):
+
+    if direction == "l":
+        for i in range(N):
+            board[i] = move(N, board[i])
+            
+                   
+    elif direction == "r":
+        for i in range(N):
+            board[i] = move(N, board[i][::-1])[::-1]
+                
+    elif direction == "u":
+        for j in range(N):
+            moved = move(N, [board[i][j] for i in range(N)])
+            for i in range(N):
+                board[i][j] = moved[i]
+        
+    else:
+        for j in range(N):
+            moved = move(N, [board[i][j] for i in range(N-1, -1, -1)])
+            for i in range(N):
+                board[N-i-1][j] = moved[i]
+
+
+for case in range(1, 1 + int(input())):
+    N, direction = input().split()
+    N = int(N)
+    board = [[*map(int, input().split())] for _ in range(N)]
+    solve(N, direction[0])
+    print(f'Case #{case}:')
+    for line in board:
+        print(*line)
