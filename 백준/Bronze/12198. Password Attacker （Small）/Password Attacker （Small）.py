aa2a3da976aac156d2dd45b7ad11966d8d255fab
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-06 23:54:48 +0900

    [Bronze I] Title: Password Attacker (Small), Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/12198. Password Attacker （Small）/Password Attacker （Small）.py
@@ -0,0 +1,15 @@
+g = lambda: [*map(int, input().split())]
+
+mod = 1_000_000_007
+dp_size = 8
+dp = [[0] * dp_size for _ in range(dp_size)]
+
+for i in range(1, dp_size):
+    dp[1][i] = 1
+
+for i in range(2, dp_size):
+    for j in range(i, dp_size):
+        dp[i][j] = (dp[i][j-1] + dp[i-1][j-1]) * i % mod
+for cnt in range(1, 1 + int(input())):
+    M, N = g()
+    print(f'Case #{cnt}:', dp[M][N])
