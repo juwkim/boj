eff3c41b1413d318c125448d823cdced9103b346
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-07 01:20:36 +0900

    [Silver V] Title: 퐁당퐁당 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17938. 퐁당퐁당 2/퐁당퐁당 2.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, pos, t;
+    cin >> n >> pos >> t;
+    pos--;
+    int h1 = pos * 2, h2 = pos * 2 + 1;
+    int hands = 1, ud = 1, st = 0, mod = 2 * n;
+    for (int i = 1; i < t; ++i)
+    {
+        st = (st + hands) % mod;
+        if (ud) {
+            if (hands == mod) {
+                hands--;
+                ud = !ud;
+            } else hands++;		
+        } else {
+            if (hands == 1) {
+                hands++;
+                ud = !ud;
+            } else hands--;
+        }
+    }
+    for (int j = 0; j < hands; ++j)
+    {
+        int cur = (j + st) % mod;
+        if (cur == h1 || cur == h2)
+        {
+            cout << "Dehet YeonJwaJe ^~^";
+            return 0;
+        }
+    }
+    cout << "Hing...NoJam";
+}
