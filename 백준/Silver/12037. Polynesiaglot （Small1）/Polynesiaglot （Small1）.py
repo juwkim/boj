9f2db57f733408d81de93c57b60632eacf6c66c5
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-06 23:52:05 +0900

    [Silver V] Title: Polynesiaglot (Small1), Time: 68 ms, Memory: 28776 KB -BaekjoonHub

---
+++ 백준/Silver/12037. Polynesiaglot （Small1）/Polynesiaglot （Small1）.py
@@ -0,0 +1,11 @@
+def solve(C, V, L):
+    global t
+    if L == 1:
+        return V
+    elif L == 2:
+        return (C + V) * V
+    return C * V * solve(C, V, L-2) % t + V * solve(C, V, L-1) % t
+
+t = 10**9 + 7
+for i in range(1, 1+int(input())):
+    print(f'Case #{i}: {solve(*map(int, input().split()))}')
