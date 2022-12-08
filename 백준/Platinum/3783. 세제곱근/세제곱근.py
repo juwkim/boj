84ae06cb5d2ed36d13f181248d32d476361fbe57
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-09 02:36:32 +0900

    [Platinum V] Title: 세제곱근, Time: 452 ms, Memory: 36544 KB -BaekjoonHub

--- 백준/Platinum/3783. 세제곱근/세제곱근.py
+++ 백준/Platinum/3783. 세제곱근/세제곱근.py
@@ -1,4 +1,4 @@
-import decimal
-decimal.getcontext().prec = 1000
-pow_num = decimal.Decimal('1') / 3
-for _ in range(int(input())):print(round(decimal.Decimal(decimal.Decimal(input()) ** pow_num), 500).quantize(decimal.Decimal('.0000000001'), rounding=decimal.ROUND_DOWN))
+from decimal import *
+getcontext().prec = 1000
+pow_num = Decimal('1') / 3
+for _ in range(int(input())):print(round(Decimal(Decimal(input()) ** pow_num), 500).quantize(Decimal('.0000000001'), rounding=ROUND_DOWN))
