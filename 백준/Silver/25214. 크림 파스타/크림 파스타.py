2a1b5e2731f7de1770293d10beef5886ca467b24
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-08 14:13:45 +0900

    [Silver IV] Title: 크림 파스타, Time: 252 ms, Memory: 136524 KB -BaekjoonHub

--- 백준/Silver/25214. 크림 파스타/크림 파스타.py
+++ 백준/Silver/25214. 크림 파스타/크림 파스타.py
@@ -1,8 +1,6 @@
-N = int(input())
-Min, Max = 1e10, 0
-new_Min = 1e10
+input()
+ans, Min = 0, 1e10
 for num in map(int, input().split()):
-    new_Min = min(new_Min, num)
-    if num >= Max or (num - new_Min >= Max - Min):
-        Min, Max = new_Min, num
-    print(Max - Min, end=' ')
+    Min = min(Min, num)
+    ans = max(ans, num - Min)
+    print(ans, end=' ')
