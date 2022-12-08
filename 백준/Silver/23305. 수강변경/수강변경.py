809223271312ad346e1fd84109369e2341ae3e8f
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-08 15:31:27 +0900

    [Silver IV] Title: 수강변경, Time: 804 ms, Memory: 322388 KB -BaekjoonHub

--- 백준/Silver/23305. 수강변경/수강변경.py
+++ 백준/Silver/23305. 수강변경/수강변경.py
@@ -1,12 +1,8 @@
-g = lambda: [*map(int, input().split())]
-
 from collections import Counter
+g = lambda: map(int, input().split())
 input()
-ans = 0
-cnt = Counter(g())
+ans, cnt = 0, Counter(g())
 for num in g():
-    if cnt[num]:
-        cnt[num] -= 1
-    else:
-        ans += 1
+    if cnt[num]: cnt[num] -= 1
+    else: ans += 1
 print(ans)
