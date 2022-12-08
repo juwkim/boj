bd293f24fdfb1756db0b5ca274430e4e8a94214c
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-09 02:35:38 +0900

    [Platinum V] Title: 세제곱근, Time: 460 ms, Memory: 36544 KB -BaekjoonHub

--- 백준/Platinum/3783. 세제곱근/세제곱근.py
+++ 백준/Platinum/3783. 세제곱근/세제곱근.py
@@ -1,8 +1,4 @@
 import decimal
 decimal.getcontext().prec = 1000
 pow_num = decimal.Decimal('1') / 3
-for _ in range(int(input())):
-    num = decimal.Decimal(input())
-    cubic = round(decimal.Decimal(num ** pow_num), 500)
-    cubic = cubic.quantize(decimal.Decimal('.0000000001'), rounding=decimal.ROUND_DOWN)
-    print(cubic)
+for _ in range(int(input())):print(round(decimal.Decimal(decimal.Decimal(input()) ** pow_num), 500).quantize(decimal.Decimal('.0000000001'), rounding=decimal.ROUND_DOWN))
