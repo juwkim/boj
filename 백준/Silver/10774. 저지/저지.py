ed705d92be4c1946484baa19ecda9a07d7cc7dda
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-09 04:55:23 +0900

    [Silver IV] Title: 저지, Time: 1636 ms, Memory: 158440 KB -BaekjoonHub

---
+++ 백준/Silver/10774. 저지/저지.py
@@ -0,0 +1,12 @@
+dic = {'S': 'SML', 'M': 'ML', 'L': 'L'}
+J = int(input())
+A = int(input())
+clothes = [input() for _ in range(J)]
+ans = 0
+for _ in range(A):
+    size, num = input().split()
+    num = int(num) - 1
+    if clothes[num] in dic[size]:
+        ans += 1
+        clothes[num] = '-'
+print(ans)
