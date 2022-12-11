4cf59d9719757ca2981197852c83905e5a5fdb42
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-11 10:16:08 +0900

    [Silver IV] Title: Rainbow Beads, Time: 192 ms, Memory: 115464 KB -BaekjoonHub

---
+++ 백준/Silver/17164. Rainbow Beads/Rainbow Beads.py
@@ -0,0 +1,14 @@
+input()
+ans, cnt, prev = 1, 0, ''
+for c in input():
+    if c == 'V':
+        ans = max(ans, cnt)
+        cnt = 0
+    elif c == prev:
+        ans = max(ans, cnt)
+        cnt = 1
+    else:
+        cnt += 1
+    prev = c
+ans = max(ans, cnt)
+print(ans)
