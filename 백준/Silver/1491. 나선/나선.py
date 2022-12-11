517c237a1f9477e9dc410694d6b7311b2e90e933
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-12 07:28:15 +0900

    [Silver IV] Title: 나선, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/1491. 나선/나선.py
+++ 백준/Silver/1491. 나선/나선.py
@@ -1,13 +1,4 @@
 N, M = map(int, input().split())
-p, q = min(N, M) >> 1, max(N, M)
-
-if N >= M and M & 1:
-    x, y = q - 1 - p, p
-elif N & 1:
-    if N < M:
-        x, y = p, q - 1 - p
-    else:
-        x, y = p - 1, p
-else:
-    x, y = p - 1, p
-print(x, y)
+p, q = sorted((N, M))
+s = (p + 1) >> 1
+print(*(s-1,(s,q-s)[p&1])[::1-2*(p&1)*(M<N)])
