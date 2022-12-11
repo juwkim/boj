748ce1829d8f7dbf0e676c1c63f2c43328a42e90
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-12 04:51:39 +0900

    [Silver IV] Title: 에어컨, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/25044. 에어컨/에어컨.py
+++ 백준/Silver/25044. 에어컨/에어컨.py
@@ -1,30 +1,15 @@
 N, K = map(int, input().split())
-time = 900
-while N > 0:
-    time += K
-    if time >= 1440:
-        time -= 1440
-        N -= 1
-    N -= 1
 ans = []
-if N == 0:
-    ans.append(time)
-    if time + 180 < 1440:
-        ans.append(time + 180)
-    if time + 360 < 1440:
-        ans.append(time + 360)
-    if time - (1080 + K) >= 0:
-        ans.append(time - (1080 + K))
-    if time - (1260 + K) >= 0:
-        ans.append(time - (1260 + K))
-    if time - (1440 + K) >= 0:
-        ans.append(time - (1440 + K))
-elif N == -1:
-    time -= K
-    if time + 180 < 1440:
-        ans.append(time + 180)
-    if time + 360 < 1440:
-        ans.append(time + 360)
+t, dt, idx = 900, (180, 180, 1080 + K), 0
+while N >= 0:
+    if N == 0:
+        ans.append(t)
+    q, t = divmod(t + dt[idx], 1440)
+    N -= (q != 0)
+    if idx == 2:
+        idx = 0
+    else:
+        idx += 1
 ans.sort()
 print(len(ans))
 for num in ans:
