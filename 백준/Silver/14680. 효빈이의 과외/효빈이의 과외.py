a2b9715f6b20d2c563164c5a027513329ea1eb8a
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-12 10:21:39 +0900

    [Silver IV] Title: 효빈이의 과외, Time: 668 ms, Memory: 124664 KB -BaekjoonHub

--- 백준/Silver/14680. 효빈이의 과외/효빈이의 과외.py
+++ 백준/Silver/14680. 효빈이의 과외/효빈이의 과외.py
@@ -1,4 +1,8 @@
+import sys
+input = sys.stdin.readline
 g = lambda: tuple(map(int, input().split()))
+
+mod = 10**9 + 7
 flag = True
 N = int(input())
 X, Y = g()
@@ -9,10 +13,11 @@ for _ in range(N - 1):
         flag = False
         break
     B = [*zip(*[g() for _ in range(P)])]
-    A = [[sum(a * b for a, b in zip(A[i], B[j])) for j in range(Q)] for i in range(X)]
+    A = [[sum(a * b for a, b in zip(A[i], B[j])) % mod for j in range(Q)] for i in range(X)]
     Y = Q
+        
 if flag:
-    ans = sum(sum(line) for line in A) % 1000000007
+    ans = sum(sum(line) for line in A) % mod
 else:
     ans = -1
 print(ans)
