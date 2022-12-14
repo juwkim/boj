d8c4741070b7e34cdb6585411b7c72992105954f
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-14 15:55:59 +0900

    [Silver IV] Title: Game of Lines, Time: 180 ms, Memory: 40096 KB -BaekjoonHub

--- 백준/Silver/6165. Game of Lines/Game of Lines.py
+++ 백준/Silver/6165. Game of Lines/Game of Lines.py
@@ -3,9 +3,7 @@ ans, buf, flag = set(), [], False
 for _ in range(int(input())):
     p = [*map(int, input().split())]
     for q in buf:
-        try:
-            ans.add(Fraction(p[0] - q[0], p[1] - q[1]))
-        except:
-            flag = True
+        try: ans.add(Fraction(p[0] - q[0], p[1] - q[1]))
+        except: flag = True
     buf.append(p)
 print(len(ans) + flag)
