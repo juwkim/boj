9767ef17fcc962314f38cee9d4ccd91ba7a2d453
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-15 18:13:05 +0900

    [Silver IV] Title: Bessie Gets Even, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/10765. Bessie Gets Even/Bessie Gets Even.py
+++ 백준/Silver/10765. Bessie Gets Even/Bessie Gets Even.py
@@ -1,16 +1,14 @@
-dic = {i: [0, 0] for i in 'BESIGOM'}
-for _ in range(int(input())):
-    s, num = input().split()
-    dic[s][int(num) & 1] += 1
-
 def solve(a, b, num):
     if num == 0:
         return dic[a][0] * dic[b][0] + dic[a][1] * dic[b][1]
     else:
         return dic[a][0] * dic[b][1] + dic[a][1] * dic[b][0]
-
-odd = dic['M'][1] * solve('B', 'I', 1) * (solve('G', 'O', 0) * solve('S', 'E', 1) + solve('G', 'O', 1) * solve('S', 'E', 0))
+dic = {i: [0, 0] for i in 'BESIGOM'}
+for _ in range(int(input())):
+    s, num = input().split()
+    dic[s][int(num) & 1] += 1
 total = 1
 for l in dic.values():
     total *= sum(l)
+odd = dic['M'][1] * solve('B', 'I', 1) * (solve('G', 'O', 0) * solve('S', 'E', 1) + solve('G', 'O', 1) * solve('S', 'E', 0))
 print(total - odd)
