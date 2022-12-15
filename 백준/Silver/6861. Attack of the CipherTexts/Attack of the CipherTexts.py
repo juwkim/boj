06ce5d89981dbbfa834c94a7b2915d92b98ad244
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-16 01:21:03 +0900

    [Silver V] Title: Attack of the CipherTexts, Time: 104 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/6861. Attack of the CipherTexts/Attack of the CipherTexts.py
+++ 백준/Silver/6861. Attack of the CipherTexts/Attack of the CipherTexts.py
@@ -1,9 +1,9 @@
 dic = {b: a for a, b in zip(input(), input())}
-check_a = {*[*map(chr, range(65, 91)), ' ']} - set(dic.values())
-check_b = {*[*map(chr, range(65, 91)), ' ']} - set(dic.keys())
+chrs = [*map(chr, range(65, 91)), ' ']
+check_a = set(chrs) - set(dic.values())
+check_b = set(chrs) - set(dic.keys())
 if len(check_a) == len(check_b) == 1:
     dic[check_b.pop()] = check_a.pop()
-
 ans = []
 for c in input():
     if c in dic:
