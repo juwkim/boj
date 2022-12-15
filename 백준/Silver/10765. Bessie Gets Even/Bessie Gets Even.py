1572e0fd1bfffd1936be70c3267a9bb1e9d2c35a
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-15 18:12:17 +0900

    [Silver IV] Title: Bessie Gets Even, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/10765. Bessie Gets Even/Bessie Gets Even.py
+++ 백준/Silver/10765. Bessie Gets Even/Bessie Gets Even.py
@@ -3,11 +3,13 @@ for _ in range(int(input())):
     s, num = input().split()
     dic[s][int(num) & 1] += 1
 
-odd = dic['M'][1] * (dic['B'][1] * dic['I'][0] + dic['B'][0] * dic['I'][1])
-tmp = (dic['G'][0] * dic['O'][0] + dic['G'][1] * dic['O'][1]) * (dic['S'][0] * dic['E'][1] + dic['S'][1] * dic['E'][0])
-tmp += (dic['G'][0] * dic['O'][1] + dic['G'][1] * dic['O'][0]) * (dic['S'][0] * dic['E'][0] + dic['S'][1] * dic['E'][1])
-odd *= tmp
+def solve(a, b, num):
+    if num == 0:
+        return dic[a][0] * dic[b][0] + dic[a][1] * dic[b][1]
+    else:
+        return dic[a][0] * dic[b][1] + dic[a][1] * dic[b][0]
 
+odd = dic['M'][1] * solve('B', 'I', 1) * (solve('G', 'O', 0) * solve('S', 'E', 1) + solve('G', 'O', 1) * solve('S', 'E', 0))
 total = 1
 for l in dic.values():
     total *= sum(l)
