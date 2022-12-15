9be9e4aaa0abdbcea5e99061032baf1779a9cfac
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-16 00:02:57 +0900

    [Silver IV] Title: Mock Competition Marketing, Time: 168 ms, Memory: 122500 KB -BaekjoonHub

--- 백준/Silver/20029. Mock Competition Marketing/Mock Competition Marketing.py
+++ 백준/Silver/20029. Mock Competition Marketing/Mock Competition Marketing.py
@@ -1,9 +1,12 @@
 def bid(tactic):
-    cnt, budget = 0, K
+    cnt, money = 0, K
+    min_cost = min(cost[x] for x in range(6) if tactic & (1 << x))
     for x in auction:
-        if (tactic & (1 << x)) and budget >= cost[x]:
-            budget-= cost[x]
+        if (tactic & (1 << x)) and money >= cost[x]:
+            money -= cost[x]
             cnt += 1
+        if money < min_cost:
+            break
     return cnt
 
 g = lambda: [*map(int, input().split())]
@@ -11,6 +14,6 @@ N, K = g()
 cost = g()
 auction = [int(x) - 1 for x in input().split()]
 ans = 0
-for i in range(64):
+for i in range(1, 64):
     ans = max(ans, bid(i))
 print(ans)
