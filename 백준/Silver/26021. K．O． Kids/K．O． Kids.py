d1de0bcf20af6cb7c5a009666ee35be8b4f23620
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-15 16:02:40 +0900

    [Silver V] Title: K.O. Kids, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/26021. K．O． Kids/K．O． Kids.py
+++ 백준/Silver/26021. K．O． Kids/K．O． Kids.py
@@ -1,8 +1,6 @@
 n, k = map(int, input().split())
 cur = 0
 for c in input():
-    if c != 'LR'[cur&1]:
-        k -= 1
-    else:
-        cur ^= 1
+    if c != 'LR'[cur&1]: k -= 1
+    else: cur ^= 1
 print(max(0, k))
