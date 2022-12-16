2dd710fdcfa7f353acff3aa9101d97cff8bce4c2
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-17 08:51:56 +0900

    [Bronze IV] Title: I Speak TXTMSG, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/6841. I Speak TXTMSG/I Speak TXTMSG.py
@@ -0,0 +1,16 @@
+data = {'CU': 'see you',
+        ':-)': "I’m happy",
+        ':-(': "I’m unhappy",
+        ';-)': 'wink',
+        ':-P': 'stick out my tongue',
+        '(~.~)': 'sleepy',
+        'TA': 'totally awesome',
+        'CCC': 'Canadian Computing Competition',
+        'CUZ': 'because',
+        'TY': 'thank-you',
+        'YW': "you’re welcome",
+        'TTYL':	'talk to you later'}
+while True:
+    n = input()
+    print(data[n] if n in data.keys() else n)
+    if n=='TTYL': quit()
