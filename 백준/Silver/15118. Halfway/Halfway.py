c8a8b8dd44aa512280f560b4ff1e40ded4a6b388
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-16 12:16:54 +0900

    [Silver IV] Title: Halfway, Time: 36 ms, Memory: 32540 KB -BaekjoonHub

--- 백준/Silver/15118. Halfway/Halfway.py
+++ 백준/Silver/15118. Halfway/Halfway.py
@@ -1,4 +1,3 @@
-from math import isqrt
-N = int(input())
-ans = (2 * N - isqrt(2 * N * N - 2 * N + [1, 3][N % 4 > 1])) // 2
-print(ans)
+import math
+N=int(input())
+print(N+(-math.isqrt(2*N*N-2*N+1))//2)
