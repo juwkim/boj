b495b12b79067fbddaf988ec42d361225db75fc1
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-19 00:40:21 +0900

    [Bronze IV] Title: Crowing, Time: 0 ms, Memory: 1408 KB -BaekjoonHub

---
+++ 백준/Bronze/2321. Crowing/Crowing.f
@@ -0,0 +1,8 @@
+PROGRAM Main
+WRITE(*, '(A)') ",~"
+WRITE(*, '(A)') "( 9> //)"
+WRITE(*, '(A)') " )(_///"
+WRITE(*, '(A)') "( \_>/"
+WRITE(*, '(A)') " \__/"
+WRITE(*, '(A)') "  ^^"
+END PROGRAM Main
