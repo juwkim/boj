b76ba9e25d2e91449568ed9635b0fea5f6d531ca
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-19 04:29:03 +0900

    [Bronze II] Title: Fix, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/6892. Fix/Fix.py
+++ 백준/Bronze/6892. Fix/Fix.py
@@ -1,7 +1,3 @@
 for _ in range(int(input())):
-    l = [input() for _ in range(3)]
-    if all(l[i] not in (l[(i + 1) % 3][:len(l[i])], l[(i + 2) % 3][:len(l[i])], l[(i + 1) % 3][-len(l[i]):], l[(i + 2) % 3][-len(l[i]):]) for i in range(3)):
-        ans = 'Yes'
-    else:
-        ans = 'No'
-    print(ans)
+ l = [input() for _ in range(3)]
+ print(["No", "Yes"][all(l[i] not in (l[(i + 1) % 3][:len(l[i])], l[(i + 2) % 3][:len(l[i])], l[(i + 1) % 3][-len(l[i]):], l[(i + 2) % 3][-len(l[i]):]) for i in range(3))])
