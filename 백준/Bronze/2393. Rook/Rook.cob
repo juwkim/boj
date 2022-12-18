ce345a4574063b5c28394476efd313198f0cd3c9
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-19 00:09:48 +0900

    [Bronze V] Title: Rook, Time: 12 ms, Memory: 70348 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cob
@@ -0,0 +1,18 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Main.
+       PROCEDURE DIVISION.
+       Begin.
+           DISPLAY "  ___  ___  ___".
+           DISPLAY "  | |__| |__| |".
+           DISPLAY "  |           |".
+           DISPLAY "   \_________/".
+           DISPLAY "    \_______/".
+           DISPLAY "     |     |".
+           DISPLAY "     |     |".
+           DISPLAY "     |     |".
+           DISPLAY "     |     |".
+           DISPLAY "     |_____|".
+           DISPLAY "  __/       \__".
+           DISPLAY " /             \".
+           DISPLAY "/_______________\".
+           STOP RUN.
