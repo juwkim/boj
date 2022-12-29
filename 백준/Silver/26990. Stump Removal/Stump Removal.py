d55f3fcaafa6c75a4d7acff720d3288c6cacba3e
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-29 17:18:49 +0900

    [Silver V] Title: Stump Removal, Time: 148 ms, Memory: 118212 KB -BaekjoonHub

---
+++ 백준/Silver/26990. Stump Removal/Stump Removal.py
@@ -0,0 +1,13 @@
+N, *nums = map(int, open(0))
+flag = 0
+for i in range(N - 1):
+    if flag:
+        if nums[i] <= nums[i+1]:
+            flag = 0
+    else:
+        if nums[i] >= nums[i+1]:
+            print(i + 1)
+        if nums[i] > nums[i+1]:
+            flag = 1
+if N == 1 or nums[-2] <= nums[-1]:
+    print(N)
