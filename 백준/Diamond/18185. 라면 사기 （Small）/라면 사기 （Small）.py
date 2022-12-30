da061dd2f9cafdd50a41e92d22a6cc6d93939f71
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-31 07:48:24 +0900

    [Diamond V] Title: 라면 사기 (Small), Time: 132 ms, Memory: 115280 KB -BaekjoonHub

--- 백준/Diamond/18185. 라면 사기 （Small）/라면 사기 （Small）.py
+++ 백준/Diamond/18185. 라면 사기 （Small）/라면 사기 （Small）.py
@@ -2,20 +2,24 @@ input = __import__('sys').stdin.readline
 
 N = int(input())
 nums = [*map(int, input().split())]
-dp = [[max(0, nums[0] - nums[1]), 0, 0], [max(0, nums[1] - nums[0]), min(nums[:2]), 0]]
+
+mem = [max(0, nums[1] - nums[0]), min(nums[:2]), 0]
+ans = 3 * max(0, nums[0] - nums[1])
 for i in range(2, N):
 
     buf = [0, 0, 0]
     for k in range(2):
-        num = min(dp[i-1][k], nums[i])
+        num = min(mem[k], nums[i])
         buf[k + 1] = num
-        dp[i-1][k] -= num
+        mem[k] -= num
         nums[i] -= num
-    
     buf[0] = nums[i]
-    dp.append(buf)
 
-ans = 0
-for a, b, c in dp:
+    a, b, c = mem
     ans += 3 * a + 5 * b + 7 * c
+
+    mem = buf
+    
+a, b, c = mem
+ans += 3 * a + 5 * b + 7 * c
 print(ans)
