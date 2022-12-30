2b6bbf6339b1464dd38d8f84c1e762932585e8f3
김주원 <66457470+juwkim@users.noreply.github.com>
2022-12-31 02:00:58 +0900

    [Gold IV] Title: 네트워크 연결, Time: 404 ms, Memory: 154724 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -0,0 +1,35 @@
+class DisjointSet:
+    def __init__(self, nodes):
+        self.parent = nodes
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        x_root = self.find(x)
+        y_root = self.find(y)
+        if x_root != y_root:
+            self.parent[x_root] = y_root
+
+def kruskal(graph):
+    edges = sorted(graph['edges'], key=lambda x: x[2])
+    mst = []
+    ds = DisjointSet(graph['nodes'])
+    for edge in edges:
+        u, v, weight = edge
+        if ds.find(u) != ds.find(v):
+            mst.append(edge)
+            ds.union(u, v)
+    return mst
+
+g = lambda: [*map(int, input().split())]
+graph = {}
+V, E = int(input()), int(input())
+graph['nodes'] = [*range(V + 1)]
+graph['edges'] = [g() for _ in range(E)]
+
+mst = kruskal(graph)
+weight_sum = sum(edge[2] for edge in mst)
+print(weight_sum)
