18bac217f6eea6b2faa69ebdc6fc90213e130c69
김주원 <66457470+juwkim@users.noreply.github.com>
2023-01-01 01:23:59 +0900

    [Gold IV] Title: 구슬 찾기, Time: 164 ms, Memory: 116480 KB -BaekjoonHub

---
+++ 백준/Gold/2617. 구슬 찾기/구슬 찾기.py
@@ -0,0 +1,26 @@
+from collections import Counter
+input = __import__('sys').stdin.readline
+g = lambda: [*map(int, input().split())]
+
+def Floyd_Warshall(graph, n):
+    for m in range(1, n + 1):
+        for s in range(1, n + 1):
+            for e in range(1, n + 1):
+                if graph[s][m] * graph[m][e] > 0:
+                    graph[s][e] = graph[s][m]
+
+n, m = g()
+graph = [[0] * (n + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    graph[i][i] = 0
+
+for _ in range(m):
+    a, b = g()
+    graph[a][b] = 1
+    graph[b][a] = -1
+
+Floyd_Warshall(graph, n)
+
+n >>= 1
+ans = sum(cnt[1] > n or cnt[-1] > n for cnt in map(Counter, graph))
+print(ans)
