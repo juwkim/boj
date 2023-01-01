40ec112363c9f0dc880e073ddaaf1c4495075a52
김주원 <66457470+juwkim@users.noreply.github.com>
2023-01-02 01:06:48 +0900

    [Silver IV] Title: Matryoshka Dolls, Time: 272 ms, Memory: 124772 KB -BaekjoonHub

--- 백준/Silver/7640. Matryoshka Dolls/Matryoshka Dolls.py
+++ 백준/Silver/7640. Matryoshka Dolls/Matryoshka Dolls.py
@@ -1,5 +1,5 @@
 from collections import Counter
 while N:= int(input()):
     cnt = Counter(int(input()) for _ in range(N))
-    ans = cnt.most_common(1)[0][1]
+    ans = max(cnt.values())
     print(ans)
