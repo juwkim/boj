acdc83b2c632e048920404192299bc648822a228
김주원 <66457470+juwkim@users.noreply.github.com>
2023-01-01 23:27:35 +0900

    [Silver IV] Title: Need For Speed, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/6039. Need For Speed/Need For Speed.py
+++ 백준/Silver/6039. Need For Speed/Need For Speed.py
@@ -1,31 +1,16 @@
 def g(): return [*map(int, input().split())]
 
 F, M, N = g()
-nums = [g() for _ in range(N)]
+ans = []
+p, q = F, M
+for f, m, idx in sorted([g() + [i + 1] for i in range(N)], key=lambda x: x[1] / x[0]):
 
-max_force, max_mass, ans = F, M, 0
-def solve(force, mass, mask, idx):
-    global ans, max_force, max_mass
-    if idx == N:
-        if max_force * mass < force * max_mass:
-            ans = mask
-            max_force = force
-            max_mass = mass
-        elif max_force * mass == force * max_mass and mass < max_mass:
-            ans = mask
-            max_force = force
-            max_mass = mass
-        return
-    solve(force, mass, mask, idx + 1)
-    solve(force + nums[idx][0], mass + nums[idx][1], mask | (1 << idx), idx + 1)
-
-solve(F, M, 0, 0)
+    if (p + f) * q <= p * (q + m):
+        break
+    p += f
+    q += m
+    ans.append(idx)
 if ans:
-    idx = 1
-    while ans:
-        ans, r = divmod(ans, 2)
-        if r:
-            print(idx)
-        idx += 1
+    print(*sorted(ans))
 else:
     print('NONE')
