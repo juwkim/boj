b39b686e199287c801aabbeea015e55641835561
김주원 <66457470+juwkim@users.noreply.github.com>
2023-01-02 04:24:54 +0900

    [Silver IV] Title: Polynomial Remains, Time: 136 ms, Memory: 115436 KB -BaekjoonHub

--- 백준/Silver/1716. Polynomial Remains/Polynomial Remains.py
+++ 백준/Silver/1716. Polynomial Remains/Polynomial Remains.py
@@ -6,12 +6,4 @@ while (l:= g()) != [-1, -1]:
     for i in range(n - k, -1, -1):
         a[i] -= a[i + k]
         a[i + k] = 0
-        
-    if a[0] == 0:
-        print(0)
-    else:
-        for i in range(n + 1):
-            if a[i] == 0:
-                break
-            print(a[i], end=' ')
-        print()
+    print(*a[:max(k,1)])
