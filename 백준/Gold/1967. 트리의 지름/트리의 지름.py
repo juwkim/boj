8e5a6663ac603c940c4981098783df2c7278614c
김주원 <66457470+juwkim@users.noreply.github.com>
2023-01-01 21:22:18 +0900

    [Gold IV] Title: 트리의 지름, Time: 176 ms, Memory: 251604 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -0,0 +1,29 @@
+import sys
+sys.setrecursionlimit(10 ** 5)
+input = __import__('sys').stdin.readline
+
+def g(): return tuple(map(int, input().split()))
+
+n = int(input())
+graph = [[] for _ in range(n + 1)]
+for _ in range(n - 1):
+    parent, child, weight = g()
+    graph[parent].append((child, weight))
+    graph[child].append((parent, weight))
+
+def dfs(node, dist):
+    global max_dist, endnode
+    if dist > max_dist:
+        max_dist = dist
+        endnode = node
+    visited[node] = 1
+    for neighbor, weight in graph[node]:
+        if visited[neighbor] == 0:
+            dfs(neighbor, dist + weight)
+
+endnode, max_dist, visited = 1, 0, bytearray(n + 1)
+dfs(endnode, 0)
+
+max_dist, visited = 0, bytearray(n + 1)
+dfs(endnode, 0)
+print(max_dist)
