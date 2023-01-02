d75a75a7c2f059055b04f2681e32a656f1cb65e5
김주원 <66457470+juwkim@users.noreply.github.com>
2023-01-03 07:03:18 +0900

    [Gold V] Title: Dreary Design (Large2), Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/12177. Dreary Design （Large2）/Dreary Design （Large2）.py
+++ 백준/Gold/12177. Dreary Design （Large2）/Dreary Design （Large2）.py
@@ -1,4 +1,4 @@
 for t in range(1, 1 + int(input())):
     k, v = map(int, input().split())
-    ans = (3 * v * (v + 1) + 1) * max(0, k - v) + (min(k, v) + 1) ** 3
+    ans = (3 * v * (v + 1) + 1) * (k - v) + (v + 1) ** 3
     print(f'Case #{t}: {ans}')
