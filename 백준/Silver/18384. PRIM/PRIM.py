93ccee3380a9311096def417a48f19f05c6e35a9
김주원 <66457470+juwkim@users.noreply.github.com>
2023-01-02 20:29:13 +0900

    [Silver IV] Title: PRIM, Time: 404 ms, Memory: 166324 KB -BaekjoonHub

---
+++ 백준/Silver/18384. PRIM/PRIM.py
@@ -0,0 +1,25 @@
+def g(): return [*map(int, input().split())]
+
+def sieve_of_eratosthenes(N):
+    is_prime = [True] * N
+    spf = [None] * N
+    primes = []
+    for i in range(2, N):
+        if is_prime[i]:
+            primes.append(i)
+            spf[i] = i
+
+        for prime in primes:
+            if prime > spf[i] or i * prime >= N:
+                break
+            is_prime[i * prime] = False
+            spf[i * prime] = prime
+
+    return primes
+                   
+from bisect import bisect_left
+
+primes = sieve_of_eratosthenes(1500000)
+for _ in range(int(input())):
+    ans = sum(primes[bisect_left(primes, num)] for num in g())
+    print(ans)
