2c257e4b0e25f7a1dc8e1dc8bc86c684b723a0af
김주원 <66457470+juwkim@users.noreply.github.com>
2023-01-03 06:08:32 +0900

    [Silver IV] Title: Captain Hammer (Small), Time: 360 ms, Memory: 115552 KB -BaekjoonHub

--- 백준/Silver/12084. Captain Hammer （Small）/Captain Hammer （Small）.py
+++ 백준/Silver/12084. Captain Hammer （Small）/Captain Hammer （Small）.py
@@ -5,14 +5,11 @@ def distance(theta, v):
 
 
 for t in range(1, 1 + int(input())):
-    # Read V and D
     v, d = map(int, input().split())
 
-    # Set lower and upper bounds for binary search
     low = 0
     high = math.pi/4
 
-    # Perform binary search
     while high - low > 1e-13:
         mid = (low + high) / 2
         dist = distance(mid, v)
@@ -21,6 +18,5 @@ for t in range(1, 1 + int(input())):
         else:
             high = mid
 
-    # Calculate the angle of takeoff in degrees and print result
     theta = (low + high) / 2
     print(f"Case #{t}: {math.degrees(theta)}")
