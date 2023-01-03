bef66987c1e746d352d5e3d6b5caed9776db3a8e
김주원 <66457470+juwkim@users.noreply.github.com>
2023-01-04 01:39:49 +0900

    [Silver IV] Title: Babelfish, Time: 216 ms, Memory: 47356 KB -BaekjoonHub

--- 백준/Silver/4368. Babelfish/Babelfish.py
+++ 백준/Silver/4368. Babelfish/Babelfish.py
@@ -1,7 +1,7 @@
 input = __import__('sys').stdin.readline
 dic = {}
 while s:= input().rstrip():
-    v, k = s.split()
-    dic[k] = v
+   v, k = s.split()
+   dic[k] = v
 while s:= input().rstrip():
-    print(dic.get(s, 'eh'))
+   print(dic.get(s, 'eh'))
