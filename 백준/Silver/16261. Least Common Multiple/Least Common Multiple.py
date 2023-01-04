d7e024c7c8e9e891f8c35564c8a69c3f174f7b28
김주원 <66457470+juwkim@users.noreply.github.com>
2023-01-05 00:20:44 +0900

    [Silver IV] Title: Least Common Multiple, Time: 264 ms, Memory: 116408 KB -BaekjoonHub

--- 백준/Silver/16261. Least Common Multiple/Least Common Multiple.py
+++ 백준/Silver/16261. Least Common Multiple/Least Common Multiple.py
@@ -1,9 +1,7 @@
+input = __import__('sys').stdin.readline
 from math import gcd, lcm
 for t in range(int(input())):
     a, b, c, d = map(int, input().split())
-
-    num1 = lcm(b, d)
-    num2 = lcm(a * num1 // b, c * num1 // d)
-    
-    GCD = gcd(num1, num2)
-    print(num2 // GCD, num1 // GCD)
+    LCM = lcm(a * d, b * c)
+    GCD = gcd(LCM, b * d)
+    print(LCM // GCD, b * d // GCD)
