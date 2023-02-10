2fc480cf571622ed8ec75a48e5dbc72e7e872f6d
김주원 <66457470+juwkim@users.noreply.github.com>
2023-02-10 10:51:38 +0900

    [Silver V] Title: 벌집 2, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/27436. 벌집 2/벌집 2.py
+++ 백준/Silver/27436. 벌집 2/벌집 2.py
@@ -1,10 +1,6 @@
 from math import isqrt
 
 N = int(input())
-num = 12 * N - 3
-tmp = isqrt(num)
-if num == tmp ** 2 and tmp % 6 == 3:
-    ans = (3 + tmp) // 6
-else:
-    ans = (3 + tmp) // 6 + 1
+tmp = isqrt(num:= 12 * N - 3)
+ans = (3 + tmp) // 6 + (num != tmp ** 2 or tmp % 6 != 3)
 print(ans)
