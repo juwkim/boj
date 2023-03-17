e648a423fdf6b0e81eb3cad01eb78d43132ac54d
김주원 <66457470+juwkim@users.noreply.github.com>
2023-03-18 06:35:25 +0900

    [Bronze II] Title: SciComLove (2023), Time: 300 ms, Memory: 118092 KB -BaekjoonHub

--- 백준/Bronze/27913. SciComLove （2023）/SciComLove （2023）.py
+++ 백준/Bronze/27913. SciComLove （2023）/SciComLove （2023）.py
@@ -1,22 +1,24 @@
 from itertools import cycle
 
+input = __import__('sys').stdin.readline
 N, Q = map(int, input().split())
-check = set()
-i = 1
+check = bytearray(N + 1)
 
+i = 1
+cur = 0
 for num in cycle((3, 3, 4)):
-    check.add(i)
+    check[i] = 1
+    cur += 1
     if i + num > N:
         break
     i += num
 
-cur = len(check)
 for _ in range(Q):
     X = int(input())
-    if X in check:
-        check.remove(X)
+    if check[X]:
+        check[X] = 0
         cur -= 1
     else:
-        check.add(X)
+        check[X] = 1
         cur += 1
     print(cur)
