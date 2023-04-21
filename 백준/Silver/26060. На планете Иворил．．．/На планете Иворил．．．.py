9465c3a76680bf2e545d0a59ec5b5a7cdd10951d
김주원 <66457470+juwkim@users.noreply.github.com>
2023-04-21 13:20:12 +0900

    [Silver IV] Title: На планете Иворил..., Time: 184 ms, Memory: 123468 KB -BaekjoonHub

--- 백준/Silver/26060. На планете Иворил．．．/На планете Иворил．．．.py
+++ 백준/Silver/26060. На планете Иворил．．．/На планете Иворил．．．.py
@@ -1,25 +1,10 @@
 input()
 ans = 0
 for s in input().split():
-    a, b = 0, 0
-    for c in s:
-        if c in 'aeiouy':
-            a += 1
-        else:
-            b += 1
-    
-    p, q = 0, 0
-    for i, c in enumerate(s):
-        if i & 1:
-            if c in 'aeiouy':
-                p += 1
-            else:
-                q += 1
-        else:
-            if c in 'aeiouy':
-                q += 1
-            else:
-                p += 1
-
-    ans += min(b, p, q)
+    cnt = [0, 0, 0]    
+    for i, t in enumerate(s):
+        if t not in 'aeiouy':
+            cnt[2] += 1
+        cnt[(i & 1) ^ (t in 'aeiouy')] += 1
+    ans += min(cnt)
 print(ans)
