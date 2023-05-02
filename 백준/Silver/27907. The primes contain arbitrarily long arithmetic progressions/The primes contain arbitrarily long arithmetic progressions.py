96fe4a02e72245512e92164dd55dea8300c59efd
김주원 <66457470+juwkim@users.noreply.github.com>
2023-05-02 21:31:05 +0900

    [Silver V] Title: The primes contain arbitrarily long arithmetic progressions, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/27907. The primes contain arbitrarily long arithmetic progressions/The primes contain arbitrarily long arithmetic progressions.py
+++ 백준/Silver/27907. The primes contain arbitrarily long arithmetic progressions/The primes contain arbitrarily long arithmetic progressions.py
@@ -2,8 +2,8 @@ a, b = 224584605939537911, 18135696597948930
 
 n = int(input())
 if n > 27:
+    assert n != 28
     print(-1)
 else:
-    assert n != 26
     ans = [a + b * i for i in range(n)]
     print(*ans)
