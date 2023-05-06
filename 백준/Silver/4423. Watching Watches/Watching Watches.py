578fde5144e94390b9baf7d624e85bbfcc0ba66b
김주원 <66457470+juwkim@users.noreply.github.com>
2023-05-06 09:32:41 +0900

    [Silver IV] Title: Watching Watches, Time: 256 ms, Memory: 116900 KB -BaekjoonHub

--- 백준/Silver/4423. Watching Watches/Watching Watches.py
+++ 백준/Silver/4423. Watching Watches/Watching Watches.py
@@ -1,13 +1,13 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
 while True:
     try:
         p = input()
         a, b = sorted(map(int, p.split()))
         s = ((86400 - a) * 43200 + 30 * (b - a)) // (b - a)
         h, s = divmod(s % 43200, 3600)
-        if h == 0:
-            h = 12
         m = s // 60
-        ans = f'{p} {h:02d}:{m:02d}'
-        print(ans)
+        if h == 0: h = 12
+        print(f'{p} {h:02d}:{m:02d}')
     except:
         break
