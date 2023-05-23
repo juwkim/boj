31e7ce5a0df8bfa308897cef2a51c037820a04ed
김주원 <66457470+juwkim@users.noreply.github.com>
2023-05-23 14:18:02 +0900

    [Bronze I] Title: 동전 복사, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/28063. 동전 복사/동전 복사.py
+++ 백준/Bronze/28063. 동전 복사/동전 복사.py
@@ -1,4 +1,3 @@
-N = int(input())
-x, y = map(int, input().split())
-ans = (x != 1) + (x != N) + (y != 1) + (y != N)
-print(ans)
+N=int(input())
+x,y=map(int,input().split())
+print((x!=1)+(x!=N)+(y!=1)+(y!=N))
