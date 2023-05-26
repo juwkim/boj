4bb692fe40a2f91029992f8b5980357a8579b529
김주원 <66457470+juwkim@users.noreply.github.com>
2023-05-26 14:15:34 +0900

    [Silver IV] Title: 마지막 문제, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/28110. 마지막 문제/마지막 문제.py
+++ 백준/Silver/28110. 마지막 문제/마지막 문제.py
@@ -1,5 +1,4 @@
 N=int(input())
 s=sorted(map(int,input().split()))
-i=max(range(N-1),key=lambda x:s[x+1]-s[x]>>1)
-a,b=s[i],s[i+1]
-print([-1,(a+b)>>1][b-a>1])
+a,b=max(zip(s,s[1:]),key=lambda x:x[1]-x[0]>>1)
+print([-1,a+b>>1][b-a>1])
