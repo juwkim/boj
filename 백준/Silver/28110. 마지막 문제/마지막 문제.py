ac11e768624369f0929bb84120f9ee0980555c2c
김주원 <66457470+juwkim@users.noreply.github.com>
2023-05-26 14:12:03 +0900

    [Silver IV] Title: 마지막 문제, Time: 120 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/28110. 마지막 문제/마지막 문제.py
+++ 백준/Silver/28110. 마지막 문제/마지막 문제.py
@@ -1,8 +1,5 @@
-N = int(input())
-nums = sorted(map(int, input().split()))
-idx = max(range(N - 1), key=lambda x: nums[x + 1] - nums[x] >> 1)
-a, b = nums[idx], nums[idx + 1]
-if b - a == 1:
-    print(-1)
-else:
-    print((b + a) >> 1)
+N=int(input())
+s=sorted(map(int,input().split()))
+i=max(range(N-1),key=lambda x:s[x+1]-s[x]>>1)
+a,b=s[i],s[i+1]
+print([-1,(a+b)>>1][b-a>1])
