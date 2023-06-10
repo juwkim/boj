e53c9b61d3661ca9625c7983f68f0c23fb295115
김주원 <66457470+juwkim@users.noreply.github.com>
2023-06-10 10:21:56 +0900

    [Bronze II] Title: Snow Calls, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/6876. Snow Calls/Snow Calls.py
@@ -0,0 +1,15 @@
+l = enumerate("ABC DEF GHI JKL MNO PQRS TUV WXYZ".split(), 2)
+dic = {c: idx for idx, alpha in l for c in alpha}
+
+for _ in range(int(input())):
+    ans = []
+    for c in input():
+        if c.isnumeric():
+            ans.append(c)
+        elif c.isalpha():
+            ans.append(dic[c])
+        if len(ans) in (3, 7):
+            ans.append('-')
+        if len(ans) == 12:
+            break
+    print(*ans, sep='')
