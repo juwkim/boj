e3340aebee7522c77d5d5e63531ae5e6f3b520c7
김주원 <66457470+juwkim@users.noreply.github.com>
2023-07-28 17:45:07 +0900

    [Bronze III] Title: Кампус, Time: 132 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Bronze/20454. Кампус/Кампус.py
@@ -0,0 +1,10 @@
+g = lambda: [*map(int, input().split())]
+
+n, k, x, y = g()
+q = int(input())
+k_sum = x + y * (k - 1)
+one_floor = (n // k) * k_sum + (n % k) * y
+for num in g():
+    a, b = divmod((num - 1) % one_floor, k_sum)
+    ans = a * k + b // y + 1
+    print(ans)
