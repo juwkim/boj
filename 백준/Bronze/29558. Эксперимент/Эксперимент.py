c75647f000b865c40bb717277a98c11d75aad68e
김주원 <66457470+juwkim@users.noreply.github.com>
2023-09-03 04:48:55 +0900

    [Bronze I] Title: Эксперимент, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/29558. Эксперимент/Эксперимент.py
+++ 백준/Bronze/29558. Эксперимент/Эксперимент.py
@@ -1,5 +1,6 @@
 N, D = map(int, input().split())
+P = N//2
 if N&1:
-    print(*range(D - N // 2, D + N // 2 + 1))
+    print(*range(D - P, D + P + 1))
 else:
-    print(*range(D - N // 2, D), *range(D + 1, D + N // 2 + 1))
+    print(*range(D - P, D), *range(D + 1, D + P + 1))
