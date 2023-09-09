5bc8c36d3a896471955ab6fc27df0d6d61ca4771
김주원 <66457470+juwkim@users.noreply.github.com>
2023-09-10 02:38:04 +0900

    [Silver IV] Title: 이상한 호텔의 송이, Time: 988 ms, Memory: 118164 KB -BaekjoonHub

--- 백준/Silver/29702. 이상한 호텔의 송이/이상한 호텔의 송이.py
+++ 백준/Silver/29702. 이상한 호텔의 송이/이상한 호텔의 송이.py
@@ -1,6 +1,6 @@
-for _ in range(int(input())):
-    b = bin(int(input()))[2:]
-    num = int(b[1:], 2) + 1
-    for floor in range(len(b), 0, -1):
-        print(f"{floor}{num:018d}")
-        num = (num + 1) // 2
+for _ in' '*int(input()):
+ b=bin(int(input()))
+ n=int(b[3:],2)+1
+ for f in range(len(b)-2,0,-1):
+  print(f"{f}{n:018d}")
+  n=(n+1)//2
