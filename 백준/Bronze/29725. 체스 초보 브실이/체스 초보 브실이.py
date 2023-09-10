42c4a73662f88ae9393428dddad316102f94f785
김주원 <66457470+juwkim@users.noreply.github.com>
2023-09-11 02:58:42 +0900

    [Bronze IV] Title: 체스 초보 브실이, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/29725. 체스 초보 브실이/체스 초보 브실이.py
+++ 백준/Bronze/29725. 체스 초보 브실이/체스 초보 브실이.py
@@ -1,6 +1,4 @@
-score = {a: b for a, b in zip(".KPNBRQ", (0, 0, 1, 3, 3, 5, 9))}
-score.update(zip(".kpnbrq", (0, 0, -1, -3, -3, -5, -9)))
-
+score = {a: b for a, b in zip(".KPNBRQkpnbrq", (0, 0, 1, 3, 3, 5, 9, 0, -1, -3, -3, -5, -9))}
 ans = 0
 for _ in range(8):
     for c in input():
