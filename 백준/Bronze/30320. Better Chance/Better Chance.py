470a58dc6a9abd998dbb2eb9ee39aa407a2b7bfc
김주원 <66457470+juwkim@users.noreply.github.com>
2023-10-10 21:41:20 +0900

    [Bronze I] Title: Better Chance, Time: 276 ms, Memory: 127760 KB -BaekjoonHub

---
+++ 백준/Bronze/30320. Better Chance/Better Chance.py
@@ -0,0 +1,22 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+g = lambda: [*map(int, input().split())]
+
+from fractions import Fraction
+
+buf = ["SAME", "TAOYUAN", "JAKARTA"]
+R1, R2, S1, S2 = input().split()
+R1, R2 = int(R1), int(R2)
+S1, S2 = Fraction(S1), Fraction(S2)
+
+if R1 == 1 and R2 == 1:
+    ans = buf[(S1 > S2) - (S1 < S2)]
+elif R1 == 1:
+    ans = buf[1]
+elif R2 == 1:
+    ans = buf[2]
+else:
+    a = (R2 - 1) * S1
+    b = (R1 - 1) * S2
+    ans = buf[(a > b) - (a < b)]
+print(ans)
