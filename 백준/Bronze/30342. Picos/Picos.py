fc4f637d9c4ab5e48ac32407e62ed90493c3e7be
김주원 <66457470+juwkim@users.noreply.github.com>
2023-10-12 12:36:49 +0900

    [Bronze III] Title: Picos, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/30342. Picos/Picos.py
+++ 백준/Bronze/30342. Picos/Picos.py
@@ -1,4 +1,3 @@
-N, M, T, K = map(int, input().split())
-n = (K + T - 1) // T
-q, r = divmod(N, M)
-print(M * min(q, n) * (2 * K - T * (min(q, n) - 1)) // 2 + (q < n) * (K - T * q) * r)
+N,M,T,K=map(int,input().split())
+n,q=(K+T-1)//T,N//M
+print(M*min(q,n)*(2*K-T*(min(q,n)-1))//2+N%M*(q<n)*(K-T*q))
