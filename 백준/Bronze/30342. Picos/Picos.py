1ed4dc5ec5807d9bd43eacb7ea1d69efc9f287bb
김주원 <66457470+juwkim@users.noreply.github.com>
2023-10-12 12:33:09 +0900

    [Bronze III] Title: Picos, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/30342. Picos/Picos.py
+++ 백준/Bronze/30342. Picos/Picos.py
@@ -1,12 +1,4 @@
-import sys
-input = lambda: sys.stdin.readline().rstrip()
-g = lambda: [*map(int, input().split())]
-
-N, M, T, K = g()
+N, M, T, K = map(int, input().split())
 n = (K + T - 1) // T
 q, r = divmod(N, M)
-if q >= n:
-    ans = M * n * (2 * K - T * (n - 1)) // 2
-else:
-    ans = M * q * (2 * K - T * (q - 1)) // 2 + (K - T * q) * r
-print(ans)
+print(M * min(q, n) * (2 * K - T * (min(q, n) - 1)) // 2 + (q < n) * (K - T * q) * r)
