375fda1f0982411efce352d52ca97de989d9ded6
김주원 <66457470+juwkim@users.noreply.github.com>
2023-11-07 08:02:37 +0900

    [Bronze I] Title: Середина игры, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/30584. Середина игры/Середина игры.py
+++ 백준/Bronze/30584. Середина игры/Середина игры.py
@@ -1,10 +1,5 @@
-import sys
-input = lambda: sys.stdin.readline().rstrip()
-g = lambda: [*map(int, input().split())]
-
 a, b = int(input()), int(input())
-d = abs(a - b)
-if d&1:
+if (a - b) & 1:
     print("Error")
 elif min(a, b) >= 2:
     print("Undefined")
