5057ac1bafc49e0846adf849ae146767ec5491c2
김주원 <66457470+juwkim@users.noreply.github.com>
2023-11-08 12:52:22 +0900

    [Bronze II] Title: Axis-Aligned Area, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/30596. Axis－Aligned Area/Axis－Aligned Area.py
+++ 백준/Bronze/30596. Axis－Aligned Area/Axis－Aligned Area.py
@@ -1,6 +1,3 @@
-import sys
-input = lambda: sys.stdin.readline().rstrip()
-g = lambda: [*map(int, input().split())]
-
-w, _, h, _ = [int(input()) for _ in range(4)]
-print(w * h)
+w=int(input())
+input()
+print(w*int(input()))
