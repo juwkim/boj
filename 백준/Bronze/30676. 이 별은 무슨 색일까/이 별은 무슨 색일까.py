c002adcc208a0516c35cae5b4ac6c6f11f88eff1
김주원 <66457470+juwkim@users.noreply.github.com>
2023-11-19 17:59:47 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.py
@@ -0,0 +1,15 @@
+lamba = int(input())
+if lamba >= 620:
+    print('Red')
+elif lamba >= 590:
+    print('Orange')
+elif lamba >= 570:
+    print('Yellow')
+elif lamba >= 495:
+    print('Green')
+elif lamba >= 450:
+    print('Blue')
+elif lamba >= 425:
+    print('Indigo')
+else:
+    print('Violet')
