58c404caaf4a12a831d2af0c63c7d254ed9ef673
김주원 <66457470+juwkim@users.noreply.github.com>
2023-11-24 08:01:30 +0900

    [Silver IV] Title: Investigating Zeroes and Ones, Time: 128 ms, Memory: 120836 KB -BaekjoonHub

--- 백준/Silver/29994. Investigating Zeroes and Ones/Investigating Zeroes and Ones.py
+++ 백준/Silver/29994. Investigating Zeroes and Ones/Investigating Zeroes and Ones.py
@@ -1,11 +1,10 @@
 N = int(input())
 px = [0, *map(int, input().split())]
-for i in range(N):
-    px[i + 1] += px[i]
-ans = 0
+ans, cur = 0, 0
 cnt = [0, 0]
 for i in range(N):
-    cnt[0] += (px[i] & 1)
-    cnt[1] += ((px[i] + 1) & 1)
-    ans += cnt[px[i+1]&1]
+    cnt[0] += cur & 1
+    cnt[1] += ((cur + 1) & 1)
+    cur += px[i+1]
+    ans += cnt[cur&1]
 print(ans)
