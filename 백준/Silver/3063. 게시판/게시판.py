c8b6cdc30af16e277425d49220fdb043d7a12195
김주원 <66457470+juwkim@users.noreply.github.com>
2023-11-30 14:43:14 +0900

    [Silver IV] Title: 게시판, Time: 128 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/3063. 게시판/게시판.py
+++ 백준/Silver/3063. 게시판/게시판.py
@@ -1,10 +1,3 @@
-import sys
-
-input = lambda: sys.stdin.readline().rstrip()
-g = lambda: [*map(int, input().split())]
-
 for _ in range(int(input())):
-    x1, y1, x2, y2, x3, y3, x4, y4 = g()
-    ans = (x2 - x1) * (y2 - y1)
-    ans -= max(0, min(x2, x4) - max(x1, x3)) * max(0, min(y2, y4) - max(y1, y3))
-    print(ans)
+ a,b,c,d,e,f,g,h=map(int,input().split())
+ print((c-a)*(d-b)-max(0,min(c,g)-max(a,e))*max(0,min(d,h)-max(b,f)))
