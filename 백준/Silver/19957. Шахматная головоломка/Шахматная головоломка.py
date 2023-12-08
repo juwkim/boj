22ee64f48c669e0d0b46c794170811b594975a66
김주원 <66457470+juwkim@users.noreply.github.com>
2023-12-08 23:43:06 +0900

    [Silver IV] Title: Шахматная головоломка, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/19957. Шахматная головоломка/Шахматная головоломка.py
+++ 백준/Silver/19957. Шахматная головоломка/Шахматная головоломка.py
@@ -1,20 +1,20 @@
 r, c = input()
-r = ord(r) - ord('a')
-c = ord(c) - ord('1')
-if r == 0:
-    if c <= 3:
+r = ord(r)
+c = ord(c)
+if r == ord('a'):
+    if c <= ord('4'):
         a1, b1 = r, c + 3
         a2, b2 = r + 1, c + 1
     else:
         a1, b1 = r, c - 3
         a2, b2 = r + 1, c - 1
 else:
-    if c <= 3:
+    if c <= ord('4'):
         a1, b1 = r, c + 3
         a2, b2 = r - 1, c + 1
     else:
         a1, b1 = r, c - 3
         a2, b2 = r - 1, c - 1
 
-print(chr(a1 + ord('a')), b1 + 1, sep='')
-print(chr(a2 + ord('a')), b2 + 1, sep='')
+print(chr(a1), chr(b1), sep='')
+print(chr(a2), chr(b2), sep='')
