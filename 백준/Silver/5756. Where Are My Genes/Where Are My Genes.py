93753a2c7d6cae3f759487533d168f8e8086c800
김주원 <66457470+juwkim@users.noreply.github.com>
2023-12-17 02:11:36 +0900

    [Silver IV] Title: Where Are My Genes, Time: 744 ms, Memory: 114196 KB -BaekjoonHub

--- 백준/Silver/5756. Where Are My Genes/Where Are My Genes.py
+++ 백준/Silver/5756. Where Are My Genes/Where Are My Genes.py
@@ -8,7 +8,10 @@ while N:=int(input()):
     gene = [*range(N + 1)]
     for _ in range(int(input())):
         i, j = g()
-        gene[i:j+1] = gene[i:j+1][::-1]
+        while i < j:
+            gene[i], gene[j] = gene[j], gene[i]
+            i += 1
+            j -= 1
     print(f"Genome {tc}")
     for _ in range(int(input())):
         print(gene.index(int(input())))
