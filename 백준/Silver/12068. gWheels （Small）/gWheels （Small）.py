7e1027fc5a06b1b20d01213ee64558e8bd5538fd
김주원 <66457470+juwkim@users.noreply.github.com>
2023-12-23 08:11:20 +0900

    [Silver IV] Title: gWheels (Small), Time: 340 ms, Memory: 110408 KB -BaekjoonHub

--- 백준/Silver/12068. gWheels （Small）/gWheels （Small）.py
+++ 백준/Silver/12068. gWheels （Small）/gWheels （Small）.py
@@ -9,22 +9,19 @@ from math import gcd
 def solve():
     P, Q = g()
     GCD1 = gcd(P, Q)
-    P //= GCD1
-    Q //= GCD1
+    P, Q = P // GCD1, Q // GCD1
     for a, b in product(p, t):
         for e1, e2 in permutations(e, 2):
             x, y = a * e2, b * e1
             GCD2 = gcd(x, y)
-            x //= GCD2
-            y //= GCD2
+            x, y = x // GCD2, y // GCD2
             if x == P and y == Q:
                 return "Yes"
     return "No"
             
 for tc in range(1, 1 + int(input())):
     l = []
-    while len(l) != 3:
-        l.extend(g())
+    while len(l) != 3: l.extend(g())
     p, e, t = g(), g(), g()
     print(f'Case #{tc}:')
     for _ in range(int(input())):
