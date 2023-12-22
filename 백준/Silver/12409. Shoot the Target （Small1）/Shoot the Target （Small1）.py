80fef45bedccd64cced3148ca1eb04180480bd9c
김주원 <66457470+juwkim@users.noreply.github.com>
2023-12-23 05:03:11 +0900

    [Silver IV] Title: Shoot the Target (Small1), Time: 120 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/12409. Shoot the Target （Small1）/Shoot the Target （Small1）.py
+++ 백준/Silver/12409. Shoot the Target （Small1）/Shoot the Target （Small1）.py
@@ -1,20 +1,16 @@
-import sys
+from math import dist, sqrt, acos, degrees
 
-input = lambda: sys.stdin.readline().rstrip()
-g = lambda: [*map(int, input().split())]
-
-from math import sqrt, acos, degrees
-
-def c(x):
-    return ((x - x1) * (x - x2) + y1 * y2) / ((x - x1) ** 2 + y1 ** 2) ** 0.5 / ((x - x2) ** 2 + y2 ** 2) ** 0.5
+def angle(x):
+    return degrees(acos(((x - x1) * (x - x2) + y1 * y2) / (dist((x, 0), (x1, y1)) * dist((x, 0), (x2, y2)))))
 
 for tc in range(1, 1 + int(input())):
-    x1, y1, x2, y2 = g()
+    x1, y1, x2, y2 = map(int, input().split())
     if y1 == y2:
         x = (x1 + x2) / 2
-        theta = degrees(acos(c(x)))
+        theta = angle(x)
     else:
-        p = (-x2 * y1 + x1 * y2 + sqrt(((x1 - x2)**2 + (y1 - y2)**2) * y1 * y2)) / (y2 - y1)
-        q = (-x2 * y1 + x1 * y2 - sqrt(((x1 - x2)**2 + (y1 - y2)**2) * y1 * y2)) / (y2 - y1)
-        theta = max(degrees(acos(c(p))), degrees(acos(c(q))))
+        sq = dist((x1, y1), (x2, y2)) * sqrt(y1 * y2)
+        l = (-x2 * y1 + x1 * y2 - sq) / (y2 - y1)
+        r = (-x2 * y1 + x1 * y2 + sq) / (y2 - y1)
+        theta = max(angle(l), angle(r))
     print(f'Case #{tc}:', theta)
