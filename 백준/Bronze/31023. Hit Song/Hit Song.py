790e5d0bd2384f67b70c67b37ac3302a8ffe1014
김주원 <66457470+juwkim@users.noreply.github.com>
2023-12-26 01:13:07 +0900

    [Bronze I] Title: Hit Song, Time: 200 ms, Memory: 113416 KB -BaekjoonHub

---
+++ 백준/Bronze/31023. Hit Song/Hit Song.py
@@ -0,0 +1,33 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+g = lambda: [*map(int, input().split())]
+
+import re
+
+for _ in range(int(input())):
+    good = {input().lower() for _ in range(int(input()))}
+    
+    words = 0
+    total_words = 0
+    for _ in range(int(input())):
+        # l = re.findall(r'\b\w+\b', input().lower())
+
+        l = []
+        p = []
+        for c in input().lower():
+            if c.islower():
+                p.append(c)
+            else:
+                if p:
+                    l.append(''.join(p))
+                    p = []
+        if p:
+            l.append(''.join(p))
+        total_words += len(l)
+        words += sum(w in good for w in l)
+    # print(words, total_words)
+    if words * 4 >= 3 * total_words:
+        print("It's a hit!")
+    else:
+        print("Delete immediately!")
