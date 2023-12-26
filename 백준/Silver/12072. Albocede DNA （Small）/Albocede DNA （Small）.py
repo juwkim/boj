6d2183d09eeab11c29126f44a1af9c201824164d
김주원 <66457470+juwkim@users.noreply.github.com>
2023-12-26 23:19:32 +0900

    [Silver IV] Title: Albocede DNA (Small), Time: 320 ms, Memory: 129268 KB -BaekjoonHub

--- 백준/Silver/12072. Albocede DNA （Small）/Albocede DNA （Small）.py
+++ 백준/Silver/12072. Albocede DNA （Small）/Albocede DNA （Small）.py
@@ -13,32 +13,26 @@ def solve(s):
             for k in range(n // 2 + 1):
                 if sum(cur[j][k]) == 0:
                     continue
-                # situation 0
                 new[j][k] = [(a + b) % MOD for a, b in zip(new[j][k], cur[j][k])]
-                if s[i] == 'a':
-                    new[j + 1][k][0] = (new[j + 1][k][0] + cur[j][k][0]) % MOD
-                elif s[i] == 'b' and j > 0:
-                    new[j][k + 1][1] = (new[j][k + 1][1] + cur[j][k][0]) % MOD
-
-                if s[i] == 'b':
-                    new[j][k + 1][1] = (new[j][k + 1][1] + cur[j][k][1]) % MOD
-                elif s[i] == 'c':
-                    if j == 1:
-                        new[0][k][3] = (new[0][k][3] + cur[j][k][1]) % MOD
-                    else:
-                        new[j - 1][k][2] = (new[j - 1][k][2] + cur[j][k][1]) % MOD
-
-                if s[i] == 'c':
-                    if j == 1:
-                        new[0][k][3] = (new[0][k][3] + cur[j][k][2]) % MOD
-                    else:
-                        new[j - 1][k][2] = (new[j - 1][k][2] + cur[j][k][2]) % MOD
-
-                if s[i] == 'd':
-                    if k == 1:
-                        new[0][0][0] = (new[0][0][0] + cur[j][k][3]) % MOD
-                    else:
-                        new[0][k - 1][3] = (new[0][k - 1][3] + cur[j][k][3]) % MOD
+                match s[i]:
+                    case 'a':
+                        new[j + 1][k][0] = (new[j + 1][k][0] + cur[j][k][0]) % MOD
+                    case 'b':
+                        if j > 0:
+                            new[j][k + 1][1] = (new[j][k + 1][1] + cur[j][k][0]) % MOD
+                        new[j][k + 1][1] = (new[j][k + 1][1] + cur[j][k][1]) % MOD
+                    case 'c':
+                        if j == 1:
+                            new[0][k][3] = (new[0][k][3] + cur[j][k][1]) % MOD
+                            new[0][k][3] = (new[0][k][3] + cur[j][k][2]) % MOD
+                        else:
+                            new[j - 1][k][2] = (new[j - 1][k][2] + cur[j][k][1]) % MOD
+                            new[j - 1][k][2] = (new[j - 1][k][2] + cur[j][k][2]) % MOD
+                    case 'd':
+                        if k == 1:
+                            new[0][0][0] = (new[0][0][0] + cur[j][k][3]) % MOD
+                        else:
+                            new[0][k - 1][3] = (new[0][k - 1][3] + cur[j][k][3]) % MOD
         cur = new
     return (cur[0][0][0] - 1) % MOD
 
