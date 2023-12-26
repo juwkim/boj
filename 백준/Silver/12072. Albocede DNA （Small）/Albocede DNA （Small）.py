bebbe7b497fa112d1288bb44ccc607f52e878437
김주원 <66457470+juwkim@users.noreply.github.com>
2023-12-26 23:06:30 +0900

    [Silver IV] Title: Albocede DNA (Small), Time: 352 ms, Memory: 128752 KB -BaekjoonHub

--- 백준/Silver/12072. Albocede DNA （Small）/Albocede DNA （Small）.py
+++ 백준/Silver/12072. Albocede DNA （Small）/Albocede DNA （Small）.py
@@ -2,48 +2,48 @@ MOD = int(1e9 + 7)
 
 def solve(s):
     n = len(s)
-    dp = [[[[0 for _ in range(4)] for _ in range(n//2 + 2)] for _ in range(n//2 + 2)] for _ in range(n + 1)]
-    dp[0][0][0][0] = 1
-
+    cur = [[[0 for _ in range(4)] for _ in range(n//2 + 2)] for _ in range(n//2 + 2)]
+    cur[0][0][0] = 1
     for i in range(n):
+        new = [[[0 for _ in range(4)] for _ in range(n//2 + 2)] for _ in range(n//2 + 2)]
         for j in range(n // 2 + 1):
             for k in range(n // 2 + 1):
-                if sum(dp[i][j][k]) == 0:
+                if sum(cur[j][k]) == 0:
                     continue
                 # situation 0
-                dp[i + 1][j][k][0] = (dp[i + 1][j][k][0] + dp[i][j][k][0]) % MOD
+                new[j][k][0] = (new[j][k][0] + cur[j][k][0]) % MOD
                 if s[i] == 'a':
-                    dp[i + 1][j + 1][k][0] = (dp[i + 1][j + 1][k][0] + dp[i][j][k][0]) % MOD
+                    new[j + 1][k][0] = (new[j + 1][k][0] + cur[j][k][0]) % MOD
                 elif s[i] == 'b' and j > 0:
-                    dp[i + 1][j][k + 1][1] = (dp[i + 1][j][k + 1][1] + dp[i][j][k][0]) % MOD
+                    new[j][k + 1][1] = (new[j][k + 1][1] + cur[j][k][0]) % MOD
 
                 # situation 1
-                dp[i + 1][j][k][1] = (dp[i + 1][j][k][1] + dp[i][j][k][1]) % MOD
+                new[j][k][1] = (new[j][k][1] + cur[j][k][1]) % MOD
                 if s[i] == 'b':
-                    dp[i + 1][j][k + 1][1] = (dp[i + 1][j][k + 1][1] + dp[i][j][k][1]) % MOD
+                    new[j][k + 1][1] = (new[j][k + 1][1] + cur[j][k][1]) % MOD
                 elif s[i] == 'c':
                     if j == 1:
-                        dp[i + 1][0][k][3] = (dp[i + 1][0][k][3] + dp[i][j][k][1]) % MOD
+                        new[0][k][3] = (new[0][k][3] + cur[j][k][1]) % MOD
                     else:
-                        dp[i + 1][j - 1][k][2] = (dp[i + 1][j - 1][k][2] + dp[i][j][k][1]) % MOD
+                        new[j - 1][k][2] = (new[j - 1][k][2] + cur[j][k][1]) % MOD
 
                 # situation 2
-                dp[i + 1][j][k][2] = (dp[i + 1][j][k][2] + dp[i][j][k][2]) % MOD
+                new[j][k][2] = (new[j][k][2] + cur[j][k][2]) % MOD
                 if s[i] == 'c':
                     if j == 1:
-                        dp[i + 1][0][k][3] = (dp[i + 1][0][k][3] + dp[i][j][k][2]) % MOD
+                        new[0][k][3] = (new[0][k][3] + cur[j][k][2]) % MOD
                     else:
-                        dp[i + 1][j - 1][k][2] = (dp[i + 1][j - 1][k][2] + dp[i][j][k][2]) % MOD
+                        new[j - 1][k][2] = (new[j - 1][k][2] + cur[j][k][2]) % MOD
 
                 # situation 3
-                dp[i + 1][j][k][3] = (dp[i + 1][j][k][3] + dp[i][j][k][3]) % MOD
+                new[j][k][3] = (new[j][k][3] + cur[j][k][3]) % MOD
                 if s[i] == 'd':
                     if k == 1:
-                        dp[i + 1][0][0][0] = (dp[i + 1][0][0][0] + dp[i][j][k][3]) % MOD
+                        new[0][0][0] = (new[0][0][0] + cur[j][k][3]) % MOD
                     else:
-                        dp[i + 1][0][k - 1][3] = (dp[i + 1][0][k - 1][3] + dp[i][j][k][3]) % MOD
-
-    return (dp[n][0][0][0] - 1) % MOD
+                        new[0][k - 1][3] = (new[0][k - 1][3] + cur[j][k][3]) % MOD
+        cur = new
+    return (cur[0][0][0] - 1) % MOD
 
 for cas in range(1, int(input()) + 1):
     print(f"Case #{cas}: {solve(input())}")
