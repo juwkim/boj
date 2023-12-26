592c037c8c7200952670ed4f73cb617384d6e5dd
김주원 <66457470+juwkim@users.noreply.github.com>
2023-12-26 21:14:27 +0900

    [Silver IV] Title: Игра, Time: 136 ms, Memory: 116852 KB -BaekjoonHub

--- 백준/Silver/29099. Игра/Игра.py
+++ 백준/Silver/29099. Игра/Игра.py
@@ -1,10 +1,2 @@
-import sys
-
-input = lambda: sys.stdin.readline().rstrip()
-g = lambda: [*map(int, input().split())]
-
-n = int(input())
-if (g()[-1] - n)&1:
-    print("First")
-else:
-    print("Second")
+n=int(input())
+print(("Second","First")[(int(input().split()[-1])-n)&1])
