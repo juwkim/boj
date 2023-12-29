b2715f2e4d4df41e18ebc98d2f3e3c205e488de5
김주원 <66457470+juwkim@users.noreply.github.com>
2023-12-30 07:04:23 +0900

    [Gold IV] Title: 알파벳, Time: 4904 ms, Memory: 152544 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,24 +1,15 @@
-def one(num):
-    cnt = 0
-    while num:
-        num, r = divmod(num, 2)
-        cnt += r
-    return cnt
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+g = lambda: [*map(int, input().split())]
 
 R, C = map(int, input().split())
-Map = tuple(tuple(map(lambda c: 1 << ord(c) - 65, input())) for _ in range(R))
-char_mask = 0
-for line in Map:
-    for c in line:
-        char_mask |= c
-ans_max = one(char_mask)
+Map = tuple(tuple(map(lambda c: 1 << ord(c) - ord('A'), input())) for _ in range(R))
 ans, mask = 0, 0
 mask |= Map[0][0]
 def dfs(i, j):
     global ans, mask
-    ans = max(ans, one(mask))
-    if ans == ans_max:
-        return
+    ans = max(ans, mask.bit_count())
     for a, b in ((i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)):
         if 0 <= a < R and 0 <= b < C and mask & Map[a][b] == 0:
             mask ^= Map[a][b]
