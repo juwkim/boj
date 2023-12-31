333b05505d40c0110c7e2b635b3a61eabe7c5ccf
김주원 <66457470+juwkim@users.noreply.github.com>
2024-01-01 06:46:14 +0900

    [Silver III] Title: 별 관찰, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/2859. 별 관찰/별 관찰.py
+++ 백준/Silver/2859. 별 관찰/별 관찰.py
@@ -1,3 +1,8 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+g = lambda: [*map(int, input().split())]
+
 def get_minutes():
     h, m = map(int, input().split(':'))
     return h * 60 + m
@@ -6,9 +11,9 @@ a1, a2 = get_minutes(), get_minutes()
 b1, b2 = get_minutes(), get_minutes()
 
 ans = 'Never'
-for t1 in range(b2):
+for t1 in range(1, 1441):
     t2, r = divmod(a1 - a2 + b1 * t1, b2)
-    if t2 > 0 and r == 0:
+    if t2 >= 0 and r == 0:
         ans = t1
         break
 if ans == 'Never':
@@ -16,5 +21,4 @@ if ans == 'Never':
 else:
     day, time = divmod(a1 + b1 * ans, 1440)
     print(["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"][day % 7])
-    h, m = divmod(time, 60)
-    print("%02d:%02d" % (h, m))
+    print("%02d:%02d" % divmod(time, 60))
