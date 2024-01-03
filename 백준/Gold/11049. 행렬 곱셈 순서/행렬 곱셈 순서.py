ab2aebb3f5fa780ff618f008333bcda6556ab512
김주원 <66457470+juwkim@users.noreply.github.com>
2024-01-03 13:48:21 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 920 ms, Memory: 113272 KB -BaekjoonHub

---
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
@@ -0,0 +1,17 @@
+import sys
+import math
+
+input = lambda: sys.stdin.readline().rstrip()
+g = lambda: [*map(int, input().split())]
+
+N = int(input())
+matrix = [g() for _ in range(N)]
+dp = [[math.inf] * N for _ in range(N)] # dp[i][j]: (i, j)까지의 최소 비용
+for i in range(N):
+    dp[i][i] = 0
+for d in range(1, N):
+    for i in range(N-d):
+        j = i + d
+        for k in range(i, j):
+            dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j] + matrix[i][0] * matrix[k][1] * matrix[j][1])
+print(dp[0][N-1])
