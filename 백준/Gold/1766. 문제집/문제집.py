916a8d433e539c23780ce6809c2ced7d700e470e
김주원 <66457470+juwkim@users.noreply.github.com>
2024-01-05 15:31:15 +0900

    [Gold II] Title: 문제집, Time: 4656 ms, Memory: 126892 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -0,0 +1,27 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+g = lambda: [*map(int, input().split())]
+
+N, M = g()
+Out = [[] for _ in range(N + 1)]
+In = [set() for _ in range(N + 1)]
+for _ in range(M):
+    A, B = g()
+    Out[A].append(B)
+    In[B].add(A)
+
+ans = []
+solved = [False] * (N + 1)
+solved_cnt = 0
+while solved_cnt != N:
+    for i in range(1, N + 1):
+        if solved[i] or In[i]:
+            continue
+        solved[i] = True
+        solved_cnt += 1
+        ans.append(i)
+        for j in Out[i]:
+            In[j].remove(i)
+        break
+print(*ans)
