c7dcc6fed83717e844376772e02f44ccaca070b0
김주원 <66457470+juwkim@users.noreply.github.com>
2024-01-05 16:15:24 +0900

    [Gold II] Title: 공항, Time: 148 ms, Memory: 111388 KB -BaekjoonHub

--- 백준/Gold/10775. 공항/공항.py
+++ 백준/Gold/10775. 공항/공항.py
@@ -1,13 +1,9 @@
 import sys
-
 input = lambda: sys.stdin.readline().rstrip()
-g = lambda: [*map(int, input().split())]
 
 G = int(input())
 P = int(input())
-
 parent = [*range(G + 1)]
-rank = [0] * (G + 1)
 
 def find(x):
     ret = x
@@ -21,6 +17,6 @@ for _ in range(P):
     x = find(int(input()))
     if x == 0:
         break
-    parent[x] = x - 1
+    parent[x] = parent[x - 1]
     ans += 1
 print(ans)
