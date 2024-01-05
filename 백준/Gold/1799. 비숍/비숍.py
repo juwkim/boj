67ff203d2ab213092fc5d324931d168072226364
김주원 <66457470+juwkim@users.noreply.github.com>
2024-01-06 04:10:13 +0900

    [Gold I] Title: 비숍, Time: 128 ms, Memory: 110864 KB -BaekjoonHub

--- 백준/Gold/1799. 비숍/비숍.py
+++ 백준/Gold/1799. 비숍/비숍.py
@@ -5,17 +5,17 @@ g = lambda: [*map(int, input().split())]
 
 N = int(input())
 board = [g() for _ in range(N)]
-ans = 0
+ans = [0, 0]
 diagonal = [False] * (2 * N - 1)
 for i in range(N):
     for j in range(N):
         if board[i][j] == 1:
             diagonal[i - j + N - 1] = True
 
-def solve(line, cnt):
+def solve(line, cnt, idx):
     global ans
-    if line == 2 * N:
-        ans = max(ans, cnt)
+    if line > 2 * N - 1:
+        ans[idx] = max(ans[idx], cnt)
         return
     flag = True
     for i in range(N):
@@ -24,9 +24,10 @@ def solve(line, cnt):
             continue
         flag = False
         diagonal[i - j + N - 1] = False
-        solve(line + 1, cnt + 1)
+        solve(line + 2, cnt + 1, idx)
         diagonal[i - j + N - 1] = True
     if flag:
-        solve(line + 1, cnt)
-solve(0, 0)
-print(ans)
+        solve(line + 2, cnt, idx)
+solve(0, 0, 0)
+solve(1, 0, 1)
+print(sum(ans))
