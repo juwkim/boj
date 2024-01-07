13874d123f206a80af122421a8d8389299e85118
김주원 <66457470+juwkim@users.noreply.github.com>
2024-01-07 12:47:11 +0900

    [Gold III] Title: 드래곤 커브, Time: 148 ms, Memory: 111592 KB -BaekjoonHub

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.py
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.py
@@ -9,14 +9,15 @@ for _ in range(int(input())):
         buf.extend((i + 1) % 4 for i in buf[::-1])
     board[x][y] = 1
     for i in buf:
-        if i == 0:
-            x += 1
-        elif i == 1:
-            y -= 1
-        elif i == 2:
-            x -= 1
-        else:
-            y += 1
+        match i:
+            case 0:
+                x += 1
+            case 1:
+                y -= 1
+            case 2:
+                x -= 1
+            case 3:
+                y += 1
         board[x][y] = 1
 ans = sum(all(board[p][q] for p, q in ((i, j), (i, j + 1), (i + 1, j), (i + 1, j + 1))) for i in range(100) for j in range(100))
 print(ans)
