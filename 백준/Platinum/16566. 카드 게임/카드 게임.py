d30f442d0255ceaba8f4298643de9ac21d2af29f
김주원 <66457470+juwkim@users.noreply.github.com>
2024-01-07 17:15:08 +0900

    [Platinum V] Title: 카드 게임, Time: 1840 ms, Memory: 586316 KB -BaekjoonHub

--- 백준/Platinum/16566. 카드 게임/카드 게임.py
+++ 백준/Platinum/16566. 카드 게임/카드 게임.py
@@ -1,4 +1,4 @@
-import bisect
+import bisect as b
 g=lambda:[*map(int,input().split())]
 def f(x):
  t=x
@@ -6,4 +6,4 @@ def f(x):
  p[x]=t;return t
 p=[*range(g()[0])]
 a=sorted(g())
-for n in g():i=f(bisect.bisect_left(a,n+1));p[i]=f(i+1);print(a[i])
+for n in g():i=f(b.bisect_left(a,n+1));p[i]=f(i+1);print(a[i])
