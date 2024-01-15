3a0a59fd09d487f3fa2ae651a488879f6a95b117
김주원 <66457470+juwkim@users.noreply.github.com>
2024-01-15 20:39:56 +0900

    [Silver III] Title: Even More Odd Photos, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/20974. Even More Odd Photos/Even More Odd Photos.py
+++ 백준/Silver/20974. Even More Odd Photos/Even More Odd Photos.py
@@ -1,17 +1,7 @@
-import sys
-
-input = lambda: sys.stdin.readline().rstrip()
-g = lambda: [*map(int, input().split())]
-
-N = int(input())
-even, odd = 0, 0
-for num in g():
-    if num & 1:
-        odd += 1
-    else:
-        even += 1
-if odd > even:
-    cnt = (odd - even + 2) // 3
-    odd -= cnt * 2
-    even += cnt
-print(odd + min(odd + 1, even))
+input()
+a=[0,0]
+for c in map(int,input().split()):a[c&1]+=1
+e,o=a
+t=max(0,(o-e+2)//3)
+o-=t*2
+print(o+min(o+1,e+t))
