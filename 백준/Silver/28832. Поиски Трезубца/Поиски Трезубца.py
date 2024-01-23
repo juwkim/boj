d570aa061195ae54b9e0c34ea7c4a4796038ab6e
김주원 <66457470+juwkim@users.noreply.github.com>
2024-01-23 22:19:38 +0900

    [Silver IV] Title: Поиски Трезубца, Time: 124 ms, Memory: 112168 KB -BaekjoonHub

--- 백준/Silver/28832. Поиски Трезубца/Поиски Трезубца.py
+++ 백준/Silver/28832. Поиски Трезубца/Поиски Трезубца.py
@@ -6,34 +6,34 @@ g = lambda: [*map(int, input().split())]
 n, m = g()
 a = [input() for i in range(n)]
 x, y = 0, 0
-ans = ""
-for i in range(1, n + m - 1):
-    if i % 2:
-        ans += "RL"
-        while x < n - 1 and y > 0:
-            ans += "D"
-            x += 1
-            if x < n - 1:
-                ans += "L"
-                y -= 1
-        if x < n - 1:
-            ans += "D"
-            x += 1
-        if (x + y) % 2 != i % 2:
-            ans += "R"
-            y += 1
-    else:
-        ans += "DU"
+ans = []
+for i in range(n + m - 1):
+    if i & 1:
+        ans.append("DU")
         while x > 0 and y < m - 1:
-            ans += "R"
+            ans.append("R")
             y += 1
             if y < m - 1:
-                ans += "U"
+                ans.append("U")
                 x -= 1
         if y < m - 1:
-            ans += "R"
+            ans.append("R")
             y += 1
-        if (x + y) % 2 != i % 2:
-            ans += "D"
+        if (x + y) & 1 == i & 1:
+            ans.append("D")
             x += 1
-print(ans)
+    else:
+        ans.append("RL")
+        while x < n - 1 and y > 0:
+            ans.append("D")
+            x += 1
+            if x < n - 1:
+                ans.append("L")
+                y -= 1
+        if x < n - 1:
+            ans.append("D")
+            x += 1
+        if (x + y) & 1 == i & 1:
+            ans.append("R")
+            y += 1
+print(*ans, sep="")
