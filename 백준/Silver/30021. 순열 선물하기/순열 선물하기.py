eed299d20488b4d3df649fed60aef2e9985236b0
김주원 <66457470+juwkim@users.noreply.github.com>
2024-01-24 00:23:21 +0900

    [Silver III] Title: 순열 선물하기, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/30021. 순열 선물하기/순열 선물하기.py
+++ 백준/Silver/30021. 순열 선물하기/순열 선물하기.py
@@ -1,33 +1,9 @@
-import sys
-
-input = lambda: sys.stdin.readline().rstrip()
-g = lambda: [*map(int, input().split())]
-
-def is_prime(n):
-    if n == 1:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
 N = int(input())
 if N == 2:
     print("NO")
-elif N == 3:
-    print("YES")
-    print(1, 3, 2)
 else:
     print("YES")
-    p = range(2, N + 1, 2)
-    print(*reversed(p), end=" ")
-    cur = sum(p)
-    nums = list(reversed(range(1, N + 1, 2)))
-    l = len(nums)
-    for _ in range(l):
-        for i in range(len(nums)):
-            if not is_prime(cur + nums[i]):
-                print(nums[i], end=" ")
-                cur += nums[i]
-                nums.pop(i)
-                break
+    if N == 1:
+        print(1)
+    else:
+        print(1, 3, 2, *range(4, N + 1))
