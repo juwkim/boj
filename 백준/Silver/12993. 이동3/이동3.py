90ddc611b4332cfc5182de02dc098304f3b4fd07
김주원 <66457470+juwkim@users.noreply.github.com>
2024-01-25 02:03:59 +0900

    [Silver III] Title: 이동3, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/12993. 이동3/이동3.py
+++ 백준/Silver/12993. 이동3/이동3.py
@@ -1,9 +1,7 @@
-x, y = map(int, input().split())
-ans = 1
+x,y=map(int,input().split())
+a=1
 while x or y:
-    x, r1 = divmod(x, 3)
-    y, r2 = divmod(y, 3)
-    if r1 + r2 != 1:
-        ans = 0
-        break  
-print(ans)
+ x,p=divmod(x,3)
+ y,q=divmod(y,3)
+ if p+q!=1:a=0;break
+print(a)
