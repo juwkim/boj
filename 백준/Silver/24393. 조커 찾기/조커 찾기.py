6f995ec8315a62d36cea49adf0783a63a2a5265e
김주원 <66457470+juwkim@users.noreply.github.com>
2024-01-24 17:30:38 +0900

    [Silver III] Title: 조커 찾기, Time: 1148 ms, Memory: 112464 KB -BaekjoonHub

---
+++ 백준/Silver/24393. 조커 찾기/조커 찾기.py
@@ -0,0 +1,20 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+g = lambda: [*map(int, input().split())]
+
+cur = 1
+for _ in range(int(input())):
+    l, r = 0, 0
+    for i, A in enumerate(g()):
+        if i & 1:
+            l += A
+            if cur <= 13 and l >= cur:
+                cur += r
+                break
+        else:
+            r += A
+            if cur > 13 and r >= cur - 13:
+                cur = l + cur - 13
+                break
+print(cur)
