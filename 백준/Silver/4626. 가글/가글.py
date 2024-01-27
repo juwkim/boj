7fee1659e19644c9f642d9df6dd171ca3f268fb2
김주원 <66457470+juwkim@users.noreply.github.com>
2024-01-28 03:02:29 +0900

    [Silver III] Title: 가글, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/4626. 가글/가글.py
+++ 백준/Silver/4626. 가글/가글.py
@@ -7,8 +7,10 @@ for tc in range(1, 1 + int(input())):
     N, D, B, E = g()
     ans = []
     s = N % D
-    for _ in range(E + 1):
+    for _ in range(B):
+        s = s * 7 % D
+    for _ in range(E - B + 1):
         q, s = divmod(s * 7, D)
         ans.append(q)
-    result = "".join(map(str, ans[B:E+1]))
+    result = "".join(map(str, ans))
     print(f"Problem set {tc}: {N} / {D}, base 7 digits {B} through {E}: {result}")
