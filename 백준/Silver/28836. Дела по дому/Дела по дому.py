22f58298c8a7f1a6f9188870780204d90926a386
김주원 <66457470+juwkim@users.noreply.github.com>
2024-01-29 20:21:14 +0900

    [Silver III] Title: Дела по дому, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/28836. Дела по дому/Дела по дому.py
+++ 백준/Silver/28836. Дела по дому/Дела по дому.py
@@ -1,4 +1,5 @@
-a, b, c, u0, u1, u2 = map(int, input().split())
-l = ((a + b, a + b, 0), (2 * a + c, 2 * a + c, 0), (2 * b + c, 2 * b + c, 0), (a, c, b), (b, c, a), (a + c, c, a), (b + c, c, b))
+*q, c, u0, u1, u2 = map(int, input().split())
+a, b = sorted(q)
+l = ((a + min(b, a + c), a + min(b, a + c), 0), (b, c, a), (a + c, c, a))
 ans = min(sum(s * t for s, t in zip(p, (1/u0, 1/u1, 1/u2))) for p in l)
 print(ans)
