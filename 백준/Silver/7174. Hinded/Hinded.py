73852391fae00cecf12a6b5a5335488e365cf71b
김주원 <66457470+juwkim@users.noreply.github.com>
2024-01-29 11:19:39 +0900

    [Silver III] Title: Hinded, Time: 252 ms, Memory: 110536 KB -BaekjoonHub

--- 백준/Silver/7174. Hinded/Hinded.py
+++ 백준/Silver/7174. Hinded/Hinded.py
@@ -3,14 +3,11 @@ input = lambda: sys.stdin.readline().rstrip()
 
 V = int(input())
 N = int(input())
-ans, idx, cur = 0, 0, 0
-for i in range(N):
+ans, idx, cur = N * V, 1, 0
+for i in range(1, N + 1):
+    cur += int(input())
     val = cur + (N - i) * V
     if val > ans:
         ans = val
         idx = i + 1
-    cur += int(input())
-if cur > ans:
-    ans = cur
-    idx = N + 1
 print(ans, idx)
