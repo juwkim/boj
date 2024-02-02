25a83dcdf6a55606ba92f583bfe95a6110aa17bc
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-02 23:31:00 +0900

    [Silver III] Title: Эскалатор, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/28951. Эскалатор/Эскалатор.py
+++ 백준/Silver/28951. Эскалатор/Эскалатор.py
@@ -1,9 +1,2 @@
-n = int(input())
-s = len(str(n))
-ans = n != 1
-if n % 10:
-    ans += s
-ans += s * 10 ** (s - 2) - (10 ** (s - 1) + 8) // 9 if s > 1 else 0
-n -= n % 10
-ans += ((n - 10 ** (s - 1)) // 10 + 1) * s
-print(ans)
+s=len(str(n:=int(input())))
+print(s*(n//10+1)+(1-10**(s-1))//9+(n!=1)*(n%10!=0)*s)
