9f95fa4a88e8346e3736d3919fada7d097255aa8
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-03 07:52:33 +0900

    [Silver III] Title: 監獄, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/22411. 監獄/監獄.py
+++ 백준/Silver/22411. 監獄/監獄.py
@@ -1,5 +1,4 @@
-N, k = map(int, input().split())
-pos = 0
-for i in range(N - 1):
-    pos += pos // (k - 1) + 1
-print(pos)
+N,k=map(int, input().split())
+p=0
+for i in range(N-1):p+=p//(k-1)+1
+print(p)
