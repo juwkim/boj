f144b17a297e11c0c0c77fcdc5da739a92c5c321
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-05 08:45:45 +0900

    [Silver III] Title: Шпаги, Time: 256 ms, Memory: 136012 KB -BaekjoonHub

--- 백준/Silver/19884. Шпаги/Шпаги.py
+++ 백준/Silver/19884. Шпаги/Шпаги.py
@@ -6,11 +6,9 @@ from collections import deque
 
 n, k = g()
 l, *a = sorted(enumerate(g()), key=lambda x: x[1], reverse=True)
-assert len(a) == n - 1
 ans = [0] * n
 dq = deque([l, l])
 for i, new in a:
-    assert 0 <= i < n
     idx, ori = dq.popleft()
     if ori - new >= k:
         ans[i] = idx + 1
