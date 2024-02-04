50dee63543d2afcf332ddb9e46bf7bbae6efca73
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-05 05:42:01 +0900

    [Silver III] Title: Интересные числа, Time: 116 ms, Memory: 108080 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20518. Интересные числа/Интересные числа.py
+++ 백준/Silver/20518. Интересные числа/Интересные числа.py
@@ -1,21 +1,23 @@
 def f(s):
+    if len(s) == 1:
+        return int(s)
     res, prev = 0, 0
     for i, c in enumerate(map(int, s)):
         if c > prev:
-            res += sum(dp[len(s) - i][10 - j] for j in range(prev, c))
+            res += sum(dp[len(s) - i][9 - j] for j in range(prev, c))
             res %= MOD
             prev = c
-        else:
-            break
-    return res
+        elif c < prev:
+            return res
+    return res + 1
 
 L, R = input(), input()
 N, MOD = len(R), 10 ** 9 + 7
-dp = [[0] * 11 for _ in range(N + 2)]
-dp[1] = [0] + [1] * 9 + [0]
-for i in range(1, N + 1):
+dp = [[0] * 10 for _ in range(N + 1)]
+dp[1] = [1] * 9 + [0]
+for i in range(1, N):
     Sum = 0
-    for j in range(1, 11):
+    for j in range(10):
         Sum += dp[i][j]
         dp[i + 1][j] = Sum
 print((f(R) - f(str(int(L) - 1))) % MOD)
