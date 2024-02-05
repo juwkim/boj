178dc5f7da14e0259585a3db15c80ac68a0456d5
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-06 05:46:45 +0900

    [Silver III] Title: Akwarium, Time: 1472 ms, Memory: 252424 KB -BaekjoonHub

--- 백준/Silver/8734. Akwarium/Akwarium.py
+++ 백준/Silver/8734. Akwarium/Akwarium.py
@@ -6,22 +6,18 @@ n = int(input())
 fish = []
 for i in range(1, n + 1):
     m, w = g()
-    fish.append([i, 2 * m, w])
+    fish.append([2 * m, w, i])
 res = [0] * (n + 1)
 day = 0
 while fish:
-    dead = len(fish) // 2
-    fish.sort(key=lambda x: (x[1], x[2]), reverse=True)
+    dead = len(fish) & 1
+    fish.sort(reverse=True)
     for i in range(len(fish) // 2):
-        i1, m1, w1 = fish[i]
-        i2, m2, w2 = fish[-i - 1]
-        # if m1 == m2:
-        #     dead = i
-        #     break
-        fish[i][1] += m2 // 2
-    for i in range(len(fish) - dead):
-        i, m, w = fish.pop()
-        res[i] = day
+        m, _, j = fish.pop()
+        fish[i][0] += m // 2
+        res[j] = day
+    if dead:
+        res[fish.pop()[2]] = day
     day += 1
 for _ in range(int(input())):
     r, x = g()
