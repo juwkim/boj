20e5484645dcef00b1eaeeca8180e7a891b9783d
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-08 23:50:29 +0900

    [Silver III] Title: A Homogeneous Country, Time: 196 ms, Memory: 132708 KB -BaekjoonHub

--- 백준/Silver/16470. A Homogeneous Country/A Homogeneous Country.py
+++ 백준/Silver/16470. A Homogeneous Country/A Homogeneous Country.py
@@ -1,3 +1,3 @@
 from collections import*
-cnt = Counter(open(0))
-print(1 - sum(V * V for V in cnt.values()) / sum(cnt.values()) ** 2)
+c=Counter(open(0)).values()
+print(1-sum(V*V for V in c)/sum(c)**2)
