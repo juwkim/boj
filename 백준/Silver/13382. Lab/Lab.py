f52d730153fa6fd5b78403a741c679eb7313393d
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-10 09:43:21 +0900

    [Silver III] Title: Lab, Time: 116 ms, Memory: 110404 KB -BaekjoonHub

--- 백준/Silver/13382. Lab/Lab.py
+++ 백준/Silver/13382. Lab/Lab.py
@@ -1,14 +1,6 @@
-import sys
-input = sys.stdin.readline
-
 for _ in range(int(input())):
-    n = int(input())
-    P = [*map(float, input().split())]
-    dp = [(P[0], 0)]
-    if n >= 2:
-        dp.append((P[1], max(dp[-1])))
-    if n >= 3:
-        dp.append((P[2], max(dp[-1])))
-    for i in range(3, n):
-        dp.append((P[i] + max(dp[i-3]), max(dp[-1])))
-    print(max(dp[-1]))
+    input()
+    a, b, c = 0, 0, 0
+    for p in map(float, input().split()):
+        a, b, c = b, c, max(a + p, c)
+    print(c)
