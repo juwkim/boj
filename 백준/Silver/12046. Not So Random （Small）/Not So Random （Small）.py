5d708ca6dbab05a0b97a73e2ac9bedcbf12f71ec
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-12 03:47:52 +0900

    [Silver III] Title: Not So Random (Small), Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/12046. Not So Random （Small）/Not So Random （Small）.py
+++ 백준/Silver/12046. Not So Random （Small）/Not So Random （Small）.py
@@ -1,18 +1,14 @@
-import sys
-input = sys.stdin.readline
-ith_bit = lambda x, i: (x >> i) & 1
 for tc in range(1, 1 + int(input())):
     N, X, K, A, B, C = map(int, input().split())
+    AB, BC = (A + B) / 100, (B + C) / 100    
     ans = 0
     for i in range(max(X, K).bit_length()):
-        a, b = ((0, 1), (1, 0))[ith_bit(X, i)]
-        if ith_bit(K, i):
+        a = X >> i & 1
+        if K >> i & 1:
             for _ in range(N):
-                a = a * (A + B) / 100 + b * (B + C) / 100
-                b = 1 - a
+                a = a * AB + (1 - a) * BC
         else:
             for _ in range(N):
-                a = a * (B + C) / 100
-                b = 1 - a
+                a *= BC
         ans += a * (1 << i)
-    print(f"Case #{tc}: {ans}")
+    print(f"Case #{tc}: {ans:.10f}")
