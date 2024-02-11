a4929e206521ad50758637a2b047aed26d8d00a7
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-12 04:50:44 +0900

    [Silver III] Title: Not So Random (Small), Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/12046. Not So Random （Small）/Not So Random （Small）.py
+++ 백준/Silver/12046. Not So Random （Small）/Not So Random （Small）.py
@@ -1,10 +1,9 @@
 for t in range(int(input())):
     N, X, K, A, B, C = map(int, input().split())
-    AB, BC = (A + B) / 100, (B + C) / 100
     a, b, c = 1, 0, 1
     for _ in range(N):
-        a *= BC
-        b = b * AB + (1 - b) * BC
-        c = c * AB + (1 - c) * BC
+        a *= (B + C) / 100
+        b = (b * A + B + (1 - b) * C) / 100
+        c = (c * A + B + (1 - c) * C) / 100
     ans = sum(((0, a), (b, c))[K >> i & 1][X >> i & 1] * (1 << i) for i in range(max(X, K).bit_length()))
     print(f"Case #{t+1}: {ans:.10f}")
