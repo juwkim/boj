deb2dababa355daffd0004eba4df4f17e33c2b13
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-12 03:49:58 +0900

    [Gold IV] Title: Not So Random (Large), Time: 960 ms, Memory: 110540 KB -BaekjoonHub

--- 백준/Gold/12047. Not So Random （Large）/Not So Random （Large）.py
+++ 백준/Gold/12047. Not So Random （Large）/Not So Random （Large）.py
@@ -1,15 +1,12 @@
-for tc in range(1, 1 + int(input())):
+for t in range(int(input())):
     N, X, K, A, B, C = map(int, input().split())
     AB, BC = (A + B) / 100, (B + C) / 100    
-    ans = 0
+    p = 0
     for i in range(max(X, K).bit_length()):
-        a, b = ((0, 1), (1, 0))[X >> i & 1]
+        a = X >> i & 1
         if K >> i & 1:
-            for _ in range(N):
-                a = a * AB + b * BC
-                b = 1 - a
+            for _ in range(N): a = a * AB + (1 - a) * BC
         else:
-            for _ in range(N):
-                a *= BC
-        ans += a * (1 << i)
-    print(f"Case #{tc}: {ans:.10f}")
+            for _ in range(N): a *= BC
+        p += a * (1 << i)
+    print(f"Case #{t+1}: {p:.10f}")
