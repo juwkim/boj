3d40e480766a9e0e7b1424a9eb954d35a9c6900d
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-12 03:44:40 +0900

    [Gold IV] Title: Not So Random (Large), Time: 972 ms, Memory: 110408 KB -BaekjoonHub

--- 백준/Gold/12047. Not So Random （Large）/Not So Random （Large）.py
+++ 백준/Gold/12047. Not So Random （Large）/Not So Random （Large）.py
@@ -1,17 +1,15 @@
-ith_bit = lambda x, i: (x >> i) & 1
 for tc in range(1, 1 + int(input())):
     N, X, K, A, B, C = map(int, input().split())
     AB, BC = (A + B) / 100, (B + C) / 100    
     ans = 0
     for i in range(max(X, K).bit_length()):
-        a, b = ((0, 1), (1, 0))[ith_bit(X, i)]
-        if ith_bit(K, i):
+        a, b = ((0, 1), (1, 0))[X >> i & 1]
+        if K >> i & 1:
             for _ in range(N):
                 a = a * AB + b * BC
                 b = 1 - a
         else:
             for _ in range(N):
-                a = a * BC
-                b = 1 - a
+                a *= BC
         ans += a * (1 << i)
     print(f"Case #{tc}: {ans:.10f}")
