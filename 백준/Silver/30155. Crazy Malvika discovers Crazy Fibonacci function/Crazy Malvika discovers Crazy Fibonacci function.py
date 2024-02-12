68029b73db5e0a09c8a0e32fc2fa5f426ba04229
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-13 06:42:01 +0900

    [Silver III] Title: Crazy Malvika discovers Crazy Fibonacci function, Time: 228 ms, Memory: 124952 KB -BaekjoonHub

--- 백준/Silver/30155. Crazy Malvika discovers Crazy Fibonacci function/Crazy Malvika discovers Crazy Fibonacci function.py
+++ 백준/Silver/30155. Crazy Malvika discovers Crazy Fibonacci function/Crazy Malvika discovers Crazy Fibonacci function.py
@@ -1,5 +1,6 @@
-for _ in range(int(input())):
-    A, B, N = map(int, input().split())
+N, *l = open(0)
+for s in l:
+    A, B, N = map(int, s.split())
     mod = 10 ** 9 + 7
     for _ in range((N - 1) % 6):
         A, B = B % mod, B - A
