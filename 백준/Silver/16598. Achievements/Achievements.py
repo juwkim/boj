65896e9814d30cc43e8b37693832e425ba726539
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-12 15:17:23 +0900

    [Silver III] Title: Achievements, Time: 156 ms, Memory: 139620 KB -BaekjoonHub

--- 백준/Silver/16598. Achievements/Achievements.py
+++ 백준/Silver/16598. Achievements/Achievements.py
@@ -1,7 +1,7 @@
 n, p, *d = map(int, open(0).read().split())
 ans, l, r = 0, 0, 0
 while r < n:
-    if d[r] - d[l] - r + l > p:
+    if d[r] - d[l] > p + r - l:
         l += 1
     else:
         r += 1
