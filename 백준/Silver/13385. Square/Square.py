4ea38933169289ceef6872536cbfadacc37ee166
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-14 06:46:04 +0900

    [Silver III] Title: Square, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/13385. Square/Square.py
+++ 백준/Silver/13385. Square/Square.py
@@ -1,10 +1,6 @@
-import sys
-input = sys.stdin.readline
-
 for _ in range(int(input())):
-    r, *a = map(int, input().split())
-    w, l = 1, a[-1]
-    for num in reversed(a[:-1]):
-        w += l * num
-        w, l = sorted([w, l])
+    _, *a, l = map(int, input().split())
+    w = 1
+    for n in reversed(a):
+        w, l = l, w + l * n
     print(l)
