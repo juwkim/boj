34b35e854965e27478f9d45ad2510466b082b010
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-15 03:54:49 +0900

    [Silver III] Title: Triangle Areas (Small), Time: 116 ms, Memory: 110268 KB -BaekjoonHub

--- 백준/Silver III/12705. Triangle Areas （Small）/Triangle Areas （Small）.py
+++ 백준/Silver III/12705. Triangle Areas （Small）/Triangle Areas （Small）.py
@@ -4,23 +4,9 @@ input = sys.stdin.readline
 for tc in range(1, 1 + int(input())):
     N, M, A = map(int, input().split())
     if A > N * M:
-        print(f"Case #{tc}: IMPOSSIBLE")
-        continue
-    ans = "IMPOSSIBLE"
-    for x1 in range(N + 1):
-        for x2 in range(N + 1):
-            for y1 in range(M + 1):
-                for y2 in range(M + 1):
-                    if A == abs(x1 * y2 - x2 * y1):
-                        ans = f"0 0 {x1} {y1} {x2} {y2}"
-                        break
-                else:
-                    continue
-                break
-            else:
-                continue
-            break
-        else:
-            continue
-        break
+        ans = "IMPOSSIBLE"
+    elif A == N * M:
+        ans = f"0 0 {N} 0 0 {M}"
+    else:
+        ans = f"0 0 {N} 1 {N - A % N} {A // N + 1}"
     print(f"Case #{tc}: {ans}")
