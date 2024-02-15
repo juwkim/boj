c088686e3159985d25e8d7991439b4fa9d93bb6f
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-15 11:35:45 +0900

    [Silver III] Title: Senate Evacuation (Small), Time: 116 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver III/14363. Senate Evacuation （Small）/Senate Evacuation （Small）.py
+++ 백준/Silver III/14363. Senate Evacuation （Small）/Senate Evacuation （Small）.py
@@ -1,25 +1,24 @@
 import sys
-input = sys.stdin.readline
 import heapq
+input = sys.stdin.readline
 
 for tc in range(1, 1 + int(input())):
-    N = int(input())
-    P = [(-p, chr(65 + i)) for i, p in enumerate(map(int, input().split()))]
+    input()
+    total = sum(nums:=[*map(int, input().split())])
+    P = [(-p, chr(65 + i)) for i, p in enumerate(nums)]
     heapq.heapify(P)
-    res = []
-    total = sum(-p for p, _ in P)
+    ans = []
     while total:
         (a1, b1), (a2, b2) = heapq.heappop(P), heapq.heappop(P)
-        MAX = max(-a1-1, -a2)
-        if MAX * 2 > total - 1:
-            res.append(b1 + b2)
+        if max(-a1-1, -a2) * 2 >= total:
+            ans.append(b1 + b2)
             total -= 2
             if a1 + 1:
                 heapq.heappush(P, (a1 + 1, b1))
                 heapq.heappush(P, (a2 + 1, b2))
         else:
-            res.append(b1)
+            ans.append(b1)
             total -= 1
             heapq.heappush(P, (a1 + 1, b1))
             heapq.heappush(P, (a2, b2))
-    print(f'Case #{tc}:', *res)
+    print(f'Case #{tc}:', *ans)
