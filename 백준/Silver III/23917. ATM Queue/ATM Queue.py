c9555a297b3aa8c782dc6957b25bf7f17b4d8ae6
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-15 09:07:28 +0900

    [Silver III] Title: ATM Queue, Time: 1088 ms, Memory: 227084 KB, Score: 13 point -BaekjoonHub

--- 백준/Silver III/23917. ATM Queue/ATM Queue.py
+++ 백준/Silver III/23917. ATM Queue/ATM Queue.py
@@ -5,5 +5,5 @@ g = lambda: [*map(int, input().split())]
 for tc in range(1, 1 + int(input())):
     N, X = g()
     A = g()
-    ans = sorted(range(1, N + 1), key=lambda i: (A[i - 1] // X, i))
+    ans = sorted(range(1, N + 1), key=lambda i: ((A[i - 1] + X - 1) // X, i))
     print(f"Case #{tc}:", *ans)
