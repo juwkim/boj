04fdcf0d17fb3edf5034c9ff198317d72859f1fb
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-15 09:06:22 +0900

    [Silver III] Title: ATM Queue, Time: 1272 ms, Memory: 225220 KB, Score: 8 point -BaekjoonHub

---
+++ 백준/Silver III/23917. ATM Queue/ATM Queue.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+g = lambda: [*map(int, input().split())]
+
+for tc in range(1, 1 + int(input())):
+    N, X = g()
+    A = g()
+    ans = sorted(range(1, N + 1), key=lambda i: (A[i - 1] // X, i))
+    print(f"Case #{tc}:", *ans)
