a8693a7d6bbbdbc46c1b2c1e7a3260850332ee3e
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-15 11:05:22 +0900

    [Silver III] Title: Divisors, Time: 848 ms, Memory: 392096 KB -BaekjoonHub

--- 백준/Silver III/15616. Divisors/Divisors.py
+++ 백준/Silver III/15616. Divisors/Divisors.py
@@ -1,14 +1,12 @@
-input()
-a = [*map(int, input().split())]
+_, *a = map(int, open(0).read().split())
 MAX = max(a)
 cnt = [0] * (MAX + 1)
 for num in a:
     cnt[num] += 1
 ans = 0
 for i in range(1, MAX + 1):
-    if cnt[i] == 0:
-        continue
-    ans += cnt[i] * (cnt[i] - 1)
-    for j in range(i * 2, MAX + 1, i):
-        ans += cnt[i] * cnt[j]
+    SUM = -1
+    for j in range(i, MAX + 1, i):
+        SUM += cnt[j]
+    ans += cnt[i] * SUM
 print(ans)
