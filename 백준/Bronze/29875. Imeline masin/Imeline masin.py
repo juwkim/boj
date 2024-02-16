a23af851b9a45940c213bc3d0e27565fd84d52d2
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-17 08:18:51 +0900

    [Bronze II] Title: Imeline masin, Time: 276 ms, Memory: 180360 KB -BaekjoonHub

--- 백준/Bronze/29875. Imeline masin/Imeline masin.py
+++ 백준/Bronze/29875. Imeline masin/Imeline masin.py
@@ -1,9 +1,9 @@
 input()
-s = [1, 0, 0, 0]
+s = 1, 0, 0, 0
 for c in map(int, input().split()):
     match c:
-        case 0: s = [s[1], s[0], 0, s[2] | s[3]]
-        case 1: s = [s[3], s[0], s[1] | s[2], 0]
-        case -1: s = [s[1] | s[3], s[0], s[1] | s[2], s[2] | s[3]]
+        case 0: s = s[1], s[0], 0, s[2] | s[3]
+        case 1: s = s[3], s[0], s[1] | s[2], 0
+        case -1: s = s[1] | s[3], s[0], s[1] | s[2], s[2] | s[3]
 for c in s:
     print(("EI", "JAH")[c])
