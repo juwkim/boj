c299e38730f4381ba68715a30807d3dd77594ad5
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-18 19:27:25 +0900

    [Silver III] Title: 소수의 주기, Time: 512 ms, Memory: 35748 KB -BaekjoonHub

--- 백준/Silver/7546. 소수의 주기/소수의 주기.py
+++ 백준/Silver/7546. 소수의 주기/소수의 주기.py
@@ -5,10 +5,7 @@ for tc in range(1, 1 + int(input())):
     x, y = int(x, b:=int(b)), int(y, b)
     check = [0] * y
     ans, i = 0, 0
-    while x := x * b % y:
-        if check[x]:
-            ans = i - check[x]
-            break
+    while (x := x * b % y) and check[x] == 0:
         check[x] = i
         i += 1
     print(f"Scenario #{tc}:\n{i - check[x]}\n")
