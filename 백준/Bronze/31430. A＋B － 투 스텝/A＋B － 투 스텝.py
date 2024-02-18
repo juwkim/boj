760095947a2421747b70952ffb53ac8396fda5d5
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-19 07:02:47 +0900

    [Bronze I] Title: A+B - 투 스텝, Time: 212 ms, Memory: 216160 KB -BaekjoonHub

--- 백준/Bronze/31430. A＋B － 투 스텝/A＋B － 투 스텝.py
+++ 백준/Bronze/31430. A＋B － 투 스텝/A＋B － 투 스텝.py
@@ -1,19 +1,14 @@
 if input() == '1':
     num = sum(map(int, input().split()))
-    buf = []
-    num, c = divmod(num, 25)
-    buf.append(chr(c + 97))
-    while num:
+    ans = ['a'] * 13
+    for i in range(12, -1, -1):
+        if num == 0:
+            break
         num, c = divmod(num, 26)
-        buf.append(chr(c + 97))
-    ans = "".join(reversed(buf))
-    if len(ans) < 13:
-        ans += "z" * (13 - len(ans))
-    print(ans)
+        ans[i] = chr(c + 97)
+    print(*ans, sep='')
 else:
     ans = 0
-    *s, e = input().rstrip('z')
-    for c in s:
+    for c in input():
         ans = ans * 26 + ord(c) - 97
-    ans = ans * 25 + ord(e) - 97
     print(ans)
