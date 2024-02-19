c1827359f526049bebd5440733167dc1ef1b30c2
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-20 07:49:06 +0900

    [Silver III] Title: Drum Decorator (Small), Time: 116 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/12145. Drum Decorator （Small）/Drum Decorator （Small）.py
+++ 백준/Silver/12145. Drum Decorator （Small）/Drum Decorator （Small）.py
@@ -1,34 +1,42 @@
 import sys
-input = sys.stdin.readline
 from math import lcm
+input = sys.stdin.readline
 
 MOD = 10**9 + 7
 for tc in range(1, 1 + int(input())):
     R, C = map(int, input().split())
-    dp = [[[0] * 13, [0] * 13] for i in range(R + 1)]
-    dp[0][0][1], dp[0][1][1] = 1, 1
+    a, b = [[0] * 13 for _ in range(4)], [[0] * 13 for _ in range(4)]
+    a[0][1], b[0][1] = 1, 1
     for i in range(R):
-        for degree, p in enumerate(dp[i][0]):
-            if p == 0:
-                continue
+        for d, v in enumerate(a[i % 4]):
+            if v == 0: continue
             if i + 2 <= R:
-                dp[i + 2][1][degree] = (dp[i + 2][1][degree] + p) % MOD
-        for degree, p in enumerate(dp[i][1]):
-            if p == 0:
-                continue
+                b[(i + 2) % 4][d] += v
+                b[(i + 2) % 4][d] %= MOD
+        for d, v in enumerate(b[i % 4]):
+            if v == 0: continue
             if i + 1 <= R:
-                dp[i + 1][0][degree] = (dp[i + 1][0][degree] + p) % MOD
+                a[(i + 1) % 4][d] += v
+                a[(i + 1) % 4][d] %= MOD
             if i + 2 <= R:
                 if C % 3 == 0:
-                    dp[i + 2][0][lcm(degree, 3)] = (dp[i + 2][0][lcm(degree, 3)] + 3 * p) % MOD
+                    l = lcm(d, 3)
+                    a[(i + 2) % 4][l] += 3 * v
+                    a[(i + 2) % 4][l] %= MOD
                 if C % 6 == 0:
-                    dp[i + 2][0][lcm(degree, 6)] = (dp[i + 2][0][lcm(degree, 6)] + 6 * p) % MOD
+                    l = lcm(d, 6)
+                    a[(i + 2) % 4][l] += 6 * v
+                    a[(i + 2) % 4][l] %= MOD
             if i + 3 <= R and C % 4 == 0:
-                dp[i + 3][0][lcm(degree, 4)] = (dp[i + 3][0][lcm(degree, 4)] + 4 * p) % MOD
+                l = lcm(d, 4)
+                a[(i + 3) % 4][l] += 4 * v
+                a[(i + 3) % 4][l] %= MOD
+        a[i % 4], b[i % 4] = [0] * 13, [0] * 13
     ans = 0
-    for t in range(2):
-        for degree, p in enumerate(dp[R][t]):
-            if p == 0:
-                continue
-            ans = (ans + p // degree) % MOD
-    print(f'Case #{tc}: {ans}')
+    for d, v in enumerate(a[R % 4]):
+        if v == 0: continue
+        ans = (ans + v // d) % MOD
+    for d, v in enumerate(b[R % 4]):
+        if v == 0: continue
+        ans = (ans + v // d) % MOD
+    print(f"Case #{tc}: {ans}")
