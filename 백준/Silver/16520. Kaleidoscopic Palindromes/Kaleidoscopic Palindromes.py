c2aaea6bb05e85b4abfa346e55047c0df0c54027
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-21 04:49:29 +0900

    [Silver III] Title: Kaleidoscopic Palindromes, Time: 1048 ms, Memory: 112364 KB -BaekjoonHub

--- 백준/Silver/16520. Kaleidoscopic Palindromes/Kaleidoscopic Palindromes.py
+++ 백준/Silver/16520. Kaleidoscopic Palindromes/Kaleidoscopic Palindromes.py
@@ -2,17 +2,12 @@ a, b, k = map(int, input().split())
 if k == 2:
     ans = 0
     for i in range(a, b + 1):
-        b = bin(i)[2:]
-        if b == b[::-1]:
-            ans += 1
-elif k == 3:
-    ans = 0
-    for num in (0, 1, 6643, 1422773):
-        if a <= num <= b:
-            ans += 1
+        c = bin(i)[2:]
+        l = len(c)
+        ans += all(c[j] == c[l - j - 1] for j in range(l//2))
 else:
-    ans = 0
-    for num in (0, 1):
-        if a <= num <= b:
-            ans += 1    
+    t = (0, 1)
+    if k == 3:
+        t += (6643, 1422773)
+    ans = sum(a <= num <= b for num in t)
 print(ans)
