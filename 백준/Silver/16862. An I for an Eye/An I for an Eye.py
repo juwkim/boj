0792c0f2e144312e6897b84b1574c45249233724
김주원 <66457470+juwkim@users.noreply.github.com>
2024-02-21 03:02:23 +0900

    [Silver III] Title: An I for an Eye, Time: 124 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Silver/16862. An I for an Eye/An I for an Eye.py
+++ 백준/Silver/16862. An I for an Eye/An I for an Eye.py
@@ -4,6 +4,9 @@ input = lambda: sys.stdin.readline().rstrip()
 dic2 = {"at": "@", "to": "2", "be": "b", "oh": "o"}
 dic3 = {"and": "&", "one": "1", "won": "1", "too": "2", "two": "2", "for": "4", "bea": "b", "bee": "b", "sea": "c", "see": "c", "eye": "i", "owe": "o", "are": "r", "you": "u", "why": "y"}
 dic4 = {"four": "4"}
+dic2.update({k.capitalize(): v.capitalize() for k, v in dic2.items()})
+dic3.update({k.capitalize(): v.capitalize() for k, v in dic3.items()})
+dic4.update({k.capitalize(): v.capitalize() for k, v in dic4.items()})
 
 for _ in range(int(input())):
     s = input()
@@ -12,23 +15,14 @@ for _ in range(int(input())):
         if s[i] == ' ':
             print(" ", end="")
             i += 1
-        elif i + 3 < len(s) and s[i:i + 4].lower() in dic4:
-            if s[i].isupper():
-                print(dic4[s[i:i + 4].lower()].upper(), end="")
-            else:
-                print(dic4[s[i:i + 4].lower()], end="")
+        elif i + 3 < len(s) and s[i:i + 4] in dic4:
+            print(dic4[s[i:i + 4]], end="")
             i += 4
         elif i + 2 < len(s) and s[i:i + 3].lower() in dic3:
-            if s[i].isupper():
-                print(dic3[s[i:i + 3].lower()].upper(), end="")
-            else:
-                print(dic3[s[i:i + 3].lower()], end="")
+            print(dic3[s[i:i + 3]], end="")
             i += 3
         elif i + 1 < len(s) and s[i:i + 2].lower() in dic2:
-            if s[i].isupper():
-                print(dic2[s[i:i + 2].lower()].upper(), end="")
-            else:
-                print(dic2[s[i:i + 2].lower()], end="")
+            print(dic2[s[i:i + 2]], end="")
             i += 2
         else:
             print(s[i], end="")
