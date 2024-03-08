3194817f755237a58b2566c421f8b5f839192356
김주원 <66457470+juwkim@users.noreply.github.com>
2024-03-09 05:57:50 +0900

    [Silver III] Title: Easy Measurements, Time: 120 ms, Memory: 109108 KB -BaekjoonHub

--- 백준/Silver/20703. Easy Measurements/Easy Measurements.py
+++ 백준/Silver/20703. Easy Measurements/Easy Measurements.py
@@ -5,7 +5,6 @@ from math import lcm
 for _ in range(int(input())):
     b, d = map(int, input().split())
     l = lcm(b, d)
-    c = (b - l) % (l // b)
-    if c == 0:
-        c += l // b
-    print(len(range(c, b, l // b)))
+    s = l // b
+    c = (b - l - 1) % s + 1
+    print((b - c + s - 1) // s)
