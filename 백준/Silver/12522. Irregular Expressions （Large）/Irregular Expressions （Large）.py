74ace34c40ae680a59364a25b71afebbd3dae421
김주원 <66457470+juwkim@users.noreply.github.com>
2024-03-17 09:20:51 +0900

    [Silver III] Title: Irregular Expressions (Large), Time: 136 ms, Memory: 110836 KB -BaekjoonHub

---
+++ 백준/Silver/12522. Irregular Expressions （Large）/Irregular Expressions （Large）.py
@@ -0,0 +1,32 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def solve(s):
+    i, v = len(s) - 1, 0
+    while i >= 0:
+        v += s[i] in "aeiou"
+        if v == 2:
+            break
+        i -= 1
+    if v != 2:
+        return False
+    for j in range(i, 0, -1):
+        l = len(s) - j
+        idx = s.find(s[j:], 0, j)
+        if idx == -1:
+            continue
+        p = s[idx + l:j]
+        if any(c in "aeiou" for c in p):
+            return True
+    return False
+for tc in range(1, 1 + int(input())):
+    s = input()
+    if any(solve(s[:i]) for i in range(5, len(s) + 1)):
+        ans = "Spell!"
+    else:
+        s = s[::-1]
+        if any(solve(s[:i]) for i in range(5, len(s) + 1)):
+            ans = "Spell!"        
+        else:
+            ans = "Nothing."
+    print(f"Case #{tc}: {ans}")
