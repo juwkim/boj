3e52ca01cb3e3d3ab3d4e6e2067c7f6af97ce4ce
김주원 <66457470+juwkim@users.noreply.github.com>
2024-03-18 16:37:46 +0900

    [Silver III] Title: Harvest, Time: 276 ms, Memory: 121612 KB -BaekjoonHub

--- 백준/Silver/26449. Harvest/Harvest.py
+++ 백준/Silver/26449. Harvest/Harvest.py
@@ -6,10 +6,6 @@ nums = [[*map(int, input().split())] for _ in range(int(input()))]
 h = [int(input()) for _ in range(int(input()))]
 ans = 0
 for s, t in nums:
-    if t < h[0] or s > h[-1]:
-        continue
-    a = bisect(h, s)
-    b = bisect(h, t)
-    if h[a - 1] == s or a != b:
-        ans += 1
+    a, b = bisect(h, s), bisect(h, t)
+    ans += h[a - 1] == s or a != b
 print(ans)
