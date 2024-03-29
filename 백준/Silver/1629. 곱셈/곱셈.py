77c36f2d59b10eb6ca03a188b832d085332cee7e
김주원 <66457470+juwkim@users.noreply.github.com>
2024-03-30 06:34:31 +0900

    [Silver I] Title: 곱셈, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,8 +1,1 @@
-a, b, c = map(int, input().split())
-res = 1
-while b > 0:
-    if b % 2:
-        res = res * a % c
-    a = a**2 % c
-    b //= 2
-print(res)
+print(pow(*map(int,input().split())))
