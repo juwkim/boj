52030647f11dba19c5508f35871af69e7f6bb8d0
김주원 <66457470+juwkim@users.noreply.github.com>
2024-04-01 07:19:41 +0900

    [Silver III] Title: 특별한 기술력, Time: 176 ms, Memory: 142224 KB -BaekjoonHub

--- 백준/Silver/31674. 특별한 기술력/특별한 기술력.py
+++ 백준/Silver/31674. 특별한 기술력/특별한 기술력.py
@@ -1,4 +1,3 @@
-_,*l=map(int,open(0).read().split())
 a,c,m=0,1,10**9+7
-for H in sorted(l):a=(a+H*c)%m;c=(c<<1)%m
+for H in sorted([*map(int,open(0).read().split())][1:]):a=(a+H*c)%m;c=(c<<1)%m
 print(a)
