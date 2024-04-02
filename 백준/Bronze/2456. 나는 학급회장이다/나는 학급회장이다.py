f4784a59d01f8031f0c20079c6aeaa55fbc80176
김주원 <66457470+juwkim@users.noreply.github.com>
2024-04-03 02:31:57 +0900

    [Bronze I] Title: 나는 학급회장이다, Time: 128 ms, Memory: 110428 KB -BaekjoonHub

--- 백준/Bronze/2456. 나는 학급회장이다/나는 학급회장이다.py
+++ 백준/Bronze/2456. 나는 학급회장이다/나는 학급회장이다.py
@@ -1,3 +1,8 @@
-s=[*map(sorted,zip(*[[*map(int,input().split())]for _ in range(int(input()))]))]
-t=max(s,key=lambda x:(sum(x),x.count(3),x.count(2)))
-print((s.index(t)+1)*(s.count(t)==1),sum(t))
+N = int(input())
+votes = [list(map(int, input().split())) for _ in range(N)]
+votes = [sorted(vote) for vote in zip(*votes)]
+winner = max(votes, key=lambda x: (sum(x), x.count(3), x.count(2)))
+if votes.count(winner) == 1:
+    print(votes.index(winner) + 1, sum(winner))
+else:
+    print(0, sum(winner))
