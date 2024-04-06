1a3c600af15b37a7dfc059f2dfe7285ae998de37
김주원 <66457470+juwkim@users.noreply.github.com>
2024-04-07 08:04:33 +0900

    [Silver IV] Title: 매우 어려운 문제, Time: 144 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/31738. 매우 어려운 문제/매우 어려운 문제.py
+++ 백준/Silver/31738. 매우 어려운 문제/매우 어려운 문제.py
@@ -1,7 +1,5 @@
-N, M = map(int, input().split())
-ans = 1
-for i in range(2, N + 1):
-    ans = ans * i % M
-    if ans == 0:
-        break
-print(ans)
+N,M=map(int,input().split())
+a=1
+for i in range(2,N+1):
+ if(a:=a*i%M)==0:break
+print(a)
