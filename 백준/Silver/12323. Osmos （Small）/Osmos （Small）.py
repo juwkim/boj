8a9b8788c365a2b168581fe1e08e7c8441eebead
김주원 <66457470+juwkim@users.noreply.github.com>
2024-04-11 20:00:16 +0900

    [Silver III] Title: Osmos (Small), Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/12323. Osmos （Small）/Osmos （Small）.py
+++ 백준/Silver/12323. Osmos （Small）/Osmos （Small）.py
@@ -1,22 +1,18 @@
-import sys
-input = lambda: sys.stdin.readline().rstrip()
 g = lambda: [*map(int, input().split())]
-
 for tc in range(1, 1 + int(input())):
     A, N = g()
     ans, cur = 1e9, 0
     for i, mote in enumerate(sorted(g())):
+        if A == 1:
+            cur += N - i
+            break
         if mote < A:
             A += mote
         else:
             ans = min(ans, cur + N - i)
-            if A == 1:
-                cur = -1
-                break
             while mote >= A:
                 A += A - 1
                 cur += 1
             A += mote
-    if cur != -1:
-        ans = min(ans, cur)
+    ans = min(ans, cur)
     print(f"Case #{tc}: {ans}")
