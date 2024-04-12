3d5a903dbaa78616479c845a7e8ff4778b6f41f5
김주원 <66457470+juwkim@users.noreply.github.com>
2024-04-13 07:43:00 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 228 ms, Memory: 157916 KB, Score: 250 point -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
@@ -1,8 +1,8 @@
 N, *nums = map(int, open(0).read().split())
 nums.sort()
-ans, w = 0, 2
+ans, w, m = 0, 2, 1_000_000_007
 l, r = 1, N - 2
 for _ in range(1, N):
-    ans = (ans + (w - 1) * (nums[l] - nums[r])) % 1_000_000_007
-    w <<= 1; l += 1; r -= 1
+    ans = (ans + (w - 1) * (nums[l] - nums[r])) % m
+    w = (w << 1) % m; l += 1; r -= 1
 print(ans)
