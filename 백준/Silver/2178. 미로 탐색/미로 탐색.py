a67aa3f01807eaf1df4e5c8b6d61369d5c2ca059
김주원 <66457470+juwkim@users.noreply.github.com>
2024-04-14 04:44:18 +0900

    [Silver I] Title: 미로 탐색, Time: 88 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,25 +1,17 @@
 from collections import deque
-import sys
-input = sys.stdin.readline
-g = lambda: [*map(int, input().split())]
 
-N, M = g()
-Map = [input() for _ in range(N)]
-visited = [[0] * M for _ in range(N)]
-
-def bfs(x, y):
-    val = 1
-    dq = deque([(x, y, val)])
-    visited[x][y] = 1
-    while dq:
-        x, y, val = dq.popleft()
-        if x == N-1 and y == M-1:
-            print(val)
-            return
-        for Next in ((x+1, y), (x-1, y), (x, y-1), (x, y+1)):
-            x, y = Next
-            if x < 0 or x >= N or y < 0 or y >= M or Map[x][y] == '0' or visited[x][y]:
-                continue
-            visited[x][y] = 1
-            dq.append((x, y, val+1))
-bfs(0, 0)
+N, M = map(int, input().split())
+board = [input() for _ in range(N)]
+dq = deque([(0, 0, 1)])
+visited = [bytearray(M) for _ in range(N)]
+visited[0][0] = 1
+while dq:
+    x, y, val = dq.popleft()
+    if (x, y) == (N - 1, M - 1):
+        break
+    for nx, ny in ((x + 1, y), (x - 1, y), (x, y - 1), (x, y + 1)):
+        if nx < 0 or nx >= N or ny < 0 or ny >= M or board[nx][ny] == '0' or visited[nx][ny]:
+            continue
+        visited[nx][ny] = 1
+        dq.append((nx, ny, val + 1))
+print(val)
