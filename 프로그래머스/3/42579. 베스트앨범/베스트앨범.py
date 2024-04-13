fa1ad03cfeb024c395fa0bd79f4ae87e334a5895
김주원 <66457470+juwkim@users.noreply.github.com>
2024-04-13 15:50:41 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,14 +1,10 @@
 from collections import defaultdict
-
 def solution(genres, plays):
-    plays_by_genre = defaultdict(int)
-    songs_by_genre = defaultdict(list)
-    
+    plays_by_genre, songs_by_genre = defaultdict(int), defaultdict(list)
     for i, (genre, play) in enumerate(zip(genres, plays)):
         plays_by_genre[genre] += play
         songs_by_genre[genre].append((i, play))
     answer = []
     for genre in sorted(songs_by_genre, key=lambda x: -plays_by_genre[x]):
-        for i, p in sorted(songs_by_genre[genre], key=lambda x: (-x[1], x[0]))[:2]:
-            answer.append(i)
+        answer.extend([*zip(*sorted(songs_by_genre[genre], key=lambda x: (-x[1], x[0]))[:2])][0])
     return answer
