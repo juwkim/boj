62511ae54d6d147e05c93044c1cb34b8c245e487
김주원 <66457470+juwkim@users.noreply.github.com>
2024-04-14 05:09:08 +0900

    [Silver III] Title: 두 수의 합, Time: 92 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,19 +1,10 @@
-g = lambda: [*map(int, input().split())]
-
-N = int(input())
-nums = sorted(g())
+n = int(input())
+a = sorted(map(int, input().split()))
 x = int(input())
-ans = 0
-
-l, r = 0, N - 1
+ans, l, r = 0, 0, n - 1
 while l < r:
-    num = nums[l] + nums[r]
-    if num == x:
-        ans += 1
-        l += 1
-        r -= 1
-    elif num > x:
-        r -= 1
-    else:
-        l += 1
+    num = a[l] + a[r]
+    if num == x: ans += 1; l += 1; r -= 1
+    elif num > x: r -= 1
+    else: l += 1
 print(ans)
