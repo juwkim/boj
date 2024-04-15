f428d3e2fc3ae8f7bdf04b5b78cd9b03f85bc71f
김주원 <66457470+juwkim@users.noreply.github.com>
2024-04-15 23:19:32 +0900

    [Silver III] Title: Good Messages, Time: 132 ms, Memory: 112004 KB -BaekjoonHub

--- 백준/Silver/24815. Good Messages/Good Messages.py
+++ 백준/Silver/24815. Good Messages/Good Messages.py
@@ -1,12 +1,13 @@
-from collections import Counter
+from collections import deque
 
 O = int(input())
 l = len(msg:=input())
-S = Counter(ord(c) - ord('a') for c in msg)
+dq = deque([0] * 26)
+for c in msg: dq[ord(c) - ord('a')] += 1
 N = int(input())
 bad = 0
 for _ in range(N):
-    S = Counter({(k + O) % 26: v for k, v in S.items()})
-    vowel = sum(S[c] for c in (0, 4, 8, 14, 20, 24))
+    dq.rotate(O)
+    vowel = sum(dq[c] for c in (0, 4, 8, 14, 20, 24))
     bad += 3 * vowel >= l
 print(("Colleague", "Boris")[N > 2 * bad])
