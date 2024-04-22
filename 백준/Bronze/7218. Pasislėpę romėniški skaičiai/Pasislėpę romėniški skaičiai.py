15a7da428bba9fd46dfa0e81898cb5ed389bee2a
김주원 <66457470+juwkim@users.noreply.github.com>
2024-04-22 17:17:42 +0900

    [Bronze III] Title: Pasislėpę romėniški skaičiai, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/7218. Pasislėpę romėniški skaičiai/Pasislėpę romėniški skaičiai.py
+++ 백준/Bronze/7218. Pasislėpę romėniški skaičiai/Pasislėpę romėniški skaičiai.py
@@ -1,5 +1,3 @@
-input()
-S = input()
-l = "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII"
-ans = [i + 1 for i in range(12) if l[i] in S]
-print(*ans)
+_,S=open(0)
+l="I II III IV V VI VII VIII IX X XI XII".split()
+print(*[i+1 for i in range(12)if l[i]in S])
