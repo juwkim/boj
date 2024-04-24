3745878c259d0a06f9e0b4b274ec7246e4d52e89
김주원 <66457470+juwkim@users.noreply.github.com>
2024-04-24 13:23:12 +0900

    [Bronze II] Title: 기숙사 바닥, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2858. 기숙사 바닥/기숙사 바닥.py
+++ 백준/Bronze/2858. 기숙사 바닥/기숙사 바닥.py
@@ -1,3 +1,3 @@
-R, B = map(int, input().split())
-s, t = 1 + R / 4, ((R/4 - 1)**2 - B)**.5
-print(*map(round, [s+t, s-t]))
+R,B=map(int,input().split())
+s,t=1+R/4,((R/4-1)**2-B)**.5
+print(f"{s+t:.0f} {s-t:.0f}")
