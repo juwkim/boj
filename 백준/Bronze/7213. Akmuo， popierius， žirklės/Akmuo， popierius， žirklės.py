a9b414b7ebbe5479329e7beeb11de8f85f7af456
김주원 <66457470+juwkim@users.noreply.github.com>
2024-04-25 20:05:54 +0900

    [Bronze I] Title: Akmuo, popierius, žirklės, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/7213. Akmuo， popierius， žirklės/Akmuo， popierius， žirklės.py
+++ 백준/Bronze/7213. Akmuo， popierius， žirklės/Akmuo， popierius， žirklės.py
@@ -1,19 +1,17 @@
 a1, p1, z1, a2, p2, z2 = map(int, open(0).read().split())
 def solve(l, r, d):
     score = 0
-    while sum(l):
-        for i in range(3):
-            if l[i] == 0:
-                continue
-            l[i] -= 1
-            if r[(i - d) % 3]:
-                score += 1
-                r[(i - d) % 3] -= 1
-            elif r[i]:
-                r[i] -= 1
-            else:
-                r[(i + d) % 3] -= 1
-                score -= 1
+    for i in range(3):
+        cnt = min(l[i], r[(i - d) % 3])
+        l[i] -= cnt; r[(i - d) % 3] -= cnt
+        score += cnt
+        if l[i]:
+            cnt = min(l[i], r[i])
+            l[i] -= cnt; r[i] -= cnt
+        if l[i]:
+            cnt = min(l[i], r[(i + d) % 3])
+            l[i] -= cnt; r[(i + d) % 3] -= cnt
+            score -= cnt
     return score
 print(solve([a1, p1, z1], [a2, p2, z2], 1))
 print(-solve([a1, p1, z1], [a2, p2, z2], -1))
