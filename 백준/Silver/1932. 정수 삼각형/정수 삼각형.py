fda4e34912d4869eb2a9a6bc9e79bf2596b9b99e
김주원 <66457470+juwkim@users.noreply.github.com>
2024-04-29 00:39:40 +0900

    [Silver I] Title: 정수 삼각형, Time: 344 ms, Memory: 40728 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,13 +1,9 @@
 N = int(input())
 nums = [[*map(int, input().split())] for _ in range(N)]
-dp = [[0 for i in range(N)] for j in range(N)]
-
+dp = [[0] * N for _ in range(N)]
 def solve(i, j):
-    if dp[i][j]:
-        return dp[i][j]
-    if i == N - 1:
-        return nums[i][j]
-    ans = nums[i][j] + max(solve(i+1, j), solve(i+1, j+1))
-    dp[i][j] = ans
-    return ans
+    if dp[i][j]:   return dp[i][j]
+    if i == N - 1: return nums[i][j]
+    dp[i][j] = nums[i][j] + max(solve(i + 1, j), solve(i + 1, j + 1))
+    return dp[i][j]
 print(solve(0, 0))
