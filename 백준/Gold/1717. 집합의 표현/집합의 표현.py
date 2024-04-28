0712939ff3d6511f4ff59256cab97245fb6acf70
김주원 <66457470+juwkim@users.noreply.github.com>
2024-04-29 01:33:31 +0900

    [Gold V] Title: 집합의 표현, Time: 276 ms, Memory: 129036 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,7 +1,8 @@
 import sys
 input = sys.stdin.readline
+
 n, m = map(int, input().split())
-parent = list(range(n + 1))
+parent = [*range(n + 1)]
 rank = [0] * (n + 1)
 
 def find(x):
@@ -12,8 +13,7 @@ def find(x):
     return p
 
 def union(x, y):
-    x = find(x)
-    y = find(y)
+    x, y = find(x), find(y)
     if x == y:
         return
     if rank[x] < rank[y]:
@@ -23,12 +23,9 @@ def union(x, y):
         if rank[x] == rank[y]:
             rank[x] += 1
 
-for i in range(m):
+for _ in range(m):
     op, a, b = map(int, input().split())
-    if op == 1:
-        if find(a) == find(b):
-            print('YES')
-        else:
-            print('NO')
+    if op:
+        print(("NO", "YES")[find(a) == find(b)])
     else:
         union(a, b)
