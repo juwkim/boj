424e7fea12327c9a91706be51aad71e96eb1eb51
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-02 22:49:25 +0900

    [Silver III] Title: Oranżada, Time: 272 ms, Memory: 214876 KB -BaekjoonHub

--- 백준/Silver/26695. Oranżada/Oranżada.py
+++ 백준/Silver/26695. Oranżada/Oranżada.py
@@ -1,11 +1,9 @@
 n, k, *a = map(int, open(0).read().split())
-ans, front, bottle = 0, 0, set()
-for num in a:
-    if num in bottle:
-        front += 1
+ans, st, front = 0, 0, set()
+for n in a:
+    if n in front: st += 1
     else:
-        bottle.add(num)
-        ans += front
-        if len(bottle) == k:
-            break
-print(ans if len(bottle) == k else -1)
+        front.add(n)
+        ans += st
+        if len(front) == k: break
+print((-1, ans)[len(front) == k])
