35144186ba68dae880a4a433da64645ec0875f87
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-02 22:35:01 +0900

    [Silver V] Title: Karmon go, Time: 264 ms, Memory: 127856 KB -BaekjoonHub

--- 백준/Silver/26224. Karmon go/Karmon go.py
+++ 백준/Silver/26224. Karmon go/Karmon go.py
@@ -1,9 +1,6 @@
 from decimal import Decimal
 
-name0, inc0, name1, mul1, inc1 = input().split()
-inc0, inc1, mul1 = map(Decimal, (inc0, inc1, mul1))
-a = (7 + 3 * inc0) * mul1
-b = 7 * mul1 + 3 * inc1
-if a == b:  print("Whatever")
-elif a > b: print("Power up, Evolve")
-else:       print("Evolve, Power up")
+_, inc0, _, mul1, inc1 = input().split()
+a = int(inc0) * Decimal(mul1)
+b = int(inc1)
+print(("Whatever", "Power up, Evolve", "Evolve, Power up")[(a > b) - (a < b)])
