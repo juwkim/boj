1d9de11e09678b51a1f4a8382d829b836e8039ee
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-04 04:02:19 +0900

    [Silver III] Title: The Trip, 2007, Time: 180 ms, Memory: 119480 KB -BaekjoonHub

--- 백준/Silver/4267. The Trip， 2007/The Trip， 2007.py
+++ 백준/Silver/4267. The Trip， 2007/The Trip， 2007.py
@@ -1,11 +1,9 @@
 from collections import Counter
 
 buf = [*map(int, open(0).read().split())]
-i = 0
-while n:=buf[i]:
-    i += 1
+i = 1
+while n:=buf[i-1]:
     nums = sorted(buf[i:i+n])
     print(k := max(Counter(nums).values()))
-    for j in range(k):
-        print(*nums[j::k])
-    i += n
+    for j in range(k): print(*nums[j::k])
+    i += n + 1
