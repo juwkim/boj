8fba1b790df20c3c34f2ecfed232f2686943f9c7
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-05 07:05:52 +0900

    [Gold III] Title: 인증된 쉬운 게임, Time: 516 ms, Memory: 112168 KB -BaekjoonHub

--- 백준/Gold/22846. 인증된 쉬운 게임/인증된 쉬운 게임.py
+++ 백준/Gold/22846. 인증된 쉬운 게임/인증된 쉬운 게임.py
@@ -1,14 +1,14 @@
-def divisors(n):
-    div = []
-    for i in range(1, int(n**.5) + 1):
-        if n % i == 0:
-            div.append(i)
-            if i != n // i:
-                div.append(n // i)
-    return div
-
 K = int(input())
 dp = [0] * (K + 1)
 for i in range(K, 0, -1):
-    dp[i] = any(dp[i + d] == 0 for d in divisors(i) if i + d <= K)
-print(("Ringo", "Kali")[dp[1]])
+    divisors = []
+    for j in range(1, int(i**.5) + 1):
+        if i % j == 0:
+            divisors.append(j)
+            if j != i // j:
+                divisors.append(i // j)
+    dp[i] = any(dp[i + d] == 0 for d in divisors if i + d <= K)
+if dp[1]:
+    print("Kali")
+else:
+    print("Ringo")
