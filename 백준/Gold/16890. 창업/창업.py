d84101947ed3b5a67ff414c0cd50c399ae26c3c2
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-05 05:53:00 +0900

    [Gold I] Title: 창업, Time: 416 ms, Memory: 181264 KB -BaekjoonHub

---
+++ 백준/Gold/16890. 창업/창업.py
@@ -0,0 +1,18 @@
+a, b = input(), input()
+q, r = divmod(N:=len(a), 2)
+l = sorted(a, reverse=True)[-(q + r):], sorted(b)[-q:]
+ans1 = []
+turn = 0
+while turn < N:
+    if all(l) and l[0][-1] >= l[1][-1]:
+        break
+    ans1.append(l[turn&1].pop())
+    turn += 1
+l[0].reverse()
+l[1].reverse()
+ans2 = []
+while turn < N:
+    ans2.append(l[turn&1].pop())
+    turn += 1
+# print(ans1, ans2[::-1])
+print(*ans1, *reversed(ans2), sep='')
