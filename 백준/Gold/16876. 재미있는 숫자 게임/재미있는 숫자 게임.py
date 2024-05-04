b1d74de9e640612e7c91c9c30a0a4ec032770d1f
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-05 02:46:55 +0900

    [Gold II] Title: 재미있는 숫자 게임, Time: 280 ms, Memory: 125268 KB -BaekjoonHub

---
+++ 백준/Gold/16876. 재미있는 숫자 게임/재미있는 숫자 게임.py
@@ -0,0 +1,26 @@
+N, M = input().split()
+d = [*map(int, N)]
+N, M = map(int, (N, M))
+dp = [[-1] * (M + 2) for _ in range(10000)]
+
+def num():
+    return d[0] * 1000 + d[1] * 100 + d[2] * 10 + d[3]
+
+def solve(m):
+    n = num()
+    if dp[n][m] != -1:
+        return dp[n][m]
+    if m > M:
+        dp[n][m] = n > N
+        return dp[n][m]
+    turn = m & 1
+    for i in range(4):
+        d[i] = (d[i] + 1) % 10
+        if solve(m + 1) == turn:
+            dp[n][m] = turn
+            d[i] = (d[i] - 1) % 10
+            return dp[n][m]
+        d[i] = (d[i] - 1) % 10
+    dp[n][m] = turn ^ 1
+    return dp[n][m]    
+print(("cubelover", "koosaga")[solve(1)])
