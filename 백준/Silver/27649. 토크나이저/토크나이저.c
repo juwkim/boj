ba3d3b7561eaf3d249f95ba668d9fcc17bed0d85
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-06 03:05:40 +0900

    [Silver III] Title: 토크나이저, Time: 8 ms, Memory: 3920 KB -BaekjoonHub

--- 백준/Silver/27649. 토크나이저/토크나이저.c
+++ 백준/Silver/27649. 토크나이저/토크나이저.c
@@ -3,29 +3,27 @@
 
 int main() {
 	char res[2000000], buf[1000000 + 1];
-	int  size = read(0, buf, 1000000);
-	int  idx = 0, cur = 0;
+	int  idx = 0, cur = 0, size = read(0, buf, 1000000);
 	while (idx < size) {
 		switch (buf[idx]) {
-			case ' ':
-				++idx;
-				break;
-			case '<': case '>': case '(': case ')':
+		case ' ':
+			++idx;
+			break;
+		case '<': case '>': case '(': case ')':
+			res[cur++] = buf[idx++];
+	        res[cur++] = ' ';
+			break;
+		case '&': case '|':
+			res[cur++] = buf[idx++];
+			res[cur++] = buf[idx++];
+			res[cur++] = ' ';
+			break;
+		default:
+			while (strchr("<>()&| ", buf[idx]) == NULL)
 				res[cur++] = buf[idx++];
-	            res[cur++] = ' ';
-				break;
-			case '&': case '|':
-				res[cur++] = buf[idx++];
-				res[cur++] = buf[idx++];
-				res[cur++] = ' ';
-				break;
-			default:
-				while (strchr("<>()&| ", buf[idx]) == NULL)
-					res[cur++] = buf[idx++];
-				res[cur++] = ' ';
+			res[cur++] = ' ';
 		}
 	}
 	res[--cur] = '\0';
     write(1, res, cur);
-	return 0;
 }
