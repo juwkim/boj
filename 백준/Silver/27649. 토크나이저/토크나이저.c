fe0faf92ad35ad0963e7dce2682eabf9d9647b11
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-06 03:16:19 +0900

    [Silver III] Title: 토크나이저, Time: 8 ms, Memory: 3920 KB -BaekjoonHub

--- 백준/Silver/27649. 토크나이저/토크나이저.c
+++ 백준/Silver/27649. 토크나이저/토크나이저.c
@@ -1,16 +1,15 @@
 #include <unistd.h>
 #include <string.h>
 #define f() r[c++] = b[i++];
-
-int main() {
+main() {
 	char r[2000000], b[1000001];
-	int  i = 0, c = 0, s = read(0, b, 1000000);
+	int i = 0, c = 0, s = read(0, b, 1000000);
 	while (i < s) {
 		switch (b[i]) {
 		case ' ': ++i; continue;
 		case '<': case '>': case '(': case ')': f() break;
 		case '&': case '|': f() f() break;
-		default: while (strchr("<>()&| ", b[i]) == NULL) f()
+		default: while (!strchr("<>()&| ", b[i])) f()
 		}
         r[c++] = ' ';
 	}
