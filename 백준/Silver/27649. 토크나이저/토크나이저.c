d38834fa329fbec0b7b62b1a9ffde4a11fd600ba
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-06 03:18:37 +0900

    [Silver III] Title: 토크나이저, Time: 8 ms, Memory: 3920 KB -BaekjoonHub

--- 백준/Silver/27649. 토크나이저/토크나이저.c
+++ 백준/Silver/27649. 토크나이저/토크나이저.c
@@ -1,18 +1,18 @@
 #include <unistd.h>
 #include <string.h>
-#define f() r[c++] = b[i++];
-main() {
-	char r[2000000], b[1000001];
-	int i = 0, c = 0, s = read(0, b, 1000000);
-	while (i < s) {
-		switch (b[i]) {
-		case ' ': ++i; continue;
-		case '<': case '>': case '(': case ')': f() break;
-		case '&': case '|': f() f() break;
-		default: while (!strchr("<>()&| ", b[i])) f()
+#define f()r[c++]=b[i++];
+int main(){
+	char r[2000000],b[1000001];
+	int i=0,c=0,s=read(0,b,1000000);
+	while(i<s){
+		switch(b[i]){
+		case' ':++i;continue;
+		case'<':case'>':case'(':case')':f()break;
+		case'&':case'|':f()f()break;
+		default:while(!strchr("<>()&| ",b[i]))f()
 		}
-        r[c++] = ' ';
+        r[c++]=' ';
 	}
-	r[--c] = '\0';
-    write(1, r, c);
+	r[--c]='\0';
+    write(1,r,c);
 }
