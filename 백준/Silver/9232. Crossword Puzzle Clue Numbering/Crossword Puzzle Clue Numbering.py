0db43668bbca43c5109c97bdd7483cd1a96954e5
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-05 16:03:15 +0900

    [Silver III] Title: Crossword Puzzle Clue Numbering, Time: 120 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/9232. Crossword Puzzle Clue Numbering/Crossword Puzzle Clue Numbering.py
+++ 백준/Silver/9232. Crossword Puzzle Clue Numbering/Crossword Puzzle Clue Numbering.py
@@ -1,42 +1,27 @@
-import sys
-input = lambda: sys.stdin.readline().rstrip()
-
-cwnum = 1
+tc = 1
 while True:
     r, c = map(int, input().split())
-    if r == c == 0:
-        break
+    if r == 0: break
     crossword = [input() for _ in range(r)]
     i = 1
-    across, down = [], []
+    across, down = [f'Crossword puzzle {tc}', "Across"], ["Down"]
     for x in range(r):
         for y in range(c):
             plus = False
             if (y == 0 or crossword[x][y-1] == '@') and crossword[x][y] != '@':
                 l = 1
-                while y + l < c and crossword[x][y+l] != '@':
-                    l += 1
+                while y + l < c and crossword[x][y+l] != '@': l += 1
                 if l >= 3:
-                    if i < 10:
-                        across.append(f"{i}.  ({l})")
-                    else:
-                        across.append(f"{i}. ({l})")
+                    if i < 10: across.append(f"{i}.  ({l})")
+                    else:      across.append(f"{i}. ({l})")
                     plus = True
             if (x == 0 or crossword[x-1][y] == '@') and crossword[x][y] != '@':
                 l = 1
-                while x + l < r and crossword[x+l][y] != '@':
-                    l += 1
+                while x + l < r and crossword[x+l][y] != '@': l += 1
                 if l >= 3:
-                    if i < 10:
-                        down.append(f"{i}.  ({l})")
-                    else:
-                        down.append(f"{i}. ({l})")
+                    if i < 10: down.append(f"{i}.  ({l})")
+                    else:      down.append(f"{i}. ({l})")
                     plus = True
             i += plus
-    print(f'Crossword puzzle {cwnum}')
-    print('Across')
-    for a in across: print(a)
-    print('Down')
-    for d in down: print(d)
-    print()
-    cwnum += 1
+    print(*across, *down, sep='\n', end='\n\n')
+    tc += 1
