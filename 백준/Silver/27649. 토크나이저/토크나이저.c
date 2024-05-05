c7f746d0a1b6540c3aa172591d3c6521bfdefc83
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-06 03:02:23 +0900

    [Silver III] Title: 토크나이저, Time: 8 ms, Memory: 3920 KB -BaekjoonHub

--- 백준/Silver/27649. 토크나이저/토크나이저.c
+++ 백준/Silver/27649. 토크나이저/토크나이저.c
@@ -1,35 +1,24 @@
 #include <unistd.h>
+#include <string.h>
 
-int main()
-{
-	char	res[2000000];
-	char	buf[1000000 + 1];
-	int		size = read(0, buf, 1000000);
-
-	if (size == 0)
-		return (0);
-	int		idx = 0;
-	int		cur = 0;
-	while (idx < size)
-	{
-		switch (buf[idx])
-		{
+int main() {
+	char res[2000000], buf[1000000 + 1];
+	int  size = read(0, buf, 1000000);
+	int  idx = 0, cur = 0;
+	while (idx < size) {
+		switch (buf[idx]) {
 			case ' ':
 				++idx;
-				break ;
-			case '<':
-			case '>':
-			case '(':
-			case ')':
+				break;
+			case '<': case '>': case '(': case ')':
 				res[cur++] = buf[idx++];
-				res[cur++] = ' ';
-				break ;
-			case '&':
-			case '|':
+	            res[cur++] = ' ';
+				break;
+			case '&': case '|':
 				res[cur++] = buf[idx++];
 				res[cur++] = buf[idx++];
 				res[cur++] = ' ';
-				break ;
+				break;
 			default:
 				while (strchr("<>()&| ", buf[idx]) == NULL)
 					res[cur++] = buf[idx++];
@@ -38,5 +27,5 @@ int main()
 	}
 	res[--cur] = '\0';
     write(1, res, cur);
-	return (0);
+	return 0;
 }
