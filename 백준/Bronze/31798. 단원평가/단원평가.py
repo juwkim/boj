7afe264806115d8ffa65388dd5ef5963d7fad3c6
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-07 18:39:15 +0900

    [Bronze III] Title: 단원평가, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/31798. 단원평가/단원평가.py
+++ 백준/Bronze/31798. 단원평가/단원평가.py
@@ -1,7 +1,3 @@
-from math import isqrt
-
-a, b, c = map(int, input().split())
-if c:
-    print(c * c - a - b)
-else:
-    print(isqrt(a + b))
+import math
+a,b,c=map(int,input().split())
+print((c*c-a-b,math.isqrt(a+b))[c==0])
