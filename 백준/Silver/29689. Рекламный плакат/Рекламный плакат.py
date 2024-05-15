a85858edbd87ea55d0f15f8fb4904d86c209efe6
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-15 13:20:03 +0900

    [Silver IV] Title: Рекламный плакат, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/29689. Рекламный плакат/Рекламный плакат.py
+++ 백준/Silver/29689. Рекламный плакат/Рекламный плакат.py
@@ -1,7 +1,6 @@
-n = int(input())
-a = input()
+n, a = int(input()), len(input())
 ans = 1
 for _ in range(n - 1):
-    ans = max(ans, min(len(a), len(b:=input())))
+    ans = max(ans, min(a, b:=len(input())))
     a = b
 print(ans)
