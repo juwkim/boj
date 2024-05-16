705469ff84add77b47e25fd13b2af53fdba7d5f8
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-16 12:23:09 +0900

    [Bronze IV] Title: Верное выражение, Time: 128 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Bronze/29649. Верное выражение/Верное выражение.py
@@ -0,0 +1,7 @@
+a, op, b, eq, c = input().split()
+ans = []
+for i in range(max(2, int(max(a + b + c)) + 1), 11):
+    p, q, r = int(a, i), int(b, i), int(c, i)
+    if eval(f"{p}{op}{q}=={r}"):
+        ans.append(i)
+print(len(ans), *ans)
