78b28b0f0751d1fbf5ec536bc47671d836087856
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-17 18:47:14 +0900

    [Silver I] Title: 쿼드트리, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.py
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -1,19 +1,10 @@
-def solve(N, x, y):
-    if N == 1:
-        return text[x][y]
-    N //= 2
-    lt = solve(N, x, y)
-    rt = solve(N, x, y+N)
-    lb = solve(N, x+N, y)
-    rb = solve(N, x+N, y+N)
-    res = [lt, rt, lb, rb]
-    if all(ans == '0' for ans in res):
-        return '0'
-    elif all(ans == '1' for ans in res):
-        return '1'
-    else:
-        return '(' + "".join(res) + ")"
-
+def solve(size, x, y):
+    if size == 0: return arr[x][y]
+    res = "".join(solve(size // 2, p, q) for p, q in ((x, y), (x, y + size), (x + size, y), (x + size, y + size)))
+    match res:
+        case '0000': return '0'
+        case '1111': return '1'
+        case _:      return f"({res})"
 N = int(input())
-text = [input() for _ in range(N)]
-print(solve(N, 0, 0))
+arr = [input() for _ in range(N)]
+print(solve(N // 2, 0, 0))
