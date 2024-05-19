0c8f117e5c7d1bfea5c0acb8984603d91c084f12
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-19 23:45:47 +0900

    [Silver IV] Title: Cowculations, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/6486. Cowculations/Cowculations.py
+++ 백준/Silver/6486. Cowculations/Cowculations.py
@@ -1,13 +1,9 @@
-import sys
-input = lambda: sys.stdin.readline().rstrip()
-
 d = {
     'V': {'V': 'V', 'U': 'U', 'C': 'C', 'D': 'D'},
     'U': {'V': 'U', 'U': 'C', 'C': 'D', 'D': 'VU'},
     'C': {'V': 'C', 'U': 'D', 'C': 'VU', 'D': 'UU'},
     'D': {'V': 'D', 'U': 'VU', 'C': 'UU', 'D': 'CU'}
 }
-
 for _ in range(int(input())):
     num1, num2 = [*input()[::-1]], [*input()[::-1]]
     for _ in range(3):
@@ -16,26 +12,16 @@ for _ in range(int(input())):
                 c = ""
                 for i in range(len(num1)):
                     if c:
-                        tmp = d[c][num1[i]]
-                        if len(tmp) == 2:
-                            tmp, c = tmp
-                        else:
-                            c = ""
-                    else:
-                        tmp = num1[i]
-                    tmp = d[tmp][num2[i]]
-                    if len(tmp) == 2:
-                        tmp, c = tmp
-                    num2[i] = tmp
+                        t = d[c][num1[i]]
+                        if len(t) == 2: t, c = t
+                        else: c = ""
+                    else: t = num1[i]
+                    t = d[t][num2[i]]
+                    if len(t) == 2: t, c = t
+                    num2[i] = t
                 if c:
                     num2.append(c)
-            case 'R':
-                num2 = num2[1:] + ['V']
-            case 'L':
-                num2 = ['V'] + num2
-    ans = input()
+            case 'R': num2 = num2[1:] + ['V']
+            case 'L': num2 = ['V'] + num2
     my = "V" * (8 - len(num2)) + ''.join(num2[::-1])
-    if ans == my:
-        print("YES")
-    else:
-        print("NO")
+    print("YES" if input() == my else "NO")
