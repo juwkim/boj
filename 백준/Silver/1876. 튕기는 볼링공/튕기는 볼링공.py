49fe86fa0a430ea9fb4fee1bdd65bd5f7ee8111b
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-21 20:18:46 +0900

    [Silver II] Title: 튕기는 볼링공, Time: 48 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/1876. 튕기는 볼링공/튕기는 볼링공.py
+++ 백준/Silver/1876. 튕기는 볼링공/튕기는 볼링공.py
@@ -1,11 +1,9 @@
-from math import radians, tan, sin, cos
+from math import sin, tan, radians
 
 for _ in range(int(input())):
     T, X = input().split()
-    X = radians(int(X))
-    O = 0.85 / tan(X)
-    T = float(T) % O
-    if abs(T * sin(X) - 0.85 * (2 * T > O) * cos(X)) <= 0.16:
-        print("yes")
-    else:
-        print("no")
+    T, X = float(T), radians(int(X))
+    one_step = 0.85 / tan(X)
+    T %= one_step
+    d = min(T, one_step - T) * sin(X)
+    print('yes' if d <= 0.16 else 'no')
