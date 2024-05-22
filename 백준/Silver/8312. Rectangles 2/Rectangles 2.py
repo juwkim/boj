e20ec012d14a4fea7ce0824abd1e6ce9a6ba2232
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-23 01:29:43 +0900

    [Silver III] Title: Rectangles 2, Time: 108 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/8312. Rectangles 2/Rectangles 2.py
+++ 백준/Silver/8312. Rectangles 2/Rectangles 2.py
@@ -1,7 +1,8 @@
 n, m, p = map(int, input().split())
 ans = 0
-for w in range(1, n + 1):
+for w in range(n, 0, -1):
     h = max(1, (p + 1) // 2 - w)
-    if h <= m:
-        ans += (n + 1 - w) * (m + 1 - h) * (m + 2 - h) // 2
+    if h > m:
+        break
+    ans += (n + 1 - w) * (m + 1 - h) * (m + 2 - h) // 2
 print(ans)
