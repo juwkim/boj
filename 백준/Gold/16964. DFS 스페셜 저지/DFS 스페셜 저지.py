f0c150ac66dbc5f5fb69a08222d807959c237ea2
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-28 00:54:50 +0900

    [Gold III] Title: DFS 스페셜 저지, Time: 504 ms, Memory: 162292 KB -BaekjoonHub

--- 백준/Gold/16964. DFS 스페셜 저지/DFS 스페셜 저지.py
+++ 백준/Gold/16964. DFS 스페셜 저지/DFS 스페셜 저지.py
@@ -1,30 +1,26 @@
 import sys
 input = sys.stdin.readline
+g = lambda: [*map(int, input().split())]
 
-N = int(input())
-graph = [set() for _ in range(N + 1)]
-for _ in range(N - 1):
-    a, b = map(int, input().split())
-    graph[a].add(b)
-    graph[b].add(a)
-
-path = [*map(int, input().split())]
-if path[0] != 1:
-    print(0)
-    exit(0)
-
-i = 0
-visited = bytearray(N + 1)
 def solve():
-    global i
-    if i == N - 1:
-        print(1)
-        exit()
-    v = path[i]
-    i += 1
-    visited[v] = 1
-    for _ in range(len(graph[v])):
-        if path[i] in graph[v] and not visited[path[i]]:
-            solve()
-solve()
-print(0)
+    N = int(input())
+    graph = [set() for _ in range(N + 1)]
+    for _ in range(N - 1):
+        a, b = g()
+        graph[a].add(b)
+        graph[b].add(a)
+    path = g()
+    if path[0] != 1: return 0
+    i = 0
+    visited = bytearray(N + 1)
+    def dfs():
+        nonlocal i
+        if i == N - 1: return 1
+        visited[v:=path[i]] = 1
+        i += 1
+        for _ in range(len(graph[v])):
+            if path[i] in graph[v] and not visited[path[i]]:
+                if dfs(): return 1
+        return 0
+    return dfs()
+print(solve())
