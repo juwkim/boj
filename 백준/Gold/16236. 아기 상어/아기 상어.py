1575d54cedc4efb429ae250e862b1d239683cda2
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-27 22:37:38 +0900

    [Gold III] Title: 아기 상어, Time: 156 ms, Memory: 34132 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,50 +1,43 @@
-g = lambda: [*map(int, input().split())]
-
-
 from collections import deque
-N = int(input())
-Map = []
-x, y = 0, 0
-for _ in range(N):
-    l = g()
-    Map.append(l)
-    if 9 in l:
-        x, y = _, l.index(9)
-        Map[x][y] = 0
 
-size, eat, ans = 2, 0, 0
-dq = deque([(x, y, ans)])
+N = int(input())
+board = []
+x, y = 0, 0
+for i in range(N):
+    board.append(l:=[*map(int, input().split())])
+    if 9 in l:
+        x, y = i, l.index(9)
+        board[x][y] = 0
+
+s, ate, cnt = 2, 0, 0
+dq = deque([(x, y, cnt)])
 visited = [bytearray(N) for _ in range(N)]
-pts = None, None
+pts = -1, -1
 while True:
     while dq:
         x, y, t = dq.popleft()
         visited[x][y] = 1
-        for a, b in ((x-1, y), (x, y-1), (x, y+1), (x+1, y)):
-            if 0 <= a < N and 0 <= b < N and visited[a][b] == 0 and Map[a][b] <= size:
+        for a, b in ((x - 1, y), (x, y - 1), (x, y + 1), (x + 1, y)):
+            if 0 <= a < N and 0 <= b < N and not visited[a][b] and board[a][b] <= s:
                 visited[a][b] = 1
-                if 0 < Map[a][b] < size:
-                    if pts == (None, None):
-                        pts = (a, b)
-                        ans = t+1
-                    elif t+1 == ans:
-                        if (a, b) < pts:
-                            pts = (a, b)
+                if 0 < board[a][b] < s:
+                    if pts == (-1, -1):
+                        pts, cnt = (a, b), t + 1
+                    elif cnt == t + 1:
+                        if (a, b) < pts: pts = (a, b)
                     else:
                         break
                 else:
-                    dq.append((a, b, t+1))
+                    dq.append((a, b, t + 1))
 
-    if pts[0] == None:
-        break
+    if pts == (-1, -1): break
     a, b = pts
-    pts = (None, None)
-    dq = deque([(a, b, ans)])
+    dq = deque([(a, b, cnt)])
     visited = [bytearray(N) for _ in range(N)]
-    
-    Map[a][b] = 0
-    eat += 1
-    if eat == size:
-        size += 1
-        eat = 0
-print(ans)
+    board[a][b] = 0
+    ate += 1
+    if ate == s:
+        s += 1
+        ate = 0
+    pts = -1, -1
+print(cnt)
