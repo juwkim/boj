e77cc8b0bb128f1611e4530e0a8f4519e2453a45
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-27 22:16:39 +0900

    [Gold III] Title: 감시, Time: 316 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.py
+++ 백준/Gold/15683. 감시/감시.py
@@ -1,27 +1,20 @@
-import sys
+N, M = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
 
-input = lambda: sys.stdin.readline().rstrip()
-g = lambda: [*map(int, input().split())]
-
-N, M = g()
-board = [g() for _ in range(N)]
-def fill(x, y, d):
+def watch(x, y, d):
     dx = [-1, 0, 1, 0][d]
     dy = [0, 1, 0, -1][d]
-    x += dx
-    y += dy
-    filled = []
+    x, y = x + dx, y + dy
+    watched = []
     while 0 <= x < N and 0 <= y < M and board[x][y] != 6:
         if board[x][y] != '#':
             board[x][y] = '#'
-            filled.append((x, y))
-        x += dx
-        y += dy
-    return filled
+            watched.append((x, y))
+        x, y = x + dx, y + dy
+    return watched
 
-def unfilled(filled):
-    for x, y in filled:
-        board[x][y] = 0
+def unwatched(watched):
+    for x, y in watched: board[x][y] = 0
 
 cctv = []
 five = []
@@ -31,40 +24,39 @@ for i in range(N):
             continue
         if 1 <= board[i][j] <= 4:
             cctv.append((i, j, board[i][j]))
-        else: # board[i][j] == 5
+        else:
             five.append((i, j))
         board[i][j] = '#'
 
 for x, y in five:
     for d in range(4):
-        fill(x, y, d)
+        watch(x, y, d)
 
-def dfs(idx):
-    if idx == len(cctv):
-        return sum(l.count(0) for l in board)
-    x, y, c = cctv[idx]
-    idx += 1
-    ans = float('inf')
-    if c == 1:
-        for d in range(4):
-            filled = fill(x, y, d)
-            ans = min(ans, dfs(idx))
-            unfilled(filled)
-    elif c == 2:
-        for d in range(2):
-            filled = fill(x, y, d) + fill(x, y, d + 2)
-            ans = min(ans, dfs(idx))
-            unfilled(filled)
-    elif c == 3:
-        for d in range(4):
-            filled = fill(x, y, d) + fill(x, y, (d + 1) % 4)
-            ans = min(ans, dfs(idx))
-            unfilled(filled)
-    elif c == 4:
-        for d in range(4):
-            filled = fill(x, y, d) + fill(x, y, (d + 1) % 4) + fill(x, y, (d + 2) % 4)
-            ans = min(ans, dfs(idx))
-            unfilled(filled)
-    return ans
-
-print(dfs(0))
+def solve(i):
+    if i == len(cctv): return sum(l.count(0) for l in board)
+    x, y, c = cctv[i]
+    i += 1
+    cnt = 1e9
+    match c:
+        case 1:
+            for d in range(4):
+                watched = watch(x, y, d)
+                cnt = min(cnt, solve(i))
+                unwatched(watched)
+        case 2:
+            for d in range(2):
+                watched = watch(x, y, d) + watch(x, y, d + 2)
+                cnt = min(cnt, solve(i))
+                unwatched(watched)
+        case 3:
+            for d in range(4):
+                watched = watch(x, y, d) + watch(x, y, (d + 1) % 4)
+                cnt = min(cnt, solve(i))
+                unwatched(watched)
+        case 4:
+            for d in range(4):
+                watched = watch(x, y, d) + watch(x, y, (d + 1) % 4) + watch(x, y, (d + 2) % 4)
+                cnt = min(cnt, solve(i))
+                unwatched(watched)
+    return cnt
+print(solve(0))
