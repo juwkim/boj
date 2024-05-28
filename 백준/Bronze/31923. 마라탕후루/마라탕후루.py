4ed73676dd7f6de1e6492228a3f302b91496ebe6
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-28 21:18:28 +0900

    [Bronze II] Title: 마라탕후루, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/31923. 마라탕후루/마라탕후루.py
+++ 백준/Bronze/31923. 마라탕후루/마라탕후루.py
@@ -1,19 +1,17 @@
 g = lambda: map(int, input().split())
 N, P, Q = g()
-ans = "YES"
+f = 1
 l = []
 for a, b in zip(g(), g()):
-    if a == b:
-        l.append(0)
+    if a == b: l.append(0)
     elif (P - Q) * (a - b) >= 0:
-        ans = "NO"
+        f = 0
         break
     else:
         q, r = divmod(abs(a - b), abs(Q - P))
         if r or q > 10000:
-            ans = "NO"
+            f = 0
             break
         l.append(q)
-print(ans)
-if ans == "YES":
-    print(*l)
+print(("NO", "YES")[f])
+if f: print(*l)
