b00a68ee155e9e62db504e4fac6365e5fdf5fca9
김주원 <66457470+juwkim@users.noreply.github.com>
2024-05-31 19:00:31 +0900

    [Bronze I] Title: Pliiatsite pööramine, Time: 104 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/29824. Pliiatsite pööramine/Pliiatsite pööramine.py
+++ 백준/Bronze/29824. Pliiatsite pööramine/Pliiatsite pööramine.py
@@ -1,6 +1,4 @@
-n = int(input())
-s = input()
-idx = [i for i in range(n - 1) if s[i] != s[i + 1]] + [n - 1]
-print(len(idx) // 2)
-for i in range(0, len(idx) - 1, 2):
-    print(f"{idx[i] + 2}-{idx[i + 1] + 1}")
+n=int(input())
+s=input()
+print(len(b:=[i for i in range(n-1)if s[i]!=s[i+1]]+[n-1])//2)
+for i in range(0,len(b)-1,2):print(f"{b[i]+2}-{b[i+1]+1}")
