583866b5e9c68e7f0cc89c8cb09e380a623578df
김주원 <66457470+juwkim@users.noreply.github.com>
2024-06-07 22:28:07 +0900

    [Silver III] Title: Demerit Points, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/4389. Demerit Points/Demerit Points.py
+++ 백준/Silver/4389. Demerit Points/Demerit Points.py
@@ -1,11 +1,15 @@
-import sys
-input = lambda: sys.stdin.readline().rstrip()
+def pr(t, p):
+    y, m, d = t // 10000, t // 100 % 100, t % 100
+    if p == 0:
+        print(f"{y}-{m:02d}-{d:02d} No merit or demerit points.")
+    elif p < 0:
+        print(f"{y}-{m:02d}-{d:02d} {-p} demerit point(s).")
+    else:
+        print(f"{y}-{m:02d}-{d:02d} {p} merit point(s).")
 
-p = 0
 t, *l = open(0).readlines()
-t = int(t)
-y, m, d = t // 10000, t // 100 % 100, t % 100
-print(f"{y}-{m:02d}-{d:02d} No merit or demerit points.")
+p, t = 0, int(t)
+pr(t, p)
 for s in l:
     cur, pp = map(int, s.split())
     while True:
@@ -17,27 +21,12 @@ for s in l:
             p = max((p + 1) // 2, min(0, p + 2))
         else:
             break
-        y, m, d = t // 10000, t // 100 % 100, t % 100
-        if p == 0:
-            print(f"{y}-{m:02d}-{d:02d} No merit or demerit points.")
-        elif p < 0:
-            print(f"{y}-{m:02d}-{d:02d} {-p} demerit point(s).")
-        else:
-            print(f"{y}-{m:02d}-{d:02d} {p} merit point(s).")
+        pr(t, p)
     t = cur
-    y, m, d = t // 10000, t // 100 % 100, t % 100
-    if p <= 0:
-        p -= pp
-    elif pp >= 2 * p:
-        p = 2 * p - pp
-    else:
-        p -= (pp + 1) // 2
-    if p == 0:
-        print(f"{y}-{m:02d}-{d:02d} No merit or demerit points.")
-    elif p < 0:
-        print(f"{y}-{m:02d}-{d:02d} {-p} demerit point(s).")
-    else:
-        print(f"{y}-{m:02d}-{d:02d} {p} merit point(s).")
+    if p <= 0: p -= pp
+    elif pp >= 2 * p: p = 2 * p - pp
+    else: p -= (pp + 1) // 2
+    pr(t, p)
 while p != 5:
     if 0 <= p < 5:
         t += 20000
@@ -45,10 +34,4 @@ while p != 5:
     else:
         t += 10000
         p = max((p + 1) // 2, min(0, p + 2))
-    y, m, d = t // 10000, t // 100 % 100, t % 100
-    if p == 0:
-        print(f"{y}-{m:02d}-{d:02d} No merit or demerit points.")
-    elif p < 0:
-        print(f"{y}-{m:02d}-{d:02d} {-p} demerit point(s).")
-    else:
-        print(f"{y}-{m:02d}-{d:02d} {p} merit point(s).")
+    pr(t, p)
