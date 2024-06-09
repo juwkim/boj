d3959a9638b08b586de17961cd3f61bf0a6fef28
김주원 <66457470+juwkim@users.noreply.github.com>
2024-06-10 03:09:05 +0900

    [Silver III] Title: Электронные часы, Time: 156 ms, Memory: 111608 KB -BaekjoonHub

--- 백준/Silver/29597. Электронные часы/Электронные часы.py
+++ 백준/Silver/29597. Электронные часы/Электронные часы.py
@@ -3,17 +3,9 @@ input = lambda: sys.stdin.readline().rstrip()
 
 n, m = map(int, input().split())
 a = [[input() for _ in range(n)] for _ in range(10)]
-num = [[0] * 10 for _ in range(10)]
-for i in range(9):
-    for j in range(i + 1, 10):
-        cnt = sum(a[i][k][l] != a[j][k][l] for k in range(n) for l in range(m))
-        num[i][j] = cnt
-        num[j][i] = cnt
-ans = num[0][2] + num[0][3] + num[0][5] + num[0][9]
-for i in range(24 * 60 - 1):
-    h1, m1 = divmod(i, 60)
-    h2, m2 = divmod(i + 1, 60)
-    (h11, h12), (m11, m12) = divmod(h1, 10), divmod(m1, 10)
-    (h21, h22), (m21, m22) = divmod(h2, 10), divmod(m2, 10)
-    ans += num[h11][h21] + num[h12][h22] + num[m11][m21] + num[m12][m22]
+dic = {(0, 2): 1, (0, 3): 1, (0, 5): 24, (0, 9): 146, (0, 0): 992, (0, 1): 172, (1, 2): 172, (2, 3): 171, (3, 4): 170, (4, 5): 170, (5, 6): 146, (6, 7): 146, (7, 8): 146, (8, 9): 146, (1, 1): 992, (2, 2): 632, (3, 3): 393, (4, 4): 334, (5, 5): 334, (6, 6): 118, (7, 7): 118, (8, 8): 118, (9, 9): 118}
+ans = 0
+for (i, j), v in dic.items():
+    cnt = sum(a[i][k][l] != a[j][k][l] for k in range(n) for l in range(m))
+    ans += cnt * v
 print(ans)
