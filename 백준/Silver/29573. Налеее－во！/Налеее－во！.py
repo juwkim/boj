bd7bf6bda07991b1d13c7e9131aca746291a08b3
김주원 <66457470+juwkim@users.noreply.github.com>
2024-06-19 19:43:09 +0900

    [Silver III] Title: Налеее-во!, Time: 104 ms, Memory: 110992 KB -BaekjoonHub

--- 백준/Silver/29573. Налеее－во！/Налеее－во！.py
+++ 백준/Silver/29573. Налеее－во！/Налеее－во！.py
@@ -21,17 +21,14 @@ for _ in range(K):
         case 'A': d = (d + 2) % 4
         case 'L': d = (d + 3) % 4
         case 'F':
-            match d:
-                case 0: x += 1
-                case 1: y -= 1
-                case 2: x -= 1
-                case 3: y += 1
+            x += (1, 0, -1, 0)[d]
+            y += (0, -1, 0, 1)[d]
 ans = [['.'] * M for _ in range(N)]
-ans[x][y] = ('-', '|', '-', '|')[d]
+ans[x][y] = '-|-|'[d]
 match d:
-    case 0: ans[x][y - 1] = '/'; ans[x][y + 1] = '\\'; assert 0 < y < M - 1
-    case 1: ans[x - 1][y] = '\\'; ans[x + 1][y] = '/'; assert 0 < x < N - 1
-    case 2: ans[x][y - 1] = '\\'; ans[x][y + 1] = '/'; assert 0 < y < M - 1
-    case 3: ans[x - 1][y] = '/'; ans[x + 1][y] = '\\'; assert 0 < x < N - 1
+    case 0: ans[x][y - 1] = '/'; ans[x][y + 1] = '\\'
+    case 1: ans[x - 1][y] = '\\'; ans[x + 1][y] = '/'
+    case 2: ans[x][y - 1] = '\\'; ans[x][y + 1] = '/'
+    case 3: ans[x - 1][y] = '/'; ans[x + 1][y] = '\\'
 for l in ans:
     print(*l, sep='')
