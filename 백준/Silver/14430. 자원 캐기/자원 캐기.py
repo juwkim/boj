5d8b47cb7876723d6baef361e366b23e56bf2666
김주원 <66457470+juwkim@users.noreply.github.com>
2024-06-23 18:58:32 +0900

    [Silver II] Title: 자원 캐기, Time: 128 ms, Memory: 113024 KB -BaekjoonHub

---
+++ 백준/Silver/14430. 자원 캐기/자원 캐기.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+a = [input().split() for _ in range(N)]
+dp = [[0] * (M + 1) for _ in range(N + 1)]
+for i in range(1, N + 1):
+    for j in range(1, M + 1):
+        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + (a[i - 1][j - 1] == '1')
+print(dp[N][M])
