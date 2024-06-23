557161809759a7d44d836d4a8dd86498adfa294d
김주원 <66457470+juwkim@users.noreply.github.com>
2024-06-23 22:30:34 +0900

    [Silver II] Title: 보이는 점의 개수, Time: 124 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/2725. 보이는 점의 개수/보이는 점의 개수.py
+++ 백준/Silver/2725. 보이는 점의 개수/보이는 점의 개수.py
@@ -1,8 +1,4 @@
-from math import gcd
-
-a = [0, 3]
-for i in range(2, 1001):
-    a.append(a[i - 1] + 2 * sum(gcd(i, j) == 1 for j in range(1, i)))
-
-for _ in range(int(input())):
-    print(a[int(input())])
+import math
+a=[0,3]
+for i in range(2,1001):a.append(a[i-1]+2*sum(1==math.gcd(i,j)for j in range(1,i)))
+for l in[*open(0)][1:]:print(a[int(l)])
