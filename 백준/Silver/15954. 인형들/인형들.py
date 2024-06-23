3e8e9ef9836bc5b5c408524e07fd232cdb1ffb47
김주원 <66457470+juwkim@users.noreply.github.com>
2024-06-24 05:56:15 +0900

    [Silver I] Title: 인형들, Time: 124 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/15954. 인형들/인형들.py
+++ 백준/Silver/15954. 인형들/인형들.py
@@ -1,17 +1,15 @@
-from math import inf
-
 N, K, *A = map(int, open(0).read().split())
 
 def pstdev(i, j):
     n = j - i + 1
-    return ((EX2[j + 1] - EX2[i]) * n - ((EX[j + 1] - EX[i])) ** 2) **.5 / n
+    return ((EX2[j + 1] - EX2[i]) * n - (EX[j + 1] - EX[i]) ** 2) **.5 / n
 
 EX2, EX = [0], [0]
 for a in A:
     EX2.append(EX2[-1] + a ** 2)
     EX.append(EX[-1] + a)
 
-ans = inf
+ans = 1e9
 for i in range(N - K + 1):
     for j in range(i + K - 1, N):
         ans = min(ans, pstdev(i, j))
