f46dfd084227e5b9d04664b78b529af002cc8ebf
김주원 <66457470+juwkim@users.noreply.github.com>
2024-07-11 05:34:11 +0900

    [Silver I] Title: 접두사 찾기, Time: 560 ms, Memory: 117216 KB -BaekjoonHub

---
+++ 백준/Silver/14426. 접두사 찾기/접두사 찾기.py
@@ -0,0 +1,41 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        current_node = self.root
+        for char in word:
+            if char not in current_node.children:
+                current_node.children[char] = TrieNode()
+            current_node = current_node.children[char]
+        current_node.is_end_of_word = True
+
+    def search(self, word):
+        current_node = self.root
+        for char in word:
+            if char not in current_node.children:
+                return False
+            current_node = current_node.children[char]
+        return current_node.is_end_of_word
+
+    def starts_with(self, prefix):
+        current_node = self.root
+        for char in prefix:
+            if char not in current_node.children:
+                return False
+            current_node = current_node.children[char]
+        return True
+
+N, M = map(int, input().split())
+trie = Trie()
+for _ in range(N):
+    trie.insert(input())
+print(sum(trie.starts_with(input()) for _ in range(M)))
