584fc3931ee281d999747424c5b019dda27d1988
김주원 <66457470+juwkim@users.noreply.github.com>
2024-07-11 01:43:05 +0900

    [Silver III] Title: Favourite Times, Time: 96 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/14684. Favourite Times/Favourite Times.py
+++ 백준/Silver/14684. Favourite Times/Favourite Times.py
@@ -4,16 +4,14 @@ h, m = 12, 1
 for _ in range(min(D, 719)):
     if h < 10:
         a, b, c = h, m // 10, m % 10
-        cnt.append(cnt[-1] + (a - b == b - c))
+        p = a - b == b - c
     else:
         a, b, c, d = h // 10, h % 10, m // 10, m % 10
-        cnt.append(cnt[-1] + (a - b == b - c == c - d))
+        p = a - b == b - c == c - d
+    cnt.append(cnt[-1] + p)
     m += 1
     if m == 60:
         m = 0
-        if h == 12:
-            h = 1
-        else:
-            h += 1
+        h = (h + 1) % 12
 q, r = divmod(D, 720)
 print(q * cnt[-1] + cnt[r])
