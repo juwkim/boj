f4325a11af6dda23d7ace1a69a935504c0858520
김주원 <66457470+juwkim@users.noreply.github.com>
2024-07-27 16:35:29 +0900

    [Silver V] Title: Побег, Time: 12 ms, Memory: 13684 KB -BaekjoonHub

--- 백준/Silver/29211. Побег/Побег.py
+++ 백준/Silver/29211. Побег/Побег.py
@@ -2,9 +2,9 @@ k = int(input())
 b = int(input())
 for i in range(2, k + 1):
     x, y = map(int, input().split())
-    if b > 1 and (i + b - 1) % 2 != (x + y) % 2:
-        print(i, b - 1, flush=True)
-    elif (i + b) % 2 != (x + y) % 2:
-        print(i, b, flush=True)
-    else:
-        print(i, b + 1, flush=True)
+    j = b + 1
+    if b > 1 and i + b - 1 + x + y & 1:
+        j = b - 1
+    elif i + b + x + y & 1:
+        j = b
+    print(i, j, flush=True)
