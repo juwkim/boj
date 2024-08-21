2692e59fb15e44e68475a07276b4b61cc72b5762
김주원 <66457470+juwkim@users.noreply.github.com>
2024-08-22 00:51:25 +0900

    [Bronze II] Title: Ксероксинатор, Time: 108 ms, Memory: 119600 KB -BaekjoonHub

--- 백준/Bronze/28656. Ксероксинатор/Ксероксинатор.py
+++ 백준/Bronze/28656. Ксероксинатор/Ксероксинатор.py
@@ -4,10 +4,8 @@ g = lambda: map(int, sys.stdin.readline().split())
 n, b = map(int, input().split())
 ans, Sum = 0, 0
 for i, num in enumerate(g()):
-    ans -= i * num
-    Sum += num
-    val = min(Sum, b)
-    ans += (i + 1) * val
-    Sum -= val
+    val = min(Sum + num, b)
+    ans += i * (val - num) + val
+    Sum -= val - num
 ans += (n + 1) * Sum
 print(ans)
