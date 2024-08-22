d8f4b226695a30e41fdbc88dbb82ed61b4886241
김주원 <66457470+juwkim@users.noreply.github.com>
2024-08-23 04:07:12 +0900

    [Silver III] Title: Palindromic Password, Time: 108 ms, Memory: 110424 KB -BaekjoonHub

--- 백준/Silver/15276. Palindromic Password/Palindromic Password.py
+++ 백준/Silver/15276. Palindromic Password/Palindromic Password.py
@@ -5,7 +5,5 @@ nums = [int(f"{i}{j}{k}{k}{j}{i}") for i in range(1, 10) for j in range(10) for 
 for _ in range(int(input())):
     ans = 1e9
     s = int(input())
-    for num in nums:
-        if abs(s - num) < abs(s - ans) or (abs(s - num) == abs(s - ans) and num < ans):
-            ans = num
+    ans = min(nums, key=lambda x: (abs(s - x), x))
     print(ans)
