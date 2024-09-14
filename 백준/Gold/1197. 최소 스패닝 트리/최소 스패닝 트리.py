0ec6ff3adfeaee4ff60ae86ec7fab7a4f304b999
김주원 <66457470+juwkim@users.noreply.github.com>
2024-09-14 15:32:42 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 328 ms, Memory: 128080 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,30 +1,38 @@
-import sys
-sys.setrecursionlimit(10**6)
-
 class DisjointSet:
     def __init__(self, nodes):
         self.parent = nodes
+        self.rank = [0] * len(nodes)  # Initialize rank array
     
     def find(self, x):
         if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
+            self.parent[x] = self.find(self.parent[x])  # Path compression
         return self.parent[x]
 
     def union(self, x, y):
         x_root = self.find(x)
         y_root = self.find(y)
+        
         if x_root != y_root:
-            self.parent[x_root] = y_root
+            # Union by rank
+            if self.rank[x_root] > self.rank[y_root]:
+                self.parent[y_root] = x_root
+            elif self.rank[x_root] < self.rank[y_root]:
+                self.parent[x_root] = y_root
+            else:
+                self.parent[y_root] = x_root
+                self.rank[x_root] += 1  # Increase rank if both have the same rank
 
 def kruskal(graph):
-    edges = sorted(graph['edges'], key=lambda x: x[2])
+    edges = sorted(graph['edges'], key=lambda x: x[2])  # Sort edges by weight
     mst = []
     ds = DisjointSet(graph['nodes'])
+    
     for edge in edges:
         u, v, weight = edge
         if ds.find(u) != ds.find(v):
             mst.append(edge)
             ds.union(u, v)
+    
     return mst
 
 g = lambda: [*map(int, input().split())]
