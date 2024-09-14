ce98a1d77fc42804125237eba9238ff6b7580438
김주원 <66457470+juwkim@users.noreply.github.com>
2024-09-14 21:49:46 +0900

    [Bronze I] Title: Конфетки, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/29429. Конфетки/Конфетки.py
+++ 백준/Bronze/29429. Конфетки/Конфетки.py
@@ -1,12 +1,11 @@
 a, b = input(), input()
 if len(a) < len(b) or a == b:
-    print("Bad luck")
+    ans = "Bad luck"
 elif len(a) > len(b) or a > b:
-    print(0)
+    ans = 0
 else:
     for i in range(len(b)):
-        if a[i] > b[i]:
-            break
         if a[i] < b[i]:
-            print(f"1\n{i + 1}")
+            ans = f"1\n{i + 1}"
             break
+print(ans)
