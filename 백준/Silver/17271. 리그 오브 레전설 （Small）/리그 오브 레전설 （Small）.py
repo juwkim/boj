984291dd61411d1f12f7474f21dd6c48a221c6e9
김주원 <66457470+juwkim@users.noreply.github.com>
2024-09-15 05:05:48 +0900

    [Silver II] Title: 리그 오브 레전설 (Small), Time: 88 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/17271. 리그 오브 레전설 （Small）/리그 오브 레전설 （Small）.py
+++ 백준/Silver/17271. 리그 오브 레전설 （Small）/리그 오브 레전설 （Small）.py
@@ -1,7 +1,5 @@
 N, M = map(int, input().split())
-dp = [1] + [0] * N
-for i in range(N):
-    dp[i + 1] = (dp[i + 1] + dp[i]) % 1000000007
-    if i + M <= N:
-        dp[i + M] = (dp[i + M] + dp[i]) % 1000000007
-print(dp[N] % 1000000007)
+dp = [1] * M + [0] * (N + 1 - M)
+for i in range(M, N + 1):
+    dp[i] = (dp[i - M] + dp[i - 1]) % 1000000007
+print(dp[N])
