4f1aca4153b5f1babd85fec3811c2c0aa93aba82
김주원 <66457470+juwkim@users.noreply.github.com>
2024-09-14 20:43:35 +0900

    [Silver II] Title: 배열 돌리기, Time: 352 ms, Memory: 124064 KB -BaekjoonHub

--- 백준/Silver/17276. 배열 돌리기/배열 돌리기.py
+++ 백준/Silver/17276. 배열 돌리기/배열 돌리기.py
@@ -2,18 +2,15 @@ import sys
 input = sys.stdin.readline
 g = lambda: [*map(int, input().split())]
 
-dx = -1, -1, -1, 0, 1, 1, 1, 0
-dy = -1, 0, 1, 1, 1, 0, -1, -1
-jump = [*zip(dx, dy)]
+jump = [*zip((-1, -1, -1, 0, 1, 1, 1, 0), (-1, 0, 1, 1, 1, 0, -1, -1))]
 for _ in range(int(input())):
     n, d = g()
-    t = n >> 1
-    d = (d % 360) // 45
+    t, d = n >> 1, d // 45 % 8
     a = [g() for _ in range(n)]
     for i in range(1, t + 1):
         idx = [(t + i * p, t + i * q) for p, q in jump]
         nums = [a[nx][ny] for nx, ny in idx]
         for j, (nx, ny) in enumerate(idx):
-            a[nx][ny] = nums[(j - d) % 8]
+            a[nx][ny] = nums[j - d]
     for r in a:
         print(*r)
