4bdea50f58d2e71015ebe0c0a54a5f2005dc82c9
김주원 <66457470+juwkim@users.noreply.github.com>
2024-09-15 15:40:10 +0900

    [Silver I] Title: 1, 2, 3 더하기 9, Time: 660 ms, Memory: 229356 KB -BaekjoonHub

--- 백준/Silver/16195. 1， 2， 3 더하기 9/1， 2， 3 더하기 9.py
+++ 백준/Silver/16195. 1， 2， 3 더하기 9/1， 2， 3 더하기 9.py
@@ -1,22 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-mod = 1_000_000_009
 mem = {}
-for _ in range(int(input())):
-    n, m = map(int, input().split())
-    assert n >= 0 and m >= 0 and m <= n
-    def solve(a, b):
-        if (a, b) in mem:
-            return mem[(a, b)]
-        if a < 0:
-            return 0
-        if a == 0:
-            return 1
-        if a > 3 * b:
-            return 0
-        if b == 1:
-            return +(a <= 3)
-        mem[(a, b)] = (solve(a - 1, b - 1) + solve(a - 2, b - 1) + solve(a - 3, b - 1)) % mod
+def solve(a, b):
+    if (a, b) in mem:
         return mem[(a, b)]
-    print(solve(n, m))
+    if a < 0:
+        return 0
+    if a == 0:
+        return 1
+    if a > 3 * b:
+        return 0
+    if b == 1:
+        return +(a <= 3)
+    mem[(a, b)] = (solve(a - 1, b - 1) + solve(a - 2, b - 1) + solve(a - 3, b - 1)) % 1_000_000_009
+    return mem[(a, b)]
+for _ in range(int(input())):
+    print(solve(*map(int, input().split())))
