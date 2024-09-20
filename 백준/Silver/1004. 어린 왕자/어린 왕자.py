5ce61e797270f05fc0179f6b87424f738786a8a0
김주원 <66457470+juwkim@users.noreply.github.com>
2024-09-20 23:43:34 +0900

    [Silver III] Title: 어린 왕자, Time: 148 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/1004. 어린 왕자/어린 왕자.py
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.py
@@ -1,22 +1,15 @@
 import math
 
-def dist(x1, y1, x2, y2):
-    return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
-
-T = int(input())
-
-for _ in range(T):
-    points = list(map(int, input().split()))
-    n = int(input())
+for _ in range(int(input())):
+    points = [*map(int, input().split())]
     total = 0
-    for i in range(n):
+    for i in range(int(input())):
         check = 0
-        planet = list(map(int, input().split()))
-        if dist(points[0], points[1], planet[0], planet[1]) > planet[2]:
+        planet = [*map(int, input().split())]
+        if math.dist((points[0], points[1]), (planet[0], planet[1])) > planet[2]:
             check += 1
-        if dist(points[2], points[3], planet[0], planet[1]) > planet[2]:
+        if math.dist((points[2], points[3]), (planet[0], planet[1])) > planet[2]:
             check += 1
         if check == 1:
             total += 1
     print(total)
-            
