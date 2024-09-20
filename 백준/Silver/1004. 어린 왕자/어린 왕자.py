df161861375e204ec9dd0e169ccda8d7211c250a
김주원 <66457470+juwkim@users.noreply.github.com>
2024-09-20 23:44:55 +0900

    [Silver III] Title: 어린 왕자, Time: 144 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/1004. 어린 왕자/어린 왕자.py
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.py
@@ -2,14 +2,8 @@ import math
 
 for _ in range(int(input())):
     points = [*map(int, input().split())]
-    total = 0
+    ans = 0
     for i in range(int(input())):
-        check = 0
         planet = [*map(int, input().split())]
-        if math.dist((points[0], points[1]), (planet[0], planet[1])) > planet[2]:
-            check += 1
-        if math.dist((points[2], points[3]), (planet[0], planet[1])) > planet[2]:
-            check += 1
-        if check == 1:
-            total += 1
-    print(total)
+        ans += (math.dist((points[0], points[1]), (planet[0], planet[1])) > planet[2]) ^ (math.dist((points[2], points[3]), (planet[0], planet[1])) > planet[2])
+    print(ans)
