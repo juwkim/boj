4bdd13077c02b8a11a2e3f8ff5dc45277dcec44a
김주원 <66457470+juwkim@users.noreply.github.com>
2024-09-23 06:39:43 +0900

    [Bronze V] Title: 선택 가능성이 가장 높은 자료형, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/32278. 선택 가능성이 가장 높은 자료형/선택 가능성이 가장 높은 자료형.py
+++ 백준/Bronze/32278. 선택 가능성이 가장 높은 자료형/선택 가능성이 가장 높은 자료형.py
@@ -1,7 +1,5 @@
 N = int(input())
-if -1 << 15 <= N < 1 << 15:
-    print("short")
-elif -1 << 31 <= N < 1 << 31:
-    print("int")
-else:
-    print("long long")
+ans = "long long"
+if -1 << 15 <= N < 1 << 15: ans = "short"
+elif -1 << 31 <= N < 1 << 31: ans = "int"
+print(ans)
