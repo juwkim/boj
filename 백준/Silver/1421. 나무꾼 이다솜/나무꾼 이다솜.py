259d7bcae75f5dbced003f21dbbc68332cf69dac
김주원 <66457470+juwkim@users.noreply.github.com>
2024-09-24 08:09:11 +0900

    [Silver I] Title: 나무꾼 이다솜, Time: 124 ms, Memory: 111396 KB -BaekjoonHub

--- 백준/Silver/1421. 나무꾼 이다솜/나무꾼 이다솜.py
+++ 백준/Silver/1421. 나무꾼 이다솜/나무꾼 이다솜.py
@@ -1,2 +1,2 @@
-N, C, W, *L = map(int, open(0).read().split())
-print(max(sum(max(0, (tree - tree % i) * W - (tree // i - (tree % i == 0)) * C) for tree in L) for i in range(1, max(L) + 1)))
+N,C,W,*L=map(int,open(0).read().split())
+print(max(sum(max(0,t//i*i*W-(t//i-(t%i==0))*C)for t in L)for i in range(1,10001)))
