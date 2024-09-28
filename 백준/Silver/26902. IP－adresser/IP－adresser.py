4201197368fd6475827166a04c4f397f1fe973e2
김주원 <66457470+juwkim@users.noreply.github.com>
2024-09-28 16:22:58 +0900

    [Silver III] Title: IP-adresser, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/26902. IP－adresser/IP－adresser.py
+++ 백준/Silver/26902. IP－adresser/IP－adresser.py
@@ -1,7 +1,4 @@
 from itertools import combinations
 
 s = input()
-ans = 0
-for i, j, k in combinations(range(1, len(s)), 3):
-    ans += all(int(x) < 256 and str(int(x)) == x for x in (s[:i], s[i:j], s[j:k], s[k:]))
-print(ans)
+print(sum(all(int(x) < 256 and str(int(x)) == x for x in (s[:i], s[i:j], s[j:k], s[k:])) for i, j, k in combinations(range(1, len(s)), 3)))
