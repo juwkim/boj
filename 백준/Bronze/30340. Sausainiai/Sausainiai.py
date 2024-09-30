adef30413133b84cf7b50cc7cd4b804f9b27efa2
김주원 <66457470+juwkim@users.noreply.github.com>
2024-09-30 15:47:05 +0900

    [Bronze I] Title: Sausainiai, Time: 844 ms, Memory: 164536 KB, Score: 21 point -BaekjoonHub

--- 백준/Bronze/30340. Sausainiai/Sausainiai.py
+++ 백준/Bronze/30340. Sausainiai/Sausainiai.py
@@ -2,11 +2,35 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-nums = sorted([[*map(int, input().split())] for _ in range(N)], key=lambda x: (-abs(x[0] - x[1]), -x[0]))
-ans = 0
+nums = [[*map(int, input().split())] for _ in range(N)]
+a = sorted(range(N), key=lambda x: nums[x], reverse=True)
+b = sorted(range(N), key=lambda x: (nums[x][1], nums[x][0]), reverse=True)
+ans, l, r = 0, 0, 0
 for i in range(N):
     if i & 1:
-        ans -= nums[i][1]
+        while l < N and nums[a[l]][0] == -1:
+            l += 1
+        while r < N and nums[b[r]][0] == -1:
+            r += 1
+        if nums[a[l]][0] - nums[a[l]][1] > nums[b[r]][1] - nums[b[r]][1]:
+            ans -= nums[a[l]][1]
+            nums[a[l]] = (-1, -1)
+            l += 1
+        else:
+            ans -= nums[b[r]][1]
+            nums[b[r]] = (-1, -1)
+            r += 1
     else:
-        ans += nums[i][0]
+        while l < N and nums[a[l]][0] == -1:
+            l += 1
+        while r < N and nums[b[r]][0] == -1:
+            r += 1
+        if nums[a[l]][0] - nums[a[l]][1] < nums[b[r]][1] - nums[b[r]][1]:
+            ans += nums[b[r]][0]
+            nums[b[r]] = (-1, -1)
+            r += 1
+        else:
+            ans += nums[a[l]][0]
+            nums[a[l]] = (-1, -1)
+            l += 1
 print(ans)
