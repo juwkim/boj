b27f4574a99a4f82c0956e72af75e159a532c7d8
김주원 <66457470+juwkim@users.noreply.github.com>
2024-10-01 21:14:11 +0900

    [Silver IV] Title: 마법의 나침반, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/32372. 마법의 나침반/마법의 나침반.py
+++ 백준/Silver/32372. 마법의 나침반/마법의 나침반.py
@@ -7,17 +7,11 @@ ymin, ymax = 1, N
 for _ in range(M):
     X, Y, K = g()
     match K:
-        case 1 | 2 | 8:
-            xmax = min(xmax, X - 1)
-        case 4 | 5 | 6:
-            xmin = max(xmin, X + 1)
-        case _:
-            xmin = xmax = X
+        case 1 | 2 | 8: xmax = min(xmax, X - 1)
+        case 4 | 5 | 6: xmin = max(xmin, X + 1)
+        case _: xmin = X
     match K:
-        case 6 | 7 | 8:
-            ymax = min(ymax, Y - 1)
-        case 2 | 3 | 4:
-            ymin = max(ymin, Y + 1)
-        case _:
-            ymin = ymax = Y
+        case 6 | 7 | 8: ymax = min(ymax, Y - 1)
+        case 2 | 3 | 4: ymin = max(ymin, Y + 1)
+        case _: ymin = Y
 print(xmin, ymin)
