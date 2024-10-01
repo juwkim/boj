1b75c2bbba73129c9b2302374e1926bd6bdd959e
김주원 <66457470+juwkim@users.noreply.github.com>
2024-10-01 21:28:05 +0900

    [Silver IV] Title: Стать сильнее, Time: 164 ms, Memory: 144008 KB -BaekjoonHub

--- 백준/Silver/28615. Стать сильнее/Стать сильнее.py
+++ 백준/Silver/28615. Стать сильнее/Стать сильнее.py
@@ -1,8 +1,9 @@
 n, *a = map(int, open(0).read().split())
 a.sort()
-cur, ans = 0, 1
-for i in range(n):
+i, cur, ans = 0, 0, 1
+while cur != n:
     while cur < n and a[cur] <= a[i] + 1:
         cur += 1
     ans = max(ans, cur - i)
+    i += 1
 print(ans)
