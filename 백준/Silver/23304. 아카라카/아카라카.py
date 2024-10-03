d2bc8a857a864be0f9e052becc4d2cd34e9ff1dc
김주원 <66457470+juwkim@users.noreply.github.com>
2024-10-03 20:43:35 +0900

    [Silver II] Title: 아카라카, Time: 508 ms, Memory: 118016 KB -BaekjoonHub

--- 백준/Silver/23304. 아카라카/아카라카.py
+++ 백준/Silver/23304. 아카라카/아카라카.py
@@ -1,10 +1,4 @@
 def solve(l, r):
-    len = r - l + 1
-    if len == 1:
-        return True
-    if any(s[l + i] != s[r - i] for i in range(len // 2)):
-        return False
-    return solve(l, l + len // 2 - 1) and solve(r - len // 2 + 1, r)
-
+    return r == l or all(s[l + i] == s[r - i] for i in range((r - l + 1) // 2)) and solve(l, (r + l - 1) // 2) and solve((r + l + 2) // 2, r)
 s = input()
 print(("IPSELENTI", "AKARAKA")[solve(0, len(s) - 1)])
