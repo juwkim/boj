e77ce2d28dea5524b090619e60e87c90a57be6f9
김주원 <66457470+juwkim@users.noreply.github.com>
2024-10-03 20:08:32 +0900

    [Bronze I] Title: Kitten of Chaos, Time: 192 ms, Memory: 148548 KB -BaekjoonHub

--- 백준/Bronze/32487. Kitten of Chaos/Kitten of Chaos.py
+++ 백준/Bronze/32487. Kitten of Chaos/Kitten of Chaos.py
@@ -2,21 +2,18 @@ s = input()
 h, v = 0, 0
 for c in input():
     match c:
-        case 'h':
-            h ^= 1
-        case 'v':
-            v ^= 1
-        case _:
-            h ^= 1
-            v ^= 1
+        case 'h': h ^= 1
+        case 'v': v ^= 1
+        case _: h ^= 1; v ^= 1
 if h and v:
-    d = {k: v for k, v in zip("bdpq", "qpdb")}
-    s = s[::-1]
+    t = "qpdb"
+    s = reversed(s)
 elif h:
-    d = {k: v for k, v in zip("bdpq", "dbqp")}
-    s = s[::-1]
+    t = "dbqp"
+    s = reversed(s)
 elif v:
-    d = {k: v for k, v in zip("bdpq", "pqbd")}
+    t = "pqbd"
 else:
-    d = {k: v for k, v in zip("bdpq", "bdpq")}
+    t = "bdpq"
+d = {k: v for k, v in zip("bdpq", t)}
 print(*[d[c] for c in s], sep='')
