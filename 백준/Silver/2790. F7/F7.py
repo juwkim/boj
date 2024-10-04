5b1ab33eba7f21bab723d6434e197955428acd2f
김주원 <66457470+juwkim@users.noreply.github.com>
2024-10-04 19:55:20 +0900

    [Silver II] Title: F7, Time: 220 ms, Memory: 138312 KB -BaekjoonHub

--- 백준/Silver/2790. F7/F7.py
+++ 백준/Silver/2790. F7/F7.py
@@ -1,4 +1,4 @@
 N, *B = map(int, open(0))
-B.sort(reverse=True)
-m = max(B[i] + i + 1 for i in range(N))
-print(sum(num + N >= m for num in B))
+B.sort()
+m = max(B[i] - i for i in range(N))
+print(sum(a >= m for a in B))
