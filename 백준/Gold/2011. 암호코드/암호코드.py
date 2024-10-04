2ac63cb18b7cf13a1c5e9e298c25ca70641ca945
김주원 <66457470+juwkim@users.noreply.github.com>
2024-10-05 07:40:17 +0900

    [Gold V] Title: 암호코드, Time: 96 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Gold/2011. 암호코드/암호코드.py
@@ -0,0 +1,12 @@
+s = input()
+dp = [0] * (len(s) + 1)
+dp[len(s)] = 1
+for i in range(len(s) - 1, -1, -1):
+    if s[i] == '0':
+        dp[i] = 0
+    else:
+        dp[i] = dp[i + 1]
+        if i + 1 < len(s) and int(s[i:i + 2]) <= 26:
+            dp[i] += dp[i + 2]
+    dp[i] %= 1000000
+print(dp[0])
