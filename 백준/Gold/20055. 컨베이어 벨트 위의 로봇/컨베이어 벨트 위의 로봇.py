17930844562f6640f58da9ec6d26bf47b5063185
김주원 <66457470+juwkim@users.noreply.github.com>
2024-10-09 03:20:24 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 376 ms, Memory: 115396 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -13,12 +13,9 @@ while zero < K:
         zero += A[s] == 0
     else:
         belt[0] = 0
-    for i in range(N - 1, 0, -1):
-        if not belt[i]:
-            continue
-        if i == N - 1:
-            belt[i] = 0
-        elif not belt[i + 1] and A[idx := ((s + i + 1) % (2 * N))]:
+    belt[-1] = 0
+    for i in range(N - 2, 0, -1):
+        if belt[i] and not belt[i + 1] and A[idx := ((s + i + 1) % (2 * N))]:
             belt[i], belt[i + 1] = 0, 1
             A[idx] -= 1
             zero += A[idx] == 0
