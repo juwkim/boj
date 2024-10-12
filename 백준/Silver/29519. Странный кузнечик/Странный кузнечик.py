d6c6b001f3c054deb5e6acbe024e0773d93fd45b
김주원 <66457470+juwkim@users.noreply.github.com>
2024-10-12 19:21:15 +0900

    [Silver V] Title: Странный кузнечик, Time: 92 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/29519. Странный кузнечик/Странный кузнечик.py
@@ -0,0 +1,6 @@
+N = int(input())
+nums = [*map(int, input().split())]
+if N & N - 1 == 0 and (all(nums[2**i - 1 if 2**i - 1 < len(nums) else 0] == 1 for i in range(1, 16))):
+    print("Yes")
+else:
+    print("No")
