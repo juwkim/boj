625f260b053e05071827cb0d99d093da03061a27
김주원 <66457470+juwkim@users.noreply.github.com>
2024-10-13 08:17:13 +0900

    [Gold IV] Title: 이모티콘, Time: 176 ms, Memory: 136828 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.py
+++ 백준/Gold/14226. 이모티콘/이모티콘.py
@@ -1,6 +1,5 @@
 from collections import deque
 
-ans = 0
 S = int(input())
 dq = deque([(1, 0, 0)])
 visited = {(1, 0, 0)}
@@ -9,12 +8,7 @@ while dq:
     if cur == S:
         print(cnt)
         break
-    if (cur, cur, cnt + 1) not in visited:
-        visited.add((cur, cur, cnt + 1))
-        dq.append((cur, cur, cnt + 1))
-    if (cur + clip, clip, cnt + 1) not in visited:
-        visited.add((cur + clip, clip, cnt + 1))
-        dq.append((cur + clip, clip, cnt + 1))
-    if cur - 1 >= 0 and (cur - 1, clip, cnt + 1) not in visited:
-        visited.add((cur - 1, clip, cnt + 1))
-        dq.append((cur - 1, clip, cnt + 1))
+    for l in (cur, cur, cnt + 1), (cur + clip, clip, cnt + 1), (cur - 1, clip, cnt + 1):
+        if l not in visited and l[0]:
+            visited.add(l)
+            dq.append(l)
