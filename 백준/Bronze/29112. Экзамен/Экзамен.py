a13fb24f81e9c7eb3c89060820daf10217818c91
김주원 <66457470+juwkim@users.noreply.github.com>
2024-10-19 00:54:16 +0900

    [Bronze I] Title: Экзамен, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/29112. Экзамен/Экзамен.py
+++ 백준/Bronze/29112. Экзамен/Экзамен.py
@@ -1,8 +1,3 @@
 l, r = map(int, input().split())
-
-def solve(num):
-    if num < 0:
-        return 0
-    return int((num // 36000) ** (1/6) + 1e-12) + 1
-
-print(solve(r) - solve(l - 1))
+s = lambda x: 0 if x < 0 else int((x // 36000) ** (1/6) + 1e-12) + 1 
+print(s(r) - s(l - 1))
