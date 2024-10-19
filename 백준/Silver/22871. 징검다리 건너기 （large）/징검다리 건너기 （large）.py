c269e046c58b3e30f55bf3708156a7a6ef985d8c
김주원 <66457470+juwkim@users.noreply.github.com>
2024-10-20 01:26:56 +0900

    [Silver I] Title: 징검다리 건너기 (large), Time: 316 ms, Memory: 111384 KB -BaekjoonHub

--- 백준/Silver/22871. 징검다리 건너기 （large）/징검다리 건너기 （large）.py
+++ 백준/Silver/22871. 징검다리 건너기 （large）/징검다리 건너기 （large）.py
@@ -2,6 +2,5 @@ N, *A = map(int, open(0).read().split())
 dp = [0] + [1e9] * (N - 1)
 for i in range(N - 1):
     for j in range(i + 1, N):
-        if dp[j] > dp[i]:
-            dp[j] = min(dp[j], max(dp[i], (j - i) * (1 + abs(A[j] - A[i]))))
+        dp[j] = min(dp[j], max(dp[i], (j - i) * (1 + abs(A[j] - A[i]))))
 print(dp[-1])
