b1113936104351ed73940b73c418678fa855ba90
김주원 <66457470+juwkim@users.noreply.github.com>
2024-10-27 16:32:47 +0900

    [Silver II] Title: 지뢰, Time: 116 ms, Memory: 118124 KB -BaekjoonHub

--- 백준/Silver/2232. 지뢰/지뢰.py
+++ 백준/Silver/2232. 지뢰/지뢰.py
@@ -1,6 +1,3 @@
 N, *nums = map(int, open(0))
-nums = [0] + nums + [0]
-
-for i in range(1, N + 1):
-    if nums[i] >= max(nums[i - 1], nums[i + 1]):
-        print(i)
+nums.append(0)
+print(*[i + 1 for i in range(N) if nums[i] >= max(nums[i - 1], nums[i + 1])])
