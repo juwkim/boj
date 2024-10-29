6e42188d724ef0303a40511a3e73139aeb3daa7f
김주원 <66457470+juwkim@users.noreply.github.com>
2024-10-30 01:26:01 +0900

    [Silver III] Title: Knitting Pattern, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/32559. Knitting Pattern/Knitting Pattern.py
+++ 백준/Silver/32559. Knitting Pattern/Knitting Pattern.py
@@ -1,7 +1,3 @@
-N, P = map(int, input().split())
-r = (N - P) // 2 % P
-if P == 2 * r:
-    ans = 0
-else:
-    ans = 2 * r
-print(ans)
+N,P=map(int,input().split())
+r=(N-P)//2%P
+print(2*r*(N%P!=0))
