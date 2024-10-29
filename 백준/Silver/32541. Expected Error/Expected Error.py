fa0e9bbace8fd830ad036b245fc137beb3fadb35
김주원 <66457470+juwkim@users.noreply.github.com>
2024-10-29 21:57:12 +0900

    [Silver V] Title: Expected Error, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/32541. Expected Error/Expected Error.py
+++ 백준/Silver/32541. Expected Error/Expected Error.py
@@ -1,11 +1,3 @@
 n, k, p = map(int, input().split())
-Continue = (n - k) + 1 + (3 + n + 1) * p / 100
-Backspace = 1 + (n - k) + 1 + (3 + n + 1) * (100 - p) / 100
-Restart = 3 + n + 1
-ans = min(Continue, Backspace, Restart)
-if ans == Continue:
-    print("continue")
-elif ans == Backspace:
-    print("backspace")
-else:
-    print("restart")
+nums = (n - k + 1) * 100 + (n + 4) * p, (2 * n - k + 6) * 100 - (n + 4) * p, (n + 4) * 100
+print(("continue", "backspace", "restart")[min(range(3), key=lambda x: nums[x])])
