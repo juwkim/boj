1454f76002608ba6101352ea2b75a4e1bcbca5ef
김주원 <66457470+juwkim@users.noreply.github.com>
2024-10-31 17:41:54 +0900

    [Silver III] Title: Avoiding the Abyss, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/32549. Avoiding the Abyss/Avoiding the Abyss.py
+++ 백준/Silver/32549. Avoiding the Abyss/Avoiding the Abyss.py
@@ -1,26 +1,7 @@
 g = lambda: map(int, input().split())
-xs, ys = g()
-xt, yt = g()
-xp, yp = g()
-ans = []
-if xs < xp:
-    P = 10001
-    ans.append((-P, ys))
-    if xt < xp:
-        ans.append((-P, yt))
-    else:
-        ans.append((-P, P))
-        ans.append((P, P))
-        ans.append((P, yt))
+(xs, ys), (xt, yt), (xp, yp) = g(), g(), g()
+P = (-10001, 10001)[xs < xp]
+if (xs - xp) * (xt - xp) < 0:
+    print(f"{4}\n{-P} {ys}\n{-P} {P}\n{P} {P}\n{P} {yt}")
 else:
-    P = -10001
-    ans.append((-P, ys))
-    if xt > xp:
-        ans.append((-P, yt))
-    else:
-        ans.append((-P, P))
-        ans.append((P, P))
-        ans.append((P, yt))
-print(len(ans))
-for x, y in ans:
-    print(x, y)
+    print(f"{2}\n{-P} {ys}\n{-P} {yt}")
