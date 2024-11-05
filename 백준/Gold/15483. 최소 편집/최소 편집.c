223a71f10a4483d73949e9ebb05aef3fc0202a9d
김주원 <66457470+juwkim@users.noreply.github.com>
2024-11-06 02:44:04 +0900

    [Gold III] Title: 최소 편집, Time: 0 ms, Memory: 1120 KB -BaekjoonHub

--- 백준/Gold/15483. 최소 편집/최소 편집.c
+++ 백준/Gold/15483. 최소 편집/최소 편집.c
@@ -4,28 +4,23 @@
 
 char A[MAX_LEN], B[MAX_LEN];
 short p[2][MAX_LEN];
-char current;
 short lenA, lenB;
+
 short min(short x, short y, short z) {
-    short m = (x < y) ? x : y;
-    return (m < z) ? m : z;
+    return (x < y) ? ((x < z) ? x : z) : ((y < z) ? y : z);
 }
 
 int main() {
-    scanf("%s%s", A, B);
+    scanf("%s %s", A, B);
     lenA = strlen(A);
     lenB = strlen(B);
-    for (short j = 0; j <= lenB; j++) {
+    for (short j = 0; j <= lenB; j++)
         p[0][j] = j;
-    }
-
-    for (short i = 0; i < lenA; i++) {
-        current = (current + 1) % 2;
-        char next = (current + 1) % 2;
-        p[current][0] = i + 1;
+    for (short i = 0, cur = 1, nxt = 0; i < lenA; i++, cur ^= 1, nxt ^= 1) {
+        p[cur][0] = i + 1;
         for (short j = 0; j < lenB; j++)
-            p[current][j + 1] = (A[i] == B[j]) ? p[next][j] : 1 + min(p[current][j], p[next][j], p[next][j + 1]);
+            p[cur][j + 1] = (A[i] == B[j]) ? p[nxt][j] : 1 + min(p[cur][j], p[nxt][j], p[nxt][j + 1]);
     }
-    printf("%d\n", p[current][lenB]);
+    printf("%d\n", p[lenA & 1][lenB]);
     return 0;
 }
