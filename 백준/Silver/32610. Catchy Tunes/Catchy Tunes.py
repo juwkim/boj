24c70805770f14c2752f6200db24f7be4248d8ea
김주원 <66457470+juwkim@users.noreply.github.com>
2024-11-06 06:40:59 +0900

    [Silver III] Title: Catchy Tunes, Time: 608 ms, Memory: 153152 KB -BaekjoonHub

--- 백준/Silver/32610. Catchy Tunes/Catchy Tunes.py
+++ 백준/Silver/32610. Catchy Tunes/Catchy Tunes.py
@@ -11,15 +11,13 @@ hq = [(-len(v), k) for k, v in d.items()]
 prev = None
 while hq:
     l1, k1 = heapq.heappop(hq)
-    if prev != k1:
-        print(d[k1].pop())
-        if l1 + 1:
-            heapq.heappush(hq, (l1 + 1, k1))
-        prev = k1
-    else:
+    if prev == k1:
         l2, k2 = heapq.heappop(hq)
-        print(d[k2].pop())
+        if l2 + 1: heapq.heappush(hq, (l2 + 1, k2))
         heapq.heappush(hq, (l1, k1))
-        if l2 + 1:
-            heapq.heappush(hq, (l2 + 1, k2))
         prev = k2
+        print(d[k2].pop())
+    else:
+        if l1 + 1: heapq.heappush(hq, (l1 + 1, k1))
+        prev = k1
+        print(d[k1].pop())
