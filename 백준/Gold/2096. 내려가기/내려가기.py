72b78d56493d4d0cedf1ffafd3c4a96ac971878d
김주원 <66457470+juwkim@users.noreply.github.com>
2024-11-06 06:59:24 +0900

    [Gold V] Title: 내려가기, Time: 160 ms, Memory: 111328 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -1,19 +1,11 @@
-input = __import__('sys').stdin.readline
-
-def g(): return [*map(int, input().split())]
+import sys
+input = sys.stdin.readline
+g = lambda: [*map(int, input().split())]
 
 N = int(input())
 Min = Max = g()
 for _ in range(N - 1):
     a, b, c = g()
-    p = a + min(Min[0], Min[1])
-    q = b + min(Min)
-    r = c + min(Min[1], Min[2])
-    
-    x = a + max(Max[0], Max[1])
-    y = b + max(Max)
-    z = c + max(Max[1], Max[2])
-    
-    Min = [p, q, r]
-    Max = [x, y, z]
+    Min = a + min(Min[0], Min[1]), b + min(Min), c + min(Min[1], Min[2])
+    Max = a + max(Max[0], Max[1]), b + max(Max), c + max(Max[1], Max[2])
 print(max(Max), min(Min))
