6c2468c48ecfcf16b4a0f5265eb7aebd2fac3ca3
김주원 <66457470+juwkim@users.noreply.github.com>
2024-11-06 02:38:35 +0900

    [Gold III] Title: 최소 편집, Time: 0 ms, Memory: 1120 KB -BaekjoonHub

--- 백준/Gold/15483. 최소 편집/최소 편집.c
+++ 백준/Gold/15483. 최소 편집/최소 편집.c
@@ -3,41 +3,29 @@
 #define MAX_LEN 1001
 
 char A[MAX_LEN], B[MAX_LEN];
-short p[2][MAX_LEN];  // 두 개의 행을 가지는 배열
-int current = 0;    // 현재 사용 중인 행 인덱스
-
-// C 스타일의 min 함수 정의
-int min(int x, int y, int z) {
-    int m = (x < y) ? x : y;
+short p[2][MAX_LEN];
+char current;
+short lenA, lenB;
+short min(short x, short y, short z) {
+    short m = (x < y) ? x : y;
     return (m < z) ? m : z;
 }
 
 int main() {
     scanf("%s%s", A, B);
-    
-    int lenA = strlen(A);
-    int lenB = strlen(B);
-
-    // Initialize the first row of p
-    for (int j = 0; j <= lenB; j++) {
-        p[0][j] = j;  // p[0] 초기화
+    lenA = strlen(A);
+    lenB = strlen(B);
+    for (short j = 0; j <= lenB; j++) {
+        p[0][j] = j;
     }
 
-    for (int i = 0; i < lenA; i++) {
-        // 현재 행과 다음 행을 전환
-        current = (current + 1) % 2; // 행 인덱스 전환
-        int next = (current + 1) % 2; // 다음 행 인덱스
-
-        // c[0] 초기화
+    for (short i = 0; i < lenA; i++) {
+        current = (current + 1) % 2;
+        char next = (current + 1) % 2;
         p[current][0] = i + 1;
-
-        for (int j = 0; j < lenB; j++) {
-            int cost = (A[i] == B[j]) ? p[next][j] : 1 + min(p[current][j], p[next][j], p[next][j + 1]);
-            p[current][j + 1] = cost;
-        }
+        for (short j = 0; j < lenB; j++)
+            p[current][j + 1] = (A[i] == B[j]) ? p[next][j] : 1 + min(p[current][j], p[next][j], p[next][j + 1]);
     }
-
-    // 결과 출력
     printf("%d\n", p[current][lenB]);
     return 0;
 }
