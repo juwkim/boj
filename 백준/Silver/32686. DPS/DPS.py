10aa7922f7828aafc5fcc7cfa56e2c6b7fcfbe1d
김주원 <66457470+juwkim@users.noreply.github.com>
2024-11-18 21:05:33 +0900

    [Silver IV] Title: DPS, Time: 136 ms, Memory: 111428 KB -BaekjoonHub

--- 백준/Silver/32686. DPS/DPS.py
+++ 백준/Silver/32686. DPS/DPS.py
@@ -7,11 +7,5 @@ for _ in range(N):
     R, A, D = g()
     q1, r1 = divmod(S, R + A)
     q2, r2 = divmod(E, R + A)
-    if q1 == q2:
-        ans += D / A * (max(r2, R) - max(r1, R))
-    else:
-        ans += D / A * (A - max(0, r1 - R))
-        ans += D / A * max(0, r2 - R)
-        ans += D * (q2 - q1 - 1)
-
+    ans += D * (q2 - q1) + D / A * (max(0, r2 - R) - max(0, r1 - R))
 print(ans / (E - S))
