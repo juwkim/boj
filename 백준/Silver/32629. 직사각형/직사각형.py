0a3dc38a78a2344fa7188f4346554791d4fa3391
김주원 <66457470+juwkim@users.noreply.github.com>
2024-11-19 11:09:13 +0900

    [Silver III] Title: 직사각형, Time: 208 ms, Memory: 111672 KB -BaekjoonHub

--- 백준/Silver/32629. 직사각형/직사각형.py
+++ 백준/Silver/32629. 직사각형/직사각형.py
@@ -1,12 +1,3 @@
 import sys
-input = sys.stdin.readline
-
-for _ in range(int(input())):
-    N = int(input())
-    P = int(N**.5)
-    if N == P * P:
-        print(4 * P)
-    elif N <= P * (P + 1):
-        print(4 * P + 2)
-    else:
-        print(4 * P + 4)
+input=sys.stdin.readline
+for _ in range(int(input())):P=int((N:=int(input()))**.5);print((N+P*P+P-1)//P*2)
