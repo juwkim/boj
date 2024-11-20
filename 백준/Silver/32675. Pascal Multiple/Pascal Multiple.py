f6f5a3432253ed84d6ac57446424f03dc31ed8b4
김주원 <66457470+juwkim@users.noreply.github.com>
2024-11-20 15:37:12 +0900

    [Silver III] Title: Pascal Multiple, Time: 104 ms, Memory: 110404 KB -BaekjoonHub

--- 백준/Silver/32675. Pascal Multiple/Pascal Multiple.py
+++ 백준/Silver/32675. Pascal Multiple/Pascal Multiple.py
@@ -1,9 +1,8 @@
 from itertools import pairwise
 
 N, K = map(int, input().split())
-ans = (2 * N + 1) * (K == 1)
-cur = (1, 1)
-for i in range(2, N + 1):
+ans, cur = (2 * N + 1) * (K == 1), (1,)
+for _ in range(N):
     cur = [1] + [(a + b) % K for a, b in pairwise(cur)] + [1]
     ans += cur.count(0)
 print(ans)
