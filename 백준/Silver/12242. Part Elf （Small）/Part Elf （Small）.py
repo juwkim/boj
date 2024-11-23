e87384de9207f33c9453c1eeaf946a11bd947876
김주원 <66457470+juwkim@users.noreply.github.com>
2024-11-23 22:14:18 +0900

    [Silver III] Title: Part Elf (Small), Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/12242. Part Elf （Small）/Part Elf （Small）.py
+++ 백준/Silver/12242. Part Elf （Small）/Part Elf （Small）.py
@@ -1,13 +1,10 @@
-import sys
-input = sys.stdin.readline
-
 for tc in range(1, 1 + int(input())):
     a, b = map(int, input().split('/'))
     if b & b - 1:
         ans = 'impossible'
     else:
-        ans, p = 1, 2
-        while a * p < b:
+        ans, p = 1, 2 * a
+        while p < b:
             ans += 1
             p <<= 1
     print(f"Case #{tc}: {ans}")
