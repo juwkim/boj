53e6e7b429c387413d1e9af67754d12d5bb6f7ec
김주원 <66457470+juwkim@users.noreply.github.com>
2024-11-25 10:12:39 +0900

    [Silver II] Title: 패턴, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/17300. 패턴/패턴.py
+++ 백준/Silver/17300. 패턴/패턴.py
@@ -6,10 +6,7 @@ visited[px][py] = 1
 ans = "YES"
 for c in a:
     x, y = divmod(c - 1, 3)
-    if visited[x][y]:
-        ans = "NO"
-        break
-    if ((((px == x or py == y) and (abs(px - x) >= 2 or abs(py - y) >= 2)) or (abs(px - x) >= 2 and abs(py - y) >= 2)) and not visited[px + x >> 1][py + y >> 1]):
+    if visited[x][y] or ((px == x) + (py == y) + (abs(px - x) >= 2) + (abs(py - y) >= 2) >= 2) and not visited[px + x >> 1][py + y >> 1]:
         ans = "NO"
         break
     visited[x][y] = 1
