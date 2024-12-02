bd290d60b4b2768716193ec967b782a4652f8782
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-02 23:07:49 +0900

    [Bronze III] Title: 가희와 서울 지하철 2호선, Time: 96 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/32777. 가희와 서울 지하철 2호선/가희와 서울 지하철 2호선.py
+++ 백준/Bronze/32777. 가희와 서울 지하철 2호선/가희와 서울 지하철 2호선.py
@@ -2,11 +2,8 @@ import sys
 input = sys.stdin.readline
 
 for _ in range(int(input())):
-    a, b = map(lambda x: int(x) - 201, input().split())
-    din, dout = (b - a) % 43, (a - b) % 43
-    if din < dout:
+    a, b = map(int, input().split())
+    if (b - a) % 43 < (a - b) % 43:
         print("Inner circle line")
-    elif din > dout:
-        print("Outer circle line")
     else:
-        print("Same")
+        print("Outer circle line")
