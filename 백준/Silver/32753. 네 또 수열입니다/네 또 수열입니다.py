76e9a45bf31563aee3741e14208f7eda40ba13d2
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-03 17:24:23 +0900

    [Silver IV] Title: 네 또 수열입니다, Time: 104 ms, Memory: 113540 KB -BaekjoonHub

--- 백준/Silver/32753. 네 또 수열입니다/네 또 수열입니다.py
+++ 백준/Silver/32753. 네 또 수열입니다/네 또 수열입니다.py
@@ -1,7 +1,5 @@
-N, K = map(int, input().split())
-if N == 1:
-    print(*[1] * (N * K))
-elif N == 2 and K == 1:
-    print(1, 2)
-else:
-    print(-1)
+N,K=map(int,input().split())
+p=print
+if N==1:p(*[1]*(N*K))
+elif(N,K)==(2,1):p(1,2)
+else:p(-1)
