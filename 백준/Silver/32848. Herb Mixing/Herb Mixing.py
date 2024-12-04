2e1ba5a61a27103c88224f07ea599888dce6b4d9
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-04 18:28:48 +0900

    [Silver III] Title: Herb Mixing, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/32848. Herb Mixing/Herb Mixing.py
+++ 백준/Silver/32848. Herb Mixing/Herb Mixing.py
@@ -1,3 +1,3 @@
-g, r = map(int, input().split())
-a, b = divmod(max(0, g - r), 3)
-print((a + min(g, r)) * 10 + (0, 1, 3)[b])
+g,r=map(int,input().split())
+a,b=divmod(max(0,g-r),3)
+print((a+min(g,r))*10+(0,1,3)[b])
