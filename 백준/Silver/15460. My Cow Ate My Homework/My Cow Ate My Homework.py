58dc55b72c2d3221fc3121159d53c92a93bca004
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-06 01:44:42 +0900

    [Silver II] Title: My Cow Ate My Homework, Time: 180 ms, Memory: 127556 KB -BaekjoonHub

--- 백준/Silver/15460. My Cow Ate My Homework/My Cow Ate My Homework.py
+++ 백준/Silver/15460. My Cow Ate My Homework/My Cow Ate My Homework.py
@@ -1,15 +1,10 @@
 N, *nums = map(int, open(0).read().split())
-s = sum(nums)
-ans, p = [], 0
-idx = sorted(range(N), key=lambda i: nums[i], reverse=True)
+ans, p, s = [], 0, sum(nums)
+idx = sorted(range(N), key=lambda i: -nums[i])
 for i in range(N - 2):
     s -= nums[i]
-    while idx[-1] <= i:
-        idx.pop()
+    while idx[-1] <= i: idx.pop()
     t = (s - nums[idx[-1]]) / (N - 2 - i)
-    if t == p:
-        ans.append(i + 1)
-    elif t > p:
-        ans, p = [i + 1], t
-for num in ans:
-    print(num)
+    if t == p: ans.append(i + 1)
+    elif t > p: ans, p = [i + 1], t
+print(*ans)
