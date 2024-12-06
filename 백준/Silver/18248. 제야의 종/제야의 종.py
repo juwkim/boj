70c6549083e85c729981375729fd09b178591290
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-07 03:04:51 +0900

    [Silver II] Title: 제야의 종, Time: 132 ms, Memory: 110960 KB -BaekjoonHub

---
+++ 백준/Silver/18248. 제야의 종/제야의 종.py
@@ -0,0 +1,21 @@
+import sys
+g = lambda: [*map(int, sys.stdin.readline().split())]
+
+N, M = g()
+mat = [[0] * M for _ in range(M)]
+def solve():
+    for _ in range(N):
+        zero, one = [], []
+        for i, c in enumerate(g()):
+            if c:
+                one.append(i)
+            else:
+                zero.append(i)
+        for p in one:
+            for q in zero:
+                if mat[p][q] == -1:
+                    return "NO"
+                mat[p][q] = 1
+                mat[q][p] = -1
+    return "YES"
+print(solve())
