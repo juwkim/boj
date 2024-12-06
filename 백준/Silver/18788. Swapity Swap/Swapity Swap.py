efdb1a050ca2c00e9199ce83728d0c4232b5357a
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-07 02:48:04 +0900

    [Silver II] Title: Swapity Swap, Time: 248 ms, Memory: 186188 KB -BaekjoonHub

---
+++ 백준/Silver/18788. Swapity Swap/Swapity Swap.py
@@ -0,0 +1,17 @@
+N, K, A1, A2, B1, B2 = map(int, open(0).read().split())
+log, check = [], set()
+cur = [*range(1, N + 1)]
+while len(log) <= K:
+    p = tuple(cur)
+    if p in check:
+        break
+    log.append(p)
+    check.add(p)
+    cur[A1-1:A2] = cur[A1-1:A2][::-1]
+    cur[B1-1:B2] = cur[B1-1:B2][::-1]
+if len(log) == K + 1:
+    ans = log[K]
+else:
+    i = log.index(p)
+    ans = log[(K - i) % (len(log) - i) + i]
+print(*ans, sep='\n')
