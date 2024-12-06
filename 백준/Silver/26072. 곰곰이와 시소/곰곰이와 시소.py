4a1afd31140a5e1437829e924e53221324bb86fd
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-07 08:19:31 +0900

    [Silver II] Title: 곰곰이와 시소, Time: 132 ms, Memory: 135764 KB -BaekjoonHub

--- 백준/Silver/26072. 곰곰이와 시소/곰곰이와 시소.py
+++ 백준/Silver/26072. 곰곰이와 시소/곰곰이와 시소.py
@@ -1,7 +1,3 @@
-import sys
-g = lambda: [*map(int, sys.stdin.readline().split())]
-
-N, L = g()
-x = g()
-w = g()
-print(sum(x[i] * w[i] for i in range(N)) / sum(w))
+g=lambda:[*map(int, input().split())]
+(N,L),x,w=g(),g(),g()
+print(sum(x[i]*w[i]for i in range(N))/sum(w))
