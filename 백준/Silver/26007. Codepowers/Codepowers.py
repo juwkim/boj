9546848ac955f2131ef54d3c59e271061140213d
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-08 07:49:33 +0900

    [Silver II] Title: Codepowers, Time: 504 ms, Memory: 128632 KB -BaekjoonHub

--- 백준/Silver/26007. Codepowers/Codepowers.py
+++ 백준/Silver/26007. Codepowers/Codepowers.py
@@ -1,11 +1,11 @@
 import sys
-g = lambda: [*map(int, sys.stdin.readline().split())]
 
+g = lambda: map(int, sys.stdin.readline().split())
 N, M, K, score = g()
-px = [0] * (N + 1)
-for i, num in enumerate(g()):
-    score += num
-    px[i + 1] = px[i] + (score < K)
+px = [0]
+for A in g():
+    score += A
+    px.append(px[-1] + (score < K))
 for _ in range(M):
     l, r = g()
     print(px[r - 1] - px[l - 1])
