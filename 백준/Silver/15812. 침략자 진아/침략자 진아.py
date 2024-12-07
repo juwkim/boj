31bf8f87692901b7c9fe2b6b2a0446742f7558d1
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-07 23:27:51 +0900

    [Silver II] Title: 침략자 진아, Time: 332 ms, Memory: 112512 KB -BaekjoonHub

--- 백준/Silver/15812. 침략자 진아/침략자 진아.py
+++ 백준/Silver/15812. 침략자 진아/침략자 진아.py
@@ -1,7 +1,7 @@
-from itertools import combinations
+from itertools import*
 
 N, M = map(int, input().split())
 A = [input() for _ in range(N)]
-ones = [(i, j) for i in range(N) for j in range(M) if A[i][j] == '1']
-zeros = [(i, j) for i in range(N) for j in range(M) if A[i][j] == '0']
-print(min(max(min(abs(i - i1) + abs(j - j1), abs(i - i2) + abs(j - j2)) for i, j in ones) for (i1, j1), (i2, j2) in combinations(zeros, 2)))
+a = [(i, j) for i in range(N) for j in range(M) if A[i][j] == '1']
+b = [(i, j) for i in range(N) for j in range(M) if A[i][j] == '0']
+print(min(max(min(abs(i - i1) + abs(j - j1), abs(i - i2) + abs(j - j2)) for i, j in a) for (i1, j1), (i2, j2) in combinations(b, 2)))
