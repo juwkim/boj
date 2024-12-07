e691e4d84723bfe7060c2233993d634a3da3624e
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-07 22:02:20 +0900

    [Silver II] Title: 고추장 괄호 문자열, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/27967. 고추장 괄호 문자열/고추장 괄호 문자열.py
+++ 백준/Silver/27967. 고추장 괄호 문자열/고추장 괄호 문자열.py
@@ -1,24 +1,3 @@
 N = int(input())
 S = input()
-ans = [*S]
-def solve(i, cnt):
-    if i == N:
-        if cnt == 0:
-            print(''.join(ans))
-            exit()
-        return
-    if ans[i] == 'G':
-        if cnt > 0:
-            ans[i] = ')'
-            solve(i + 1, cnt - 1)
-            ans[i] = 'G'
-        ans[i] = '('
-        solve(i + 1, cnt + 1)
-        ans[i] = 'G'
-    elif ans[i] == '(':
-        solve(i + 1, cnt + 1)
-    elif cnt:
-        solve(i + 1, cnt - 1)
-    else:
-        return
-solve(0, 0)
+print(S.replace('G', '(', N // 2 - S.count('(')).replace('G', ')'))
