e169f9c3dec5abcffeadd84c1a02d98994b20204
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-08 21:17:03 +0900

    [Silver II] Title: 다음 다양한 단어, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/16923. 다음 다양한 단어/다음 다양한 단어.py
+++ 백준/Silver/16923. 다음 다양한 단어/다음 다양한 단어.py
@@ -1,16 +1,12 @@
-S = input()
+S, ans = input(), "abcdefghijklmnopqrstuvwxzy"
 if len(S) < 26:
-    t = min(set("abcdefghijklmnopqrstuvwxyz") - set(S))
-    print(S + t)
+    ans = S + min(set("abcdefghijklmnopqrstuvwxyz") - set(S))
 elif S == "zyxwvutsrqponmlkjihgfedcba":
-    print(-1)
+    ans = -1
 else:
-    i = 24
-    ans = "abcdefghijklmnopqrstuvwxzy"
-    while i > 0:
+    for i in range(24, 0, -1):
         p = set(range(ord(S[i]) + 1, ord('z') + 1)) - set(map(ord, S[:i]))
         if p:
             ans = S[:i] + chr(min(p))
             break
-        i -= 1
-    print(ans)
+print(ans)
