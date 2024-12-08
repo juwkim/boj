fc3b952a53bc4cf340b3ffb35e34a750e7ef7e03
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-09 08:42:41 +0900

    [Silver III] Title: 자석 놀이, Time: 244 ms, Memory: 173952 KB -BaekjoonHub

--- 백준/Silver/32931. 자석 놀이/자석 놀이.py
+++ 백준/Silver/32931. 자석 놀이/자석 놀이.py
@@ -12,18 +12,11 @@ if N == 2:
         print(a1 + b2 + max(a2, b1))
 else:
     s, t = a1 + a[0] + max(b1, 0), a1 + b1 + b[0]
-    if N == 3:
-        if a2 > 0 and b[-1] > 0:
-            s = s + b2 + a2 + b[-1]
-        else:
-            s = s + b2 + max(a2, b[-1])
-        t = t + b2 + max(a[-1], 0) + max(a2, 0)
+    for i in range(1, N - 2):
+        s, t = max(s + a[i] + max(b[i - 1], 0), t + a[i - 1] + a[i]), max(s + b[i - 1] + b[i], t + b[i] + max(a[i - 1], 0))
+    if a2 > 0 and b[-1] > 0:
+        s = s + b2 + a2 + b[-1]
     else:
-        for i in range(1, N - 2):
-            s, t = max(s + a[i] + max(b[i - 1], 0), t + a[i - 1] + a[i]), max(s + b[i - 1] + b[i], t + b[i] + max(a[i - 1], 0))
-        if a2 > 0 and b[-1] > 0:
-            s = s + b2 + a2 + b[-1]
-        else:
-            s = s + b2 + max(a2, b[-1])
-        t = t + b2 + max(a[-1], 0) + max(a2, 0)
+        s = s + b2 + max(a2, b[-1])
+    t = t + b2 + max(a[-1], 0) + max(a2, 0)
     print(max(s, t))
