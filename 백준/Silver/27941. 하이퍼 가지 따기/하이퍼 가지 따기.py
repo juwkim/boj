bf197e9f11b9d4d688eeb452c4ff1b3a56363fda
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-09 01:02:51 +0900

    [Silver II] Title: 하이퍼 가지 따기, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/27941. 하이퍼 가지 따기/하이퍼 가지 따기.py
+++ 백준/Silver/27941. 하이퍼 가지 따기/하이퍼 가지 따기.py
@@ -1,11 +1,5 @@
-from collections import Counter
-
-cnt = [Counter() for _ in range(11)]
+ans = [0] * 11
 for l in open(0):
     for i, c in enumerate(l.split()):
-        cnt[i][c] += 1
-for i in range(11):
-    for k, v in sorted(cnt[i].items()):
-        if v == 1023:
-            print(k, end=' ')
-            break
+        ans[i] ^= int(c)
+print(*ans)
