3392683ad32ff10380eee23e485774521f386bf1
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-08 19:54:18 +0900

    [Silver II] Title: 블록, Time: 144 ms, Memory: 110576 KB -BaekjoonHub

--- 백준/Silver/23252. 블록/블록.py
+++ 백준/Silver/23252. 블록/블록.py
@@ -1,3 +1,3 @@
 for _ in range(int(input())):
-    A, B, C = map(int, input().split())
-    print(("No", "Yes")[C and A >= C and (A - C) % 2 == 0 or not C and A % 2 == 0 and (B % 2 == 0 or A > 1)])
+    A,B,C=map(int,input().split())
+    print(("No","Yes")[C and A>=C and(A-C)%2==0 or C==0 and A%2==0 and(B%2==0 or A>1)])
