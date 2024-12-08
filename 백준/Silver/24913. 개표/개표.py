bbe1d9b8dc4a3f763fa8ac479f48ff8c52be1400
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-09 03:19:49 +0900

    [Silver II] Title: 개표, Time: 224 ms, Memory: 116864 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/24913. 개표/개표.py
+++ 백준/Silver/24913. 개표/개표.py
@@ -2,21 +2,20 @@ import sys
 g = lambda: map(int, sys.stdin.readline().split())
 
 N, Q = g()
-cnt = [0] * (N + 2)
-Max = 0
+vote = [0] * (N + 1)
+Max, cnt = 0, -N
 for _ in range(Q):
     cmd, x, num = g()
     if cmd == 1:
-        cnt[num] += x
-        if num != N + 1:
-            Max = max(Max, cnt[num])
+        vote[num - 1] += x
+        if num == N + 1:
+            cnt += x * N
+        else:
+            Max = max(Max, vote[num - 1])
+            cnt -= x
     else:
-        p = 0
-        ans = "NO"
-        if Max < cnt[N + 1] + x:
-            for i in range(1, N + 1):
-                p += max(cnt[N + 1] + x - cnt[i] - 1, 0)
-                if p >= num:
-                    ans = "YES"
-                    break
+        if Max < vote[N] + x and num <= cnt + x * N:
+            ans = "YES"
+        else:
+            ans = "NO"
         print(ans)
