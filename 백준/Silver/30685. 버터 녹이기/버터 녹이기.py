b5abe37d0e8191f5768c08d59b55ba5f8d5b60bb
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-10 03:38:05 +0900

    [Silver II] Title: 버터 녹이기, Time: 948 ms, Memory: 163008 KB -BaekjoonHub

--- 백준/Silver/30685. 버터 녹이기/버터 녹이기.py
+++ 백준/Silver/30685. 버터 녹이기/버터 녹이기.py
@@ -5,8 +5,7 @@ from itertools import pairwise
 ans = 1e9
 for (x1, h1), (x2, h2) in pairwise(sorted([*map(int, input().split())] for _ in range(int(input())))):
     gap = x2 - x1 - 1
-    a, b = sorted((h1 // 2, h2 // 2))
-    if gap >= a + b:
+    if gap >= h1 // 2 + h2 // 2:
         continue
-    ans = min(ans, max(gap - a, gap // 2))
+    ans = min(ans, max(gap - h1 // 2, gap - h2 // 2, gap // 2))
 print((ans, "forever")[ans == 1e9])
