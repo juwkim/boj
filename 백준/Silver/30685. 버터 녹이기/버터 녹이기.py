a2b14b61516d0dad6e59ed9e999911dbf48245ad
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-10 03:36:53 +0900

    [Silver II] Title: 버터 녹이기, Time: 1104 ms, Memory: 163012 KB -BaekjoonHub

---
+++ 백준/Silver/30685. 버터 녹이기/버터 녹이기.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+from itertools import pairwise
+
+ans = 1e9
+for (x1, h1), (x2, h2) in pairwise(sorted([*map(int, input().split())] for _ in range(int(input())))):
+    gap = x2 - x1 - 1
+    a, b = sorted((h1 // 2, h2 // 2))
+    if gap >= a + b:
+        continue
+    ans = min(ans, max(gap - a, gap // 2))
+print((ans, "forever")[ans == 1e9])
