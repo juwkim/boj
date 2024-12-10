cbcef5f89ed72c32644355d7d5a3fa4d766c94c0
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-10 21:38:18 +0900

    [Silver II] Title: 플립과 시프트, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/7347. 플립과 시프트/플립과 시프트.py
+++ 백준/Silver/7347. 플립과 시프트/플립과 시프트.py
@@ -1,3 +1,3 @@
 for _ in range(int(input())):
     l, *a = map(int, input().split())
-    print("YNEOS"[l%2==0 and abs(sum((-1,1)[i&1]for i,c in enumerate(a) if c))>1::2])
+    print("YNEOS"[l%2==0 and abs(sum(a[::2])-sum(a[1::2]))>1::2])
