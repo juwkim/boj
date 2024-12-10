e0e4c17890bc0d2acf3a3b5b81cfd947ef0491a0
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-11 03:51:31 +0900

    [Silver II] Title: 정제헌을 팔자!, Time: 2308 ms, Memory: 115936 KB -BaekjoonHub

--- 백준/Silver/9273. 정제헌을 팔자！/정제헌을 팔자！.py
+++ 백준/Silver/9273. 정제헌을 팔자！/정제헌을 팔자！.py
@@ -1,10 +1,3 @@
 for s in open(0):
     n = int(s.split('/')[1])
-    ans, x = 0, n + 1
-    while True:
-        y, r = divmod(n * x, x - n)
-        if y < x:
-            break
-        ans += r == 0
-        x += 1
-    print(ans)
+    print(sum(n * n % i == 0 for i in range(1, n + 1)))
