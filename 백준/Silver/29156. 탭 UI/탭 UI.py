f7d01bf39bf6b6c8a61026429f25f966211f0f07
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-10 09:35:32 +0900

    [Silver II] Title: 탭 UI, Time: 436 ms, Memory: 117684 KB -BaekjoonHub

--- 백준/Silver/29156. 탭 UI/탭 UI.py
+++ 백준/Silver/29156. 탭 UI/탭 UI.py
@@ -8,11 +8,9 @@ for i in range(N): px[i + 1] = px[i] + l[i]
 L = int(input())
 ans = [0] * (N + 1)
 for i in range(1, N + 1):
-    if px[i] - l[i - 1] / 2 <= L / 2:
-        ans[i] = 0
-    elif px[N] - (px[i] - l[i - 1] / 2) <= L / 2:
-        ans[i] = px[N] - min(px[N], L)
-    else:
-        ans[i] = px[i] - l[i - 1] / 2 - L / 2
+    # if px[i] - l[i - 1] / 2 - L / 2 <= 0:
+    #     ans[i] = 0
+    # else:
+    ans[i] = max(0, min(px[N] - L, px[i] - l[i - 1] / 2 - L / 2))
 for _ in range(int(input())):
     print("%.2f" % ans[int(input())])
