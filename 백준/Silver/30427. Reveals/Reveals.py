e567fe75df65e5596c26d9b594f4c6d580b7b5cf
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-11 03:13:30 +0900

    [Silver II] Title: Reveals, Time: 100 ms, Memory: 110576 KB -BaekjoonHub

--- 백준/Silver/30427. Reveals/Reveals.py
+++ 백준/Silver/30427. Reveals/Reveals.py
@@ -3,20 +3,13 @@ input = lambda: sys.stdin.readline().rstrip()
 
 def solve():
     input()
-    home = [input() for _ in range(int(input()))]
-    if "dongho" in home:
-        return "dongho"
-    names = {*home}
-    for _ in range(int(input())):
-        names.discard(input())
-    if not names:
-        return "swi"
-    if "bumin" in names:
-        return "bumin"
-    if "cake" in names:
-        return "cake"
-    if "lawyer" in names:
-        return "lawyer"
-    return min(names)
+    home = {input() for _ in range(int(input()))}
+    if "dongho" in home: return "dongho"
+    for _ in range(int(input())): home.discard(input())
+    if not home: return "swi"
+    if "bumin" in home: return "bumin"
+    if "cake" in home: return "cake"
+    if "lawyer" in home: return "lawyer"
+    return min(home)
 
 print(solve())
