949530eaed9bfe52d221fa5ac3716ae18ef0b890
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-10 22:54:00 +0900

    [Silver II] Title: 아이템 배치하기, Time: 272 ms, Memory: 193504 KB, Score: 150 point -BaekjoonHub

--- 백준/Silver/21599. 아이템 배치하기/아이템 배치하기.py
+++ 백준/Silver/21599. 아이템 배치하기/아이템 배치하기.py
@@ -1,2 +1,7 @@
 N, *A = map(int, open(0).read().split())
-print(N - A.count(0))
+ans = 0
+for i, a in enumerate(sorted(A, reverse=True)):
+    if a == 0:
+        break
+    ans = max(ans, i + a)
+print(min(ans, N))
