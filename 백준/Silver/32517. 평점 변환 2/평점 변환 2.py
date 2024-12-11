b07ef170b3664ace6b74c4b42a76b9ad92af9ba5
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-11 11:39:43 +0900

    [Silver II] Title: 평점 변환 2, Time: 280 ms, Memory: 203424 KB -BaekjoonHub

--- 백준/Silver/32517. 평점 변환 2/평점 변환 2.py
+++ 백준/Silver/32517. 평점 변환 2/평점 변환 2.py
@@ -5,7 +5,7 @@ for i in range(N):
         a[i] = b[i] + 1
     elif i * b[i] >= sum:
         a[i] = b[i]
-    if a[i] < 1 or a[i] > 10**9:
+    if a[i] == 0:
         print(-1)
         break
     sum += a[i]
