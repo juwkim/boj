946b9d488308278d59d4fb01f98128be60817302
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-15 03:02:16 +0900

    [Silver II] Title: Amsterdam Distance, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/15003. Amsterdam Distance/Amsterdam Distance.py
+++ 백준/Silver/15003. Amsterdam Distance/Amsterdam Distance.py
@@ -1,3 +1,3 @@
-M, N, R = input().split()
-ax, ay, bx, by = map(int, input().split())
-print(min(ay + by, abs(ay - by) + min(ay, by) * abs(ax - bx) * 3.141592 / int(M)) * float(R) / int(N))
+M,N,R=input().split()
+ax,ay,bx,by=map(int,input().split())
+print((min(1,abs(ax-bx)*3.141592/int(M)-1)*min(ay,by)+max(ay,by))*float(R)/int(N))
