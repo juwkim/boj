1db02c5232038ea8b8404c1cf2d7e1bb1dda1199
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-15 08:39:17 +0900

    [Silver II] Title: たのしいカードゲーム (Card Game is Fun), Time: 164 ms, Memory: 110780 KB -BaekjoonHub

--- 백준/Silver/5539. たのしいカードゲーム （Card Game is Fun）/たのしいカードゲーム （Card Game is Fun）.py
+++ 백준/Silver/5539. たのしいカードゲーム （Card Game is Fun）/たのしいカードゲーム （Card Game is Fun）.py
@@ -1,16 +1,15 @@
-A, B, *nums = map(int, open(0).read().split())
-P, Q = nums[:A], nums[A:]
-def solve():
-    ans = 0
-    for start in range(B):
-        i, j = 0, start
-        while j < B:
-            while i < A and P[i] != Q[j]:
-                i += 1
-            if i == A:
-                break
+A, B, *P = map(int, open(0).read().split())
+ans = 0
+for start in range(B):
+    i, j = 0, start
+    while j < B:
+        while i < A and P[i] != P[A + j]:
             i += 1
-            j += 1
-        ans = max(ans, j - start)
-    return ans
-print(solve())
+        if i == A:
+            break
+        i += 1
+        j += 1
+    ans = max(ans, j - start)
+    if ans >= B - start:
+        break
+print(ans)
