1dcfdcf4efbd800ca99db99193b22e5349eaa2ff
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-15 05:47:06 +0900

    [Silver II] Title: Down the Pyramid, Time: 288 ms, Memory: 199784 KB -BaekjoonHub

--- 백준/Silver/16065. Down the Pyramid/Down the Pyramid.py
+++ 백준/Silver/16065. Down the Pyramid/Down the Pyramid.py
@@ -1,4 +1,3 @@
-l, r = 0, 1e9
-for num in map(int, [*open(0)][1].split()):
-    l, r = num - min(num, r), num - l
-print(max(0, r - l + 1))
+l,r=0,1e9
+for a in map(int,[*open(0)][1].split()):l,r=max(0,a-r),a-l
+print(max(0,r-l+1))
