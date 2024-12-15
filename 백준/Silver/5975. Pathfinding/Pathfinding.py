5a0da9e1eda4eb1fc202ab03e0065321fc5ecdcf
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-16 04:02:15 +0900

    [Silver II] Title: Pathfinding, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Silver/5975. Pathfinding/Pathfinding.py
@@ -0,0 +1,21 @@
+import sys
+g = lambda: [*map(int, sys.stdin.readline().split())]
+
+N, M = g()
+C = [[0] * (N + 1)] + [[0] + g() for _ in range(N)]
+visited = bytearray(N + 1)
+visited[M] = 1
+st = [M]
+print(M)
+while True:
+    nst = []
+    while st:
+        x = st.pop()
+        for i in range(1, N + 1):
+            if x != i and not visited[i] and C[x][i]:
+                visited[i] = 1
+                nst.append(i)
+    st = sorted(nst)
+    if not st:
+        break
+    print(*st)
