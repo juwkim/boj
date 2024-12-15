216fb705a38d7b515f5ab8a275be1124bdc6129d
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-15 10:32:14 +0900

    [Silver II] Title: To the Max, Time: 316 ms, Memory: 110916 KB -BaekjoonHub

--- 백준/Silver/6374. To the Max/To the Max.py
+++ 백준/Silver/6374. To the Max/To the Max.py
@@ -1,10 +1,9 @@
-a = lambda a, b: nums[a * N + b]
-N, *nums = map(int, open(0).read().split())
+N, *a = map(int, open(0).read().split())
 px = [[0] * (N + 1) for _ in range(N + 1)]
 for i in range(N):
     cur = 0
     for j in range(N):
-        cur += a(i, j)
+        cur += a[i * N + j]
         px[i + 1][j + 1] = cur + px[i][j + 1]
 ans = -1e9
 for i in range(N):
