65aacd435de6b4a8aefec9354f2a4ba2892d6118
김주원 <66457470+juwkim@users.noreply.github.com>
2024-12-17 20:20:57 +0900

    [Silver II] Title: Distance, Time: 316 ms, Memory: 151508 KB -BaekjoonHub

--- 백준/Silver/21203. Distance/Distance.py
+++ 백준/Silver/21203. Distance/Distance.py
@@ -1,3 +1,3 @@
-N = int(input())
-a, s = map(sorted, zip(*[[*map(int, input().split())] for _ in range(N)]))
-print(sum((a[i + 1] - a[i] + s[i + 1] - s[i]) * (i + 1) * (N - i - 1) for i in range(N - 1)))
+N=int(input())
+a,s=map(sorted,zip(*[[*map(int,input().split())]for _ in range(N)]))
+print(sum((a[i]-a[i-1]+s[i]-s[i-1])*i*(N-i)for i in range(1,N)))
