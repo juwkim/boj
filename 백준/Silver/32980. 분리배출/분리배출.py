c0be5296e88b44a41ed0189dde4a375b4005a6ce
김주원 <66457470+juwkim@users.noreply.github.com>
2025-01-07 07:50:17 +0900

    [Silver V] Title: 분리배출, Time: 268 ms, Memory: 121236 KB -BaekjoonHub

--- 백준/Silver/32980. 분리배출/분리배출.py
+++ 백준/Silver/32980. 분리배출/분리배출.py
@@ -8,7 +8,7 @@ cost['O'] = int(input())
 for k, v in cost.items(): cost[k] = min(cost[k], cost['O'])
 ans = 0
 for s in trash:
-    if s[0] == 'O' or len({*s}) > 1:
+    if s[0] == 'O' or any(s[i] != s[0] for i in range(1, len(s))):
         ans += cost['O'] * len(s)
     else:
         ans += cost[s[0]] * len(s)
