62f7095f7f8cf1eba5ec81436ab77ed788f1961f
김주원 <66457470+juwkim@users.noreply.github.com>
2025-01-08 05:51:30 +0900

    [Silver II] Title: Chessboard Dancing, Time: 116 ms, Memory: 111532 KB -BaekjoonHub

--- 백준/Silver/14979. Chessboard Dancing/Chessboard Dancing.py
+++ 백준/Silver/14979. Chessboard Dancing/Chessboard Dancing.py
@@ -1,18 +1,7 @@
 for S, C in map(str.split, open(0)):
-    S = int(S)
     match C:
-        case 'K':
-            if S > 1:
-                ans = 4
-            else:
-                ans = 1
-        case 'N':
-            if S > 2:
-                ans = 2
-            else:
-                ans = 1
-        case 'B':
-            ans = S
-        case 'R':
-            ans = S
-    print(ans)
+        case 'K': a = (1, 4)[int(S) > 1]
+        case 'N': a = (1, 2)[int(S) > 2]
+        case 'B': a = S
+        case 'R': a = S
+    print(a)
