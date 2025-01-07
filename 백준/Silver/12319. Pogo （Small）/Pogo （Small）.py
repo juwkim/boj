179ebb75816259fcd1d8ef5c1a067115f568c32a
김주원 <66457470+juwkim@users.noreply.github.com>
2025-01-08 04:41:05 +0900

    [Silver II] Title: Pogo (Small), Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/12319. Pogo （Small）/Pogo （Small）.py
+++ 백준/Silver/12319. Pogo （Small）/Pogo （Small）.py
@@ -1,15 +1,8 @@
-import sys
-input = sys.stdin.readline
-
 for tc in range(1, 1 + int(input())):
     x, y = map(int, input().split())
     ans = ""
-    if x > 0:
-        ans = "WE" * x
-    elif x < 0:
-        ans = "EW" * -x
-    if y > 0:
-        ans += "SN" * y
-    elif y < 0:
-        ans += "NS" * -y
+    if x > 0: ans = "WE" * x
+    elif x < 0: ans = "EW" * -x
+    if y > 0: ans += "SN" * y
+    elif y < 0: ans += "NS" * -y
     print(f"Case #{tc}: {ans}")
