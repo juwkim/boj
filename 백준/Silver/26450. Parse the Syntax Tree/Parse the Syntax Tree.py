52744fa2ff764a5309bc3dcd2e399ec12c998026
김주원 <66457470+juwkim@users.noreply.github.com>
2025-01-07 16:36:14 +0900

    [Silver II] Title: Parse the Syntax Tree, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/26450. Parse the Syntax Tree/Parse the Syntax Tree.py
+++ 백준/Silver/26450. Parse the Syntax Tree/Parse the Syntax Tree.py
@@ -2,21 +2,12 @@ H, W = map(int, input().split())
 s = [input() for _ in range(H)]
 
 def solve(i, j):
-    if s[i][j].isdigit():
-        return int(s[i][j])
+    if s[i][j].isdigit(): return int(s[i][j])
     r = i + 1
-    c1 = j - 1
-    while c1 and s[r][c1] == '.':
-        c1 -= 1
-    c2 = j + 1
-    while c2 < W and s[r][c2] == '.':
-        c2 += 1
-    left, right = solve(r, c1), solve(r, c2)
-    if s[i][j] == '*':
-        return left * right
-    if s[i][j] == '+':
-        return left + right
-    return left - right
+    c1, c2 = j - 1, j + 1
+    while c1 and s[r][c1] == '.': c1 -= 1
+    while c2 < W and s[r][c2] == '.': c2 += 1
+    return eval(f"{solve(r, c1)}{s[i][j]}{solve(r, c2)}")
 j = 0
 while j < W and s[0][j] == '.':
     j += 1
