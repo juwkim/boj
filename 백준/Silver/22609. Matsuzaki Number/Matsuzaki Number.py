353600a6469f408b78eb30df6d93bcea8685c3b6
김주원 <66457470+juwkim@users.noreply.github.com>
2025-01-08 08:42:09 +0900

    [Silver II] Title: Matsuzaki Number, Time: 180 ms, Memory: 111572 KB -BaekjoonHub

---
+++ 백준/Silver/22609. Matsuzaki Number/Matsuzaki Number.py
@@ -0,0 +1,16 @@
+from bisect import bisect
+
+def seive(n):
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False
+    for i in range(2, int(n ** 0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = False
+    return [i for i in range(2, n + 1) if is_prime[i]]
+
+primes = seive(101000)
+for N, P in map(lambda s: map(int, s.split()), [*open(0)][:-1]):
+    idx = bisect(primes, N)
+    nums = sorted(primes[i] + primes[j] for i in range(idx, idx + 30) for j in range(i, idx + 30))
+    print(nums[P - 1])
