9130316409272750672988cdf5494c146fd2907b
김주원 <66457470+juwkim@users.noreply.github.com>
2025-01-07 20:15:14 +0900

    [Silver II] Title: Unreliable Messengers, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/3932. Unreliable Messengers/Unreliable Messengers.py
+++ 백준/Silver/3932. Unreliable Messengers/Unreliable Messengers.py
@@ -1,19 +1,12 @@
 for _ in range(int(input())):
     order = input()
-    s = input()
+    l = len(s:=[*input()])
     for c in order[::-1]:
         match c:
-            case 'J':
-                s = s[-1] + s[:-1]
-            case 'C':
-                s = s[1:] + s[0]
-            case 'E':
-                l = len(s)
-                s = s[l//2+1:] + s[l//2] + s[:l//2] if l & 1 else s[l//2:] + s[:l//2]
-            case 'A':
-                s = s[::-1]
-            case 'P':
-                s = ''.join(c if c.isalpha() else "9012345678"[int(c)] for c in s)
-            case 'M':
-                s = ''.join(c if c.isalpha() else "1234567890"[int(c)] for c in s)
-    print(s)
+            case 'J': s = [s[-1]] + s[:-1]
+            case 'C': s = s[1:] + [s[0]]
+            case 'E': s = (s[l//2+1:] + [s[l//2]] if l & 1 else s[l//2:]) + s[:l//2]
+            case 'A': s = s[::-1]
+            case 'P': s = [c if c.isalpha() else "9012345678"[int(c)] for c in s]
+            case 'M': s = [c if c.isalpha() else "1234567890"[int(c)] for c in s]
+    print(*s, sep='')
