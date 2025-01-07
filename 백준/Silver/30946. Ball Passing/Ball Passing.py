0f0554e2f1a60ed158a469df56102895b873f58a
김주원 <66457470+juwkim@users.noreply.github.com>
2025-01-07 19:36:13 +0900

    [Silver II] Title: Ball Passing, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/30946. Ball Passing/Ball Passing.py
+++ 백준/Silver/30946. Ball Passing/Ball Passing.py
@@ -1,19 +1,10 @@
 import sys
 input = sys.stdin.readline
-from math import atan2, dist
+from math import dist
 
 N = int(input())
 S = input()
-nums = [[], []]
-for i in range(N):
-    nums[S[i] == 'B'].append([*map(int, input().split())])
-ans = 0
-for i in range(2):
-    l = nums[i]
-    if not l:
-        continue
-    p0 = min(l, key=lambda p: (p[1], p[0]))
-    l.sort(key=lambda p: atan2(p[1] - p0[1], p[0] - p0[0]))
-    for i in range(len(l)//2):
-        ans += dist(l[i], l[i + len(l)//2])
-print(ans)
+l = [[], []]
+for i in range(N): l[S[i] == 'B'].append([*map(int, input().split())])
+a, b = l[0], l[1]
+print(sum(dist(a[i], a[i + len(a) // 2]) for i in range(len(a) // 2)) + sum(dist(b[i], b[i + len(b) // 2]) for i in range(len(b) // 2)))
