c3b90c6cd186739f3e07c03e38260648ebdf250b
김주원 <66457470+juwkim@users.noreply.github.com>
2025-01-08 20:11:05 +0900

    [Silver II] Title: Auxiliary Question of the Universe, Time: 92 ms, Memory: 109412 KB -BaekjoonHub

--- 백준/Silver/3550. Auxiliary Question of the Universe/Auxiliary Question of the Universe.py
+++ 백준/Silver/3550. Auxiliary Question of the Universe/Auxiliary Question of the Universe.py
@@ -1,9 +1,6 @@
-s = input()
-def solve(l, r):
-    if l > r:
-        print('0', end='')
-    else:
-        c = s[l] if s[l].isdigit() else '0'
-        print(f"({c})+", end='')
-        solve(l + 1, r)
-solve(0, len(s) - 1)
+ans = []
+for c in input():
+    t = c if c.isdigit() else '0'
+    ans.append(f"({t})+")
+ans.append("0")
+print(*ans, sep="")
