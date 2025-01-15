c539b49ecea75436c9aa2140d746b443544ea536
김주원 <66457470+juwkim@users.noreply.github.com>
2025-01-16 07:02:39 +0900

    [Bronze IV] Title: Yalda, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/33191. Yalda/Yalda.py
@@ -0,0 +1,9 @@
+b = int(input())
+if int(input()) <= b:
+    print("Watermelon")
+elif int(input()) <= b:
+    print("Pomegranates")
+elif int(input()) <= b:
+    print("Nuts")
+else:
+    print("Nothing")
