c8c69ba455f177d9950c0774072105fd075b4c12
김주원 <66457470+juwkim@users.noreply.github.com>
2025-01-26 13:08:39 +0900

    [Bronze II] Title: Integrity Overflow, Time: 104 ms, Memory: 110576 KB -BaekjoonHub

--- 백준/Bronze/33246. Integrity Overflow/Integrity Overflow.py
+++ 백준/Bronze/33246. Integrity Overflow/Integrity Overflow.py
@@ -1,17 +1,9 @@
-import sys
-input = lambda: sys.stdin.readline().rstrip()
-
 ans = "SYSTEM SECURE"
 n = int(input())
 password = input()
 for _ in range(n):
     s, p = input().split()
-    if p == "ALLOWED":
-        if len(s) != len(password) or sum(a != b for a, b in zip(s, password)) > 1:
-            ans = "INTEGRITY OVERFLOW"
-            break
-    else:
-        if len(s) == len(password) and sum(a != b for a, b in zip(s, password)) <= 1:
-            ans = "INTEGRITY OVERFLOW"
-            break
+    if (p[0] == "A") ^ (len(s) == len(password) and sum(a != b for a, b in zip(s, password)) < 2):
+        ans = "INTEGRITY OVERFLOW"
+        break
 print(ans)
