8047f1fc7ae36c06c90a5d937f503c0240eaa729
김주원 <66457470+juwkim@users.noreply.github.com>
2025-02-07 18:30:48 +0900

    [Silver II] Title: A1 Paper, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11218. A1 Paper/A1 Paper.py
+++ 백준/Silver/11218. A1 Paper/A1 Paper.py
@@ -1,12 +1,11 @@
 n, *A = map(int, open(0).read().split())
-ans, need = 0, 2
-l = 2 ** -.75
+ans, need, l = 0, 2, -1.75
 for num in A:
-    ans += l * need / 2
-    l /= 2 ** .5
+    ans += need * 2 ** l
     if num >= need:
         break
-    need = (need - num) << 1
+    need = need - num << 1
+    l -= .5
 else:
     ans = "impossible"
 print(ans)
