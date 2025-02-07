704b3690a18257966d01ad031cdc5c0df6f5e9fc
김주원 <66457470+juwkim@users.noreply.github.com>
2025-02-07 17:33:58 +0900

    [Silver II] Title: Taśma, Time: 288 ms, Memory: 175532 KB -BaekjoonHub

--- 백준/Silver/8618. Taśma/Taśma.py
+++ 백준/Silver/8618. Taśma/Taśma.py
@@ -4,12 +4,12 @@ input = sys.stdin.readline
 for _ in range(int(input())):
     n = int(input())
     a = [*map(int, input().split())]
-    if len(set(a)) == 1:
-        print("BRAK")
-    else:
-        def solve(l):
-            i = n - 1
-            while i > 1 and l[i] == l[0]:
-                i -= 1
-            return i
-        print(max(solve(a), solve(a[::-1])))
+    def solve(l):
+        i = n - 1
+        while i and l[i] == l[0]:
+            i -= 1
+        return i
+    ans = max(solve(a), solve(a[::-1]))
+    if ans == 0:
+        ans = "BRAK"
+    print(ans)
