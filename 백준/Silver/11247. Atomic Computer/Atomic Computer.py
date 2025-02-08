06d321a5d078f52288e49e091d97d5178c12dfcf
김주원 <66457470+juwkim@users.noreply.github.com>
2025-02-09 00:04:52 +0900

    [Silver II] Title: Atomic Computer, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11247. Atomic Computer/Atomic Computer.py
+++ 백준/Silver/11247. Atomic Computer/Atomic Computer.py
@@ -1,16 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-mem = {(0, 0): 1}
+mem = {}
 def solve(x, y):
     if (x, y) in mem:
         return mem[(x, y)]
     if x >= (1 << y) or x <= -(1 << y):
         return 0
+    if x == 0 or y == 0:
+        return 1
     if x & 1:
-        return solve(x + 1 >> 1, y - 1) + solve(x - 1 >> 1, y - 1)
-    return solve(x >> 1, y - 1)
-
+        mem[(x, y)] = solve(x + 1 >> 1, y - 1) + solve(x - 1 >> 1, y - 1)
+    else:
+        mem[(x, y)] = solve(x >> 1, y - 1)
+    return mem[(x, y)]
 for _ in range(int(input())):
     x, y = map(int, input().split())
     print(solve(x, y))
