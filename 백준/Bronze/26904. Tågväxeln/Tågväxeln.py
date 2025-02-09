5171ab78e86ca88a76f8d6ec47ca0020c940d643
김주원 <66457470+juwkim@users.noreply.github.com>
2025-02-09 20:39:58 +0900

    [Bronze II] Title: Tågväxeln, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/26904. Tågväxeln/Tågväxeln.py
+++ 백준/Bronze/26904. Tågväxeln/Tågväxeln.py
@@ -1,5 +1,2 @@
-n, m = map(int, input().split())
-ans = 1439 // m * 2
-if 1439 - 1439 % m >= 1439 // n * n:
-    ans -= 1
-print(ans)
+n,m=map(int,input().split())
+print(1439//m*2-(1439%m<=1439%n))
