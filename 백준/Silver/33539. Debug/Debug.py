110bc5255c3c0afaf239ac4201c871fa7b33a629
김주원 <66457470+juwkim@users.noreply.github.com>
2025-02-22 14:59:20 +0900

    [Silver IV] Title: Debug, Time: 88 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/33539. Debug/Debug.py
+++ 백준/Silver/33539. Debug/Debug.py
@@ -1,2 +1,2 @@
-n = int(input())
-print(("no", "yes")[n > 1 and all(n % i for i in range(2, int(n ** 0.5) + 1))])
+n=int(input())
+print(("no","yes")[n>1 and all(n%i for i in range(2,int(n**.5)+1))])
