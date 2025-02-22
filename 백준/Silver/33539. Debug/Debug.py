44e6a4bd9171b63efc050f46a5eb074154ec5507
김주원 <66457470+juwkim@users.noreply.github.com>
2025-02-22 14:58:59 +0900

    [Silver IV] Title: Debug, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/33539. Debug/Debug.py
+++ 백준/Silver/33539. Debug/Debug.py
@@ -1,6 +1,2 @@
 n = int(input())
-if n > 1 and all(n % i for i in range(2, int(n ** 0.5) + 1)):
-    ans = "yes"
-else:
-    ans = "no"
-print(ans)
+print(("no", "yes")[n > 1 and all(n % i for i in range(2, int(n ** 0.5) + 1))])
