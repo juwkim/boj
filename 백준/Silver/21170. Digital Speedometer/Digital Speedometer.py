a2e0602aabf517026ff8ec9c8f7ab83145483213
김주원 <66457470+juwkim@users.noreply.github.com>
2025-02-23 14:53:13 +0900

    [Silver II] Title: Digital Speedometer, Time: 96 ms, Memory: 109412 KB -BaekjoonHub

---
+++ 백준/Silver/21170. Digital Speedometer/Digital Speedometer.py
@@ -0,0 +1,17 @@
+tf, tr, *l = map(float, open(0))
+print(prv:=0)
+for idx in range(1, len(l)):
+    i = int(l[idx])
+    r = l[idx] - i
+    if 0 < l[idx] < 1:
+        cur = 1
+    elif r < tf:
+        cur = i
+    elif r > tr:
+        cur = i + 1
+    elif prv < i + tr:
+        cur = i
+    else:
+        cur = i + 1
+    print(cur)
+    prv = cur
