c214d92771571b5ca13157be397fcb82dcd0b606
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-12 04:19:25 +0900

    [Silver V] Title: 슈퍼 나이트, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/13437. 슈퍼 나이트/슈퍼 나이트.py
+++ 백준/Silver/13437. 슈퍼 나이트/슈퍼 나이트.py
@@ -1,16 +1,10 @@
 *l, N, M, k = map(int, input().split())
 a, b = sorted(l)
+ans = 0
 match k:
-    case 0 | 1 | 5 | 7:
-        ans = 0
-    case 2:
-        ans = 4 * a ** 2
-    case 3:
-        ans = 8 * a * (b - a)
-    case 4:
-        ans = 2 * a * (N + M - 4 * b) + 4 * (b - a) ** 2
-    case 6:
-        ans = 2 * (b - a) * (N + M - 4 * b)
-    case 8:
-        ans = (N - 2 * b) * (M - 2 * b)
+    case 2: ans = 4 * a ** 2
+    case 3: ans = 8 * a * (b - a)
+    case 4: ans = 2 * a * (N + M - 4 * b) + 4 * (b - a) ** 2
+    case 6: ans = 2 * (b - a) * (N + M - 4 * b)
+    case 8: ans = (N - 2 * b) * (M - 2 * b)
 print(ans)
