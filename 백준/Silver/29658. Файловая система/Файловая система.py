29fdfc12e8de8bb9db4d37e3329d1c0fd2cf3ca1
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-12 19:43:21 +0900

    [Silver V] Title: Файловая система, Time: 156 ms, Memory: 113236 KB -BaekjoonHub

--- 백준/Silver/29658. Файловая система/Файловая система.py
+++ 백준/Silver/29658. Файловая система/Файловая система.py
@@ -1,33 +1,23 @@
 import re
 
-def is_valid_8_3(s):
+def is_valid(s):
     if re.sub(r"[A-Z0-9'\-!#\$%&()@^_`{}.~]", "", s) != "" or s.count('.') > 1:
         return False
-    if '.' not in s:
-        return len(s) <= 8
-    return len(s) - 4 <= s.index('.') <= 8
+    if '.' in s:
+        return len(s) - 4 <= s.index('.') <= 8
+    return len(s) <= 8
 
 def solve():
     file_name = input().upper()
-    
-    if is_valid_8_3(file_name):
+    if is_valid(file_name):
         return file_name
-    
-    file_name = file_name.replace(" ", "")
-    understike_replace = "\"/\\[]:;=,"
-    file_name = ''.join('_' if c in understike_replace else c for c in file_name)
-    
-    while file_name and file_name[-1] == '.':
-        file_name = file_name[:-1]
-    
-    p = file_name.rfind('.')
-    if p != -1:
-        s1 = file_name[:p].replace('.', '')
-        s2 = file_name[p + 1:]
-        file_name = s1[:6] + "~1." + s2[:3]
-    else:
+    file_name = ''.join('_' if c in "\"/\\[]:;=," else c for c in file_name.replace(" ", "")).rstrip('.')
+    idx = file_name.rfind('.')
+    if idx == -1:
         file_name = file_name[:6] + "~1"
-    
+    else:
+        s1 = file_name[:idx].replace('.', '')
+        s2 = file_name[idx + 1:]
+        file_name = s1[:6] + "~1." + s2[:3]
     return file_name
-
 print(solve())
