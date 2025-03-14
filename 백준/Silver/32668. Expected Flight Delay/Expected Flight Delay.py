6bb36ac82a9f1863789994728f50c975c69c6c33
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-15 05:42:22 +0900

    [Silver III] Title: Expected Flight Delay, Time: 88 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/32668. Expected Flight Delay/Expected Flight Delay.py
+++ 백준/Silver/32668. Expected Flight Delay/Expected Flight Delay.py
@@ -1,12 +1,9 @@
 day, *l = map(int, input().split())
 pwday, pwend, pimp = map(lambda x: x / 100, l)
 day -= 1
-p = pwend, pwday, pwday, pwday, pwday, pwday, pwend
-cur, i = 0, 1
-while True:
-    cur = p[(day - i) % 7] * (1 - cur) + cur
-    if cur > pimp:
-        break
+cur, i = 0, 0
+while cur < pimp:
     i += 1
+    cur += (pwend, pwday)[0 < (day - i) % 7 < 6] * (1 - cur)
 day_name = "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
 print(f"Try to leave on {day_name[(day - i) % 7]}, {i} day{('s', '')[i == 1]} before the {day_name[day]} meeting.")
