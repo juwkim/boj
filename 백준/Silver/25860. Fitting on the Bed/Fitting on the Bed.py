9f8d1c7114c50be2c545d949a217473d9fb4c887
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-14 23:01:48 +0900

    [Silver II] Title: Fitting on the Bed, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/25860. Fitting on the Bed/Fitting on the Bed.py
+++ 백준/Silver/25860. Fitting on the Bed/Fitting on the Bed.py
@@ -1,7 +1,7 @@
 from math import cos, sin, radians
 
 L, W, H, x, y, a = map(int, open(0).read().split())
-if -1e-9 <= x + H * cos(radians(a)) <= W + 1e-9 and -1e-9 <= y + H * sin(radians(a)) <= L + 1e-9:
+if -1e-9 < x + H * cos(radians(a)) < W + 1e-9 and -1e-9 < y + H * sin(radians(a)) < L + 1e-9:
     print('YES')
 else:
     print('NO')
