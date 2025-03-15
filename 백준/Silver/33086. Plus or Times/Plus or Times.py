281f1df663123f536483078adb7bd24a04416269
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-16 01:32:49 +0900

    [Silver III] Title: Plus or Times, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/33086. Plus or Times/Plus or Times.py
@@ -0,0 +1,9 @@
+N, P = map(int, input().split())
+lo, hi = P, P
+for _ in range(N):
+    op1, num1, op2, num2 = input().split()
+    if op1 == 'x': op1 = '*'
+    if op2 == 'x': op2 = '*'
+    l = eval(f"{lo}{op1}{num1}"), eval(f"{lo}{op2}{num2}"), eval(f"{hi}{op1}{num1}"), eval(f"{hi}{op2}{num2}")
+    lo, hi = min(l), max(l)
+print(max(lo, hi))
