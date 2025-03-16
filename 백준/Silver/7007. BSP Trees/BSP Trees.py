cbb645fe1872f1ee277644d1b7db6e4cf83fdb11
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-17 07:00:36 +0900

    [Silver II] Title: BSP Trees, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/7007. BSP Trees/BSP Trees.py
+++ 백준/Silver/7007. BSP Trees/BSP Trees.py
@@ -1,9 +1,9 @@
 n = int(input())
-xP, zP = [0] * n, [0] * n
+x_coords, z_coords = [0] * n, [0] * n
 for i in range(n):
     *_, x, z = map(int, input().split())
-    xP[i] = x
-    zP[i] = z
+    x_coords[i] = x
+    z_coords[i] = z
 
 p = int(input())
 A, B, C = [0] * p, [0] * p, [0] * p
@@ -13,39 +13,36 @@ for i in range(p):
     A[i] = z1 - z2
     B[i] = x2 - x1
     C[i] = x1 * z2 - z1 * x2
-    if B[i] < 0: A[i], B[i], C[i] = -A[i], -B[i], -C[i]
+    if B[i] < 0:
+        A[i], B[i], C[i] = -A[i], -B[i], -C[i]
 
 order = list(range(n))
-isDiv = bytearray(n + 1)
-isDiv[0], isDiv[n] = True, True
+is_dividing = bytearray(n + 1)
+is_dividing[0], is_dividing[n] = True, True
 
 for i in range(p):
-    pnc = 0
-    while pnc < n:
-        next_div = pnc + 1
-        while not isDiv[next_div]:
+    current_index = 0
+    while current_index < n:
+        next_div = current_index + 1
+        while not is_dividing[next_div]:
             next_div += 1
-        
-        if next_div == pnc + 1:
-            pnc = next_div
+        if next_div == current_index + 1:
+            current_index = next_div
             continue
         
-        xianNo, houNo = 0, 0
-        tempHou = [0] * n
+        front_count, back_count = 0, 0
+        temp_back = [0] * n
         
-        for j in range(next_div - pnc):
-            if A[i] * xP[order[pnc + j]] + B[i] * zP[order[pnc + j]] + C[i] > 0:
-                order[pnc + xianNo] = order[pnc + j]
-                xianNo += 1
+        for j in range(next_div - current_index):
+            if A[i] * x_coords[order[current_index + j]] + B[i] * z_coords[order[current_index + j]] + C[i] > 0:
+                order[current_index + front_count] = order[current_index + j]
+                front_count += 1
             else:
-                tempHou[houNo] = order[pnc + j]
-                houNo += 1
-        
-        isDiv[pnc + xianNo] = True
-        
-        for j in range(houNo):
-            order[pnc + xianNo + j] = tempHou[j]
-        
-        pnc = next_div
+                temp_back[back_count] = order[current_index + j]
+                back_count += 1
+        is_dividing[current_index + front_count] = True
+        for j in range(back_count):
+            order[current_index + front_count + j] = temp_back[j]        
+        current_index = next_div
 
 print("".join(chr(order[i] + ord('A')) for i in range(n)))
