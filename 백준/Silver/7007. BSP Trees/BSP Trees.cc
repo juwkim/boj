e5f0ba6b721b8226f08645b14b8e528d59a2dc82
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-17 06:46:25 +0900

    [Silver II] Title: BSP Trees, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/7007. BSP Trees/BSP Trees.cc
+++ 백준/Silver/7007. BSP Trees/BSP Trees.cc
@@ -3,51 +3,51 @@
 using namespace std;
 
 int main() {
-	int nP;
-	scanf("%d", &nP);
+	int n;
+	scanf("%d", &n);
 	int xP[26], zP[26];
-	for(int i = 0; i < nP; i++){
+	for(int i = 0; i < n; ++i){
 		int num;
 		scanf("%d", &num);
-		for(int j = 0; j < num; j++){
-			scanf("%d%d", &xP[i], &zP[i]);
+		for(int j = 0; j < num; ++j){
+			scanf("%d %d", &xP[i], &zP[i]);
 		}
 	}
-	int nL;
-	scanf("%d", &nL);
+	int p;
+	scanf("%d", &p);
 	int A[26], B[26], C[26];
-	for(int i = 0; i < nL; i++){
+	for(int i = 0; i < p; ++i){
 		int x1, z1, x2, z2;
-		scanf("%d%d%d%d", &x1, &z1, &x2, &z2);
-		A[i] = z1-z2, B[i] = x2-x1, C[i] = x1*z2-z1*x2;
-		if(B[i] < 0){
+		scanf("%d %d %d %d", &x1, &z1, &x2, &z2);
+		A[i] = z1 - z2, B[i] = x2 - x1, C[i] = x1 * z2 - z1 * x2;
+		if (B[i] < 0) {
 			A[i] = -A[i], B[i] = -B[i], C[i] = -C[i];
 		}
 	}
-	int order[26];
+
+    int order[26];
 	bool isDiv[27] = {0};
-	isDiv[0] = 1, isDiv[nP] = 1;
-	for(int i = 0; i < nP; i++) order[i] = i;
-	for(int i = 0; i < nL; i++){
+	isDiv[0] = 1, isDiv[n] = 1;
+	for(int i = 0; i < n; i++) order[i] = i;
+	for(int i = 0; i < p; i++){
 		int pnc = 0;
-		while(pnc < nP){
+		while(pnc < n){
 			int next;
-			for(int j = pnc+1; ; j++){
+			for(int j = pnc + 1; ; ++j){
 				if(isDiv[j]) {
 					next = j;
 					break;
 				}
 			}
-			if(next == pnc+1){
-				//区间长度为1，不用再分
+			if(next == pnc + 1){
 				pnc = next;
 				continue;
 			}
 			int xianNo = 0;
 			int tempHou[26];
 			int houNo = 0;
-			for(int j = 0; j < next-pnc; j++){
-				if(A[i] * xP[order[pnc+j]] + B[i] * zP[order[pnc+j]]+ C[i] > 0){
+			for(int j = 0; j < next - pnc; ++j){
+				if (A[i] * xP[order[pnc+j]] + B[i] * zP[order[pnc+j]]+ C[i] > 0){
 					order[pnc+xianNo] = order[pnc+j];
 					xianNo ++;
 				}
@@ -63,7 +63,7 @@ int main() {
 			pnc = next;
 		}
 	}
-	for(int i = 0; i < nP; i++){
+	for(int i = 0; i < n; i++){
 		printf("%c", order[i]+'A');
 	}
 	printf("\n");
