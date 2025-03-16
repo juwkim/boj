be626ba7f7cc52a5c8a3b6b410888318dd50c5af
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-17 07:01:31 +0900

    [Silver II] Title: BSP Trees, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/7007. BSP Trees/BSP Trees.py
+++ 백준/Silver/7007. BSP Trees/BSP Trees.py
@@ -1,48 +1,34 @@
 n = int(input())
-x_coords, z_coords = [0] * n, [0] * n
-for i in range(n):
-    *_, x, z = map(int, input().split())
-    x_coords[i] = x
-    z_coords[i] = z
+xC, zC = [0] * n, [0] * n
+for i in range(n): *_, x, z = map(int, input().split()); xC[i], zC[i] = x, z
 
 p = int(input())
 A, B, C = [0] * p, [0] * p, [0] * p
-
 for i in range(p):
     x1, z1, x2, z2 = map(int, input().split())
-    A[i] = z1 - z2
-    B[i] = x2 - x1
-    C[i] = x1 * z2 - z1 * x2
-    if B[i] < 0:
-        A[i], B[i], C[i] = -A[i], -B[i], -C[i]
+    A[i], B[i], C[i] = z1 - z2, x2 - x1, x1 * z2 - z1 * x2
+    if B[i] < 0: A[i], B[i], C[i] = -A[i], -B[i], -C[i]
 
 order = list(range(n))
-is_dividing = bytearray(n + 1)
-is_dividing[0], is_dividing[n] = True, True
+isD = bytearray(n + 1)
+isD[0], isD[n] = 1, 1
 
 for i in range(p):
-    current_index = 0
-    while current_index < n:
-        next_div = current_index + 1
-        while not is_dividing[next_div]:
-            next_div += 1
-        if next_div == current_index + 1:
-            current_index = next_div
-            continue
+    idx = 0
+    while idx < n:
+        nxt = idx + 1
+        while not isD[nxt]: nxt += 1
+        if nxt == idx + 1: idx = nxt; continue
         
-        front_count, back_count = 0, 0
-        temp_back = [0] * n
-        
-        for j in range(next_div - current_index):
-            if A[i] * x_coords[order[current_index + j]] + B[i] * z_coords[order[current_index + j]] + C[i] > 0:
-                order[current_index + front_count] = order[current_index + j]
-                front_count += 1
+        fCnt, bCnt, tmpB = 0, 0, [0] * n
+        for j in range(nxt - idx):
+            if A[i] * xC[order[idx + j]] + B[i] * zC[order[idx + j]] + C[i] > 0:
+                order[idx + fCnt] = order[idx + j]; fCnt += 1
             else:
-                temp_back[back_count] = order[current_index + j]
-                back_count += 1
-        is_dividing[current_index + front_count] = True
-        for j in range(back_count):
-            order[current_index + front_count + j] = temp_back[j]        
-        current_index = next_div
+                tmpB[bCnt] = order[idx + j]; bCnt += 1
+        
+        isD[idx + fCnt] = 1
+        for j in range(bCnt): order[idx + fCnt + j] = tmpB[j]
+        idx = nxt
 
-print("".join(chr(order[i] + ord('A')) for i in range(n)))
+print("".join(chr(order[i] + 65) for i in range(n)))
