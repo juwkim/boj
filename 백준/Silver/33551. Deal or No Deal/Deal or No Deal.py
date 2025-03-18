fe7287c69bdcde2fadb645d7074c9e12124ffec7
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-19 04:29:22 +0900

    [Silver IV] Title: Deal or No Deal, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/33551. Deal or No Deal/Deal or No Deal.py
+++ 백준/Silver/33551. Deal or No Deal/Deal or No Deal.py
@@ -4,8 +4,8 @@ input = sys.stdin.readline
 n = int(input())
 m = [*map(float, input().split())]
 a, q = map(int, input().split())
-me = m[a - 1]
-print(f"{me:.2f}")
+ans = m[a - 1]
+print(f"{ans:.2f}")
 cnt, m[a - 1] = n - 1, 0
 for _ in range(q):
     cmd, x = input().split()
@@ -15,6 +15,4 @@ for _ in range(q):
     elif x == 'D':
         ans = 0.9 * sum(m) / cnt
         break
-else:
-    ans = me
 print(f"{ans:.2f}")
