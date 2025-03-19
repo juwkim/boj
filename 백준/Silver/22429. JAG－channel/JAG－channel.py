6815d01d1f2b9343f8979a4dc1813f6ec210edf3
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-20 04:43:58 +0900

    [Silver II] Title: JAG-channel, Time: 248 ms, Memory: 117520 KB -BaekjoonHub

--- 백준/Silver/22429. JAG－channel/JAG－channel.py
+++ 백준/Silver/22429. JAG－channel/JAG－channel.py
@@ -2,29 +2,26 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 while n:=int(input()):
-    lines = [input() for _ in range(n)]
-    result = []
-    for i, line in enumerate(lines):
-        depth = line.count('.')
-        content = line.lstrip('.')
-        
-        if depth == 0:
-            result.append(content)
+    res = []
+    for _ in range(n):
+        s = input()
+        depth, content = s.count('.'), s.lstrip('.')
+        if depth:
+            res.append(' ' * (depth - 1) + '+' + content)
         else:
-            prefix = ' ' * (depth - 1) + '+'
-            result.append(prefix + content)
+            res.append(content)
 
-    for i in range(len(result) - 1):
-        curr_depth = result[i].find('+')
-        if curr_depth == -1:
+    for i in range(len(res) - 1):
+        d = res[i].find('+')
+        if d == -1:
             continue
         j = i + 1
-        while j < len(result) and len(result[j]) > curr_depth and result[j][curr_depth] == ' ':
+        while j < len(res) and len(res[j]) > d and res[j][d] == ' ':
             j += 1
-        if j < len(result) and len(result[j]) > curr_depth and result[j][curr_depth] == '+':
+        if j < len(res) and len(res[j]) > d and res[j][d] == '+':
             for k in range(i + 1, j):
-                if curr_depth:
-                    result[k] = result[k][:curr_depth] + '|' + result[k][curr_depth + 1:]
+                if d:
+                    res[k] = res[k][:d] + '|' + res[k][d + 1:]
                 else:
-                    result[k] = '|' + result[k][1:]
-    print(*result, sep='\n')
+                    res[k] = '|' + res[k][1:]
+    print(*res, sep='\n')
