20acb044b4438da49bd6510bb7c18569ae5bf036
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-21 06:34:31 +0900

    [Silver IV] Title: Химеры, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/29513. Химеры/Химеры.py
+++ 백준/Silver/29513. Химеры/Химеры.py
@@ -1,2 +1,2 @@
-a, b = map(int, input().split())
-print(sum(i in {1, 3, 9, 11, 33, 99} for i in range(a, b + 1)))
+a,b=map(int,input().split())
+print(sum(i in{1,3,9,11,33,99}for i in range(a,b+1)))
