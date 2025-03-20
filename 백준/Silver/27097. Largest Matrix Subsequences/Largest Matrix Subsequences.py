47dcecc584c39f2599e923fbc702516b8ddef9f5
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-21 03:52:36 +0900

    [Silver II] Title: Largest Matrix Subsequences, Time: 388 ms, Memory: 112820 KB -BaekjoonHub

--- 백준/Silver/27097. Largest Matrix Subsequences/Largest Matrix Subsequences.py
+++ 백준/Silver/27097. Largest Matrix Subsequences/Largest Matrix Subsequences.py
@@ -1,28 +1,27 @@
 R, C, *nums = map(int, open(0).read().split())
-ans, Max = [], 1
 a = [nums[i:i+C] for i in range(0, R*C, C)]
-px = [[0] * C for _ in range(R + 1)]
+px1 = [[0] * C for _ in range(R + 1)]
 for i in range(1, R + 1):
     cur = 0
     for j in range(1, C):
         cur += a[i-1][j-1] >= a[i-1][j]
-        px[i][j] = cur + px[i-1][j]
-
-# for l in px:
-#     print(*l)
-# print()
-        
+        px1[i][j] = cur + px1[i-1][j]
+px2 = [[0] * (C + 1) for _ in range(R)]
+for j in range(1, C + 1):
+    cur = 0
+    for i in range(1, R):
+        cur += a[i-1][j-1] >= a[i][j-1]
+        px2[i][j] = cur + px2[i][j-1]
+ans, Max = [], 1
 for i in range(R):
     for j in range(C):
         for k in range((Max + C - j - 1) // (C - j), R - i + 1):
             for l in range((Max + k - 1) // k, C - j + 1):
                 def solve():
-                    if px[i+k][j+l-1] - px[i+k][j] - px[i][j+l-1] + px[i][j]:
+                    if px1[i+k][j+l-1] - px1[i+k][j] - px1[i][j+l-1] + px1[i][j]:
                         return False
-                    for s in range(i, i + k - 1):
-                        for t in range(j, j + l):
-                            if a[s][t] >= a[s+1][t]:
-                                return False
+                    if px2[i+k-1][j+l] - px2[i][j+l] - px2[i+k-1][j] + px2[i][j]:
+                        return False
                     return True
                 if solve():
                     if k * l > Max:
