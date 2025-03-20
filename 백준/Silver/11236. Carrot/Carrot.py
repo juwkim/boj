342231b5e3f798e887ac2b2b5860512215a68c0e
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-20 19:15:38 +0900

    [Silver II] Title: Carrot, Time: 992 ms, Memory: 297916 KB -BaekjoonHub

--- 백준/Silver/11236. Carrot/Carrot.py
+++ 백준/Silver/11236. Carrot/Carrot.py
@@ -1,16 +1,11 @@
-T, *nums = map(int, open(0))
-p = [True] * 10000001
+T, *Ns = map(int, open(0))
+p = [1] * 10000001
 for i in range(2, 3163):
     if p[i]:
-        for j in range(i * i, 10000001, i):
-            p[j] = False
-
+        for j in range(i * i, 10000001, i): p[j] = 0
 a = [0] * 10000001
 a[1], a[2] = 1, 1
 for i in range(3, 10000001):
-    if p[i]:
-        a[i] = a[i - 2] + 1
-    else:
-        a[i] = a[i - 1] + 1
-for num in nums:
-    print(a[num])
+    if p[i]: a[i] = a[i - 2] + 1
+    else: a[i] = a[i - 1] + 1
+for N in Ns: print(a[N])
