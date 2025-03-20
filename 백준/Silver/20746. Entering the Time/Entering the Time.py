d542e803f96ef3475076e159fd2dc8dcfba14da7
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-21 00:18:28 +0900

    [Silver V] Title: Entering the Time, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/20746. Entering the Time/Entering the Time.py
+++ 백준/Silver/20746. Entering the Time/Entering the Time.py
@@ -1,26 +1,16 @@
 ans = [input()]
 t1, t2 = [*ans[0]], [*input()]
-for i in (4, 3, 1, 0):
-    t1[i] = int(t1[i])
-    t2[i] = int(t2[i])
 order = 4, 3, 1, 0
-if t1[0] == 2 and t2[0] != 2:
-    order = 4, 3, 0, 1
+if t1[0] == '2' and t2[0] != '2': order = 4, 3, 0, 1
 for i in order:
-    if i == 4 or i == 1:
+    t1[i], t2[i] = int(t1[i]), int(t2[i])
+    if i in (1, 4):
         while t1[i] != t2[i]:
-            if (t2[i] - t1[i]) % 10 < 5:
-                t1[i] = (t1[i] + 1) % 10
-            else:
-                t1[i] = (t1[i] - 1) % 10
+            t1[i] = (t1[i] + (-1, 1)[(t2[i] - t1[i]) % 10 < 5]) % 10
             ans.append("".join(map(str, t1)))
     else:
         while t1[i] != t2[i]:
-            if t1[i] < t2[i]:
-                t1[i] += 1
-            else:
-                t1[i] -= 1
+            t1[i] += (-1, 1)[t1[i] < t2[i]]
             ans.append("".join(map(str, t1)))
 print(len(ans))
-for l in ans:
-    print(*l, sep='')
+for l in ans: print(*l, sep='')
