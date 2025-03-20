4beb722e68d37ef098acaf79a08dbd5305d23502
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-21 02:32:37 +0900

    [Silver IV] Title: Bokhyllor, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/26929. Bokhyllor/Bokhyllor.py
+++ 백준/Silver/26929. Bokhyllor/Bokhyllor.py
@@ -1,14 +1,13 @@
 a, b, c, S = map(int, open(0).read().split())
 ans = 0
-while a + 2 * b + 3 * c > S:
+while a | b | c:
     ans += 1
     cnt3 = min(c, S // 3)
-    cnt2 = min(b, (S - 3 * cnt3) // 2)
+    cnt2 = min(b, S - 3 * cnt3 >> 1)
     cnt1 = min(a, S - 2 * cnt2 - 3 * cnt3)
     if cnt3 and b - cnt2 >= 2 and (cnt1 or S > 2 * cnt2 + 3 * cnt3):
-        cnt1 -= 1
+        cnt1 = max(0, cnt1 - 1)
         cnt2 += 2
         cnt3 -= 1
     a, b, c = a - cnt1, b - cnt2, c - cnt3
-if a | b | c: ans += 1
 print(ans)
