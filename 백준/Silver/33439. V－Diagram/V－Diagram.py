eefb03625c2df857c967ef432e20d58789fa8298
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-22 01:42:39 +0900

    [Silver III] Title: V-Diagram, Time: 276 ms, Memory: 157500 KB -BaekjoonHub

--- 백준/Silver/33439. V－Diagram/V－Diagram.py
+++ 백준/Silver/33439. V－Diagram/V－Diagram.py
@@ -5,15 +5,5 @@ for _ in range(int(input())):
     n = int(input())
     a = [*map(int, input().split())]
     i = 1
-    while a[i] > a[i - 1] or a[i] > a[i + 1]:
-        i += 1
-    ans = 0
-    tot = sum(a[:i + 1])
-    for j in range(i + 1, n): 
-        tot += a[j]
-        ans = max(ans, tot / (j + 1))
-    tot = sum(a[i:])
-    for j in range(i - 1, -1, -1):
-        tot += a[j]
-        ans = max(ans, tot / (n - j))    
-    print(ans)
+    while a[i] > a[i - 1] or a[i] > a[i + 1]: i += 1
+    print(max(sum(a) / n, sum(a[i-1:]) / (n - i + 1), sum(a[:i+2]) / (i + 2)))
