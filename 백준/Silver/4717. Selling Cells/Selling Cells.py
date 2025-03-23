08df96e0cb88d06f6b14b0950c86ef4517d96dff
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-24 07:32:50 +0900

    [Silver IV] Title: Selling Cells, Time: 176 ms, Memory: 111600 KB -BaekjoonHub

--- 백준/Silver/4717. Selling Cells/Selling Cells.py
+++ 백준/Silver/4717. Selling Cells/Selling Cells.py
@@ -5,12 +5,9 @@ g = lambda: [*map(float, input().split())]
 while N:=int(input()):
     x, y, r = g()
     nums = [g() for _ in range(N - 1)]
-    cnt = 0
-    for dx in range(-100, 101):
-        for dy in range(-100, 101):
-            p, q = x + dx * r / 100, y + dy * r / 100
-            if (p - x) ** 2 + (q - y) ** 2 > r ** 2:
-                continue
-            if any((p - a) ** 2 + (q - b) ** 2 <= r ** 2 for a, b, r in nums):
-                cnt += 1
-    print(f"{cnt / 31416:.2f}")
+    ans = 0
+    for dx in range(-50, 51):
+        for dy in range(-50, 51):
+            p, q = x + dx * r / 50, y + dy * r / 50
+            ans += (p - x) ** 2 + (q - y) ** 2 <= r ** 2 and any((p - a) ** 2 + (q - b) ** 2 <= r ** 2 for a, b, r in nums)
+    print(f"{ans / 7854:.2f}")
