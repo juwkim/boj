1b05b076c0dee7e24059511f0ceda26ca5faa2a3
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-24 09:34:36 +0900

    [Silver II] Title: Metaprogramavimas, Time: 96 ms, Memory: 108384 KB, Score: 17 point -BaekjoonHub

--- 백준/Silver/7266. Metaprogramavimas/Metaprogramavimas.py
+++ 백준/Silver/7266. Metaprogramavimas/Metaprogramavimas.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-nums = [[*map(int, input().split())] for _ in range(N)]
+nums = sorted([*map(int, input().split())] for _ in range(N))
 if N == 1:
     a, b = nums[0]
     if a == b:
@@ -13,6 +13,7 @@ if N == 1:
         print(f"3\nmultiply 0\nadd {b}\nprint")
 else:
     (x1, y1), (x2, y2) = nums[:2]
+    assert x1 != x2
     if any((x2 - x1) * (y3 - y1) != (x3 - x1) * (y2 - y1) for x3, y3 in nums[2:]):
         print(-1)
     else:
@@ -21,7 +22,14 @@ else:
         num2 = y1 - num1 * x1
         if num1 < 0 or num2 < 0:
             print(-1)
-        elif num2:
-            print(f"3\nmultiply {num1}\nadd {num2}\nprint")
         else:
-            print(f"2\nmultiply {num1}\nprint")
+            if num1 == 1:
+                if num2:
+                    print(f"2\nadd {num2}\nprint")
+                else:
+                    print(f"1\nprint")
+            else:
+                if num2:
+                    print(f"3\nmultiply {num1}\nadd {num2}\nprint")
+                else:
+                    print(f"2\nmultiply {num1}\nprint")
