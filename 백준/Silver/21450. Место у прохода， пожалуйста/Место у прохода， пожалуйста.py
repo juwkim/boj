70c99b5d5528376479a01c301b46c0aba5d657b3
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-26 07:15:11 +0900

    [Silver V] Title: Место у прохода, пожалуйста, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/21450. Место у прохода， пожалуйста/Место у прохода， пожалуйста.py
+++ 백준/Silver/21450. Место у прохода， пожалуйста/Место у прохода， пожалуйста.py
@@ -1,9 +1,8 @@
 n, l, w, x, y, a = map(int, input().split())
-
-if n <= ((w - a) // y) * (l // x):
+if n > ((w - a) // y) * (l // x):
+    print(-1)
+else:
     col = l // x
     row = (n + col - 1) // col
     alley = (w - row * y) // a
     print(min(n, min(row, 2 * alley) * col))
-else:
-    print(-1)
