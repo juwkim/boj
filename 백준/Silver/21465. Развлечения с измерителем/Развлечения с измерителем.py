87aa912aca840fa169497f0f3b694e6fca0f29e2
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-27 00:37:58 +0900

    [Silver III] Title: Развлечения с измерителем, Time: 252 ms, Memory: 168340 KB -BaekjoonHub

--- 백준/Silver/21465. Развлечения с измерителем/Развлечения с измерителем.py
+++ 백준/Silver/21465. Развлечения с измерителем/Развлечения с измерителем.py
@@ -1,5 +1,3 @@
-from math import sqrt
-
 n = int(input())
 nums = [[*map(int, input().split())] for _ in range(n)]
 tu = [set() for _ in range(n)]
@@ -8,10 +6,6 @@ for i in range(n - 1):
         num = (nums[i][0] - nums[j][0]) ** 2 + (nums[i][1] - nums[j][1]) ** 2
         tu[i].add(num)
         tu[j].add(num)
-ans = []
-for num in sorted(tu[0]):
-    if all(num in tu[i] for i in range(1, n)):
-        ans.append(num)
+ans = [num**.5 for num in sorted(tu[0]) if all(num in tu[i] for i in range(1, n))]
 print(len(ans))
-for num in ans:
-    print(sqrt(num))
+print(*ans, sep='\n')
