c95704c6b0fc15ee99c651b2f4859bb66d3042d0
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-27 08:49:53 +0900

    [Silver II] Title: Ландшафтный дизайн, Time: 156 ms, Memory: 132740 KB -BaekjoonHub

--- 백준/Silver/28910. Ландшафтный дизайн/Ландшафтный дизайн.py
+++ 백준/Silver/28910. Ландшафтный дизайн/Ландшафтный дизайн.py
@@ -1,10 +1,6 @@
 n, k, *a = map(int, open(0).read().split())
-ans = 1e18
-for _ in range(2):
-    b = a[:]
-    for i in range(1, n, 2):
-        b[i] += k
-    m = sorted(b)[n >> 1]
-    ans = min(ans, sum(abs(num - m) for num in b))
-    k = -k
-print(ans)
+def solve(p):
+    b = sorted(num + p * (i & 1) for i, num in enumerate(a))
+    m = b[n >> 1]
+    return sum(abs(num - m) for num in b)
+print(min(solve(k), solve(-k)))
