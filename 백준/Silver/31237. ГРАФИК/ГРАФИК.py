21c786fc146af3b4fd340ce8e86c4125f609c63e
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-28 03:51:00 +0900

    [Silver V] Title: ГРАФИК, Time: 112 ms, Memory: 111792 KB -BaekjoonHub

--- 백준/Silver/31237. ГРАФИК/ГРАФИК.py
+++ 백준/Silver/31237. ГРАФИК/ГРАФИК.py
@@ -1,50 +1,30 @@
 import sys
-input = lambda: sys.stdin.readline().rstrip()
-g = lambda: [*map(int, input().split())]
-from itertools import combinations
 import datetime
+from itertools import combinations
+input = lambda: sys.stdin.readline().rstrip()
 
-off = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday").index(input())
-day, monthname = input().split()
-day = int(day)
-dic = {
-    "January": 1,
-    "February": 2,
-    "March": 3,
-    "April": 4,
-    "May": 5,
-    "June": 6,
-    "July": 7,
-    "August": 8,
-    "September": 9,
-    "October": 10,
-    "November": 11,
-    "December": 12
+day_names = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+month_dict = {
+    "January": 1, "February": 2, "March": 3, "April": 4, "May": 5, "June": 6,
+    "July": 7, "August": 8, "September": 9, "October": 10, "November": 11, "December": 12
 }
-month = dic[monthname]
-base = datetime.date(2012, 1, 1)
-start = datetime.date(2012, 9, 16)
-end = datetime.date(2012, month, day)
-cnt = [0] * 7
-i = off + 1
-while start < end:
-    cnt[i % 7] += 1
-    i += 1
-    start += datetime.timedelta(days=1)
-ans = 0
+
+off = day_names.index(input())
+day, month_name = input().split()
+month = month_dict[month_name]
+
+date_start = datetime.date(2012, 9, 16)
+date_end = datetime.date(2012, month, int(day))
+
+count = [0] * 7
+for i, date in enumerate(range((date_end - date_start).days), start=off + 1):
+    count[i % 7] += 1
+
 X, Y = map(int, input().split())
-for l in range(1, 8):
-    for c in combinations(range(7), l):
-        if X <= sum(cnt[i] for i in c) <= Y:
-            ans = c
-            break
-    else:
-        continue
-    break
-if ans:
-    print(len(ans))
-    d = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    for i in ans:
-        print(d[i])
-else:
-    print(0)
+for size in range(1, 8):
+    for comb in combinations(range(7), size):
+        if X <= sum(count[i] for i in comb) <= Y:
+            print(size)
+            print(*[day_names[i] for i in comb], sep="\n")
+            exit()
+print(0)
