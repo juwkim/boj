b8d9efa76e941a0da6e879b13133001a67766fb5
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-30 03:00:43 +0900

    [Silver III] Title: Windows, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/4051. Windows/Windows.py
+++ 백준/Silver/4051. Windows/Windows.py
@@ -10,8 +10,8 @@ while n:=int(input()):
         cr, cc = g()
         ans = "background"
         for k in range(n - 1, -1, -1):
-            r, c, h, w = windows[k]
-            if r <= cr < r + w and c <= cc < c + h:
+            r, c, w, h = windows[k]
+            if r <= cr < r + h and c <= cc < c + w:
                 ans = f"window {k + 1}"
                 break
         print(ans)
