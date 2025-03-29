e36fe30be279366b23ef8154b6ccf307fb5e21ef
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-30 03:33:15 +0900

    [Silver III] Title: Таймер, Time: 128 ms, Memory: 111660 KB -BaekjoonHub

--- 백준/Silver/22109. Таймер/Таймер.py
+++ 백준/Silver/22109. Таймер/Таймер.py
@@ -4,8 +4,8 @@ input = sys.stdin.readline
 for _ in range(int(input())):
     x, y, k, t = map(int, input().split())
     r = (x - 1) % y + 1
-    if t - y + r >= k:
-        x += y * k + (t - k)
-    else:
+    if t < k + y - r:
         x += (y - 1) * min(k - 1, t) + t + y - r
+    else:
+        x += (y - 1) * k + t
     print(x)
