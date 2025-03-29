8178c829a986b7f412c227ede645db409e92a20e
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-29 12:10:55 +0900

    [Silver III] Title: Занос, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/20476. Занос/Занос.py
+++ 백준/Silver/20476. Занос/Занос.py
@@ -1,10 +1,3 @@
-v, x, y, a, b = map(int, input().split())
-if v * v < 2 * b * x:
-    t = v / (2 * b) + x / v
-else:
-    t = (2 * x / b) **.5
-if v * v < 2 * a * y:
-    t += v / (2 * a) + y / v
-else:
-    t += (2 * y / a) **.5
-print(t)
+v,x,y,a,b=map(int,input().split())
+f=lambda p,q:((2*q/p)**.5,v/p/2+q/v)[v*v<2*p*q]
+print(f(b,x)+f(a,y))
