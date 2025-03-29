84b24b04cd84a13339b388010f8a6c3248795f91
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-30 08:56:01 +0900

    [Silver III] Title: 우주 여행, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/33676. 우주 여행/우주 여행.py
+++ 백준/Silver/33676. 우주 여행/우주 여행.py
@@ -1,7 +1,3 @@
-g = lambda: map(int, input().split())
-
-N, M, K, L = g()
-if L < N + M - 2 or (N ^ M ^ L) & 1:
-    print(-1)
-else:
-    print((N - 1) * 'D' + (L - N - M + 2) // 2 * 'RL' + (M - 1) * 'R')
+N,M,K,L=map(int,input().split())
+a=L-N-M+2
+print(((N-1)*'D'+a//2*'RL'+(M-1)*'R',-1)[a<0 or a&1])
