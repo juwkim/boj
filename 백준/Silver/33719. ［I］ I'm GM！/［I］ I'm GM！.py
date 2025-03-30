292c34d8e51e88fcdbd191fc3406e195f0cf3a6b
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-31 01:51:44 +0900

    [Silver IV] Title: [I] I'm GM!, Time: 228 ms, Memory: 110708 KB -BaekjoonHub

--- 백준/Silver/33719. ［I］ I'm GM！/［I］ I'm GM！.py
+++ 백준/Silver/33719. ［I］ I'm GM！/［I］ I'm GM！.py
@@ -1,9 +1,8 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
 r = 0
-for _ in range(N):
+for _ in range(int(input())):
     a, b, p = map(int, input().split())
     r = max(r, (a * p + b * r + (a + b) // 2) // (a + b))
 print(r)
