d6899b853af65a902d411a64a4e5ec89e44629a6
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-31 01:45:40 +0900

    [Silver II] Title: Verdis Quo, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/4531. Verdis Quo/Verdis Quo.py
+++ 백준/Silver/4531. Verdis Quo/Verdis Quo.py
@@ -1,11 +1,7 @@
 for _ in range(int(input())):
     dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     ans, prv = 0, 0
-    for c in input()[::-1]:
-        v = dic[c]
-        if v < prv:
-            ans -= v
-        else:
-            ans += v
+    for v in map(dic.get, input()[::-1]):
+        ans += (v, -v)[v < prv]
         prv = v
     print(ans)
