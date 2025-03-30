e75b5a12be7508be9ea5e2df74970c1753e1c97b
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-30 12:31:03 +0900

    [Silver II] Title: Rummy Score, Time: 152 ms, Memory: 111924 KB -BaekjoonHub

--- 백준/Silver/26345. Rummy Score/Rummy Score.py
+++ 백준/Silver/26345. Rummy Score/Rummy Score.py
@@ -1,10 +1,7 @@
-import sys
-input = sys.stdin.readline
-
 for _ in range(int(input())):
-    hand = [*map(int, input().split())]
-    print("Rummy Hand:", *hand)
-    hand.sort()
+    a = [*map(int, input().split())]
+    print("Rummy Hand:", *a)
+    a.sort()
     ans, nums = 1e9, []
     def solve(idx):
         global ans
@@ -12,11 +9,11 @@ for _ in range(int(input())):
             ans = min(ans, sum(sum(l) for l in nums if len(l) < 3))
             return
         for l in nums:
-            if (len(l) == 1 or l[0] != l[-1]) and l[-1] + 1 == hand[idx] or l[0] == l[-1] == hand[idx]:
-                l.append(hand[idx])
+            if (len(l) == 1 or l[0] != l[-1]) and l[-1] + 1 == a[idx] or l[0] == l[-1] == a[idx]:
+                l.append(a[idx])
                 solve(idx + 1)
                 l.pop()
-        nums.append([hand[idx]])
+        nums.append([a[idx]])
         solve(idx + 1)
         nums.pop()
     solve(0)
