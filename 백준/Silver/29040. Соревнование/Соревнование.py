5efc24ed8730594bd3d091c8314abd7f76f133bc
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-31 05:57:50 +0900

    [Silver II] Title: Соревнование, Time: 104 ms, Memory: 110840 KB -BaekjoonHub

--- 백준/Silver/29040. Соревнование/Соревнование.py
+++ 백준/Silver/29040. Соревнование/Соревнование.py
@@ -1,77 +1,58 @@
-def gen4(n, k):
+def gen4():
     ans = []
     now = [10, 8, 7, 6]
-    for i in range(k - 4):
-        now.append(3 if i % 2 == 0 else 2)
+    for i in range(k - 4): now.append((3, 2)[i & 1])
     ans.append(now)
     
     now = [0, 9, 8, 7]
-    for i in range(k - 4):
-        now.append(0 if i % 2 == 0 else 3)
+    for i in range(k - 4): now.append((0, 3)[i & 1])
     ans.append(now)
     
     now = [1, 0, 9, 8]
-    for i in range(k - 4):
-        now.append(1 if i % 2 == 0 else 0)
+    for i in range(k - 4): now.append((1, 0)[i & 1])
     ans.append(now)
     
     now = [2, 1, 0, 9]
-    for i in range(k - 4):
-        now.append(0 if i % 2 == 0 else 1)
+    for i in range(k - 4): now.append((0, 1)[i & 1])
     ans.append(now)
     
     for it in range(n - 4):
-        if it % 4 == 0:
-            now = [3, 2, 1, 0]
-        elif it % 4 == 1:
-            now = [0, 3, 2, 1]
-        elif it % 4 == 2:
-            now = [1, 0, 3, 2]
-        else:
-            now = [2, 1, 0, 3]
-        
-        for i in range(k - 4):
-            now.append((it + 1) % 2 if i % 2 == 0 else it % 2)
+        match it % 4:
+            case 0: now = [3, 2, 1, 0]
+            case 1: now = [0, 3, 2, 1]
+            case 2: now = [1, 0, 3, 2]
+            case 3: now = [2, 1, 0, 3]
+        for i in range(k - 4): now.append((it + 1 & 1, it & 1)[i & 1])
         ans.append(now)
-    
     return ans
 
-def gen3(n, k):
+def gen3():
     ans = []
     now = [10, 8, 7]
-    for i in range(k - 3):
-        now.append(3 if i % 2 == 0 else 2)
+    for i in range(k - 3): now.append((3, 2)[i & 1])
     ans.append(now)
     
     now = [0, 9, 8]
-    for i in range(k - 3):
-        now.append(0 if i % 2 == 0 else 3)
+    for i in range(k - 3): now.append((0, 3)[i & 1])
     ans.append(now)
     
     now = [1, 0, 9]
-    for i in range(k - 3):
-        now.append(1 if i % 2 == 0 else 0)
+    for i in range(k - 3): now.append((1, 0)[i & 1])
     ans.append(now)
     
     for it in range(n - 3):
-        if it % 3 == 0:
-            now = [2, 1, 0]
-        elif it % 3 == 1:
-            now = [0, 2, 1]
-        else:
-            now = [1, 0, 2]
-        
-        for i in range(k - 3):
-            now.append(it % 2 if i % 2 == 0 else (it + 1) % 2)
+        match it % 3:
+            case 0: now = [2, 1, 0]
+            case 1: now = [0, 2, 1]
+            case 2: now = [1, 0, 2]        
+        for i in range(k - 3): now.append((it & 1, it + 1 & 1)[i & 1])
         ans.append(now)
-    
     return ans
 
 n, k = map(int, input().split())
-if k % 2 == 1:
-    ans = gen3(n, k)
+if k & 1:
+    ans = gen3()
 else:
-    ans = gen4(n, k)
-
+    ans = gen4()
 for row in ans:
     print(*row)
