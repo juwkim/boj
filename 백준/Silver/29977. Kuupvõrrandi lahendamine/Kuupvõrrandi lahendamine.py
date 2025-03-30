9fb49006ed9a4718981cda6fe7491ab66f7786e4
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-31 07:11:08 +0900

    [Silver II] Title: Kuupvõrrandi lahendamine, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/29977. Kuupvõrrandi lahendamine/Kuupvõrrandi lahendamine.py
+++ 백준/Silver/29977. Kuupvõrrandi lahendamine/Kuupvõrrandi lahendamine.py
@@ -12,29 +12,23 @@ def solve():
                 return k
 
 x1 = solve()
-if x1:
-    s, t = -x1 - b, d // x1
-else:
-    s, t = -b, c
+s = -x1 - b
+t = d // x1 if x1 else c
 num = s * s + 4 * t
 if num == 1:
     if s & 1:
-        x2 = s >> 1
-        x3 = s + 1 >> 1
+        x2, x3 = s >> 1, s + 1 >> 1
     else:
-        x2 = f"{s - 1}/2"
-        x3 = f"{s + 1}/2"
+        x2, x3 = f"{s - 1}/2", f"{s + 1}/2"
 elif num % 4 == 0 and num >= 0 and num == isqrt(num)**2:
-    x2 = s // 2 - isqrt(num)
-    x3 = s // 2 + isqrt(num)
+    x2, x3 = s // 2 - isqrt(num), s // 2 + isqrt(num)
 else:
     p = (s >> 1, f"{s}/2")[s & 1]
     if num % 4 == 0:
-        q = f"sqrt({num >> 2})"
+        q = f"{num >> 2}"
     elif num % 2 == 0:
-        q = f"sqrt({num >> 1}/2)"
+        q = f"{num >> 1}/2"
     else:
-        q = f"sqrt({num}/4)"
-    x2 = f"{p}-{q}"
-    x3 = f"{p}+{q}"
+        q = f"{num}/4"
+    x2, x3 = f"{p}-sqrt({q})", f"{p}+sqrt({q})"
 print(x1, x2, x3)
