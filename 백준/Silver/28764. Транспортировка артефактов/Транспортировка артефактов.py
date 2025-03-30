e64f066a521015f0469ac91c7e3ef29032029d63
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-31 03:21:16 +0900

    [Silver II] Title: Транспортировка артефактов, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/28764. Транспортировка артефактов/Транспортировка артефактов.py
+++ 백준/Silver/28764. Транспортировка артефактов/Транспортировка артефактов.py
@@ -1,11 +1,8 @@
-from itertools import permutations, product
+from itertools import product
 
 a1, b1, a2, b2, a3, b3 = map(int, open(0))
 ans = 1e9
-for (s1, t1), (s2, t2), (s3, t3) in product(((a1, b1), (b1, a1)), ((a2, b2), (b2, a2)), ((a3, b3), (b3, a3))):
-    ans = min(ans, (s1 + s2 + s3) * max(t1, t2, t3))
-
-for (p1, q1), (p2, q2), (p3, q3) in permutations(((a1, b1), (a2, b2), (a3, b3))):
-    for (s1, t1), (s2, t2), (s3, t3) in product(((p1, q1), (q1, p1)), ((p2, q2), (q2, p2)), ((p3, q3), (q3, p3))):
-        ans = min(ans, (max(s1, s2) + s3) * max(t1 + t2, t3))
+for l in product(((a1, b1), (b1, a1)), ((a2, b2), (b2, a2)), ((a3, b3), (b3, a3))):
+    s, t = map(sorted, zip(*l))
+    ans = min(ans, sum(s) * t[2], (s[1] + s[2]) * max(t[0] + t[1], t[2]))
 print(ans)
