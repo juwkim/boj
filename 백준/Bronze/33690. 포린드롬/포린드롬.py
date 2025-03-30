1daed50672bced53bedeca42b2300891f2d706aa
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-30 09:22:37 +0900

    [Bronze I] Title: 포린드롬, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/33690. 포린드롬/포린드롬.py
+++ 백준/Bronze/33690. 포린드롬/포린드롬.py
@@ -1,2 +1,2 @@
-N = int(s:=input())
-print(1 + sum(int(c * i) <= N for i in range(1, len(s) + 1) for c in "123456789"))
+N=int(input())
+print(1+sum(N>=int(i*c+c)for i in range(9)for c in"123456789"))
