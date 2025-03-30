10c4309a4552da5dcb8da815c0c8c4a2a771a2ed
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-30 12:31:31 +0900

    [Silver II] Title: Rummy Score, Time: 152 ms, Memory: 111408 KB -BaekjoonHub

--- 백준/Silver/26345. Rummy Score/Rummy Score.py
+++ 백준/Silver/26345. Rummy Score/Rummy Score.py
@@ -1,21 +1,22 @@
+def solve(idx):
+    global ans
+    if idx == 7:
+        ans = min(ans, sum(sum(l) for l in nums if len(l) < 3))
+        return
+    for l in nums:
+        if (len(l) == 1 or l[0] != l[-1]) and l[-1] + 1 == a[idx] or l[0] == l[-1] == a[idx]:
+            l.append(a[idx])
+            solve(idx + 1)
+            l.pop()
+    nums.append([a[idx]])
+    solve(idx + 1)
+    nums.pop()
+
 for _ in range(int(input())):
     a = [*map(int, input().split())]
     print("Rummy Hand:", *a)
     a.sort()
     ans, nums = 1e9, []
-    def solve(idx):
-        global ans
-        if idx == 7:
-            ans = min(ans, sum(sum(l) for l in nums if len(l) < 3))
-            return
-        for l in nums:
-            if (len(l) == 1 or l[0] != l[-1]) and l[-1] + 1 == a[idx] or l[0] == l[-1] == a[idx]:
-                l.append(a[idx])
-                solve(idx + 1)
-                l.pop()
-        nums.append([a[idx]])
-        solve(idx + 1)
-        nums.pop()
     solve(0)
     print(ans)
     print()
