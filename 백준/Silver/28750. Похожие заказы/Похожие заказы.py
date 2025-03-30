05c6a3d154928a54675aa98982b58ef7d2a595f6
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-31 02:45:46 +0900

    [Silver II] Title: Похожие заказы, Time: 196 ms, Memory: 119648 KB -BaekjoonHub

--- 백준/Silver/28750. Похожие заказы/Похожие заказы.py
+++ 백준/Silver/28750. Похожие заказы/Похожие заказы.py
@@ -1,10 +1,9 @@
 n = int(input())
-t, s = input(), input()
+t, s = [ord(c) - 97 for c in input()], input() * 2
 ans = "Impossible"
-for k in range(n):
-    shifted_s = s[k:] + s[:k]
-    d = (ord(shifted_s[0]) - ord(t[0])) % 26
-    if all((ord(shifted_s[i]) - ord(t[i])) % 26 == d for i in range(n)):
+for d in range(26):
+    k = s.find("".join(chr((c + d) % 26 + 97) for c in t))
+    if k != -1:
         ans = f"Success\n{k} {d}"
         break
 print(ans)
