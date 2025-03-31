76449b125e862017bfa27111a6c8b0ad4c4ed263
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-01 05:35:06 +0900

    [Silver III] Title: Справедливая последовательность, Time: 100 ms, Memory: 111084 KB -BaekjoonHub

--- 백준/Silver/21464. Справедливая последовательность/Справедливая последовательность.py
+++ 백준/Silver/21464. Справедливая последовательность/Справедливая последовательность.py
@@ -1,24 +1,15 @@
-n = len(s := input())
-even, odd = s[::2].count('1'), s[1::2].count('1')
-curr_even, curr_odd = 0, 0
-ans = 0
+n = len(s:=input())
+a, b = s[::2].count('1'), s[1::2].count('1')
+ans, p = 0, 0
 for i in range(n):
-    if s[i] == '1':
-        if i & 1:
-            a = curr_odd + even - curr_even
-            b = curr_even + odd - curr_odd - 1
-        else:
-            a = curr_odd + even - curr_even - 1
-            b = curr_even + odd - curr_odd
-    else:
-        a = curr_odd + even - curr_even
-        b = curr_even + odd - curr_odd
-    if a == b:
+    num = [a + p, b - p]
+    num[i & 1] -= s[i] == '1'
+    if num[0] == num[1]:
         ans = i + 1
         break
-
-    if i & 1:
-        curr_odd += s[i] == '1'
-    else:
-        curr_even += s[i] == '1'
+    if s[i] == '1':
+        if i & 1:
+            p += 1
+        else:
+            p -= 1
 print(ans)
