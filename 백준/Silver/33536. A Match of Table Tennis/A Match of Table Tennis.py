9d9f0a205347822f6eba5f5c184f28720814bd47
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-31 10:15:49 +0900

    [Silver III] Title: A Match of Table Tennis, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/33536. A Match of Table Tennis/A Match of Table Tennis.py
+++ 백준/Silver/33536. A Match of Table Tennis/A Match of Table Tennis.py
@@ -1,20 +1,19 @@
 def solve():
     n = int(input())
-    l = input().split()
     a, b = 0, 0
-    for i, s in enumerate(l):
-        s, t = map(int, s.split('-'))
+    for i, l in enumerate(input().split()):
+        s, t = map(int, l.split('-'))
         if (s == 11 and t <= 9) or (s >= 12 and t == s - 2):
             if a == 3 or b == 3:
-                return "INCONSISTENT"
+                return 2
             a += 1
         elif (t == 11 and s <= 9) or (t >= 12 and s == t - 2):
             if a == 3 or b == 3:
-                return "INCONSISTENT"
+                return 2
             b += 1
         elif i != n - 1 or (max(s, t) >= 12 and abs(s - t) >= 3):
-            return "INCONSISTENT"
+            return 2
     if s < 10 or t < 10:
-        return ("HOST", "GUEST")[((s + t) % 4 > 1) + n & 1]
-    return ("HOST", "GUEST")[s + t + n & 1]
-print(solve())
+        return (s + t & 0x3 > 1) + n & 1
+    return s + t + n & 1
+print(("HOST", "GUEST", "INCONSISTENT")[solve()])
