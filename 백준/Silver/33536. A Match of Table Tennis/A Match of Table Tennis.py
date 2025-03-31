d493b74612aa4605e8e2e2ce8f71e1dffccd25de
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-31 10:21:14 +0900

    [Silver III] Title: A Match of Table Tennis, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/33536. A Match of Table Tennis/A Match of Table Tennis.py
+++ 백준/Silver/33536. A Match of Table Tennis/A Match of Table Tennis.py
@@ -1,17 +1,13 @@
 def solve():
     n = int(input())
-    a, b = 0, 0
+    score = [0, 0]
     for i, l in enumerate(input().split()):
         s, t = map(int, l.split('-'))
-        if (s == 11 and t <= 9) or (s >= 12 and t == s - 2):
-            if a == 3 or b == 3:
-                return 2
-            a += 1
-        elif (t == 11 and s <= 9) or (t >= 12 and s == t - 2):
-            if a == 3 or b == 3:
-                return 2
-            b += 1
-        elif i != n - 1 or (max(s, t) >= 12 and abs(s - t) >= 3):
+        p, q = sorted((s, t))
+        if q == 11 and p <= 9 or q >= 12 and q == p + 2:
+            if 3 in score: return 2
+            score[t == q] += 1
+        elif i != n - 1 or q >= 12 and q >= p + 3:
             return 2
     if s < 10 or t < 10:
         return (s + t & 0x3 > 1) + n & 1
