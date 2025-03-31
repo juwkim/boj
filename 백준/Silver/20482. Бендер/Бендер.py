8ef1527b1420e3ec172b95cba2bf46eafddaaf31
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-01 04:11:21 +0900

    [Silver III] Title: Бендер, Time: 676 ms, Memory: 110760 KB -BaekjoonHub

--- 백준/Silver/20482. Бендер/Бендер.py
+++ 백준/Silver/20482. Бендер/Бендер.py
@@ -1,14 +1,14 @@
 from itertools import product
 
-def solve():
-    n, k, r, l = map(int, input().split())
-    for a, b, c in product(range(k), repeat=3):
-        nums = [*range(k)]
-        x = c
-        for _ in range(n):
-            nums[x], nums[(x + 1) % k] = nums[(x + 1) % k], nums[x]
-            x = (a * x + b) % k
-        if nums[l - 1] == r - 1:
-            return f"{a} {b} {c}"
-    return "Impossible"
-print(solve())
+ans = "Impossible"
+n, k, r, l = map(int, input().split())
+for a, b, c in product(range(k), repeat=3):
+    nums = [*range(k)]
+    x = c
+    for _ in range(n):
+        nums[x], nums[(x + 1) % k] = nums[(x + 1) % k], nums[x]
+        x = (a * x + b) % k
+    if nums[l - 1] == r - 1:
+        ans = f"{a} {b} {c}"
+        break
+print(ans)
