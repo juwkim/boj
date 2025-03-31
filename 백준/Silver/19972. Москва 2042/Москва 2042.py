3a92d794f8c65189dc06830e46ffc4e222966c2d
김주원 <66457470+juwkim@users.noreply.github.com>
2025-03-31 15:45:08 +0900

    [Silver II] Title: Москва 2042, Time: 224 ms, Memory: 141052 KB -BaekjoonHub

--- 백준/Silver/19972. Москва 2042/Москва 2042.py
+++ 백준/Silver/19972. Москва 2042/Москва 2042.py
@@ -1,21 +1,15 @@
 import sys
 g = lambda: [*map(int, sys.stdin.readline().split())]
-from math import radians
+from math import radians as rad
 
 n, m = g()
 a = [g() for _ in range(n)]
 b = [int(input()) for _ in range(m)]
 i1, j1 = g()
 i2, j2 = g()
-angle = (b[j2 - 1] - b[j1 - 1]) / 10**6 % 360
-l = radians((360 - angle) % 360), radians(angle)
+num = (b[j2 - 1] - b[j1 - 1]) / 10**6 % 360
+l = [rad(num), rad((360 - num) % 360)]
+l.insert(0, min(l))
 ans = 1e9
-for r, t in a:
-    if t == 1:
-        theta = l[1]
-    elif t == -1:
-        theta = l[0]
-    else:
-        theta = min(l)
-    ans = min(ans, r * theta + abs(r - a[i1 - 1][0]) + abs(r - a[i2 - 1][0]))
+for r, t in a: ans = min(ans, r * l[t] + abs(r - a[i1 - 1][0]) + abs(r - a[i2 - 1][0]))
 print(ans)
