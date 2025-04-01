f8b2193c5417679e186aa6bb938c5d8c429323d3
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-01 09:00:39 +0900

    [Silver II] Title: Епрестановка, Time: 180 ms, Memory: 123572 KB -BaekjoonHub

--- 백준/Silver/22144. Епрестановка/Епрестановка.py
+++ 백준/Silver/22144. Епрестановка/Епрестановка.py
@@ -3,23 +3,21 @@ from collections import deque
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-p = [0] + [*map(int, input().split())]
-group = [-1] * (n + 1)
-id = 1
-for i in range(1, n + 1):
+p = [*map(int, input().split())]
+group = [-1] * n
+id = 0
+for i in range(n):
     if group[i] != -1:
         continue
-    nums, cur = [i], p[i]
+    group[i], cur = id, p[i] - 1
     while cur != i:
-        nums.append(cur)
-        cur = p[cur]
-    for num in nums:
-        group[num] = id
+        group[cur] = id
+        cur = p[cur] - 1
     id += 1
-if group[2] == 2:
-    for i in range(1, n + 1):
-        if group[i] == 2:
-            group[i] = 1
+if group[1] == 1:
+    for i in range(1, n):
+        if group[i] == 1:
+            group[i] = 0
 for _ in range(m):
     a, b = map(int, input().split())
-    print(("No", "Yes")[group[a] == group[b]])
+    print(("No", "Yes")[group[a-1] == group[b-1]])
