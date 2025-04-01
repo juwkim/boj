4e05ed8549d0c49af189f85794dcf61f8be9b12d
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-02 03:19:34 +0900

    [Silver II] Title: Intensive Training, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/33111. Intensive Training/Intensive Training.py
+++ 백준/Silver/33111. Intensive Training/Intensive Training.py
@@ -5,8 +5,4 @@ for _ in range(int(input())):
     N, R, K = map(int, input().split())
     q1, r1 = divmod(R, N)
     q2, r2 = divmod(K, N)
-    if r1 + r2 > N:
-        ans = (N - r2) * (q1 + 1) * q2 + (N - r1) * (q2 + 1) * q1 + (r1 + r2 - N) * (q1 + 1) * (q2 + 1)
-    else:
-        ans = r1 * (q1 + 1) * q2 + r2 * (q2 + 1) * q1 + (N - r1 - r2) * q1 * q2
-    print(ans)
+    print(N * q1 * q2 + r2 * q1 + r1 * q2 + max(0, r1 + r2 - N))
