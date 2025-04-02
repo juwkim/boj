9a039ee16262c11117a32a5dc6a134bb6eca367f
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-02 10:57:50 +0900

    [Silver II] Title: На далекой Амазонке, Time: 128 ms, Memory: 112648 KB -BaekjoonHub

---
+++ 백준/Silver/22088. На далекой Амазонке/На далекой Амазонке.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n, a, b = map(int, input().split())
+    if a >= n or b >= n or a > b:
+        print("IMPOSSIBLE")
+    else:
+        for i in range(2, a + 1):
+            print(1, i)
+        print(b - a + 1, *range(a + 1, b + 2))
+        for _ in range(n - a):
+            print(0)
