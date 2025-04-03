9e7c5602632a7493d766411d27aca17b721f8bfe
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-04 05:25:52 +0900

    [Silver IV] Title: 격자 막기, Time: 132 ms, Memory: 133576 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/33754. 격자 막기/격자 막기.py
+++ 백준/Silver/33754. 격자 막기/격자 막기.py
@@ -3,16 +3,13 @@ g = lambda: [*map(int, input().split())]
 N = int(input())
 A, B = g(), g()
 if A[0] == 0 or B[-1] == 0:
-    print(0)
+    assert False
 elif all(A) and all(B):
     print(2)
 else:
     ans = 1
     for i in range(N - 1):
-        if A[i] == 0 and B[i + 1] == 0:
-            ans = 0
-            break
-        if A[i + 1] == 0 and B[i] == 0:
+        if (0, 0) in ((A[i], B[i + 1]), (A[i + 1], B[i]), (A[i], B[i])):
             ans = 0
             break
     print(ans)
