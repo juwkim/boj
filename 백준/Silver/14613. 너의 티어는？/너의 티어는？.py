5b945c829c0630e1d70a4e7263dbe0752116ddce
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-03 13:37:39 +0900

    [Silver I] Title: 너의 티어는?, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/14613. 너의 티어는？/너의 티어는？.py
+++ 백준/Silver/14613. 너의 티어는？/너의 티어는？.py
@@ -2,9 +2,9 @@ from math import comb
 
 W, L, D = map(float, input().split())
 ans = [0] * 5
-for draw in range(21):
-    for win in range(21 - draw):
-        lose = 20 - draw - win
+for win in range(21):
+    for lose in range(21 - win):
+        draw = 20 - win - lose
         ans[(win - lose) // 10 + 2] += comb(20, win) * comb(20 - win, lose) * W**win * L**lose * D**draw
 for i in range(5):
     print(f"{ans[i]:.8f}")
