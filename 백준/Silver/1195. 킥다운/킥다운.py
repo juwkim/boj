82b5d4ccc3c9f7adeabf49be46f26e57429d7c1b
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-03 10:37:11 +0900

    [Silver I] Title: 킥다운, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1195. 킥다운/킥다운.py
+++ 백준/Silver/1195. 킥다운/킥다운.py
@@ -1,6 +1,6 @@
 def f(s,t):
  i=0
- while i<len(s)and any(s[i+j]+t[j]=="22"for j in range(min(len(s)-i,len(t)))):i+=1
+ while any(s[i+j]+t[j]=="22"for j in range(min(len(s)-i,len(t)))):i+=1
  return max(len(s),i+len(t))
 a,b=input(),input()
 print(min(f(a,b),f(b,a)))
