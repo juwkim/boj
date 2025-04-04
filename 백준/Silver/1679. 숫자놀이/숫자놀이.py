9985d3f9b6be9dbbbb30e43e264e07883fb3028c
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-04 12:56:52 +0900

    [Silver I] Title: 숫자놀이, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/1679. 숫자놀이/숫자놀이.py
@@ -0,0 +1,13 @@
+N, *nums, K = map(int, open(0).read().split())
+cnt = [0] * (2 + nums[-1] * K)
+for num in nums:
+    cnt[num] = 1
+i = 1
+while cnt[i]:
+    if cnt[i] < K:
+        for num in nums:
+            if cnt[i + num]:
+                continue
+            cnt[i + num] = cnt[i] + 1
+    i += 1
+print(f"{('hol', 'jjak')[i & 1]}soon win at {i}")
