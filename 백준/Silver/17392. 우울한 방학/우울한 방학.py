dcba6c22a67067f6708937d00295ac9ab5c38c39
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-04 16:26:24 +0900

    [Silver I] Title: 우울한 방학, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/17392. 우울한 방학/우울한 방학.py
+++ 백준/Silver/17392. 우울한 방학/우울한 방학.py
@@ -1,9 +1,3 @@
-f = lambda x: x * (x + 1) * (2 * x + 1) // 6
-
-N, M, *H = map(int, open(0).read().split())
-cnt = N + sum(H)
-if cnt >= M:
-    print(0)
-else:
-    q, r = divmod(M - cnt, N + 1)
-    print(r * f(q + 1) + (N + 1 - r) * f(q))
+N,M,*H=map(int,open(0).read().split())
+q,r=divmod(M-N-sum(H),N+1)
+print(max(0,r*(q+1)**2+(N+1)*q*(q+1)*(2*q+1)//6))
