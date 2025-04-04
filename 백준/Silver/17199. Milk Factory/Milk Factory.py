6a2fa9af37252859cd7268a1297d4fceb0ce9fb0
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-04 15:01:28 +0900

    [Silver I] Title: Milk Factory, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/17199. Milk Factory/Milk Factory.py
+++ 백준/Silver/17199. Milk Factory/Milk Factory.py
@@ -2,23 +2,8 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-graph = [[] for _ in range(N + 1)]
+cnt = [0 for _ in range(N)]
 for _ in range(N - 1):
     a, b = map(int, input().split())
-    graph[b].append(a)
-
-ans = -1
-for i in range(1, N + 1):
-    visited = bytearray(N + 1)
-    visited[i] = 1
-    st = [i]
-    while st:
-        node = st.pop()
-        for child in graph[node]:
-            if visited[child] == 0:
-                visited[child] = 1
-                st.append(child)
-    if visited.count(1) == N:
-        ans = i
-        break
-print(ans)
+    cnt[a - 1] += 1
+print(cnt.index(0) + 1 if cnt.count(0) == 1 else -1)
