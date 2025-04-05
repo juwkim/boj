7e95a6d263c42e4355b83a2ca185e0c18f0b4a4a
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-05 22:34:49 +0900

    [Silver I] Title: 장기자랑, Time: 120 ms, Memory: 123892 KB -BaekjoonHub

--- 백준/Silver/27277. 장기자랑/장기자랑.py
+++ 백준/Silver/27277. 장기자랑/장기자랑.py
@@ -1,3 +1,3 @@
-N, *a = map(int, open(0).read().split())
+N,*a=map(int,open(0).read().split())
 a.sort()
-print(a[N >> 1] + sum(a[-1-i] - a[i] for i in range(N - 1 >> 1)))
+print(a[N>>1]+sum(a[-1-i]-a[i]for i in range(N-1>>1)))
