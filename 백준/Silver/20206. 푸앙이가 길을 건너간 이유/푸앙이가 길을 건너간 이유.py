9aca06d396fd7b9ed6d0bed3519fedce47bdc3fd
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-06 00:01:30 +0900

    [Silver I] Title: 푸앙이가 길을 건너간 이유, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/20206. 푸앙이가 길을 건너간 이유/푸앙이가 길을 건너간 이유.py
+++ 백준/Silver/20206. 푸앙이가 길을 건너간 이유/푸앙이가 길을 건너간 이유.py
@@ -1,12 +1,4 @@
 A, B, C, X1, X2, Y1, Y2 = map(int, open(0).read().split())
-if B:
-    P, Q = (-C - A * X1) / B, (-C - A * X2) / B
-    if P <= Y1:
-        i = Q > Y1
-    elif P >= Y2:
-        i = Q < Y2
-    else:
-        i = 1
-else:
-    i = (A * X1 + C) * (A * X2 + C) < 0
-print(("Lucky", "Poor")[i])
+l = lambda x, y: A * x + B * y + C
+s = l(X1, Y1), l(X1, Y2), l(X2, Y1), l(X2, Y2)
+print(("Poor", "Lucky")[all(i >= 0 for i in s) or all(i <= 0 for i in s)])
