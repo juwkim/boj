de4adfafc488bb3b1f5d94d742be3f9b8ae6a517
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-05 21:57:45 +0900

    [Silver I] Title: 튀김 소보루, Time: 340 ms, Memory: 137492 KB -BaekjoonHub

--- 백준/Silver/12842. 튀김 소보루/튀김 소보루.py
+++ 백준/Silver/12842. 튀김 소보루/튀김 소보루.py
@@ -1,9 +1,9 @@
 import heapq
 
 n, s, m, *t = map(int, open(0).read().split())
-hq = [(0, i, num) for i, num in enumerate(t)]
+hq = [(0, i, speed) for i, speed in enumerate(t, 1)]
 heapq.heapify(hq)
 for _ in range(n - s):
-    time, num, speed = heapq.heappop(hq)
-    heapq.heappush(hq, (time + speed, num, speed))
-print(num + 1)
+    time, i, speed = heapq.heappop(hq)
+    heapq.heappush(hq, (time + speed, i, speed))
+print(i)
