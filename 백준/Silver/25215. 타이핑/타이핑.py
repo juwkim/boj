7a6bb88834c6c9f9a0a97b3f53da81af21267cff
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-06 02:36:45 +0900

    [Silver I] Title: 타이핑, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/25215. 타이핑/타이핑.py
+++ 백준/Silver/25215. 타이핑/타이핑.py
@@ -1,5 +1,4 @@
 N=len(s:=input())
 a,c=N,0
-for i in range(N-1):
-    if c!=s[i].isupper():a+=1;c^=c!=s[i+1].isupper()
-print(a+(c!=s[-1].isupper()))
+for i in range(N-1):p=(s[i]<'[')^c;a+=p;c^=(c^(s[i+1]<'['))&p
+print(a+(c^(s[-1]<'[')))
