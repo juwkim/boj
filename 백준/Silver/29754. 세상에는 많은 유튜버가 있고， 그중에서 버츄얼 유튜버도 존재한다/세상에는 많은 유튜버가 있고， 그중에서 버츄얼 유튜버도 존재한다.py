e80968833f8f9652c398420dc6a4843d90398db0
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-06 22:35:26 +0900

    [Silver I] Title: 세상에는 많은 유튜버가 있고, 그중에서 버츄얼 유튜버도 존재한다, Time: 152 ms, Memory: 114876 KB -BaekjoonHub

--- 백준/Silver/29754. 세상에는 많은 유튜버가 있고， 그중에서 버츄얼 유튜버도 존재한다/세상에는 많은 유튜버가 있고， 그중에서 버츄얼 유튜버도 존재한다.py
+++ 백준/Silver/29754. 세상에는 많은 유튜버가 있고， 그중에서 버츄얼 유튜버도 존재한다/세상에는 많은 유튜버가 있고， 그중에서 버츄얼 유튜버도 존재한다.py
@@ -8,14 +8,10 @@ for _ in range(N):
     name, day, s, e = input().split()
     h1, m1 = map(int, s.split(':'))
     h2, m2 = map(int, e.split(':'))
-    time = (h2 - h1) * 60 + (m2 - m1)
-    idx = (int(day) - 1) // 7
-    dic[name][idx][0] += time
-    dic[name][idx][1] += 1
-ans = []
-for name, l in dic.items():
-    if all(a >= 3600 and b >= 5 for a, b in l):
-        ans.append(name)
+    i = (int(day) - 1) // 7
+    dic[name][i][0] += (h2 - h1) * 60 + (m2 - m1)
+    dic[name][i][1] += 1
+ans = [name for name, l in dic.items() if all(a >= 3600 and b >= 5 for a, b in l)]
 if ans:
     print(*sorted(ans), sep='\n')
 else:
