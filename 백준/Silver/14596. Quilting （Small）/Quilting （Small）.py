5d082f707b077d04e7ee2b936342f7c1b4f9f4ac
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-08 21:27:32 +0900

    [Silver I] Title: Quilting (Small), Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/14596. Quilting （Small）/Quilting （Small）.py
+++ 백준/Silver/14596. Quilting （Small）/Quilting （Small）.py
@@ -4,15 +4,10 @@ g = lambda: [*map(int, sys.stdin.readline().split())]
 H, W = g()
 B1 = [g() for _ in range(H)]
 B2 = [g() for _ in range(H)]
-dp = [(a - b) ** 2 for a, b in zip(B1[0], B2[0])]
+dp = [1e9] + [(a - b) ** 2 for a, b in zip(B1[0], B2[0])] + [1e9]
 for i in range(1, H):
-    new = [(a - b) ** 2 for a, b in zip(B1[i], B2[i])]
-    for j in range(W):
-        if j == 0:
-            new[j] += dp[0] if W == 1 else min(dp[0], dp[1])
-        elif j == W - 1:
-            new[j] += min(dp[-1], dp[-2])
-        else:
-            new[j] += min(dp[j - 1], dp[j], dp[j + 1])
+    new = [1e9] + [(a - b) ** 2 for a, b in zip(B1[i], B2[i])] + [1e9]
+    for j in range(1, W + 1):
+        new[j] += min(dp[j-1:j+2])
     dp = new
 print(min(dp))
