ff316a54748a5a86248057eb216a2b5cb039c7f3
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-08 21:29:39 +0900

    [Silver I] Title: Quilting (Small), Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/14596. Quilting （Small）/Quilting （Small）.py
+++ 백준/Silver/14596. Quilting （Small）/Quilting （Small）.py
@@ -4,7 +4,7 @@ g = lambda: [*map(int, sys.stdin.readline().split())]
 H, W = g()
 B1 = [g() for _ in range(H)]
 B2 = [g() for _ in range(H)]
-dp = [1e9] + [(a - b) ** 2 for a, b in zip(B1[0], B2[0])] + [1e9]
-for i in range(1, H):
+dp = [0] * (W + 2)
+for i in range(H):
     dp = [1e9] + [(B1[i][j] - B2[i][j])**2 + min(dp[j:j+3]) for j in range(W)] + [1e9]
 print(min(dp))
