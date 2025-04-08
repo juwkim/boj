17b225876070c13f0b70f1956b547d4832b9a62a
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-08 21:01:33 +0900

    [Silver I] Title: 정사각형으로 가리기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1569. 정사각형으로 가리기/정사각형으로 가리기.py
+++ 백준/Silver/1569. 정사각형으로 가리기/정사각형으로 가리기.py
@@ -5,7 +5,7 @@ xs, ys = zip(*[[*map(int, input().split())] for _ in range(int(input()))])
 xmin, xmax = min(xs), max(xs)
 ymin, ymax = min(ys), max(ys)
 a, b = xmax - xmin, ymax - ymin
-if a == 0 or b == 0 or xmax - xmin == ymax - ymin and all(x in (xmin, xmax) or y in (ymin, ymax) for x, y in zip(xs, ys)) or all(x == xmin or y in (ymin, ymax) for x, y in zip(xs, ys)) or all(x == xmax or y in (ymin, ymax) for x, y in zip(xs, ys)) or all(y == ymin or x in (xmin, xmax) for x, y in zip(xs, ys)) or all(y == ymax or x in (xmin, xmax) for x, y in zip(xs, ys)):
+if a == 0 or b == 0 or a == b and all(x in (xmin, xmax) or y in (ymin, ymax) for x, y in zip(xs, ys)) or all(x == xmin or y in (ymin, ymax) for x, y in zip(xs, ys)) or all(x == xmax or y in (ymin, ymax) for x, y in zip(xs, ys)) or all(y == ymin or x in (xmin, xmax) for x, y in zip(xs, ys)) or all(y == ymax or x in (xmin, xmax) for x, y in zip(xs, ys)):
     print(max(a, b))
 else:
     print(-1)
