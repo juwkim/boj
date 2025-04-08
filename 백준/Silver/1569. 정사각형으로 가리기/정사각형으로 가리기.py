28a79eb16b6698fbbdfe1e492ff5185005c9bcfe
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-08 21:01:02 +0900

    [Silver I] Title: 정사각형으로 가리기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1569. 정사각형으로 가리기/정사각형으로 가리기.py
+++ 백준/Silver/1569. 정사각형으로 가리기/정사각형으로 가리기.py
@@ -1,16 +1,11 @@
 import sys
 input = sys.stdin.readline
 
-def solve():
-    xs, ys = zip(*[[*map(int, input().split())] for _ in range(int(input()))])
-    xmin, xmax = min(xs), max(xs)
-    ymin, ymax = min(ys), max(ys)
-    a, b = xmax - xmin, ymax - ymin
-    if xmin == xmax:
-        return b
-    if ymin == ymax:
-        return a
-    if xmax - xmin == ymax - ymin and all(x in (xmin, xmax) or y in (ymin, ymax) for x, y in zip(xs, ys)) or all(x == xmin or y in (ymin, ymax) for x, y in zip(xs, ys)) or all(x == xmax or y in (ymin, ymax) for x, y in zip(xs, ys)) or all(y == ymin or x in (xmin, xmax) for x, y in zip(xs, ys)) or all(y == ymax or x in (xmin, xmax) for x, y in zip(xs, ys)):
-        return max(a, b)
-    return -1
-print(solve())
+xs, ys = zip(*[[*map(int, input().split())] for _ in range(int(input()))])
+xmin, xmax = min(xs), max(xs)
+ymin, ymax = min(ys), max(ys)
+a, b = xmax - xmin, ymax - ymin
+if a == 0 or b == 0 or xmax - xmin == ymax - ymin and all(x in (xmin, xmax) or y in (ymin, ymax) for x, y in zip(xs, ys)) or all(x == xmin or y in (ymin, ymax) for x, y in zip(xs, ys)) or all(x == xmax or y in (ymin, ymax) for x, y in zip(xs, ys)) or all(y == ymin or x in (xmin, xmax) for x, y in zip(xs, ys)) or all(y == ymax or x in (xmin, xmax) for x, y in zip(xs, ys)):
+    print(max(a, b))
+else:
+    print(-1)
