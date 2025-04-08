755178b4b9baa0b6ae09e7d263c57bae16aaa753
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-08 21:28:51 +0900

    [Silver I] Title: Quilting (Small), Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/14596. Quilting （Small）/Quilting （Small）.py
+++ 백준/Silver/14596. Quilting （Small）/Quilting （Small）.py
@@ -6,8 +6,5 @@ B1 = [g() for _ in range(H)]
 B2 = [g() for _ in range(H)]
 dp = [1e9] + [(a - b) ** 2 for a, b in zip(B1[0], B2[0])] + [1e9]
 for i in range(1, H):
-    new = [1e9] + [(a - b) ** 2 for a, b in zip(B1[i], B2[i])] + [1e9]
-    for j in range(1, W + 1):
-        new[j] += min(dp[j-1:j+2])
-    dp = new
+    dp = [1e9] + [(B1[i][j] - B2[i][j])**2 + min(dp[j:j+3]) for j in range(W)] + [1e9]
 print(min(dp))
