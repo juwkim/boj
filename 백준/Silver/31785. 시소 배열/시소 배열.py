9185a23cb5c7f322d00635c02ed867471b37553c
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-09 12:26:14 +0900

    [Silver I] Title: 시소 배열, Time: 308 ms, Memory: 192868 KB -BaekjoonHub

--- 백준/Silver/31785. 시소 배열/시소 배열.py
+++ 백준/Silver/31785. 시소 배열/시소 배열.py
@@ -1,25 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-A = []
-px = [0]
+A, px = [], [0]
 for _ in range(int(input())):
     cmd, *x = map(int, input().split())
     match cmd:
         case 1:
-            x = x[0]
-            A.append(x)
-            px.append(px[-1] + x)
+            A.append(x[0])
+            px.append(px[-1] + x[0])
         case 2:
-            l = len(A)
-            idx = l >> 1
-            a, b = px[idx] - px[0], px[l] - px[idx]
+            i = len(A) >> 1
+            a, b = px[i] - px[0], px[-1] - px[i]
             if b < a:
                 print(b)
-                A = A[:idx]
-                px = px[:idx+1]
+                A, px = A[:i], px[:i+1]
             else:
                 print(a)
-                A = A[idx:]
-                px = px[idx:]
+                A, px = A[i:], px[i:]
 print(*A)
