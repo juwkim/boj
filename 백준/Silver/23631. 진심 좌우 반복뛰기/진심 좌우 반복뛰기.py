0fe42bf4adfa79997a8ac73cec547694208f110d
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-09 15:02:36 +0900

    [Silver I] Title: 진심 좌우 반복뛰기, Time: 176 ms, Memory: 113052 KB -BaekjoonHub

--- 백준/Silver/23631. 진심 좌우 반복뛰기/진심 좌우 반복뛰기.py
+++ 백준/Silver/23631. 진심 좌우 반복뛰기/진심 좌우 반복뛰기.py
@@ -4,7 +4,7 @@ input = sys.stdin.readline
 for _ in range(int(input())):
     N, K = map(int, input().split())
     i = int((2 * (N - 1) / K + .25)**.5 - .5)
-    x = N - 1 - (i * (i + 1) // 2 + (i + 1) // 2) * K
+    x = N - 1 - (i * (i + 2) + 1) // 2 * K
     if i & 1:
         x = -x
     print(x, "RL"[i & 1])
