820608561a441313e57d289f8053dd1b474574f2
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-10 22:32:33 +0900

    [Silver I] Title: 2000문제 푼 임스, Time: 108 ms, Memory: 120248 KB -BaekjoonHub

--- 백준/Silver/25822. 2000문제 푼 임스/2000문제 푼 임스.py
+++ 백준/Silver/25822. 2000문제 푼 임스/2000문제 푼 임스.py
@@ -1,4 +1,4 @@
-freeze = min(int(float(input()) / 0.99), 2)
+f = min(int(float(input()) / 0.99), 2)
 input()
 a, b, c = 0, 0, 0
 ans, m = 0, 0
@@ -7,9 +7,7 @@ for num in map(int, input().split()):
     if num:
         a, b, c = a + 1, b + 1, c + 1
     else:
-        c = (0, b + 1)[freeze >= 2]
-        b = (0, a + 1)[freeze >= 1]
-        a = 0
+        a, b, c = 0, (0, a + 1)[f > 0], (0, b + 1)[f > 1]
     ans = max(ans, a, b, c)
 print(ans)
 print(m)
