57ed49b7f6bcf1e355beffe7ed04d7a7692d6dd8
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-12 02:16:04 +0900

    [Silver I] Title: 카더가든, Time: 112 ms, Memory: 112612 KB -BaekjoonHub

--- 백준/Silver/28420. 카더가든/카더가든.py
+++ 백준/Silver/28420. 카더가든/카더가든.py
@@ -1,5 +1,6 @@
 import sys
 g = lambda: [*map(int, sys.stdin.readline().split())]
+get = lambda i, j, h, w: px[i + h][j + w] - px[i][j + w] - px[i + h][j] + px[i][j]
 
 N, M = g()
 a, b, c = g()
@@ -10,16 +11,14 @@ for i in range(N):
     for j in range(M):
         cur += A[i][j]
         px[i + 1][j + 1] = cur + px[i][j + 1]
-def get_sum(i, j, h, w):
-    return px[i + h][j + w] - px[i][j + w] - px[i + h][j] + px[i][j]
 ans = 1e9
 for i in range(N + 1 - a):
     for j in range(M + 1 - b - c):
-        ans = min(ans, get_sum(i, j, a, b + c))
+        ans = min(ans, get(i, j, a, b + c))
 for i in range(N + 1 - a - b):
     for j in range(M + 1 - a - c):
-        ans = min(ans, get_sum(i, j, a, c) + get_sum(i + a, j + c, b, a))
+        ans = min(ans, get(i, j, a, c) + get(i + a, j + c, b, a))
 for i in range(N + 1 - a - c):
     for j in range(M + 1 - a - b):
-        ans = min(ans, get_sum(i, j, a, b) + get_sum(i + a, j + b, c, a))
+        ans = min(ans, get(i, j, a, b) + get(i + a, j + b, c, a))
 print(ans)
