f3792d131f0762fa1bc4481231921579c35eb52e
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-12 21:05:34 +0900

    [Silver I] Title: 풀이 전달, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/32202. 풀이 전달/풀이 전달.py
+++ 백준/Silver/32202. 풀이 전달/풀이 전달.py
@@ -1,4 +1,3 @@
-a, b, m = 1, 0, 1000000007
-for _ in range(int(input())):
-    a, b = 2 * (a + b) % m, a
-print((a + b) % m)
+a,b,m=1,0,10**9+7
+for _ in range(int(input())):a,b=2*(a+b)%m,a
+print((a+b)%m)
