410976525976ae8ffe510394f8f5280e934b277b
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-12 13:01:39 +0900

    [Silver I] Title: 숫자 놀이, Time: 164 ms, Memory: 113448 KB -BaekjoonHub

--- 백준/Silver/32160. 숫자 놀이/숫자 놀이.py
+++ 백준/Silver/32160. 숫자 놀이/숫자 놀이.py
@@ -1,11 +1,7 @@
 q, r = divmod(N:=int(input()), 4)
 print(N - (r > 1))
-for i in range((2, 1)[r&1], N, 2):
-    print(i, i + 1)
-for _ in range(q):
-    print(1, 1)
-for _ in range(q - 1):
-    print(0, 0)
-if q and r > 1:
-    print(0, 1)
+for i in range((2, 1)[r&1], N, 2): print(i, i + 1)
+for _ in range(q): print(1, 1)
+for _ in range(q - 1): print(0, 0)
+if q and r > 1: print(0, 1)
 print(+(r > 1), N)
