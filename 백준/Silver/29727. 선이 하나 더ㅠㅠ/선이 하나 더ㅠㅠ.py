6216f02570991606098cab4c286f471922ba6282
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-13 12:39:52 +0900

    [Silver I] Title: 선이 하나 더ㅠㅠ, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/29727. 선이 하나 더ㅠㅠ/선이 하나 더ㅠㅠ.py
+++ 백준/Silver/29727. 선이 하나 더ㅠㅠ/선이 하나 더ㅠㅠ.py
@@ -1,11 +1,9 @@
 n, xa, ya, xb, yb = map(int, open(0).read().split())
-ans = (n * (n + 1) >> 1)**2
+d = 0
 if xa == xb:
     ya, yb = sorted((ya, yb))
     d = max(0, min(n, yb) - max(-1, ya))
-    ans += d * (d - 1) * (n + 1) >> 1
 elif ya == yb:
     xa, xb = sorted((xa, xb))
     d = max(0, min(n, xb) - max(-1, xa))
-    ans += d * (d - 1) * (n + 1) >> 1
-print(ans)
+print((n * (n + 1) >> 1)**2 + d * (d - 1) * (n + 1) // 2)
