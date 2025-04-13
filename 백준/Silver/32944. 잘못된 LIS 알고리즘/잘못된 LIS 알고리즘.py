869f6f4463e6817cf4556e5766b5f44ac0eab759
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-13 21:00:49 +0900

    [Silver I] Title: 잘못된 LIS 알고리즘, Time: 128 ms, Memory: 123752 KB -BaekjoonHub

--- 백준/Silver/32944. 잘못된 LIS 알고리즘/잘못된 LIS 알고리즘.py
+++ 백준/Silver/32944. 잘못된 LIS 알고리즘/잘못된 LIS 알고리즘.py
@@ -2,8 +2,4 @@ N, M, K = map(int, input().split())
 if M == N:
     print(-1)
 else:
-    ans = [*range(1, K)]
-    ans.append(N)
-    ans.extend(range(N - 1, N - 1 - (N - 1 - M), -1))
-    ans.extend(range(K, N - 1 - (N - 1 - M) + 1))
-    print(*ans)
+    print(*range(1, K), N, *range(N - 1, M, -1), *range(K, M + 1))
