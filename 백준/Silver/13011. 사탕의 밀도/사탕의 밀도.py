f533b01b2fd29e5edcd99df220fef5d35ab4d298
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-13 16:32:53 +0900

    [Silver I] Title: 사탕의 밀도, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/13011. 사탕의 밀도/사탕의 밀도.py
+++ 백준/Silver/13011. 사탕의 밀도/사탕의 밀도.py
@@ -1,8 +1,6 @@
-g=lambda:[*map(int,input().split())]
-n=int(input())
-c=g();w=g()
+n,*l=map(int,open(0).read().split())
+c,w=l[:n],l[n:]
 d=sorted(zip(w,c),key=lambda x:x[0]/x[1])
 a,i=sum(c),-1
 while i<n-1 and a>0:a-=2*d[i:=i+1][1]
-r=d[i][0]/d[i][1]
-print(sum(abs(x-r*y)for x,y in zip(w,c)))
+print(sum(abs(x-y*d[i][0]/d[i][1])for x,y in zip(w,c)))
