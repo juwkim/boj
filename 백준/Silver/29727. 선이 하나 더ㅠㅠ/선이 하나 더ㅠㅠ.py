b51491ace29bf6c008e84f40bed065a509e7a783
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-13 12:51:07 +0900

    [Silver I] Title: 선이 하나 더ㅠㅠ, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/29727. 선이 하나 더ㅠㅠ/선이 하나 더ㅠㅠ.py
+++ 백준/Silver/29727. 선이 하나 더ㅠㅠ/선이 하나 더ㅠㅠ.py
@@ -1,6 +1,4 @@
 n,x,y,X,Y=map(int,open(0).read().split())
-d=0
-if x==X:d=min(n,max(y,Y))-max(-1,min(y,Y))
-elif y==Y:d=min(n,max(x,X))-max(-1,min(x,X))
-a,d=n+1,max(0,d)
+d=max(0,(0,min(n,max(y,Y))-max(-1,min(y,Y)),min(n,max(x,X))-max(-1,min(x,X)))[(x==X)-(y==Y)])
+a=n+1
 print(a*(a*n*n//2+d*d-d)>>1)
