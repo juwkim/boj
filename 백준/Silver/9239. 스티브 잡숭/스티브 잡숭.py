611c7df4cdff3f2de9f86c414385c7056b28cebf
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-13 23:32:54 +0900

    [Silver I] Title: 스티브 잡숭, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/9239. 스티브 잡숭/스티브 잡숭.py
@@ -0,0 +1,16 @@
+a, b = input().split('.')
+ans = []
+if len(a) == 1:
+    w = 10**len(b)
+    num = (10 - int(a)) * w - int(b)
+    for i in range(1, 10):
+        for l in range(1, 9):
+            q, r = divmod(i * (10**l - 1) * w, num)
+            if r == 0:
+                s = str(q)
+                if len(s) == l and int(s[0]) == i:
+                    ans.append(q)
+if ans:
+    print(*sorted(ans), sep="\n")
+else:
+    print("No solution")
