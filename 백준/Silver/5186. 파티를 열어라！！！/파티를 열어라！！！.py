78a55d9a62c4adef14e4342103b89e0d6b054bb3
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-13 22:59:31 +0900

    [Silver I] Title: 파티를 열어라!!!, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/5186. 파티를 열어라！！！/파티를 열어라！！！.py
+++ 백준/Silver/5186. 파티를 열어라！！！/파티를 열어라！！！.py
@@ -4,23 +4,22 @@ g = lambda: map(int, input().split())
 
 for tc in range(1, 1 + int(input())):
     n, c, l = g()
-    friedns = [[0, 0] for _ in range(l + 1)]
+    friedns = [[0, 0] for _ in range(l)]
     for _ in range(n):
         num, state = input().split()
-        friedns[int(num)][state == 'I'] += 1
-    cars = [[] for _ in range(l + 1)]
+        friedns[int(num) - 1][state == 'I'] += 1
+    cars = [[] for _ in range(l)]
     for _ in range(c):
         num, cnt = g()
-        cars[num].append(cnt)
+        cars[num - 1].append(cnt)
     ans = 0
-    for i in range(1, l + 1):
+    for i in range(l):
         sober, intoxicated = friedns[i]
         for j, car in enumerate(sorted(cars[i], reverse=True)):
-            if sober <= 0:
+            if sober == 0:
                 break
             num = min(intoxicated, car - 1)
-            car -= num
             intoxicated -= num
-            sober -= min(car, max(1, sober - (len(cars[i]) - j) + 1))
-        ans += max(sober, 0) + max(intoxicated, 0)
+            sober -= min(car - num, max(1, sober - len(cars[i]) + j + 1))
+        ans += sober + intoxicated
     print(f"Data Set {tc}:\n{ans}")
