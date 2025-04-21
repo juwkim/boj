2223c3ca6884ccd589e606a87d3d187920407a6b
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-21 14:20:11 +0900

    [Silver I] Title: Odometer, Time: 108 ms, Memory: 110896 KB -BaekjoonHub

---
+++ 백준/Silver/10035. Odometer/Odometer.py
@@ -0,0 +1,20 @@
+X, Y = map(int, input().split())
+ans = 0
+def solve(cur, num, other_used):
+    if cur > 10**16:
+        return
+    global ans
+    ans += other_used and X <= cur <= Y
+    if cur < 10:
+        for i in range(1, 10):
+            if i != num:
+                solve(i * 10 + cur, cur, True)
+    if cur:
+        solve(cur * 10 + num, num, other_used)
+        if not other_used:
+            for i in range(10):
+                if i != num:
+                    solve(cur * 10 + i, num, True)
+for i in range(10):
+    solve(i, i, False)
+print(ans)
