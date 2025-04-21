a3098d6da690e2840de7bce8b13cf4aab8efc2ab
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-21 20:40:24 +0900

    [Silver I] Title: JJOOII (JJOOII), Time: 144 ms, Memory: 113676 KB -BaekjoonHub

--- 백준/Silver/5538. JJOOII （JJOOII）/JJOOII （JJOOII）.py
+++ 백준/Silver/5538. JJOOII （JJOOII）/JJOOII （JJOOII）.py
@@ -6,18 +6,11 @@ for c in input():
         case "JJ": J += 1
         case "JO": O = 1
         case "JI": J = 0
-        case "OJ": J = 1; O = 0
+        case "OJ": J, O = 1, 0
         case "OO": O += 1
-        case "OI":
-            I = 1
-            if J >= O and O == I and I > ans: ans = I
-        case "IJ":
-            if J >= O and O == I and I > ans: ans = I
-            J = 1; O = 0; I = 0
-        case "IO":
-            if J >= O and O == I and I > ans: ans = I
-            J = 0; O = 0; I = 0
-        case "II":
+        case "IJ": J, O, I = 1, 0, 0
+        case "IO": J, O, I = 0, 0, 0
+        case _:
             I += 1
             if J >= O and O == I and I > ans: ans = I
     prv = c
