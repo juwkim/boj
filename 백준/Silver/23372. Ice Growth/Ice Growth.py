ea8ed679ca5ab9cc162d4bca9aa6d3a76f8f7936
김주원 <66457470+juwkim@users.noreply.github.com>
2025-04-27 18:39:37 +0900

    [Silver I] Title: Ice Growth, Time: 192 ms, Memory: 130240 KB -BaekjoonHub

--- 백준/Silver/23372. Ice Growth/Ice Growth.py
+++ 백준/Silver/23372. Ice Growth/Ice Growth.py
@@ -1,11 +1,9 @@
 import sys
 g = lambda: map(int, sys.stdin.readline().split())
-from bisect import bisect_left
+from bisect import bisect
 
 n, k = g()
-prv, nums = 0, []
-for a in g():
-    nums.append(prv:=max(0, prv - a))
+nums = [0] * (n + 1)
+for i, a in enumerate(g()): nums[i+1] = min(0, nums[i] + a)
 nums.sort()
-for b in g():
-    print(n - bisect_left(nums, 5 * b), end=' ')
+print(*[bisect(nums, -5 * b) for b in g()])
