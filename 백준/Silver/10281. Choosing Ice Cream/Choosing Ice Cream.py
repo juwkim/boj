96b85ca6b5531e277b547a6a2e1a79915589b2a4
김주원 <66457470+juwkim@users.noreply.github.com>
2025-05-03 22:25:02 +0900

    [Silver I] Title: Choosing Ice Cream, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/10281. Choosing Ice Cream/Choosing Ice Cream.py
+++ 백준/Silver/10281. Choosing Ice Cream/Choosing Ice Cream.py
@@ -1,22 +1,11 @@
-import sys
-input = lambda: sys.stdin.readline().rstrip()
-g = lambda: [*map(int, input().split())]
-
-def solve_case(n, k):
-    if k == 1:
-        return 1 if n == 1 else "unbounded"
-    if n == 1:
-        return 0
-    
-    max_iter = 1000
-    prod = 1
-    for i in range(1, max_iter + 1):
-        prod = (prod * k) % n
-        if prod == 0:
-            return i
-    return "unbounded"
-
-T = int(input())
-for _ in range(T):
-    n, k = g()
-    print(solve_case(n, k))
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    ans = "unbounded"
+    if k > 1:
+        prod = 1 % n
+        for i in range(30):
+            if prod == 0:
+                ans = i
+                break
+            prod = (prod * k) % n
+    print(ans)
