1848a257fdfbfd3790a5e9d725621d0caabe3a6f
김주원 <66457470+juwkim@users.noreply.github.com>
2025-05-04 14:49:20 +0900

    [Silver I] Title: Where is the Boundary, Time: 128 ms, Memory: 110844 KB -BaekjoonHub

--- 백준/Silver/16805. Where is the Boundary/Where is the Boundary.py
+++ 백준/Silver/16805. Where is the Boundary/Where is the Boundary.py
@@ -1,14 +1,9 @@
 n, m = map(int, input().split())
-cnt_w = [0] * (n + 1)
-cnt_e = [0] * (n + 1)
+E = [0] * (n + 1)
 for _ in range(m):
-    for i, c in enumerate(input()):
-        if c == 'W':
-            cnt_w[i + 1] += 1
-        else:
-            cnt_e[i + 1] += 1
-for i in range(n):
-    cnt_w[i + 1] += cnt_w[i]
-    cnt_e[i + 1] += cnt_e[i]
-idx = min((cnt_w[n] - cnt_w[i] + cnt_e[i], i) for i in range(n + 1))[1]
+    for i, c in enumerate(input(), 1):
+        E[i] += c == 'E'
+for i in range(1, n):
+    E[i + 1] += E[i]
+idx = min((2 * E[i] - i * m, i) for i in range(n + 1))[1]
 print(idx, idx + 1)
