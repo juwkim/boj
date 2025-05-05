6be58d063db44e5c6ffdb3ebf579454fdf43a159
김주원 <66457470+juwkim@users.noreply.github.com>
2025-05-06 08:44:37 +0900

    [Silver I] Title: Wonderful Fours, Time: 128 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Silver/7106. Wonderful Fours/Wonderful Fours.py
+++ 백준/Silver/7106. Wonderful Fours/Wonderful Fours.py
@@ -1,4 +1,4 @@
 from itertools import permutations, combinations
 
-perms = {int(''.join(p)) for p in permutations(input().split()) if p[0] != '0'}
-print(sum(sum(l) in perms for l in combinations(perms, 3)))
+p = {int(''.join(p)) for p in permutations(input().split()) if p[0] != '0'}
+print(sum(sum(l) in p for l in combinations(p, 3)))
