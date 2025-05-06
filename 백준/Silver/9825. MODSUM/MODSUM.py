da6fbee1bf00fa16695e4a149934ea62266e533c
김주원 <66457470+juwkim@users.noreply.github.com>
2025-05-06 13:45:58 +0900

    [Silver I] Title: MODSUM, Time: 384 ms, Memory: 112328 KB -BaekjoonHub

--- 백준/Silver/9825. MODSUM/MODSUM.py
+++ 백준/Silver/9825. MODSUM/MODSUM.py
@@ -1,14 +1,12 @@
 n, *rest = map(int, input().split())
 ranges = [(rest[i], rest[i+1]) for i in range(0, len(rest), 2)]
 l = n * 100
-dp = [0] * (l + 1)
-dp[0] = 1
+dp = [1] + [0] * l
 for v, w in ranges:
-    ndp = [0] * (l + 1)
     for s in range(l - v, -1, -1):
         if dp[s]:
+            cnt, dp[s] = dp[s], 0
             for x in range(w, v - 1, -1):
                 if s + x <= l:
-                    ndp[s + x] += dp[s]
-    dp = ndp
+                    dp[s + x] += cnt
 print(sum(dp[s] * ((s ** 4 + 2 * s ** 2) % 5 + 1) for s in range(l + 1)))
