b4d56a53e4f57687d1de73de114764883e59a3e8
김주원 <66457470+juwkim@users.noreply.github.com>
2025-05-06 15:05:06 +0900

    [Silver I] Title: Removing Pairs, Time: 92 ms, Memory: 110792 KB -BaekjoonHub

--- 백준/Silver/23414. Removing Pairs/Removing Pairs.py
+++ 백준/Silver/23414. Removing Pairs/Removing Pairs.py
@@ -1,13 +1,13 @@
 def solve():
     s, t = input(), input()
-    i, j = 0, 0
-    while i < len(s) and j < len(t):
+    i, j = len(s) - 1, len(t) - 1
+    while i >= 0 and j >= 0:
         if s[i] == t[j]:
-            i += 1
-            j += 1
+            i -= 1
+            j -= 1
         else:
-            j += 2
-    if i == len(s) and (j - len(t)) % 2 == 0:
+            j -= 2
+    if i == -1 and j & 1:
         return "YES"
     return "NO"
 print(solve())
