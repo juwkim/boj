4e9b6da34e9e8c95f86719db79df474434503fe9
김주원 <66457470+juwkim@users.noreply.github.com>
2025-05-09 21:11:52 +0900

    [Silver I] Title: Count, Time: 324 ms, Memory: 189036 KB -BaekjoonHub

--- 백준/Silver/15794. Count/Count.py
+++ 백준/Silver/15794. Count/Count.py
@@ -3,28 +3,24 @@ from collections import Counter
 n = int(input())
 cnt = Counter(map(int, input().split()))
 v = int(input())
-
 ans, min_diff = 1, 1e9
 i, j = 0, len(cnt) - 1
 a = sorted(cnt)
-while i <= j:
-    if i == j and cnt[a[i]] < 2:
-        break
+while i < j:
     total = a[i] + a[j]
     diff = abs(total - v) 
     if diff < min_diff:
-        if i == j:
-            ans = cnt[a[i]] * (cnt[a[i]] - 1) >> 1
-        else:
-            ans = cnt[a[i]] * cnt[a[j]]
-        min_diff = diff
+        ans, min_diff = cnt[a[i]] * cnt[a[j]], diff
     elif diff == min_diff:
-        if i == j:
-            ans += cnt[a[i]] * (cnt[a[i]] - 1) >> 1
-        else:
-            ans += cnt[a[i]] * cnt[a[j]]
+        ans += cnt[a[i]] * cnt[a[j]]
     if total < v:
         i += 1
     else:
         j -= 1
+if cnt[a[i]] > 1:
+    diff = abs(a[i] * 2 - v)
+    if diff < min_diff:
+        ans = cnt[a[i]] * (cnt[a[i]] - 1) >> 1
+    elif diff == min_diff:
+        ans += cnt[a[i]] * (cnt[a[i]] - 1) >> 1
 print(ans)
