250d8dc94d48259c717a76b0be73dffa578a7393
김주원 <66457470+juwkim@users.noreply.github.com>
2025-05-10 03:21:04 +0900

    [Silver I] Title: Circle, Time: 88 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/7115. Circle/Circle.py
+++ 백준/Silver/7115. Circle/Circle.py
@@ -1,17 +1,16 @@
 dist2 = lambda x, y: x * x + y * y
 
 k, r = map(int, input().split())
-x1, y1 = 0, (r - 1) // k
-x2 = y2 = int(r / (k * 2**.5))
+x, y = 0, (r - 1) // k
 p = (r/k)**2
 cnt = 4
-while (x1, y1) != (x2, y2):
+while y > x:
     cnt += 8
-    if p > dist2(x1 + 1, y1):
-        x1 += 1
-    elif dist2(x1, y1 - 1) < p < dist2(x1 + 1, y1):
-        y1 -= 1
+    if p > dist2(x + 1, y):
+        x += 1
+    elif dist2(x, y - 1) < p < dist2(x + 1, y):
+        y -= 1
     else:
-        x1 += 1
-        y1 -= 1
+        x += 1
+        y -= 1
 print(cnt)
