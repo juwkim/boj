f83b85b96b9ad04bc8645c92ab980fec7c606d11
김주원 <66457470+juwkim@users.noreply.github.com>
2025-05-10 03:44:31 +0900

    [Silver I] Title: Counting Pixels, Time: 8 ms, Memory: 1128 KB -BaekjoonHub

---
+++ 백준/Silver/7639. Counting Pixels/Counting Pixels.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    long long x, y, r;
+    while (1) {
+        scanf("%d %d %d", &x, &y, &r);
+        if (x == 0) break;
+        long long count = 0;
+        for (long long i = 0; i < r; i++)
+            count += (long long)sqrt(r * r - i * i - 1) + 1;
+        printf("%lld\n", count * 4);
+    }
+    return 0;
+}
