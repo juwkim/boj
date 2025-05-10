e4ae20f12d4997d58e05a4cfa147b02a6d002685
김주원 <66457470+juwkim@users.noreply.github.com>
2025-05-11 01:01:17 +0900

    [Silver I] Title: Anti-Tetris, Time: 112 ms, Memory: 111124 KB -BaekjoonHub

---
+++ 백준/Silver/30305. Anti－Tetris/Anti－Tetris.py
@@ -0,0 +1,22 @@
+h, w = map(int, input().split())
+block = [input() for _ in range(h)]
+for i in range(4):
+    for j in range(len(block[0])):
+        i = 0
+        while i < len(block) and block[i][j] == '#':
+            i += 1
+        if any(block[k][j] == '#' for k in range(i + 1, len(block))):
+            break
+    else:
+        print(len(block), len(block[0]))
+        for row in block:
+            for c in row:
+                if c == '#':
+                    print('.', end='')
+                else:
+                    print('#', end='')
+            print()
+        break
+    block = [l[::-1] for l in zip(*block)]
+else:
+    print("impossible")
