55374b3c34e3988adc7717790e83c27aaf599d8a
김주원 <66457470+juwkim@users.noreply.github.com>
2025-05-11 20:15:03 +0900

    [Silver I] Title: Izbori, Time: 180 ms, Memory: 115972 KB -BaekjoonHub

--- 백준/Silver/15361. Izbori/Izbori.py
+++ 백준/Silver/15361. Izbori/Izbori.py
@@ -4,30 +4,27 @@ g = lambda: [*map(int, input().split())]
 N, M, K = g()
 prefs = [g() for _ in range(N)]
 
-def simulate(active):
+def simulate(retired):
     votes = [0] * (M + 1)
     for county in prefs:
         for cand in county:
-            if active[cand]:
+            if not (retired >> cand) & 1:
                 votes[cand] += 1
                 break
     return max(range(1, M + 1), key=lambda x: votes[x])
 
-active = tuple([1] * (M + 1))
-print(winner:=simulate(active))
-dq = deque([(active, 0)])
-visited = {active}
+print(winner:=simulate(0))
+dq = deque([(0, 0)])
+visited = {0}
 while dq:
-    active, cnt = dq.popleft()
-    winner = simulate(active)
+    retired, cnt = dq.popleft()
+    winner = simulate(retired)
     if winner == K:
         print(cnt)
         break
     for i in range(1, M + 1):
-        if active[i] and i != K:
-            new_active = list(active)
-            new_active[i] = 0
-            new_active = tuple(new_active)
-            if new_active not in visited:
-                visited.add(new_active)
-                dq.append((new_active, cnt + 1))
+        if not (retired >> i) & 1 and i != K:
+            new_retired = retired | (1 << i)
+            if new_retired not in visited:
+                visited.add(new_retired)
+                dq.append((new_retired, cnt + 1))
