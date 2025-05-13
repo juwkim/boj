541b9945ffb99a315e48552c888d903f766b1082
김주원 <66457470+juwkim@users.noreply.github.com>
2025-05-14 05:09:05 +0900

    [Silver I] Title: Pollock's conjecture, Time: 2724 ms, Memory: 159608 KB -BaekjoonHub

--- 백준/Silver/4957. Pollock's conjecture/Pollock's conjecture.py
+++ 백준/Silver/4957. Pollock's conjecture/Pollock's conjecture.py
@@ -1,20 +1,19 @@
 import sys
 input = sys.stdin.readline
+t = lambda x: x * (x + 1) * (x + 2) // 6
 
 MAX_N = 1_000_000
 dp = [0] + [1e9] * MAX_N
 odd_dp = [0] + [1e9] * MAX_N
-tetra = []; a = 1
-odd_tetra = []; b = 1
+tetra = [t(i) for i in range(1, 182)]; a = 0
+odd_tetra = [t(i) for i in range(1, 188, 4)]; b = 0
 for i in range(1, MAX_N + 1):
-    if i == a * (a + 1) * (a + 2) // 6:
-        tetra.append(a * (a + 1) * (a + 2) // 6)
+    if i == tetra[a]:
         a += 1
-    if i == b * (b + 1) * (b + 2) // 6:
-        odd_tetra.append(b * (b + 1) * (b + 2) // 6)
-        b += 4        
-    dp[i] = min(dp[i - num] + 1 for num in tetra)
-    odd_dp[i] = min(odd_dp[i - num] + 1 for num in odd_tetra)
+    if i == odd_tetra[b]:
+        b += 1
+    dp[i] = min(dp[i - tetra[k]] + 1 for k in range(a))
+    odd_dp[i] = min(odd_dp[i - odd_tetra[k]] + 1 for k in range(b))
 
 while n:=int(input()):
     print(f"{dp[n]} {odd_dp[n]}")
