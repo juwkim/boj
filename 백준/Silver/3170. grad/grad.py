ee939022ca11df78a10cb1dae9890b935ddb22f0
김주원 <66457470+juwkim@users.noreply.github.com>
2025-05-14 04:16:28 +0900

    [Silver I] Title: grad, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/3170. grad/grad.py
+++ 백준/Silver/3170. grad/grad.py
@@ -1,4 +1,3 @@
-from collections import deque
 g = lambda: [*map(int, input().split())]
 
 def dist2(x1, y1, x2, y2):
@@ -11,20 +10,20 @@ cranes = [g() for _ in range(N)]
 entrance = X // 2, 0
 reachable = set()
 visited = bytearray(N)
-dq = deque()
+st = []
 for i, (cx, cy, r) in enumerate(cranes):
     if dist2(cx, cy, entrance[0], entrance[1]) <= r * r:
-        dq.append(i)
+        st.append(i)
         reachable.add(i)
         visited[i] = True
-while dq:
-    x1, y1, r1 = cranes[dq.popleft()]
+while st:
+    x1, y1, r1 = cranes[st.pop()]
     for i, (x2, y2, r2) in enumerate(cranes):
         if not visited[i]:
             if dist2(x1, y1, x2, y2) <= (r1 + r2)**2:
                 visited[i] = True
                 reachable.add(i)
-                dq.append(i)
+                st.append(i)
 
 for _ in range(int(input())):
     D, E = g()
