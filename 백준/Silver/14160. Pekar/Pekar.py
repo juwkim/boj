a75957b29efecac80c3dad6578cfe19874db185d
김주원 <66457470+juwkim@users.noreply.github.com>
2025-05-17 00:34:38 +0900

    [Silver II] Title: Pekar, Time: 140 ms, Memory: 135116 KB -BaekjoonHub

--- 백준/Silver/14160. Pekar/Pekar.py
+++ 백준/Silver/14160. Pekar/Pekar.py
@@ -1,9 +1,6 @@
 g = lambda: map(int, input().split())
 input()
-ans = -1
-T_sum, A_sum = -1, 0
+ans, t, a = -1, -1, 0
 for T, A in zip(g(), g()):
-    T_sum += T
-    A_sum += A
-    ans = max(ans, T_sum // A_sum)
+    ans = max(ans, (t:=t+T) // (a:=a+A))
 print(ans * 5 + 5)
