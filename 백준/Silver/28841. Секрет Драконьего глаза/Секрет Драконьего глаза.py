b7ba2bcee752ca4ef6226c6c8f782d7a16014ca6
김주원 <66457470+juwkim@users.noreply.github.com>
2025-05-18 20:29:01 +0900

    [Silver I] Title: Секрет Драконьего глаза, Time: 96 ms, Memory: 111324 KB -BaekjoonHub

--- 백준/Silver/28841. Секрет Драконьего глаза/Секрет Драконьего глаза.py
+++ 백준/Silver/28841. Секрет Драконьего глаза/Секрет Драконьего глаза.py
@@ -2,8 +2,6 @@ def solve(c):
     i, j = s.find(c), s.rfind(c)
     if i == -1 or i == j:
         return -1, -1
-    p = s[i: j + 1]
-    assert s.find(p) == i and s.rfind(p) == i
     return i + 1, j, i + 2, j + 1
 s = input()
 a, b = solve('0'), solve('1')
