ae7a77357390e1c6b4a699c840bb7d20cd66b676
김주원 <66457470+juwkim@users.noreply.github.com>
2025-05-20 13:26:00 +0900

    [Silver IV] Title: 가로수, Time: 124 ms, Memory: 132064 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.py
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -1,15 +1,7 @@
 from math import gcd
-N = int(input())
-buf = sorted(int(input()) for _ in range(N))
-diff = []
+from itertools import pairwise
 
-GCD = None
-for i in range(N-1):
-    num = buf[i+1] - buf[i]
-    diff.append(num)
-    if GCD:
-        GCD = gcd(GCD, num)
-    else:
-        GCD = num
-ans = sum(num // GCD for num in diff) - (N - 1)
-print(ans)
+N, *nums = map(int, open(0).read().split())
+diff = [b - a for a, b in pairwise(nums)]
+g = gcd(*diff)
+print(sum(d // g - 1 for d in diff))
