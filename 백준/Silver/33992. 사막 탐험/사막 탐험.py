ccfa445f0b4c45f93f54925d3fe9c4c590498425
김주원 <66457470+juwkim@users.noreply.github.com>
2025-05-26 03:34:59 +0900

    [Silver III] Title: 사막 탐험, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/33992. 사막 탐험/사막 탐험.py
+++ 백준/Silver/33992. 사막 탐험/사막 탐험.py
@@ -1,4 +1,5 @@
 from math import dist
 
 ax, ay, bx, by, px, py, r = map(int, input().split())
-print(min(dist((ax, ay), (bx, by)), max(0, dist((ax, ay), (px, py)) - r) + max(0, dist((bx, by), (px, py)) - r)))
+a, b, p = (ax, ay), (bx, by), (px, py)
+print(min(dist(a, b), max(0, dist(a, p) - r) + max(0, dist(b, p) - r)))
