95e0961615112d8dcb6f1b9dcadd9a37d34d8a9f
김주원 <66457470+juwkim@users.noreply.github.com>
2025-06-15 00:33:03 +0900

    [Silver III] Title: Провода, Time: 88 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/21408. Провода/Провода.py
+++ 백준/Silver/21408. Провода/Провода.py
@@ -1,12 +1,3 @@
-n = int(input())
-triangles = sorted(map(int, input().split()))
-circles = sorted(map(int, input().split()))
-
-dp = [[float('inf')] * (n + 1) for _ in range(n + 1)]
-dp[0] = [0] * (n + 1)
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        dp[i][j] = dp[i][j-1]
-        if triangles[i-1] < circles[j-1]:
-            dp[i][j] = min(dp[i][j], dp[i-1][j-1] + abs(triangles[i-1] - circles[j-1]))
-print(dp[n][n])
+g=lambda:sum(map(int,input().split()))
+g()
+print(-g()+g())
