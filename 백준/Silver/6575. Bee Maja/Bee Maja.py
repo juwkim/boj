26236d42c0370b5ca99712f8cd6f68955a8f4d83
김주원 <66457470+juwkim@users.noreply.github.com>
2025-06-28 21:32:58 +0900

    [Silver I] Title: Bee Maja, Time: 120 ms, Memory: 110916 KB -BaekjoonHub

--- 백준/Silver/6575. Bee Maja/Bee Maja.py
+++ 백준/Silver/6575. Bee Maja/Bee Maja.py
@@ -1,5 +1,5 @@
 for n in map(int, open(0)):
-    level, total = 1, 1
+    level, total = 0, 1
     while True:
         next_total = total + 6 * level
         if next_total >= n:
@@ -7,17 +7,12 @@ for n in map(int, open(0)):
         total = next_total
         level += 1
     idx_in_ring = n - total
-    x, y = level - 1, 0
+    x, y = level, 0
     cnt = 0
-    for dx, dy in (0, 1), (-1, 1), (-1, 0), (0, -1), (1, -1), (1, 0), (0, 1):
+    for dx, dy in (-1, 1), (-1, 0), (0, -1), (1, -1), (1, 0), (0, 1):
         if idx_in_ring == 0:
             break
-        if cnt == 0:
-            step = 1
-        elif cnt == 1:
-            step = min(idx_in_ring, level - 1)
-        else:
-            step = min(idx_in_ring, level)
+        step = min(idx_in_ring, level)
         cnt += 1
         x += dx * step
         y += dy * step
