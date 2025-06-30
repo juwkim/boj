0c7bdf58631dc3f9c19fec45fcbde8fe380781c2
김주원 <66457470+juwkim@users.noreply.github.com>
2025-06-30 09:36:49 +0900

    [Silver I] Title: Светофоры, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/21511. Светофоры/Светофоры.py
+++ 백준/Silver/21511. Светофоры/Светофоры.py
@@ -2,5 +2,4 @@ a, b, x = map(int, input().split())
 if x <= 1000 * a:
     print(1000)
 else:
-    t = (x + 1000 * b - 1) // (1000 * (a + b))
-    print(min(1000, x / (t * (a + b) - a)))
+    print(min(1000,x/((x+1000*b-1)//(1000*(a+b))*(a+b)-a)))
