26604f1af96cb882e3eec728342af71cdbd7a6c9
김주원 <66457470+juwkim@users.noreply.github.com>
2025-07-03 06:12:16 +0900

    [Silver I] Title: Least Number, Time: 108 ms, Memory: 115996 KB -BaekjoonHub

--- 백준/Silver/23585. Least Number/Least Number.py
+++ 백준/Silver/23585. Least Number/Least Number.py
@@ -3,22 +3,17 @@ if d == 9:
     q, r = divmod(N, 8)
     digits = [8] * q
     if r: digits.append(r)
-elif d == 0:
+else:
     q, r = divmod(N, 9)
     digits = [9] * q
-    if r: digits.append(r)
-else:
-    digits = []
-    while N > 9:
-        digits.append(9)
-        N -= 9
-    if N == d:
-        if digits and d != 8:
-            digits[-1] -= 1
-            digits.append(N + 1)
+    if r:
+        if r == d:
+            if digits and d != 8:
+                digits[-1] -= 1
+                digits.append(r + 1)
+            else:
+                digits.append(r - 1)
+                digits.append(1)
         else:
-            digits.append(N - 1)
-            digits.append(1)
-    else:
-        digits.append(N)
+            digits.append(r)
 print(*digits[::-1], sep="")
