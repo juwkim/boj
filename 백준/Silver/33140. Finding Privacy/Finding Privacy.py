524d0ba754ef2d9acdb45c757327f42b76e6025e
김주원 <66457470+juwkim@users.noreply.github.com>
2025-07-23 06:00:45 +0900

    [Silver I] Title: Finding Privacy, Time: 96 ms, Memory: 111324 KB -BaekjoonHub

--- 백준/Silver/33140. Finding Privacy/Finding Privacy.py
+++ 백준/Silver/33140. Finding Privacy/Finding Privacy.py
@@ -2,9 +2,6 @@ K, N = map(int, input().split())
 if N == 2 * K - 1:
     print("X-" * (K - 1) + "X")
 elif 2 * K <= N <= 3 * K:
-    for _ in range(N - 2 * K):
-        print("-X-", end="")
-    for _ in range(3 * K - N):
-        print("-X", end="")
+    print("-X-" * (N - 2 * K) + "-X" * (3 * K - N))
 else:
     print("*")
