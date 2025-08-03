b9f3b4b93584727988c2d55e31633141fefe8a42
김주원 <66457470+juwkim@users.noreply.github.com>
2025-08-03 22:38:16 +0900

    [Silver I] Title: Building for UN, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/3583. Building for UN/Building for UN.py
+++ 백준/Silver/3583. Building for UN/Building for UN.py
@@ -1,13 +1,10 @@
-def country_char(i):
+def country(i):
     if i < 26:
         return chr(ord('A') + i)
     return chr(ord('a') + i - 26)
 
 n = int(input())
-h, w, l = n, n, 2
-s = "".join(country_char(i) for i in range(n))
-print(h, w, l)
-for z in range(h):
-    print(country_char(z) * w)
-    print(s)
-    print()
+s = "".join(country(i) for i in range(n))
+print(n, n, 2)
+for z in range(n):
+    print(country(z) * n, s, '', sep='\n')
