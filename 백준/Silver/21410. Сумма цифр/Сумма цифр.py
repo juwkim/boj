c3921cf7a8f5f54fae071fcdf05297353eb76c63
김주원 <66457470+juwkim@users.noreply.github.com>
2025-08-03 18:32:13 +0900

    [Silver I] Title: Сумма цифр, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/21410. Сумма цифр/Сумма цифр.py
+++ 백준/Silver/21410. Сумма цифр/Сумма цифр.py
@@ -2,9 +2,8 @@ def digit_sum(x):
     return sum(map(int, str(x)))
 
 A = int(s:=input())
-best_sum = digit_sum(A)
-best_B, best_C = 0, A
 n = len(s)
+ans = digit_sum(A), A, 0
 for i in range(n):
     if s[i] == '0':
         continue
@@ -12,13 +11,8 @@ for i in range(n):
     B_digits[i] = str(int(s[i]) - 1)
     for j in range(i + 1, n):
         B_digits[j] = '9'
-    B_str = ''.join(B_digits).lstrip('0')
-    B_val = int(B_str) if B_str else 0
-
+    B_val = int(''.join(B_digits))
     C_val = A - B_val
-    cur_sum = digit_sum(B_val) + digit_sum(C_val)
-    if cur_sum > best_sum:
-        best_sum = cur_sum
-        best_B, best_C = B_val, C_val
-print(best_sum)
-print(best_B, best_C)
+    ans = max(ans, (digit_sum(B_val) + digit_sum(C_val), B_val, C_val))
+print(ans[0])
+print(*ans[1:])
