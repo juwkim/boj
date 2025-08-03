febcb184acc32c435e36f09aba21f960e8a00838
김주원 <66457470+juwkim@users.noreply.github.com>
2025-08-04 03:12:01 +0900

    [Silver I] Title: Liczby piwne, Time: 220 ms, Memory: 113308 KB -BaekjoonHub

--- 백준/Silver/7956. Liczby piwne/Liczby piwne.py
+++ 백준/Silver/7956. Liczby piwne/Liczby piwne.py
@@ -2,14 +2,5 @@ import sys
 input = sys.stdin.readline
 
 for _ in range(int(input())):
-    n = int(input())
-    b = bin(n)[2:]
-    if n & 1:
-        if b == b[::-1]:
-            print("TAK")
-        else:
-            print("NIE")
-    elif all(b[i] != b[-i-1] for i in range(len(b) + 1 >> 1)):
-        print("TAK")
-    else:
-        print("NIE")
+    b = bin(n:=int(input()))
+    print("NTIAEK"[all((b[i+2] == b[-i-1]) == n & 1 for i in range(len(b) - 1 >> 1))::2])
