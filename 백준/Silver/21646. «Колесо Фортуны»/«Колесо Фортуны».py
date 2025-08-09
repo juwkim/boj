5854e4fd70d92fe81975f7d20fa45b412f3f837c
김주원 <66457470+juwkim@users.noreply.github.com>
2025-08-10 04:45:40 +0900

    [Silver I] Title: «Колесо Фортуны», Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/21646. «Колесо Фортуны»/«Колесо Фортуны».py
+++ 백준/Silver/21646. «Колесо Фортуны»/«Колесо Фортуны».py
@@ -1,9 +1,6 @@
 n, *vals, a, b, k = map(int, open(0).read().split())
-L = (a - 1) // k
-R = (b - 1) // k
-seen = bytearray(n)
-for t in range(L, min(L + n, R + 1)):
-    idx = t % n
-    seen[idx] = True
-    seen[-idx] = True
-print(max(vals[i] for i in range(n) if seen[i]))
+ans, L = 0, (a - 1) // k
+for t in range(L, min(L + n, (b - 1) // k + 1)):
+    i = t % n
+    ans = max(ans, vals[i], vals[-i])
+print(ans)
