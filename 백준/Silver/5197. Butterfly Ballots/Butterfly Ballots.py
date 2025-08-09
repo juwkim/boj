4e7438fcab8ae8e9eb2afe48a016c1032a2109fb
김주원 <66457470+juwkim@users.noreply.github.com>
2025-08-09 23:31:37 +0900

    [Silver I] Title: Butterfly Ballots, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/5197. Butterfly Ballots/Butterfly Ballots.py
+++ 백준/Silver/5197. Butterfly Ballots/Butterfly Ballots.py
@@ -3,20 +3,17 @@ input = sys.stdin.readline
 
 for tc in range(1, int(input()) + 1):
     n = int(input())
-    me, *v = [int(input()) for _ in range(n)]
+    me = int(input())
     ans = "Possible"
-    if n >= 3:
-        v.sort()
-        me += v[-1] >> 1
-        cur = v[-1] >> 1
-        v.pop()
+    if n > 1:
+        *v, cur = sorted(int(input()) >> 1 for _ in range(n - 1))
+        me += cur
         while v:
             idx = -1
-            while idx + 1 < len(v) and cur + v[idx + 1] // 2 <= me:
+            while idx + 1 < len(v) and cur + v[idx + 1] <= me:
                 idx += 1
             if idx == -1:
                 ans = "Impossible"
                 break
-            cur = v[idx] // 2
-            v.pop(idx)
+            cur = v.pop(idx)
     print(f"Data Set {tc}:\n{ans}")
