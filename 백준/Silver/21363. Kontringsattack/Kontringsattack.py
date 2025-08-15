dffa40f926591a6579b229b936c9307be847475d
김주원 <66457470+juwkim@users.noreply.github.com>
2025-08-15 20:24:51 +0900

    [Silver I] Title: Kontringsattack, Time: 140 ms, Memory: 118648 KB -BaekjoonHub

--- 백준/Silver/21363. Kontringsattack/Kontringsattack.py
+++ 백준/Silver/21363. Kontringsattack/Kontringsattack.py
@@ -2,14 +2,13 @@ import sys
 input = sys.stdin.readline
 
 l = 1000000
-N = int(input())
-diff = [0] * (l + 1)
-for _ in range(N):
+d = [0] * (l + 1)
+for _ in range(int(input())):
     F, S = map(int, input().split())
-    diff[abs(F - S)] += (F > S) - (F < S)
+    d[abs(F - S)] += (F > S) - (F < S)
 ans = 0, -l
 cur = 0
 for i in range(l, 0, -1):
-    cur += diff[i]
-    ans = max(ans, (cur, -i + 1))
+    cur += d[i]
+    ans = max(ans, (cur, 1 - i))
 print(-ans[1])
