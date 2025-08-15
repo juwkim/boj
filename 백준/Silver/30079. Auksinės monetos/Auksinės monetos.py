7fbf0f9a363b8801cdf91f9fee96240e00f8e8ed
김주원 <66457470+juwkim@users.noreply.github.com>
2025-08-15 21:47:13 +0900

    [Silver I] Title: Auksinės monetos, Time: 108 ms, Memory: 112932 KB -BaekjoonHub

--- 백준/Silver/30079. Auksinės monetos/Auksinės monetos.py
+++ 백준/Silver/30079. Auksinės monetos/Auksinės monetos.py
@@ -1,12 +1,7 @@
 N, M = map(int, input().split())
 grid = [input() for _ in range(N)]
 dp = [[0] * (M + 1) for _ in range(N + 1)]
-ans = 0
 for i in range(N):
     for j in range(M):
-        if grid[i][j] == 'x':
-            dp[i + 1][j + 1] = -1e9
-        else:
-            dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j]) + (grid[i][j] == 'o')
-        ans = max(ans, dp[i + 1][j + 1])
-print(ans)
+        dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j]) + (grid[i][j] == 'o') - (grid[i][j] == 'x') * 10**9
+print(max(max(row) for row in dp))
