a1b41eee00abc17f0c934a1eb4d24f75929e21ed
김주원 <66457470+juwkim@users.noreply.github.com>
2025-08-16 02:11:24 +0900

    [Silver III] Title: 래환이의 초콜릿 포장 이야기, Time: 356 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/34126. 래환이의 초콜릿 포장 이야기/래환이의 초콜릿 포장 이야기.py
+++ 백준/Silver/34126. 래환이의 초콜릿 포장 이야기/래환이의 초콜릿 포장 이야기.py
@@ -3,9 +3,4 @@ input = sys.stdin.readline
 
 for _ in range(int(input())):
     A, B, C = map(int, input().split())
-    ans = (C + 1) // 2 * 3
-    cnt = A + 2 * B
-    if C & 1:
-        cnt -= 3
-    ans += max(0, cnt + 1 >> 1)
-    print(ans)
+    print((C + 1 >> 1) * 3 + max(0, A + 2 * B + 1 - (C & 1) * 3 >> 1))
